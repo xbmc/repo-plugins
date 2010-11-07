bd8840a49232ad83600df712e1022f439290056e
spiff <spiff@xbmc.org>
2010-11-07 21:14:48 +0100

    [plugin.audio.listenliveeu] updated to version 0.4.3. note that id was lowercased so users will have to reinstall

--- plugin.audio.listenliveeu/default.py
+++ plugin.audio.listenliveeu/default.py
@@ -10,18 +10,21 @@ import traceback
 from pprint import pprint
 
 __plugin__ = "ListenLiveEU"
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'bootsy [bootsy82@gmail.com]'
-__date__ = '15-09-2010'
-__svn__ = 'http://xbmc-addons.googlecode.com/svn/addons/plugin.audio.ListenLiveEU/'
+__date__ = '07-11-2010'
+__svn__ = 'http://xbmc-addons.googlecode.com/svn/addons/plugin.audio.listenliveeu/'
 
 BASE_URL = 'http://www.listenlive.eu'
 URL_INDEX = '/'.join( [BASE_URL, 'index.html'] )
 URL_NEW = '/'.join( [BASE_URL, 'new.html'] )
 
-DIR_HOME = os.getcwd().replace(';','')
+addon = xbmcaddon.Addon(id='plugin.audio.listenliveeu')
+DIR_HOME = addon.getAddonInfo('path')
+DIR_HOME = xbmc.translatePath(DIR_HOME)
 FILE_INDEX_PAGE = os.path.join(DIR_HOME, 'index.html')
-DIR_SETTINGS = 'special://home/userdata/addon_data/plugin.audio.ListenLiveEU'
+DIR_SETTINGS = addon.getAddonInfo('profile')
+DIR_SETTINGS = xbmc.translatePath(DIR_SETTINGS)
 FILE_FAVS = os.path.join(DIR_SETTINGS, 'favorites.xml')
 
 dialogProgress = xbmcgui.DialogProgress()
@@ -351,9 +354,9 @@ def addDirectoryItem(name, url, mode, label2='', infoType="Music", infoLabels = 
 	liz.setInfo( infoType, infoLabels )
 	
 	v = "?name=%s&url=%s" % (urllib.quote_plus(name.encode('utf-8')), urllib.quote_plus(url.encode('utf-8')), )
-	action1 = 'XBMC.RunPlugin(plugin://plugin.audio.ListenLiveEU/?add%s%s)' % (v, '\n')
-	action2 = 'XBMC.RunPlugin(plugin://plugin.audio.ListenLiveEU/?remfav%s%s)' % (v, '\n')
-	action3 = 'XBMC.RunPlugin(plugin://plugin.audio.ListenLiveEU/?removeall)'
+	action1 = 'XBMC.RunPlugin(plugin://plugin.audio.listenliveeu/?add%s%s)' % (v, '\n')
+	action2 = 'XBMC.RunPlugin(plugin://plugin.audio.listenliveeu/?remfav%s%s)' % (v, '\n')
+	action3 = 'XBMC.RunPlugin(plugin://plugin.audio.listenliveeu/?removeall)'
 	
 	if mode==2:
 		try:
@@ -395,7 +398,7 @@ def deleteFile(fn):
 # BEGIN !
 #######################################################################################################################
 try:
-	__settings__ = xbmcaddon.Addon(id='plugin.audio.ListenLiveEU')
+	__settings__ = xbmcaddon.Addon(id='plugin.audio.listenliveeu')
 	__language__ = __settings__.getLocalizedString
 except:
 	errorOK()

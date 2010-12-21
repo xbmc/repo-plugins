8529171a165ba70d184ecad014bb555dde12077f
spiff <spiff@xbmc.org>
2010-12-21 19:07:34 +0100

    [plugin.video.animeftw] initial version (1.5.0). thanks to maruchan

---
+++ plugin.video.animeftw/default.py
@@ -0,0 +1,21 @@
+"""
+    AnimeFTW
+    maruchan
+"""
+import sys
+import xbmcaddon
+#import xbmcaddon
+
+#plugin constants
+__plugin__ = "AnimeFTW"
+__author__ = "maruchan"
+__version__ = "1.5.0"
+__settings__ = xbmcaddon.Addon(id='plugin.video.animeftw')
+
+print "[PLUGIN] '%s: version %s' initialized!" % (__plugin__, __version__)
+
+if __name__ == "__main__":
+    from resources.lib import main_ftw
+    main_ftw.Main()
+
+sys.modules.clear()

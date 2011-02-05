c0d1afbecee92968287a83d4a963f52ac550f3a3
amet <amet.nospam@gmail.com>
2011-02-05 18:34:02 +0400

    [plugin.video.eevblog] -v0.1.6 - regex update for episodes >140

--- plugin.video.eevblog/default.py
+++ plugin.video.eevblog/default.py
@@ -2,8 +2,8 @@ import xbmc, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, tracebac
 
 __plugin__ =  'EEVblog'
 __author__ = 'Clumsy <clumsy@xbmc.org>'
-__date__ = '08-27-2010'
-__version__ = '0.1.5'
+__date__ = '02-05-2010'
+__version__ = '0.1.6'
 __settings__ = xbmcaddon.Addon(id='plugin.video.eevblog')
 
 # Thanks to some of the other plugin authors, where I borrowed some ideas from !
@@ -61,6 +61,10 @@ def play_video(ep_url):
   ep_data = open_url(ep_url)
   plot = re.compile('<div class="info">.+?<p>(.+?)</p>.', re.DOTALL).findall(ep_data)
   youtube_video_id = re.compile('<param name="movie" value=".*?/v/(.+?)[&\?].').findall(ep_data)
+  
+  # Ugly hack for a change in the page src from videos 140 onwards. 
+  if not youtube_video_id or youtube_video_id[0].find("youtube") == -1:
+	youtube_video_id = re.compile('src="http://www.youtube.com/embed/(.*?)"').findall(ep_data)
   
   quality = int(__settings__.getSetting('quality'))
   if quality == 0:

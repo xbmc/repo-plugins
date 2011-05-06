56d837e09d7dfa5b85581e399ac1a695130aa4cc
spiff <spiff@xbmc.org>
2011-05-06 18:43:49 +0200

    [plugin.audio.dr.dk.netradio] updated to version 1.1.0

--- plugin.audio.dr.dk.netradio/addon.py
+++ plugin.audio.dr.dk.netradio/addon.py
@@ -1,76 +1,31 @@
-import os
-import re
 import sys
+import simplejson
+import urllib2
 
 import xbmcgui
 import xbmcplugin
 
-import danishaddons
-import danishaddons.web
-
-BASE_URL = 'http://www.dr.dk/netradio/wmp.asp'
+CHANNELS_URL = 'http://www.dr.dk/LiveNetRadio/datafeed/channels.js.drxml'
 
 def showChannels():
-    icon = os.path.join(danishaddons.ADDON_PATH, 'icon.png')
+    u = urllib2.urlopen(CHANNELS_URL)
+    data = u.read()
+    u.close()
 
-    format = danishaddons.ADDON.getSetting('format')
-    if format == 'WMA':
-        html = danishaddons.web.downloadAndCacheUrl(BASE_URL, os.path.join(danishaddons.ADDON_DATA_PATH, 'channels.html'), 24 * 60)
+    channels = simplejson.loads(data[39:-3])
 
-        for m in re.finditer('<td nowrap="nowrap">(.*?)</td>.*?\n.*?<a href="([^"]+)">%s</a>' % getQuality(), html):
-            name = danishaddons.web.decodeHtmlEntities(m.group(1))
-            asxUrl = m.group(2)
+    for channel in channels:
+        item = xbmcgui.ListItem(channel['title'], iconImage = channel['logo'])
+        item.setProperty('IsPlayable', 'true')
+        item.setProperty("IsLive", "true")
+        item.setInfo(type = 'audio', infoLabels = {
+                'title' : channel['title']
+        })
+        xbmcplugin.addDirectoryItem(HANDLE, channel['mediaFile'], item)
 
-            item = xbmcgui.ListItem(name, iconImage = icon)
-            item.setProperty('IsPlayable', 'true')
-            item.setInfo(type = 'audio', infoLabels = {
-                    'title' : name
-            })
-            url = danishaddons.ADDON_PATH + '?url=' + asxUrl
-            xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
-    else: # format == 'AAC'
-        for idx in range(1, 31):
-            name = danishaddons.msg(30100 + idx)
-            url = danishaddons.msg(30200 + idx)
-            item = xbmcgui.ListItem(name, iconImage = icon)
-            item.setProperty('IsPlayable', 'true')
-            item.setProperty("IsLive", "true")
-            item.setInfo(type = 'audio', infoLabels = {
-                    'title' : name
-            })
-            xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
-
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
-
-def playStream(url):
-    playlist = danishaddons.web.downloadUrl(url)
-    m = re.search('<TITLE>(.*?)</TITLE>.*?<Ref href="(.*?)"/>', playlist, re.DOTALL)
-    
-    title = m.group(1)
-    streamUrl = m.group(2)
-
-    item = xbmcgui.ListItem(title = title, path = streamUrl)
-    item.setInfo('music', {
-        'artist' : 'DR',
-        'title' : title
-    })
-    xbmcplugin.setResolvedUrl(danishaddons.ADDON_HANDLE, True, item)
-
-
-def getQuality():
-    quality = danishaddons.ADDON.getSetting('quality')
-    if quality == 'High':
-        return 'H\&oslash;j'
-    elif quality == 'Medium':
-        return 'Mellem'
-    else:
-        return 'Lav'
+    xbmcplugin.endOfDirectory(HANDLE)
 
 if __name__ == '__main__':
-    danishaddons.init(sys.argv)
-
-    if danishaddons.ADDON_PARAMS.has_key('url'):
-        playStream(danishaddons.ADDON_PARAMS['url'])
-    else:
-        showChannels()
+    HANDLE = int(sys.argv[1])
+    showChannels()
 

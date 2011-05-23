9b38533a9bdf5a908cbd040be7681157a6e4e934
spiff <spiff@xbmc.org>
2011-05-23 20:55:14 +0200

    [plugin.video.zapiks] updated to version 0.0.6

--- plugin.video.zapiks/default.py
+++ plugin.video.zapiks/default.py
@@ -1,89 +1,80 @@
-import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcaddon
+import urllib,urllib2,re,os
+import xbmcplugin,xbmcgui,xbmcaddon
 from BeautifulSoup import BeautifulSoup
 
 __settings__ = xbmcaddon.Addon(id='plugin.video.zapiks')
 __language__ = __settings__.getLocalizedString
 sort = __settings__.getSetting('sort_by')
+home = __settings__.getAddonInfo('path')
 
 
-def CATEGORIES():
+def categories():
+    if sort==__language__(30013):
+        u = '/premium_1.php'
+        addDir(__language__(30000),'http://www.zapiks.com/_surf_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/surf.png' ) ))
+        addDir(__language__(30001),'http://www.zapiks.com/_snowboard_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/snowboard.png' ) ))
+        addDir(__language__(30002),'http://www.zapiks.com/_mountainbike_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/vtt.png' ) ))
+        addDir(__language__(30003),'http://www.zapiks.com/_bmx_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/bmx.png' ) ))
+        addDir(__language__(30004),'http://www.zapiks.com/_skate_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/skate.png' ) ))
+        addDir(__language__(30005),'http://www.zapiks.com/_ski_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/ski.png' ) ))
+    else:
         if sort==__language__(30010):
-		    u = '1'
-		    addDir(__language__(30000),'http://www.zapiks.fr/surf_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/surf.png')
-		    addDir(__language__(30001),'http://www.zapiks.fr/snowboard_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/snowboard.png')
-		    addDir(__language__(30002),'http://www.zapiks.fr/vtt_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/vtt.png')
-		    addDir(__language__(30003),'http://www.zapiks.fr/bmx_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/bmx.png')
-		    addDir(__language__(30004),'http://www.zapiks.fr/skate_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/skate.png')
-		    addDir(__language__(30005),'http://www.zapiks.fr/ski_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/ski.png')
-        elif sort==__language__(30011):
-		    u = '/popular_1.php'
-		    addDir(__language__(30000),'http://www.zapiks.fr/surf_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/surf.png')
-		    addDir(__language__(30001),'http://www.zapiks.fr/snowboard_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/snowboard.png')
-		    addDir(__language__(30002),'http://www.zapiks.fr/vtt_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/vtt.png')
-		    addDir(__language__(30003),'http://www.zapiks.fr/bmx_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/bmx.png')
-		    addDir(__language__(30004),'http://www.zapiks.fr/skate_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/skate.png')
-		    addDir(__language__(30005),'http://www.zapiks.fr/ski_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/ski.png')
-        elif sort==__language__(30012):
-		    u = '/alltimebuzzed_1.php'
-		    addDir(__language__(30000),'http://www.zapiks.fr/surf_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/surf.png')
-		    addDir(__language__(30001),'http://www.zapiks.fr/snowboard_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/snowboard.png')
-		    addDir(__language__(30002),'http://www.zapiks.fr/vtt_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/vtt.png')
-		    addDir(__language__(30003),'http://www.zapiks.fr/bmx_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/bmx.png')
-		    addDir(__language__(30004),'http://www.zapiks.fr/skate_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/skate.png')
-		    addDir(__language__(30005),'http://www.zapiks.fr/ski_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/ski.png')
-        elif sort==__language__(30013):
-		    u = '/premium_1.php'
-		    addDir(__language__(30000),'http://www.zapiks.fr/_surf_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/surf.png')
-		    addDir(__language__(30001),'http://www.zapiks.fr/_snowboard_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/snowboard.png')
-		    addDir(__language__(30002),'http://www.zapiks.fr/_vtt_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/vtt.png')
-		    addDir(__language__(30003),'http://www.zapiks.fr/_bmx_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/bmx.png')
-		    addDir(__language__(30004),'http://www.zapiks.fr/_skate_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/skate.png')
-		    addDir(__language__(30005),'http://www.zapiks.fr/_ski_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/ski.png')
-        else:
-		    u ='1'
-		    addDir(__language__(30000),'http://www.zapiks.fr/surf_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/surf.png')
-		    addDir(__language__(30001),'http://www.zapiks.fr/snowboard_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/snowboard.png')
-		    addDir(__language__(30002),'http://www.zapiks.fr/vtt_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/vtt.png')
-		    addDir(__language__(30003),'http://www.zapiks.fr/bmx_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/bmx.png')
-		    addDir(__language__(30004),'http://www.zapiks.fr/skate_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/skate.png')
-		    addDir(__language__(30005),'http://www.zapiks.fr/ski_'+u,1,'special://home/addons/plugin.video.zapiks/resources/images/ski.png')
+            u = '1'
+        if sort==__language__(30012):
+            u = '/alltimebuzzed_1.php'
+        if sort==__language__(30011):
+            u = '/popular_1.php'
+        addDir(__language__(30000),'http://www.zapiks.com/surf_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/surf.png' ) ))
+        addDir(__language__(30001),'http://www.zapiks.com/snowboard_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/snowboard.png' ) ))
+        addDir(__language__(30002),'http://www.zapiks.com/mountainbike_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/vtt.png' ) ))
+        addDir(__language__(30003),'http://www.zapiks.com/bmx_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/bmx.png' ) ))
+        addDir(__language__(30004),'http://www.zapiks.com/skate_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/skate.png' ) ))
+        addDir(__language__(30005),'http://www.zapiks.com/ski_'+u,1,xbmc.translatePath( os.path.join( home, 'resources/images/ski.png' ) ))
 
-			
-def INDEX(url):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        link=link.replace('&rsquo;',"'").replace('&#039;',"'").replace('&egrave;','e').replace('&amp;',' & ').replace('&ecirc;','e').replace('&uuml;','u').replace('&eacute;','e').replace('&agrave;','a').replace('&Auml;','A').replace('&quot;','"').replace('&euml;','e').replace('&iuml;','i')
-        match=re.compile('<div class="media_thumbnail medium">\n        \t\t\t\t<a href="(.+?)" title="(.+?)">\n        \t\t\t\t\t<img class="thumb" src="(.+?)" alt="video" /><br />\n        \t\t\t\t\t<span class="description">.+?</span>\n').findall(link)
-        for url,name,thumbnail in match:
-                addLink(name,'http://www.zapiks.fr'+url,2,thumbnail)
-        page=re.compile('<h4 class=".+?"><span class="prev"></span><span class="next"><a href=".+?"><img src=".+?" alt="&gt;|" title="&gt;|" height="16" width="16"></a><a href=".+?"><img src=".+?" alt="&gt;" title="&gt;" height="16" width="16"></a></span><span class="pages"> <a href=".+?" class="current">.+?</a>  <a href="(.+?)" class="normal">.+?</a>  <a href=".+?" class="normal">.+?</a>.+?<a href=".+?">.+?</a></span></h4>').findall(link)
-        if len(page)>1:del page[0];del page[0]
-        for url in page:
-                addDir(__language__(30006),'http://www.zapiks.fr'+url,1,'special://home/addons/plugin.video.zapiks/resources/images/next.png')
 
-				
-def VIDEOLINKS(url):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        match=re.compile('</div>\t\n\t\t\t<div class="embed">\n\t\t\t\t        <div id="flashvideo(.+?)" class="video" align="center">\n').findall(link)
-        for id in match:
-                u = 'http://www.zapiks.fr/view/index.php?file='+id+'&lang=fr'
-                req = urllib2.Request(u)
-                req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-                response = urllib2.urlopen(req)
-                link=response.read()
-                response.close()
-                soup = BeautifulSoup(link)
-                video_link=soup.find('file').contents[0]
-                play=xbmc.Player().play(video_link)
+def indexPage(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    videos = soup.findAll('div', attrs={'class' : "media_thumbnail medium"})
+    for video in videos:
+        try:
+            url = video('a')[0]['href']
+            name = video('a')[0]['title']
+            thumb = video('img')[0]['src']
+            addLink(name,'http://www.zapiks.com'+url,2,thumb)
+        except:
+            pass
+    try:
+        nextPage = soup.find('span', attrs={'class' : "next"})('a')[1]['href']
+        addDir(__language__(30006),'http://www.zapiks.com'+nextPage,1,'special://home/addons/plugin.video.zapiks/resources/images/next.png')
+    except:
+        pass
+
+
+def videoLinks(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    soup = BeautifulSoup(link)
+    vid = soup.find('link', attrs={'rel' : "video_src"})['href']
+    vidId = vid[-5:]
+    req = urllib2.Request('http://www.zapiks.com/view/index.php?file='+vidId+'&lang=fr')
+    req.addheaders = [('Referer', 'http://www.zapiks.com'),
+            ('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)')]
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    soup = BeautifulSoup(link)
+    url = soup.find('file').string
+    item = xbmcgui.ListItem(path=url)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 	
-                
+
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -99,7 +90,7 @@ def get_params():
                         splitparams=pairsofparams[i].split('=')
                         if (len(splitparams))==2:
                                 param[splitparams[0]]=splitparams[1]
-                                
+
         return param
 
 
@@ -108,7 +99,7 @@ def addLink(name,url,mode,iconimage):
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        liz.setProperty("IsPlayable","false")
+        liz.setProperty("IsPlayable","true")
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
         return ok
 
@@ -120,8 +111,8 @@ def addDir(name,url,mode,iconimage):
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
-        
-              
+
+
 params=get_params()
 url=None
 name=None
@@ -146,14 +137,14 @@ print "Name: "+str(name)
 
 if mode==None or url==None or len(url)<1:
         print ""
-        CATEGORIES()
-       
+        categories()
+
 elif mode==1:
         print ""+url
-        INDEX(url)
-        
+        indexPage(url)
+
 elif mode==2:
-        print "NEW LINK: "+url
-        VIDEOLINKS(url)
-		
+        print ""
+        videoLinks(url)
+
 xbmcplugin.endOfDirectory(int(sys.argv[1]))

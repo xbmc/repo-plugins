756a6ebe2936d2a71d3f5741b36aadcb6b06ef87
spiff <spiff@xbmc.org>
2011-09-14 11:17:09 +0200

    [plugin.video.gametest.dk] updated to version 1.1.0

--- plugin.video.gametest.dk/addon.py
+++ plugin.video.gametest.dk/addon.py
@@ -1,13 +1,12 @@
 import re
 import os
 import sys
+import urllib2
+import cgi as urlparse
 
 import xbmcgui
+import xbmcaddon
 import xbmcplugin
-
-import danishaddons
-import danishaddons.web
-import danishaddons.info
 
 BASE_URL = 'http://www.gametest.dk/'
 FLV_URL = BASE_URL + 'gametesttest/reviews/%s/%s.flv'
@@ -15,99 +14,117 @@ REVIEWS_URL = BASE_URL + 'index.php?anmeldelser=alle'
 PAGE_URL = BASE_URL + 'index.php?page=%s'
 
 CATEGORIES = [
-    {'title' : 30010, 'params' : 'reviews'},
-    {'title' : 30011, 'params' : 'retro'},
-    {'title' : 30012, 'params' : 'stunts'},
+    {'title' : 30010, 'params' : 'reviews=1'},
+    {'title' : 30011, 'params' : 'retro=1'},
+    {'title' : 30012, 'params' : 'stunts=1'},
 ]
 
-def showOverview():
-    html = danishaddons.web.downloadAndCacheUrl(BASE_URL, os.path.join(danishaddons.ADDON_DATA_PATH, 'overview.html'), 24 * 60)
+class Gametest(object):
+    def showOverview(self):
+        html = self.downloadUrl(BASE_URL)
 
-    m = re.search('Sendt den ([^<]+).*?<ul>(.*?)</ul>', html, re.DOTALL)
-    date = m.group(1)
-    title = danishaddons.msg(30000) % date
-    games = m.group(2).replace('<li>', '').replace('</li>', '\n')
+        # Latest show
+        m = re.search('Sendt den ([^<]+).*?<ul>(.*?)</ul>', html, re.DOTALL)
+        date = m.group(1)
+        title = ADDON.getLocalizedString(30000) % date
+        games = m.group(2).replace('<li>', '').replace('</li>', '\n')
 
-    path = danishaddons.ADDON.getAddonInfo('path')
-    icon = os.path.join(path, 'icon.png')
-    item = xbmcgui.ListItem(title, iconImage = icon)
-    item.setInfo('video', {
-        'title' : title,
-        'plot' : str(danishaddons.msg(30001)) + games,
-        'date' : date.replace('-', '.')
-    })
-    url = FLV_URL % ('file', 'Programmet')
-    xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
-
-    for idx, c in enumerate(CATEGORIES):
-        item = xbmcgui.ListItem(danishaddons.msg(c['title']), iconImage = icon)
-        url = danishaddons.ADDON_PATH + '?' + c['params']
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item, True)
-
-    xbmcplugin.setContent(danishaddons.ADDON_HANDLE, 'episodes')
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
-
-def showReviews():
-    html = danishaddons.web.downloadAndCacheUrl(REVIEWS_URL, os.path.join(danishaddons.ADDON_DATA_PATH, 'reviews.html'), 24 * 60)
-
-    for m in re.finditer("index.php\?play=(.*?)&type=anmeldelse'><img src=' (.*?)'.*?anmeldelse'>(.*?)</a>.*?af: (.*?)</a>(.*?)time'>(.*?)</p>", html, re.DOTALL):
-        slug = m.group(1)
-        icon = m.group(2)
-        title = m.group(3)
-        author = m.group(4)
-        rating = m.group(5).count('good.png')
-        date = m.group(6)
-
-        item = xbmcgui.ListItem(title, iconImage = BASE_URL + icon)
+        path = ADDON.getAddonInfo('path')
+        icon = os.path.join(path, 'icon.png')
+        item = xbmcgui.ListItem(title, iconImage = icon)
+        item.setProperty('Fanart_Image', FANART)
         item.setInfo('video', {
             'title' : title,
-            'date' : date.replace('-', '.'),
-            'year' : int(date[6:]),
-            'credits' : author,
-            'plot' : title,
-            'rating' : rating
+            'plot' : str(ADDON.getLocalizedString(30001)) + games,
+            'date' : date.replace('-', '.')
         })
-        url = FLV_URL % ('file', slug)
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
+        url = FLV_URL % ('file', 'Programmet')
+        xbmcplugin.addDirectoryItem(HANDLE, url, item)
 
-    xbmcplugin.setContent(danishaddons.ADDON_HANDLE, 'episodes')
-    xbmcplugin.addSortMethod(danishaddons.ADDON_HANDLE, xbmcplugin.SORT_METHOD_DATE)
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
+        # Categories
+        for idx, c in enumerate(CATEGORIES):
+            item = xbmcgui.ListItem(ADDON.getLocalizedString(c['title']), iconImage = icon)
+            item.setProperty('Fanart_Image', FANART)
+            url = PATH + '?' + c['params']
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
 
-def showPage(page):
-    html = danishaddons.web.downloadAndCacheUrl(PAGE_URL % page, os.path.join(danishaddons.ADDON_DATA_PATH, '%s.html' % page), 24 * 60)
+        xbmcplugin.setContent(HANDLE, 'episodes')
+        xbmcplugin.endOfDirectory(HANDLE)
 
-    for m in re.finditer("index.php\?play=(.*?)&type=.*?src=[ \"](.*?)[ \"](.*?<b>(.*?)</b>.*?af:.*?>(.*?)</a>)?", html, re.DOTALL):
-        slug = m.group(1)
-        icon = m.group(2)
-        title = m.group(4)
-        author = m.group(5)
+    def showReviews(self):
+        html = self.downloadUrl(REVIEWS_URL)
 
-        item = xbmcgui.ListItem(title, iconImage = BASE_URL + icon)
-        item.setInfo('video', {
-            'title' : title,
-            'credits' : author,
-            'plot' : title
-        })
+        for m in re.finditer("index.php\?play=(.*?)&type=anmeldelse'><img src=' (.*?)'.*?anmeldelse'>(.*?)</a>.*?af: (.*?)</a>(.*?)time'>(.*?)</p>", html, re.DOTALL):
+            slug = m.group(1)
+            icon = m.group(2)
+            title = m.group(3)
+            author = m.group(4)
+            rating = m.group(5).count('good.png')
+            date = m.group(6)
 
-        url = FLV_URL % ('stunts', slug)
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item)
+            item = xbmcgui.ListItem(title, iconImage = BASE_URL + icon)
+            item.setProperty('Fanart_Image', FANART)
+            item.setInfo('video', {
+                'title' : title,
+                'date' : date.replace('-', '.'),
+                'year' : int(date[6:]),
+                'credits' : author,
+                'plot' : title,
+                'rating' : rating
+            })
+            url = FLV_URL % ('file', slug)
+            xbmcplugin.addDirectoryItem(HANDLE, url, item)
 
-    xbmcplugin.setContent(danishaddons.ADDON_HANDLE, 'episodes')
-    xbmcplugin.addSortMethod(danishaddons.ADDON_HANDLE, xbmcplugin.SORT_METHOD_LABEL)
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
+        xbmcplugin.setContent(HANDLE, 'episodes')
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
+        xbmcplugin.endOfDirectory(HANDLE)
 
+    def showPage(self, page):
+        html = self.downloadUrl(PAGE_URL % page)
+
+        for m in re.finditer("index.php\?play=(.*?)&type=.*?src=[ \"](.*?)[ \"](.*?<b>(.*?)</b>.*?af:.*?>(.*?)</a>)?", html, re.DOTALL):
+            slug = m.group(1)
+            icon = m.group(2)
+            title = m.group(4)
+            author = m.group(5)
+
+            item = xbmcgui.ListItem(title, iconImage = BASE_URL + icon)
+            item.setProperty('Fanart_Image', FANART)
+            item.setInfo('video', {
+                'title' : title,
+                'credits' : author,
+                'plot' : title
+            })
+
+            url = FLV_URL % ('stunts', slug)
+            xbmcplugin.addDirectoryItem(HANDLE, url, item)
+
+        xbmcplugin.setContent(HANDLE, 'episodes')
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_LABEL)
+        xbmcplugin.endOfDirectory(HANDLE)
+
+    def downloadUrl(self, url):
+        u = urllib2.urlopen(url)
+        data = u.read()
+        u.close()
+        return data
 
 
 if __name__ == '__main__':
-    danishaddons.init(sys.argv)
+    ADDON = xbmcaddon.Addon(id = 'plugin.video.gametest.dk')
+    PATH = sys.argv[0]
+    HANDLE = int(sys.argv[1])
+    PARAMS = urlparse.parse_qs(sys.argv[2][1:])
 
-    if danishaddons.ADDON_PARAMS.has_key('reviews'):
-        showReviews()
-    elif danishaddons.ADDON_PARAMS.has_key('retro'):
-        showPage('retro')
-    elif danishaddons.ADDON_PARAMS.has_key('stunts'):
-        showPage('stunts')
+    FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
+
+    gt = Gametest()
+    if PARAMS.has_key('reviews'):
+        gt.showReviews()
+    elif PARAMS.has_key('retro'):
+        gt.showPage('retro')
+    elif PARAMS.has_key('stunts'):
+        gt.showPage('stunts')
     else:
-        showOverview()
+        gt.showOverview()
 
--- plugin.video.gametest.dk/test.py
+++ plugin.video.gametest.dk/test.py
@@ -1,4 +1,3 @@
-import os
 import re
 import sys
 import unittest
@@ -14,10 +13,11 @@ import addon
 class TestGametestDk(unittest.TestCase):
 
     def setUp(self):
-        danishaddons.init([os.getcwd(), '12345', ''])
+        danishaddons.init(['.', '12345', ''])
         xbmcplugin.items = list()
 
     def testShowOverview(self):
+	xbmcaddon.strings[30000] = 'Date: %s'
         addon.showOverview()
 
         self.assertNotEquals(0, len(xbmcplugin.items), msg = 'Expected at least one ListItem')

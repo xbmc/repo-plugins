b4fb16d95dbd7133a3178100072cf7d1c83f1147
spiff <spiff@xbmc.org>
2011-09-16 16:56:00 +0200

    [plugin.video.onside.tv] updated to version 1.1.1

--- plugin.video.onside.tv/addon.py
+++ plugin.video.onside.tv/addon.py
@@ -15,36 +15,47 @@ class OnsideTV(object):
             name = m.group(2)
 
             item = xbmcgui.ListItem(name[5:], iconImage=ICON)
+            item.setProperty('Fanart_Image', FANART)
             xbmcplugin.addDirectoryItem(HANDLE, PATH + '?slug='+ slug + '&page=1', item, True)
 
-        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_TITLE)
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_LABEL)
         xbmcplugin.endOfDirectory(HANDLE)
 
-
     def listPrograms(self, slug, page):
+        print BASE_URL + '/onsidetv/' + slug + '/' + str(page)
         html = self.downloadUrl(BASE_URL + '/onsidetv/' + slug + '/' + str(page))
         for m in re.finditer('<a href="/fodbold-video/([^"]+)".*?<img src="(.*?)".*?class="video-title">(.*?)</div>.*?class="desc">(.*?)</div>.*?class="date">(.*?)</div>', html, re.DOTALL):
             slug = m.group(1)
             image = m.group(2)
             title = m.group(3)
             description = m.group(4)
-            date = m.group(5)
+            dateStr = m.group(5)
 
             icon = BASE_URL + image
             icon = icon.replace('136x91', 'onsidetv_658x366')
-            item = xbmcgui.ListItem(title, iconImage = icon)
+            item = xbmcgui.ListItem(title, iconImage = icon, thumbnailImage = icon)
             item.setProperty('IsPlayable', 'true')
             item.setProperty('Fanart_Image', icon)
-            item.setInfo(type = 'Video', infoLabels = {
+            item.setInfo(type = 'video', infoLabels = {
                 'title' : title,
-                'plot' : description
+                'plot' : description,
+                'date' : dateStr[0:10]
             })
             xbmcplugin.addDirectoryItem(HANDLE, PATH + '?play=' + slug, item)
 
+        if ADDON.getSetting('show.load.next.page') == 'true':
+            m = re.search('<a href="/onsidetv/([^/]+)/\d+">&gt;&gt;', html)
+            if m:
+                item = xbmcgui.ListItem(ADDON.getLocalizedString(30000), iconImage=ICON)
+                item.setProperty('Fanart_Image', FANART)
+                xbmcplugin.addDirectoryItem(HANDLE, PATH + '?slug='+ m.group(1) + '&page=' + str(page + 1), item, True)
+
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
         xbmcplugin.endOfDirectory(HANDLE)
 
     def playProgram(self, slug):
-        html = self.downloadUrl(BASE_URL + '/onsidetv/' + slug)
+        url = BASE_URL + '/fodbold-video/' + slug
+        html = self.downloadUrl(url)
         m = re.search('id="onside_video_player" href="(.*?)"', html)
 
         item = xbmcgui.ListItem(path = m.group(1))
@@ -62,7 +73,7 @@ if __name__ == '__main__':
     HANDLE = int(sys.argv[1])
     PARAMS = urlparse.parse_qs(sys.argv[2][1:])
 
-    #FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
+    FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
 
     otv = OnsideTV()

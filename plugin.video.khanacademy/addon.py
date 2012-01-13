fdb3deb40988c2a033ead1dadb54cfdc85998432
spiff <spiff@xbmc.org>
2012-01-13 21:11:59 +0100

    plugin.video.khanacademy] updated to version 1.4

--- plugin.video.khanacademy/addon.py
+++ plugin.video.khanacademy/addon.py
@@ -1,74 +1,104 @@
 #!/usr/bin/env python
-# Copyright 2011 Jonathan Beluch.  
-# 
-# This program is free software: you can redistribute it and/or modify 
-# it under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or 
-# (at your option) any later version. 
-# 
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-# GNU General Public License for more details. 
-# 
-# You should have received a copy of the GNU General Public License 
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-from xbmcswift import Plugin, download_page
-from BeautifulSoup import BeautifulSoup as BS
+# Copyright 2011 Jonathan Beluch.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+import time
+try:
+    import json
+except ImportError:
+    import simplejson as json
 from urlparse import urljoin
+from xbmcswift import Plugin, download_page, xbmc
+from BeautifulSoup import BeautifulSoup as BS
+from resources.lib.khan import KhanData, download_playlists_json
+from resources.lib.cache import get_cached_data, put_cached_data
 
 __plugin_name__ = 'Khan Academy'
 __plugin_id__ = 'plugin.video.khanacademy'
-
 plugin = Plugin(__plugin_name__, __plugin_id__, __file__)
-
 BASE_URL = 'http://www.khanacademy.org'
+
+
+# Ugly temporary hack until xbmcswift is fixed. Need to ensure the basedirs
+# for the cache already exist before we attempt to use it. Also, in pyton 2.4
+# we don't get the lovely os.makedirs :(
+def make_cache_dirs():
+    '''Make plugin_id and .cache dirs for the current plugin.'''
+    def make_if_not_exist(path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+    cache_root = xbmc.translatePath('special://profile/addon_data')
+    make_if_not_exist(os.path.join(cache_root, __plugin_id__))
+    make_if_not_exist(os.path.join(cache_root, __plugin_id__, '.cache'))
+make_cache_dirs()
+
+
 def full_url(path):
+    '''Returns the full url for the given path. Uses BASE_URL.'''
     return urljoin(BASE_URL, path)
 
+
 def htmlify(url):
+    '''Returns a BeautifulSoup object for a give url's response.'''
     return BS(download_page(url))
 
-# Default View
+
+def get_khan_data():
+    '''Returns a KhanData instance containg playlist data.
+
+    Behind the scenes, it checks for a local cached copy first. If the cached
+    copy's lifetime has not expired it will use the local copy. Otherwise, it
+    will fetch fresh data from the API and cache it locally.
+    '''
+    json_fn = plugin.cache_fn('playlists.json')
+    timestamp_fn = plugin.cache_fn('playlists.json.ts')
+
+    _json = get_cached_data(json_fn, timestamp_fn)
+    if _json is None:
+        _json = download_playlists_json()
+        put_cached_data(json.dumps(_json), json_fn, timestamp_fn)
+
+    return KhanData(_json)
+
+
+KHAN_DATA = get_khan_data()
+
+
 @plugin.route('/')
-def show_subjects():
-    '''Displays subjects from the homepage.'''
-    html = htmlify(BASE_URL)
-    container = html.find('div', {'id': 'library-content-main'})
-    subjects = container.findAll('div', {'data-role': 'page'})
+@plugin.route('/<category>/', name='show_category')
+def main_menu(category='_root'):
+    '''This view displays Categories or Playlists.
 
-    items = [{
-        'label': s.h2.string,
-        'url': plugin.url_for('show_videos', subject_id=s['id']),
-    } for s in subjects]
-    
+    This method does a lookup based on the passed category/playlist name to get
+    the members. The "root" or base category is name "_root"
+    '''
+    items = [item.to_listitem(plugin)
+             for item in KHAN_DATA.get_items(category)]
     return plugin.add_items(items)
 
-@plugin.route('/videos/<subject_id>/')
-def show_videos(subject_id):
-    '''Displays lessons for a give subject id.'''
-    html = htmlify(BASE_URL)
-    subject = html.find('div', {'id': subject_id})
-    videos = subject.findAll('a', {'class': 'vl'})
 
-    items =  [{
-        'label': a.span.string,
-        'url': plugin.url_for('play_video', url=full_url(a['href'])),
-        'is_playable': True,
-        'is_folder': False,
-    } for a in videos]
-
-    return plugin.add_items(items)
-
-@plugin.route('/play/<url>/')
-def play_video(url):
-    '''Resolves a lesson page's url to a playable video url.'''
+@plugin.route('/play/<video_slug>/')
+def play_video(video_slug):
+    '''Resolves a video page's url to a playable video url.'''
     # Videos are both in .mp4 format and youtube. For simplicity's sake just
     # use mp4 for now.
+    url = 'http://www.khanacademy.org/video/%s' % video_slug
     html = htmlify(url)
     a = html.find('a', {'title': 'Download this lesson'})
     plugin.set_resolved_url(a['href'])
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     plugin.run()
---
+++ plugin.video.khanacademy/resources/lib/khan.py
@@ -0,0 +1,114 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import time
+import urllib
+
+
+def api_url():
+    '''Returns the API url with a timestamp url paremeter set to
+    int(time.time() * 1000)
+    '''
+    return 'http://www.khanacademy.org/api/v1/playlists/library/compact?v=%d' %
+    int(time.time() * 1000)
+
+
+API_URL = api_url()
+
+
+def download_playlists_json():
+    '''Fetches the playlist JSON from the Khan Academy API and returns the
+    parsed JSON object.
+    '''
+    conn = urllib.urlopen(API_URL)
+    resp = json.load(conn)
+    conn.close()
+    return resp
+
+
+class Video(object):
+
+    def __init__(self, _json):
+        self.key_id = _json['key_id']
+        self.title = _json['title']
+        self.readable_id = _json['readable_id']
+
+    def to_listitem(self, plugin):
+        '''Returns a dict suitable for passing to xbmcswift.plugin.add_items'''
+        return {
+            'label': self.title,
+            'url': plugin.url_for('play_video', video_slug=self.readable_id),
+            'is_playable': True,
+            'is_folder': False,
+        }
+
+
+class Playlist(object):
+
+    def __init__(self, _json):
+        self.slugged_title = _json['slugged_title']
+        self.title = _json['title']
+        self.videos = [Video(item) for item in _json['videos']]
+
+    def __iter__(self):
+        return iter(self.videos)
+
+    def to_listitem(self, plugin):
+        '''Returns a dict suitable for passing to xbmcswift.plugin.add_items'''
+        return {
+            'label': self.title,
+            'url': plugin.url_for('show_category', category=self.title),
+        }
+
+
+class Category(object):
+
+    def __init__(self, _json):
+        self.name = _json['name']
+
+    def to_listitem(self, plugin):
+        '''Returns a dict suitable for passing to xbmcswift.plugin.add_items'''
+        return {
+            'label': self.name,
+            'url': plugin.url_for('show_category', category=self.name),
+        }
+
+
+class KhanData(object):
+    '''This class repurposes the Khan Academy playlist JSON into more
+    convenient data structures.
+    '''
+
+    def __init__(self, _json):
+        self._items = {}
+        self._parse(_json, '_root')
+
+    def _is_playlist(self, item):
+        return 'playlist' in item.keys()
+
+    def _create_keys(self, key):
+        if key not in self._items.keys():
+            self._items[key] = []
+
+    def _parse(self, _json, current_key):
+        '''Recursive parsing function to format the _json input in a single
+        dict keyable on item['name']
+        '''
+        self._create_keys(current_key)
+
+        for item in _json:
+            if self._is_playlist(item):
+                playlist = Playlist(item['playlist'])
+                self._items[current_key].append(playlist)
+                self._items[item['name']] = playlist
+            else:
+                category = Category(item)
+                self._items[current_key].append(category)
+                self._parse(item['items'], item['name'])
+
+    def get_items(self, key):
+        '''For a given key, returns a list of items (Category or Playlist) or
+        if the key is a playlist name, returns a Playlist object.
+        '''
+        return self._items[key]

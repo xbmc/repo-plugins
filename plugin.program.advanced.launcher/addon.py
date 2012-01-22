df3fa30773c3d171959c6f40c39056909753a1ff
spiff <spiff@xbmc.org>
2012-01-22 21:51:34 +0100

    [plugin.program.advanced.launcher] updated to version 1.7.4

--- plugin.program.advanced.launcher/addon.py
+++ plugin.program.advanced.launcher/addon.py
@@ -12,7 +12,7 @@ __author__ = "Angelscry"
 __url__ = "http://code.google.com/p/xbmc-advanced-launcher/"
 __svn_url__ = "http://xbmc-advanced-launcher.googlecode.com/svn/trunk/plugin.program.advanced.launcher/"
 __credits__ = "Leo212, CinPoU, JustSomeUser, Zerqent, Zosky, Atsumori"
-__version__ = "1.7.2"
+__version__ = "1.7.4"
 
 if ( __name__ == "__main__" ):
     import resources.lib.launcher_plugin as plugin

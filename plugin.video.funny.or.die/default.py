975dd4a61e3cbcdafe7c7b5dd714d9524e923bbb
spiff <spiff@xbmc.org>
2012-01-28 11:10:40 +0100

    [plugin.video.funny.or.die] updated to version 1.0.1

--- plugin.video.funny.or.die/default.py
+++ plugin.video.funny.or.die/default.py
@@ -1,210 +1,214 @@
-import urllib,urllib2,re,os
-import xbmcplugin,xbmcgui,xbmcaddon
+import urllib
+import urllib2
+import re
+import os
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
 from BeautifulSoup import BeautifulSoup
 
 __settings__ = xbmcaddon.Addon(id='plugin.video.funny.or.die')
 __language__ = __settings__.getLocalizedString
-sort = __settings__.getSetting('sort_by')
+sort = __settings__.getSetting('sort_by').replace(' ','_').lower()
+sort_time = __settings__.getSetting('sort_time').replace(' ','_').lower()
 home = __settings__.getAddonInfo('path')
 icon = xbmc.translatePath( os.path.join( home, 'icon.png' ) )
 
 
-def CATEGORIES():
-	if sort==__language__(30015):
-		u = ''
-	elif sort==__language__(30016):
-		u = 'most_recent'
-	elif sort==__language__(30017):
-		u = 'most_viewed'
-	elif sort==__language__(30018):
-		u = 'most_favorited'
-	elif sort==__language__(30019):
-		u = 'highest_rated'
-	else:
-		u = ''
-	addDir(__language__(30000),'http://www.funnyordie.com/browse/videos/all/exclusives/'+u,1,icon)
-	addDir(__language__(30001),'http://www.funnyordie.com/browse/videos/all/immortal/'+u,1,icon)
-	addDir(__language__(30002),'http://www.funnyordie.com/browse/videos/stand_up/all/'+u,1,icon)
-	addDir(__language__(30003),'http://www.funnyordie.com/browse/videos/animation/all/'+u,1,icon)
-	addDir(__language__(30004),'http://www.funnyordie.com/browse/videos/web_series/all/'+u,1,icon)
-	addDir(__language__(30005),'http://www.funnyordie.com/browse/videos/nsfw/all/'+u,1,icon)
-	addDir(__language__(30006),'http://www.funnyordie.com/browse/videos/sketch/all/'+u,1,icon)
-	addDir(__language__(30007),'http://www.funnyordie.com/browse/videos/sports/all/'+u,1,icon)
-	addDir(__language__(30008),'http://www.funnyordie.com/browse/videos/clean_comedy/all/'+u,1,icon)
-	addDir(__language__(30009),'http://www.funnyordie.com/browse/videos/politics/all/'+u,1,icon)
-	addDir(__language__(30010),'http://www.funnyordie.com/browse/videos/music/all/'+u,1,icon)
-	addDir(__language__(30011),'http://www.funnyordie.com/browse/videos/parody/all/'+u,1,icon)
-	addDir(__language__(30012),'http://www.funnyordie.com/browse/videos/real_life/all/'+u,1,icon)
-	addDir(__language__(30013),'http://www.funnyordie.com/browse/videos/all/all/'+u,1,icon)
+def categories():
+        addDir(__language__(30000),'http://www.funnyordie.com/browse/videos/all/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30001),'http://www.funnyordie.com/browse/videos/all/exclusives/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30002),'http://www.funnyordie.com/browse/videos/all/immortal/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30003),'http://www.funnyordie.com/browse/videos/animation/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30004),'http://www.funnyordie.com/browse/videos/animals/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30005),'http://www.funnyordie.com/browse/videos/clean_comedy/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30006),'http://www.funnyordie.com/browse/videos/dumb_people/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30007),'http://www.funnyordie.com/browse/videos/fails/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30008),'http://www.funnyordie.com/browse/videos/memes/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30009),'http://www.funnyordie.com/browse/videos/kids/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30010),'http://www.funnyordie.com/browse/videos/mashups/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30011),'http://www.funnyordie.com/browse/videos/music/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30012),'http://www.funnyordie.com/browse/videos/news_fails/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30013),'http://www.funnyordie.com/browse/videos/nostalgia/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30014),'http://www.funnyordie.com/browse/videos/nsfw/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30015),'http://www.funnyordie.com/browse/videos/parody/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30016),'http://www.funnyordie.com/browse/videos/politics/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30017),'http://www.funnyordie.com/browse/videos/rants/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30018),'http://www.funnyordie.com/browse/videos/real_life/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30019),'http://www.funnyordie.com/browse/videos/recaps/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30020),'http://www.funnyordie.com/browse/videos/sketch/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30021),'http://www.funnyordie.com/browse/videos/sports/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30022),'http://www.funnyordie.com/browse/videos/stand_up/all/'+sort+'/'+sort_time,1,icon)
+        addDir(__language__(30023),'http://www.funnyordie.com/browse/videos/web_series/all/'+sort+'/'+sort_time,1,icon)
 
 
-def INDEX(url):
-	req = urllib2.Request(url)
-	req.addheaders = [('Referer', 'http://www.funnyordie.com/videos'),
-			('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)')]
-	response = urllib2.urlopen(req)
-	link=response.read()
-	response.close()
-	soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
-	videos = soup.findAll('div', attrs={'class' : "detailed_vp"})
-	for video in videos:
-		try:
-			link = video('a')[0]['href'].split('/')[2]
-			name = video('a')[0]['title']
-			thumbnail = video('img')[0]['src'].replace('medium','fullsize')
-			addLink(name,link,2,thumbnail)
-		except:
-			pass
-	try:
-		page = soup.find('a', attrs={'class' : "next_page"})['href']
-		addDir(__language__(30021),'http://www.funnyordie.com'+page,1,'')
-	except:
-		pass
+def getRequest(url):
+        headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1',
+                   'Referer' : 'http://www.funnyordie.com/browse/videos'}
+        req = urllib2.Request(url, None, headers)
+        response = urllib2.urlopen(req)
+        link = response.read()
+        response.close()
+        return link
 
 
-def getPlayList():
-	url = sys.argv[2]
-	req = urllib2.Request(url)
-	req.addheaders = [('Referer', 'http://www.funnyordie.com/videos'),
-			('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)')]
-	response = urllib2.urlopen(req)
-	link=response.read()
-	response.close()
-	soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
-	videos = soup.findAll('div', attrs={'class' : "detailed_vp"})
-	playlist = xbmc.PlayList(1)
-	playlist.clear()
-	for video in videos:
-		try:
-			link = video('a')[0]['href'].split('/')[2]
-			name = video('a')[0]['title']
-			thumbnail = video('img')[0]['src'].replace('medium','fullsize')
-			url = get_smil(link)
-			info = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumbnail)
-			playlist.add(url, info)
-		except:
-			pass
-	play=xbmc.Player( xbmc.PLAYER_CORE_DVDPLAYER ).play(playlist)
+def index(url, play=False):
+        if play == True:
+            playlist = xbmc.PlayList(1)
+            playlist.clear()
+        link = getRequest(url)
+        link = link.replace("\\",'').replace('\n','').replace("\'",'')
+        items = re.compile('<div class="detailed_vp">(.+?)</a></div></div>').findall(link)
+        if len(items) < 1:
+            items = re.compile('<div class="detailed_vp">(.+?)</a>n</div></div>').findall(link)
+        for i in items:
+            name = re.compile('title="(.+?)"').findall(i)[0].replace('&amp;','&').replace('&quot;','"').replace('&apos;',"'")
+            try:
+                thumb = re.compile('class="thumbnail" height="90" src="(.+?)"').findall(i)[0].replace('medium','fullsize')
+            except:
+                try:
+                    thumb = re.compile('class="thumbnail" src="(.+?)"').findall(i)[0]
+                except:
+                    thumb = ''
+            vid_id = re.compile('data-viewkey="(.+?)"').findall(i)[0][1:]
+            try:
+                duration = re.compile('<span class="duration">(.+?)</span>').findall(i)[0]
+            except:
+                duration = ''
+            if not play == True:
+                addLink(name, vid_id, duration, 2, thumb)
+            else:
+                url = get_smil(vid_id)
+                info = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+                playlist.add(url, info)
+        try:
+            page_num = re.compile('next_page = (.+?);').findall(link)[0]
+            page = '?page='+page_num
+            if not 'more' in url:
+                url = url.replace('videos','more/videos')
+            url = url.split('?page')[0]+page
+            addDir(__language__(30031), url, 1, xbmc.translatePath( os.path.join( home, 'resources', 'next.png' ) ))
+        except: pass
+        if play == True:
+            xbmc.executebuiltin('playlist.playoffset(video,0)')
 
 
 def playVid(url):
-	url = get_smil(url)
-	item = xbmcgui.ListItem(path=url)
-	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+        url = get_smil(url)
+        item = xbmcgui.ListItem(path=url)
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 
-def addToPlayList():
-	url = sys.argv[2]
-	name = sys.argv[3]
-	iconimage = sys.argv[4]
-	url = get_smil(url)
-	info = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-	playlist = xbmc.PlayList(1)
-	playlist.add(url, info)
+def addToPlayList(name, url, iconimage):
+        url = get_smil(url)
+        info = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        playlist = xbmc.PlayList(1)
+        playlist.add(url, info)
 
 
 def get_smil(id):
-	req = urllib2.Request("http://www.funnyordie.com/player/"+id+"?v=3")
-	response = urllib2.urlopen(req)
-	smil = response.read()
-	response.close()
-	soup = BeautifulSoup(smil)
-	title = soup.find('title').string
-	stream_list = soup.findAll('stream')
-	if len(stream_list) < 1:
-		stream = soup.find('location').contents[0]
-		return stream
-	streams = []
-	for stream in stream_list:
-		streams.append(stream.file.contents[0])
-	return streams[0]
+        link = getRequest("http://www.funnyordie.com/player/"+id+"?v=3")
+        soup = BeautifulSoup(link)
+        title = soup.find('title').string
+        stream_list = soup.findAll('stream')
+        if len(stream_list) < 1:
+            stream = soup.find('location').contents[0]
+            return stream
+        else:
+            if __settings__.getSetting('video_quality') == '0':
+                return stream_list[0].file.contents[0]
+            if __settings__.getSetting('video_quality') == '1':
+                return stream_list[1].file.contents[0]
 
 
 def get_params():
-	param=[]
-	paramstring=sys.argv[2]
-	if len(paramstring)>=2:
-		params=sys.argv[2]
-		cleanedparams=params.replace('?','')
-		if (params[len(params)-1]=='/'):
-			params=params[0:len(params)-2]
-		pairsofparams=cleanedparams.split('&')
-		param={}
-		for i in range(len(pairsofparams)):
-			splitparams={}
-			splitparams=pairsofparams[i].split('=')
-			if (len(splitparams))==2:
-				param[splitparams[0]]=splitparams[1]
-				
-	return param
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+            params=sys.argv[2]
+            cleanedparams=params.replace('?','')
+            if (params[len(params)-1]=='/'):
+                params=params[0:len(params)-2]
+            pairsofparams=cleanedparams.split('&')
+            param={}
+            for i in range(len(pairsofparams)):
+                splitparams={}
+                splitparams=pairsofparams[i].split('=')
+                if (len(splitparams))==2:
+                    param[splitparams[0]]=splitparams[1]
+
+        return param
 
 
-def addLink(name,url,mode,iconimage,showcontext=True):
-	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-	ok=True
-	liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-	liz.setInfo( type="Video", infoLabels={ "Title": name } )
-	liz.setProperty('IsPlayable', 'true')
-	if showcontext:
-		contextMenu = [(__language__(30022),'XBMC.RunScript(special://home/addons/plugin.video.funny.or.die/default.py,addToPlayList,'+url+','+name+','+iconimage+')')]
-		liz.addContextMenuItems(contextMenu)
-	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,totalItems=20)
-	return ok
+def addLink(name,url,duration,mode,iconimage,showcontext=True):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Duration": duration } )
+        liz.setProperty('IsPlayable', 'true')
+        if showcontext:
+            contextMenu = [(__language__(30033),'XBMC.Container.Update(%s?url=%s&mode=3&name=%s&iconimage=%s)' %(sys.argv[0], urllib.quote_plus(url), urllib.quote_plus(name), urllib.quote_plus(iconimage)))]
+            liz.addContextMenuItems(contextMenu)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,totalItems=20)
+        return ok
+
 
 def addDir(name,url,mode,iconimage,showcontext=True):
-	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-	ok=True
-	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-	liz.setInfo( type="Video", infoLabels={ "Title": name } )
-	if showcontext:
-		contextMenu = [(__language__(30023),'XBMC.RunScript(special://home/addons/plugin.video.funny.or.die/default.py,autoPlay,'+url+')')]
-		liz.addContextMenuItems(contextMenu)
-	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-	return ok
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        if showcontext:
+            contextMenu = [(__language__(30032),'XBMC.Container.Update(%s?url=%s&mode=4)' %(sys.argv[0], urllib.quote_plus(url)))]
+            liz.addContextMenuItems(contextMenu)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
 
 
-def startPlugin():	
-	params=get_params()
-	url=None
-	name=None
-	mode=None
+params=get_params()
+url=None
+name=None
+mode=None
 
-	try:
-		url=urllib.unquote_plus(params["url"])
-	except:
-		pass
-	try:
-		name=urllib.unquote_plus(params["name"])
-	except:
-		pass
-	try:
-		mode=int(params["mode"])
-	except:
-		pass
+try:
+    url=urllib.unquote_plus(params["url"])
+except:
+    pass
+try:
+    name=urllib.unquote_plus(params["name"])
+except:
+    pass
+try:
+    iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+    pass
+try:
+    mode=int(params["mode"])
+except:
+    pass
 
-	print "Mode: "+str(mode)
-	print "URL: "+str(url)
-	print "Name: "+str(name)
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
 
-	if mode==None or url==None or len(url)<1:
-		print ""
-		CATEGORIES()
+if mode==None or url==None or len(url)<1:
+    print ""
+    categories()
 
-	elif mode==1:
-		print ""+url
-		INDEX(url)
+elif mode==1:
+    print ""+url
+    index(url)
 
-	elif mode==2:
-		print "NEW LINK: "+url
-		playVid(url)
+elif mode==2:
+    print ""
+    playVid(url)
 
-	xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
+elif mode==3:
+    print ""
+    addToPlayList(name, url, iconimage)
 
-if sys.argv[1] == 'addToPlayList':
-		addToPlayList()
+elif mode==4:
+    print ""
+    index(url, True)
 
-elif sys.argv[1] == 'autoPlay':
-		getPlayList()
-
-else:
-		startPlugin()
+xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))

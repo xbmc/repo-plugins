d6f1bbf21039017e138753cf5f99bf5ae25ca623
beenje <beenje@xbmc.org>
2012-02-03 14:25:47 +0100

    [plugin.video.eyetv.parser] updated to version 2.1.1

--- plugin.video.eyetv.parser/resources/lib/eyetv_parser.py
+++ plugin.video.eyetv.parser/resources/lib/eyetv_parser.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
-"""EyeTV parser
+#
+# Copyright (C) 2012 Benjamin Bertrand
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# http://www.gnu.org/copyleft/gpl.html
 
-Inspired by XBMCEyetvParser plugin from prophead and Nic Wolfe (midgetspy)
-http://xbmc-addons.googlecode.com/svn/trunk/plugins/video/XBMCEyetvParser
-
-Re-written for Dharma by beenje
-"""
-import sys
 import os
 import base64
 import datetime
 import re
 import xml.etree.ElementTree as ET
-import xbmcplugin
-import xbmcgui
-import xbmcaddon
+from xbmcswift import xbmc
 try:
     import xbmcvfs
 except ImportError:
@@ -29,13 +37,6 @@ SORTKEYS = ('Date Recorded', 'Date Recorded', 'Display Title')
 # 0 : recorded date
 # 1 : reverse recorded date
 # 2 : title
-
-__url__ = "http://github.com/beenje/plugin.video.eyetv.parser"
-__addon__ = xbmcaddon.Addon(id='plugin.video.eyetv.parser')
-__plugin__ = __addon__.getAddonInfo('name')
-__version__ = __addon__.getAddonInfo('version')
-
-getLS = __addon__.getLocalizedString
 
 
 class Plist:
@@ -76,11 +77,9 @@ class Plist:
 class Eyetv:
     """Class to retrieve EyeTV recordings"""
 
-    def __init__(self):
+    def __init__(self, archivePath, sortMethod):
         """Initialize the list of EyeTV recordings"""
-        # Get settings
-        self.archivePath = __addon__.getSetting('archivePath')
-        sortMethod = int(__addon__.getSetting('sortMethod'))
+        self.archivePath = archivePath
         # Parse the 'EyeTV Archive.xml' plist - copy it locally first
         # (needed for external python when the file is on a smb share for example)
         archiveXml = os.path.join(self.archivePath, u'EyeTV Archive.xml')
@@ -120,29 +119,3 @@ class Eyetv:
     def nbRecordings(self):
         """Return the number of recordings available"""
         return len(self.recordings)
-
-
-def main():
-    """Entry point"""
-
-    print '%s version %s' % (__plugin__, __version__)
-
-    # Open settings if EyeTV Archive path is not set
-    if not __addon__.getSetting('archivePath'):
-        __addon__.openSettings()
-
-    try:
-        eyetv = Eyetv()
-    except IOError:
-        xbmcgui.Dialog().ok(getLS(30100), getLS(30101))
-    else:
-        nbItems = eyetv.nbRecordings()
-        for (url, icon, thumbnail, info) in eyetv.recordingsInfo():
-            li = xbmcgui.ListItem(label=info['title'], iconImage=icon, thumbnailImage=thumbnail)
-            li.setInfo(type='Video', infoLabels=info)
-            xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, totalItems=nbItems)
-        xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
-
-
-if __name__ == "__main__":
-    main()

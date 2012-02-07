3a8229b9d982aa722ca6122072973d1b749bbdfe
spiff <spiff@xbmc.org>
2012-02-07 19:40:30 +0100

    [plugin.video.fox.news] updated to version 2.0.3

--- plugin.video.fox.news/default.py
+++ plugin.video.fox.news/default.py
@@ -16,8 +16,7 @@ __settings__ = xbmcaddon.Addon(id='plugin.video.fox.news')
 __language__ = __settings__.getLocalizedString
 home = __settings__.getAddonInfo('path')
 icon = xbmc.translatePath( os.path.join( home, 'icon.png' ) )
-cache = StorageServer.StorageServer()
-cache.table_name = "FoxNews"
+cache = StorageServer.StorageServer("FoxNews", 24)
 
 
 def getCategories():
@@ -62,10 +61,15 @@ def getSubcategories(url):
             ok=True
             liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=icon)
             liz.setInfo( type="Video", infoLabels={ "Title": name } )
+            contextMenu = [(__language__(30019),'RunPlugin(%s?url=%s&mode=3&play=True)' %(sys.argv[0], urllib.quote_plus(url)))]
+            liz.addContextMenuItems(contextMenu)
             ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
 
 
-def getVideos(url):
+def getVideos(url, play=False):
+        if play:
+            playlist = xbmc.PlayList(1)
+            playlist.clear()
         headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1',
                    'Referer' : 'http://video.foxnews.com'}
         req = urllib2.Request('http://video.foxnews.com/v/feed/playlist/'+url+'.js?',None,headers)
@@ -85,9 +89,15 @@ def getVideos(url):
             name = name.replace('&amp;',' & ')
             url = url.replace('HIGH',__settings__.getSetting('video_quality'))
             duration = video['media-content']['mvn-duration']
-            liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
-            liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":desc, "Duration":duration} )
-            ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+            if play:
+                info = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+                playlist.add(url, info)
+            else:
+                liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumb)
+                liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":desc, "Duration":duration} )
+                ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        if play:
+            xbmc.executebuiltin('playlist.playoffset(video,0)')
 
 
 def addDir(name,url,mode,iconimage):
@@ -138,6 +148,10 @@ try:
     mode=int(params["mode"])
 except:
     pass
+try:
+    play=eval(params["play"])
+except:
+    pass
 
 print "Mode: "+str(mode)
 print "URL: "+str(url)
@@ -148,11 +162,15 @@ if mode==None:
     getCategories()
 
 elif mode==1:
-    print ""+url
+    print ""
     getSubcategories(url)
 
 elif mode==2:
-    print ""+url
+    print ""
     getVideos(url)
+
+elif mode==3:
+    print ""
+    getVideos(url, True)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))

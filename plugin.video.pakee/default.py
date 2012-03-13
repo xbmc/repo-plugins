5f0347fe3f375f23c5defa492fdd14dd560ba042
spiff <spiff@xbmc.org>
2012-03-13 21:09:40 +0100

    [plugin.video.pakee] updated to version 1.0.6

--- plugin.video.pakee/default.py
+++ plugin.video.pakee/default.py
@@ -9,9 +9,10 @@ __plugin__ = 'Pakee'
 __author__ = 'pakeeapp@gmail.com'
 __url__ = 'http://code.google.com/p/pakee/'
 __date__ = '01-04-2011'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __settings__ = xbmcaddon.Addon(id='plugin.video.pakee')
-__rooturl__ = 'http://pakee.hopto.org/pakee/pakee-xbmc.xml?a=3'
+__rooturl__ = 'http://pakee.hopto.org/pakee/pakee.php?id=xbmc&z=9'
+#__rooturl__ = 'http://pakee.hopto.org/pakee/pakee-test.xml?x=5'
 __language__ = __settings__.getLocalizedString
 
 #plugin modes
@@ -67,7 +68,7 @@ def play_youtube_video(video_id, name):
 		
 #Play a single song	
 def play_stream(url, name):
-	print "playing audio file. name: " + name + " url: " + url
+	print "playing video stream name: " + str(name) + " url: " + str(url)
 	listitem = xbmcgui.ListItem( label = str(name), iconImage = "DefaultVideo.png", thumbnailImage = xbmc.getInfoImage( "ListItem.Thumb" ), path=url )
 	listitem.setInfo( type="video", infoLabels={ "Title": name, "Plot" : name } )
 	xbmc.Player( xbmc.PLAYER_CORE_DVDPLAYER ).play( str(url), listitem)
@@ -188,9 +189,28 @@ def build_show_directory(origurl):
 	if items is None:
 		return
 
-
-
 	itemCount=0
+
+	if origurl == __rooturl__ and 'pakee-test.xml' in __rooturl__:
+		html = open_url("http://www.mast.tv/embed.php?stream=humtv&location=france")
+		match=re.compile('<param name="URL" value="(.+?)">').findall(html)
+		isFolder = False
+		mode = PLUGIN_MODE_PLAY_STREAM
+
+		for streamurl in match:			
+			streamurl = streamurl.replace("http","mms")
+			streamurl = streamurl.replace(" ","%20")
+			xbmc.log("Found url: " + str(streamurl))
+			listitem = xbmcgui.ListItem( label = 'Hum', iconImage = pakee_thumb, thumbnailImage = pakee_thumb )
+			u = sys.argv[0] + "?mode=" + str(PLUGIN_MODE_PLAY_STREAM) + "&name=" + urllib.quote_plus( "Hum" ) + "&url=" + urllib.quote_plus( streamurl ) + "&index=" + str(itemCount)
+			xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = isFolder )
+			
+
+		#shows = common.parseDOM(html, "ul", attrs = { "id": "shows" })[0]
+		#url_name = re.compile('<h3><a href="(.+?)">(.+?)</a></h3>').findall(shows)
+		#image = re.compile('class="thumbnail"><img src="(.+?)" /></a>').findall(shows)
+		#plot = common.parseDOM(shows, "p", attrs = { "class": "description" })
+
 
 	for item in items:
 
@@ -213,6 +233,9 @@ def build_show_directory(origurl):
 
 		mode = PLUGIN_MODE_BUILD_DIR
 		isFolder = True
+	
+
+
 
 		#if weird characters found in label or description, instead of erroring out, empty their values (empty listitem will be shown)
 		try:
@@ -285,7 +308,7 @@ def build_show_directory(origurl):
 					url = origurl
 
 
-			if 'rtmp://' in url or 'mms://' in url:
+			if 'rtmp://' in url or 'mms://' in url or 'rtsp://' in url:
 				isFolder = False
 				mode = PLUGIN_MODE_PLAY_STREAM
 

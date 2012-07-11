7e7f0e51c2dc0b5887c422f0377108a2035068ca
spiff <spiff@xbmc.org>
2012-07-11 21:45:22 +0200

    [plugin.video.filmstarts_de] updated to version 1.0.5

--- plugin.video.filmstarts_de/default.py
+++ plugin.video.filmstarts_de/default.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon
+import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon,socket
 
 pluginhandle = int(sys.argv[1])
+xbox = xbmc.getCondVisibility("System.Platform.xbox")
 settings = xbmcaddon.Addon(id='plugin.video.filmstarts_de')
 translation = settings.getLocalizedString
 
+showAllTrailers=settings.getSetting("showAllTrailers")
+forceViewMode=settings.getSetting("forceViewMode")
+if forceViewMode=="true":
+  forceViewMode=True
+else:
+  forceViewMode=False
+viewMode=str(settings.getSetting("viewMode"))
+
 def index():
+        addDir('Trailer: '+translation(30008),'',"search",'')
         addDir('Trailer: '+translation(30001),'http://www.filmstarts.de/trailer/aktuell_im_kino.html?version=1',"showSortDirection",'')
         addDir('Trailer: '+translation(30002),'http://www.filmstarts.de/trailer/bald_im_kino.html?version=1',"showSortDirection",'')
-        addDir('Trailer: Archiv','http://www.filmstarts.de/trailer/archiv.html?version=1',"showSortDirection",'')
+        #addDir('Trailer: Archiv','http://www.filmstarts.de/trailer/archiv.html?version=1',"showSortDirection",'')
+        addDir('Filmstarts: Interviews','http://www.filmstarts.de/trailer/interviews/',"listVideosInterview",'')
         addDir('Filmstarts: Fünf Sterne','http://www.filmstarts.de/videos/shows/funf-sterne',"listVideosMagazin",'')
         addDir('Filmstarts: Fehlerteufel','http://www.filmstarts.de/videos/shows/filmstarts-fehlerteufel',"listVideosMagazin",'')
         addDir('Meine Lieblings-Filmszene','http://www.filmstarts.de/videos/shows/meine-lieblings-filmszene',"listVideosMagazin",'')
-        addDir('Video-Interviews','http://www.filmstarts.de/trailer/interviews/',"listVideosInterview",'')
         addDir('Serien-Trailer','http://www.filmstarts.de/trailer/serien/',"listVideosTV",'')
         xbmcplugin.endOfDirectory(pluginhandle)
+        if forceViewMode==True:
+          xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
 def showSortDirection(url):
         addDir(translation(30003),url.replace("?version=1","?sort_order=0&version=1"),"listVideosTrailer",'')
@@ -23,6 +35,8 @@ def showSortDirection(url):
         addDir(translation(30005),url.replace("?version=1","?sort_order=3&version=1"),"listVideosTrailer",'')
         addDir(translation(30006),url.replace("?version=1","?sort_order=2&version=1"),"listVideosTrailer",'')
         xbmcplugin.endOfDirectory(pluginhandle)
+        if forceViewMode==True:
+          xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
 def listVideos(urlFull):
         content = getUrl(urlFull)
@@ -42,7 +56,11 @@ def listVideos(urlFull):
         if mode=="listVideosTrailer":
           match=re.compile('<img src=\'(.+?)\' alt="(.+?)" title="(.+?)" />\n</span>\n</div>\n<div class="contenzone">\n<div class="titlebar">\n<a class="link" href="(.+?)">\n<span class=\'bold\'>(.+?)</span>', re.DOTALL).findall(content)
           for thumb,temp1,temp2,url,title in match:
-                addLink(title,'http://www.filmstarts.de' + url,"playVideo",thumb)
+                if showAllTrailers=="true":
+                  url=url[:url.find("/trailer/")]+"/trailers/"
+                  addDir(title,'http://www.filmstarts.de' + url,"listTrailers",thumb)
+                else:
+                  addLink(title,'http://www.filmstarts.de' + url,"playVideo",thumb)
         elif mode=="listVideosMagazin":
           if currentPage==1:
             match=re.compile('<a href="(.+?)">\n<img src="(.+?)" alt="" />\n</a>\n</div>\n<div style="(.+?)">\n<h2 class="(.+?)" style="(.+?)"><b>(.+?)</b> (.+?)</h2><br />\n<span style="(.+?)" class="purehtml fs11">\n(.+?)<a class="btn" href="(.+?)"', re.DOTALL).findall(content)
@@ -83,6 +101,57 @@ def listVideos(urlFull):
             urlNew=urlFull + "?page="+str(currentPage+1)
           addDir(translation(30007)+" ("+str(currentPage+1)+")",urlNew,mode,'')
         xbmcplugin.endOfDirectory(pluginhandle)
+        if forceViewMode==True:
+          xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
+
+def listTrailers(url):
+        content = getUrl(url)
+        splMain=content.split('<div class="media_list_02')
+        for i in range(1,len(splMain),1):
+          entryMain = splMain[i]
+          entryMain = entryMain[:entryMain.find('</div>')]
+          spl=entryMain.split("<li>")
+          for i in range(1,len(spl),1):
+            entry=spl[i]
+            match=re.compile("src='(.+?)'", re.DOTALL).findall(entry)
+            thumb=match[0]
+            match=re.compile("title='(.+?)'", re.DOTALL).findall(entry)
+            title=match[0].replace(" DF", " - "+str(translation(30009))).replace(" OV", " - "+str(translation(30010)))
+            title=cleanTitle(title)
+            match=re.compile('href="(.+?)"', re.DOTALL).findall(entry)
+            url="http://www.filmstarts.de"+match[0]
+            addLink(title,url,'playVideo',thumb)
+        xbmcplugin.endOfDirectory(pluginhandle)
+        if forceViewMode==True:
+          xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
+
+def cleanTitle(title):
+        title=title.replace("&lt;","<").replace("&gt;",">").replace("&amp;","&").replace("&#39;","'").replace("&quot;","\"").replace("&szlig;","ß").replace("&ndash;","-")
+        title=title.replace("&#38;","&").replace("&#8230;","...").replace("&#8211;","-").replace("&#8220;","-").replace("&#8221;","-").replace("&#8217;","'")
+        title=title.replace("&#196;","Ä").replace("&#220;","Ü").replace("&#214;","Ö").replace("&#228;","ä").replace("&#252;","ü").replace("&#246;","ö").replace("&#223;","ß").replace("&#176;","°").replace("&#233;","é").replace("&#224;","à")
+        title=title.strip()
+        return title
+
+def search():
+        keyboard = xbmc.Keyboard('', str(translation(30008)))
+        keyboard.doModal()
+        if keyboard.isConfirmed() and keyboard.getText():
+          search_string = keyboard.getText().replace(" ","+")
+          content = getUrl("http://www.filmstarts.de/suche/1/?q="+search_string)
+          spl=content.split('<tr><td style=" vertical-align:middle;">')
+          for i in range(1,len(spl),1):
+            entry=spl[i]
+            match=re.compile("src='(.+?)'", re.DOTALL).findall(entry)
+            thumb=match[0]
+            match=re.compile("'>\n(.+?)</a>", re.DOTALL).findall(entry)
+            title=match[0].replace("<b>","").replace("</b>","")
+            title=cleanTitle(title)
+            match=re.compile("href='(.+?)'", re.DOTALL).findall(entry)
+            url="http://www.filmstarts.de"+match[0].replace(".html","/trailers/")
+            addDir(title,url,'listTrailers',thumb)
+          xbmcplugin.endOfDirectory(pluginhandle)
+          if forceViewMode==True:
+            xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
 def playVideo(url):
         content = getUrl(url)
@@ -105,7 +174,11 @@ def playVideo(url):
 def getUrl(url):
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0')
-        response = urllib2.urlopen(req)
+        if xbox==True:
+          socket.setdefaulttimeout(30)
+          response = urllib2.urlopen(req)
+        else:
+          response = urllib2.urlopen(req,timeout=30)
         link=response.read()
         response.close()
         return link
@@ -148,7 +221,11 @@ if mode=="playVideo":
     playVideo(url)
 elif mode=="showSortDirection":
     showSortDirection(url)
+elif mode=="listTrailers":
+    listTrailers(url)
 elif mode=="listVideosMagazin" or mode=="listVideosInterview" or mode=="listVideosTV" or mode=="listVideosTrailer":
     listVideos(url)
+elif mode=="search":
+    search()
 else:
     index()

4c5474e29f723996d9d74b318d68d5d1c1bf6097
Martijn Kaijser <mcm.kaijser@gmail.com>
2012-08-16 19:09:23 +0200

    [plugin.video.nrk] -v4.0.0

--- plugin.video.nrk/data.py
+++ plugin.video.nrk/data.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
 '''
-    NRK plugin for XBMC
-    Copyright (C) 2010 Thomas Amland
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -17,161 +13,108 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import urllib, re
-from BeautifulSoup import BeautifulSoup
-from DataItem import DataItem
-from dataStatic import *
-from utils import *
+import re
+
+import BeautifulSoup
+html_decode = lambda string: BeautifulSoup.BeautifulSoup(string,
+    convertEntities=BeautifulSoup.BeautifulSoup.HTML_ENTITIES).contents[0]
+
+import CommonFunctions
+parseDOM = CommonFunctions.parseDOM
+
+import requests
+requests = requests.session(headers={'User-Agent':'xbmc.org'})
 
 
-global BITRATES, BITRATE_ID
-def setQuality(id):
-    global BITRATES, BITRATE_ID
-    BITRATE_ID = id
-    BITRATES = ['400', '800', '1200']
+def parse_by_letter(arg):
+  """ returns: </serie/newton> or </program/koif45000708> """
+  url = "http://tv.nrk.no/programmer/%s?filter=rettigheter" % arg
+  html = requests.get(url).text
+  html = parseDOM(html, 'div', {'id':'programList'})
+  return _parse_list(html)
+
+def parse_by_category(arg):
+  url = "http://tv.nrk.no/kategori/%s" % arg
+  html = requests.get(url).text
+  html = parseDOM(html, 'div', {'class':'alpha-list clear'})
+  return _parse_list(html)
+
+def parse_categories():
+  url = "http://tv.nrk.no/kategori/"
+  html = requests.get(url).text
+  html = parseDOM(html, 'ul', {'id':'categoryList'})
+  return _parse_list(html)
+
+def _parse_list(html):
+  titles = parseDOM(html, 'a')
+  titles = map(html_decode, titles)
+  urls = parseDOM(html, 'a', ret='href')
+  return titles, urls
 
 
-def getLive():
-    url_1 = [
-        'http://a897.l11601532896.c116015.g.lm.akamaistream.net/D/897/116015/v0001/reflector:32896',
-        'http://a48.l11601648047.c116016.g.lm.akamaistream.net/D/48/116016/v0001/reflector:48047',
-        'http://a1377.l11673952706.c116739.g.lm.akamaistream.net/D/1377/116739/v0001/reflector:52706'
-        ][BITRATE_ID]
-    
-    url_2 = [
-        'http://a1562.l11601643115.c116016.g.lm.akamaistream.net/D/1562/116016/v0001/reflector:43115',
-        'http://a1382.l11674038974.c116740.g.lm.akamaistream.net/D/1382/116740/v0001/reflector:38974',
-        'http://a746.l11674151924.c116741.g.lm.akamaistream.net/D/746/116741/v0001/reflector:51924'
-        ][BITRATE_ID]
-    
-    url_3 = [
-        'http://a397.l11601753777.c116017.g.lm.akamaistream.net/D/397/116017/v0001/reflector:53777',
-        'http://a805.l11674254919.c116742.g.lm.akamaistream.net/D/805/116742/v0001/reflector:54919',
-        'http://a1372.l11674333102.c116743.g.lm.akamaistream.net/D/1372/116743/v0001/reflector:33102'
-        ][BITRATE_ID]
-    
-    return [DataItem(title="NRK 1", url=url_1, thumb=os.path.join(R_PATH, "nrk1.jpg"), isPlayable=True),
-            DataItem(title="NRK 2", url=url_2, thumb=os.path.join(R_PATH, "nrk2.jpg"), isPlayable=True),
-            DataItem(title="NRK 3", url=url_3, thumb=os.path.join(R_PATH, "nrk3.jpg"), isPlayable=True)]
+def parse_recommended():
+  url = "http://tv.nrk.no/"
+  html = requests.get(url).text
+  html = parseDOM(html, 'ul', {'id':'introSlider'})[0]
+  
+  h1s = parseDOM(html, 'h1')
+  titles2 = parseDOM(h1s, 'a')
+  titles1 = parseDOM(html, 'strong')
+  titles = [ "%s - %s" % (t1, t2) for t1, t2 in zip(titles1, titles2) ]
+  
+  urls = parseDOM(h1s, 'a', ret='href')
+  imgs = re.findall(r'1900":"([^"]+)', html)
+  return titles, urls, imgs
 
 
-def getLatest():
-    soup = BeautifulSoup(urllib.urlopen("http://www.nrk.no/nett-tv/"))
-    li = soup.find('div', attrs={'class' : 'nettv-latest'}).findAll('li')
-    items = []
-    for e in li:
-        anc = e.find('a', attrs={'href' : re.compile('^/nett-tv/klipp/[0-9]+')})
-        items.append( _getVideo(anc['href']) )
-    return items
+def parse_most_recent():
+  url = "http://tv.nrk.no/listobjects/recentlysent"
+  html = requests.get(url).text
+  urls = parseDOM(html, 'a', {'class':'listobject-link'}, ret='href')
+  thumbs = parseDOM(html, 'img', ret='src')[::2] #extract even elements
+  html = ''.join(parseDOM(html, 'span', {'class':'listobject-title'}))
+  titles = parseDOM(html, 'strong')
+  titles = map(html_decode, titles)
+  return titles, urls, thumbs
 
 
-def getSearchResults(query):
-    url = "http://www.nrk.no/nett-tv/sok/" + urllib.quote(query)
-    return _getSearchResults(url)
-
-def _getSearchResults(url):
-    soup = BeautifulSoup(urllib.urlopen(url))
-    li = soup.find('div', attrs={'id' : 'search-results'}).findAll('li')
-    items = []
-    for e in li:
-        url = e.find('em').string
-        if contains(url, "klipp"):
-            items.append( _getVideo(url) )
-        elif contains(url, "prosjekt"):
-            title = decodeHtml(e.find('a').string)
-            descr = decodeHtml(e.find('p').string)
-            items.append( DataItem(title=title, description=descr, url="/nett-tv/prosjekt/"+getId(url)) )
-    #next page
-    url = soup.find('a', attrs={'id' : 'ctl00_contentPlaceHolder_ucSearch_nextLink'})['href']
-    if not (url.endswith('0') or url.endswith('1')):
-        items.append(DataItem(title="Neste »", url=url))
-    return items
+def parse_seasons(arg):
+  """ returns: </program/Episodes/aktuelt-tv/11998> """
+  url = "http://tv.nrk.no/serie/%s" % arg
+  html = requests.get(url).text
+  html = parseDOM(html, 'div', {'id':'seasons'})
+  html = parseDOM(html, 'noscript')
+  titles = parseDOM(html, 'a', {'class':'seasonLink'})
+  titles = [ "Sesong %s" % html_decode(t) for t in titles ]
+  ids = parseDOM(html, 'a', {'class':'seasonLink'}, ret='href')
+  return titles, ids
 
 
-def getMostWatched(days):
-    url = "http://www.nrk.no/nett-tv/ml/topp12.aspx?dager=%s" % days
-    soup = BeautifulSoup(urllib.urlopen(url))
-    li = soup.findAll('li')
-    items = []
-    for e in li:
-        url = e.find('a')['href']
-        if contains(url, "klipp"):
-            items.append( _getVideo(url))
-    return items
+def parse_episodes(series_id, season_id):
+  """ returns: </serie/aktuelt-tv/nnfa50051612/16-05-2012..> """
+  url = "http://tv.nrk.no/program/Episodes/%s/%s" % (series_id, season_id)
+  html = requests.get(url).text
+  html = parseDOM(html, 'table', {'class':'episodeTable'})
+  trs = parseDOM(html, 'tr', {'class':'has-programtooltip episode-row js-click *'})
+  titles = [ parseDOM(tr, 'a', {'class':'p-link'})[0] for tr in trs ]
+  titles = map(html_decode, titles)
+  ids = [ parseDOM(tr, 'a', {'class':'p-link'}, ret='href')[0] for tr in trs ]
+  ids = [ e.split('http://tv.nrk.no')[1] for e in ids ]
+  descr = [lambda x=x: _get_descr(x) for x in ids ]
+  return titles, ids, descr
 
 
-def getByUrl(url):
-    if (url.startswith("/nett-tv/tema") or url.startswith("/nett-tv/bokstav")):
-        url = "http://www.nrk.no" + url
-        soup = BeautifulSoup(urllib.urlopen(url))
-        return _getAllProsjekt(soup)
-    
-    elif (url.startswith("/nett-tv/sok")):
-        url = "http://www.nrk.no" + url
-        return _getSearchResults(url)
-    
-    elif (url.startswith("/nett-tv/prosjekt")):
-        url = "http://www.nrk.no" + url
-        
-    elif (url.startswith("/nett-tv/kategori")):
-        url = "http://www.nrk.no/nett-tv/menyfragment.aspx?type=category&id=" + getId(url)
-    
-    soup = BeautifulSoup(urllib.urlopen(url))
-    items = []
-    items.extend(_getAllKlipp(soup))
-    items.extend(_getAllKategori(soup))
-    return items
+def parse_media_url(video_id, bitrate):
+  bitrate = 4 if bitrate > 4 else bitrate
+  url = "http://nrk.no/serum/api/video/%s" % video_id
+  url = requests.get(url).json['mediaURL']
+  url = url.replace('/z/', '/i/', 1)
+  url = url.rsplit('/', 1)[0]
+  url = url + '/index_%s_av.m3u8' % bitrate
+  return url
 
-
-def _getAllKlipp(soup):
-    items = soup.findAll('a', attrs={'class':re.compile('icon-(video|sound)-black.*'), 'href':re.compile('.*nett-tv/klipp/[0-9]+$')})
-    return [ _getVideo(e['href']) for e in items ]
-
-
-def _getAllKategori(soup):
-    items = soup.findAll('a', attrs={'class':re.compile('icon-closed-black.*'), 'href':re.compile('^/nett-tv/kategori/[0-9]+')})
-    return [ DataItem(title=decodeHtml(e['title']), url=e['href']) for e in items ]
-
-
-def _getAllProsjekt(soup):
-    items = []
-    li = soup.find('div', attrs={'class' : 'nettv-category clearfix'}).findAll('li') 
-    for e in li:
-        anc = e.find('a', attrs={'href' : re.compile('^/nett-tv/prosjekt/[0-9]+')})
-        title = decodeHtml(anc['title'])
-        url   = anc['href']
-        img   = _getImg(e.find('img')['src'])
-        descr = decodeHtml(str(e.find('div', attrs={'class':'summary'}).find('p').string))
-        
-        items.append( DataItem(title=title, description=descr, thumb=img, url=url) )
-    return items
-
-
-def _getVideo(url):
-    id = getId(url)
-    (soup, url) = _findVideoUrl(id, BITRATE_ID)
-    url = url.split("mms://", 1)[1] #some uri's contais illegal chars so need to fix this
-    url = url.encode('latin-1') #urllib cant unicode
-    url = urllib.quote(url)
-    url = "mms://%s?UseSilverlight=1" % url
-    
-    title = decodeHtml(soup.find('title').string)
-    descr = decodeHtml(str(soup.find('description').string))
-    img   = _getImg(soup.find('imageurl').string)
-    return DataItem(title=title, description=descr, thumb=img, url=url, isPlayable=True)
-
-
-def _findVideoUrl(id, bitrate):
-    if bitrate >= len(BITRATES):
-        return (None, None)
-    url = "http://www.nrk.no/nett-tv/silverlight/getmediaxml.ashx?id=%s&hastighet=%s" % (id, BITRATES[bitrate])
-    soup = BeautifulSoup(urllib.urlopen(url))
-    url = soup.find('mediaurl').string
-    if not url:
-        return _findVideoUrl(id, bitrate+1)
-    return (soup, url)
-     
-
-def _getImg(url):
-    return re.sub("^(.*cropid.*)w[0-9]+$", "\\1w650", url)
+def _get_descr(url):
+  url = "http://nrk.no/serum/api/video/%s" % url.split('/')[3]
+  descr = requests.get(url).json['description']
+  return descr
 

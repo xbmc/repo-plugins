112dc3c340acd7d87b69068d4efe222ab3219cbb
spiff <spiff@xbmc.org>
2012-09-09 23:03:37 +0200

    [plugin.video.twitch] updated to version 0.2.1

--- plugin.video.twitch/default.py
+++ plugin.video.twitch/default.py
@@ -14,6 +14,7 @@ thisPlugin = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.twitch')
 httpHeaderUserAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'
 translation = settings.getLocalizedString
+ITEMS_PER_SITE=20
 
 def downloadWebData(url):
     try:
@@ -27,11 +28,12 @@ def downloadWebData(url):
         xbmc.executebuiltin("XBMC.Notification("+translation(31000)+"," + translation(32001) +")")
 	
 def createMainListing():
-	addDir(translation(30001),'','games','')
-	addDir(translation(30002),'','following','')
-	addDir(translation(30003),'','search','')
-	addDir(translation(30004),'','settings','')
-	xbmcplugin.endOfDirectory(thisPlugin)
+    addDir(translation(30005),'','featured','')
+    addDir(translation(30001),'','games','')
+    addDir(translation(30002),'','following','')
+    addDir(translation(30003),'','search','')
+    addDir(translation(30004),'','settings','')
+    xbmcplugin.endOfDirectory(thisPlugin)
 
 def createFollowingList():
     username = settings.getSetting('username').lower()
@@ -53,27 +55,52 @@ def createFollowingList():
             addLink(name,loginname,'play',image,loginname)
     xbmcplugin.endOfDirectory(thisPlugin)
 	
-def createListOfGames(index=0):
-    htmlData=downloadWebData(url='http://de.twitch.tv/directory?page='+str(index+1))
-    if htmlData is None:
-        return
-    gameDiv=re.compile("(?<=<div class='boxart'>).+?</div>.+?(?=</div>)", re.MULTILINE|re.DOTALL).findall(htmlData)
-    for x in gameDiv:
-        name = re.compile("(?<=<h5 class='title'>).+?(?=</h5>)").findall(x)[0]
-        dir = 'http://de.twitch.tv/directory/?category=' + urllib.quote_plus(name)
-        image = re.compile('(?<=setPlaceholder\(this\);" src="http://).+?(?=" />)').findall(x)[0]
-        image = urllib.quote(image)
-        image = 'http://' + image
-        addDir(name,dir,'channel',image)
-    addDir(translation(31001),'','games','',index+1)
+def createListOfFeaturedStreams():
+    jsonString=downloadWebData(url='https://api.twitch.tv/kraken/streams/featured')
+    jsonData=json.loads(jsonString)
+    for x in jsonData['featured']:
+        try:
+            image = x['stream']['channel']['logo']
+            image = image.replace("http://","",1)
+            image = urllib.quote(image)
+            image = 'http://' + image
+        except:
+            image = ""
+        name = x['stream']['channel']['status']
+        if name == '':
+            name = x['stream']['channel']['display_name']
+        channelname = x['stream']['channel']['name']
+        addLink(name,'...','play',image,channelname)
     xbmcplugin.endOfDirectory(thisPlugin)
-	
+ 
+def createListOfGames(index=0):
+    jsonString=downloadWebData(url='https://api.twitch.tv/kraken/games/top?limit='+str(ITEMS_PER_SITE)+'&offset='+str(index*ITEMS_PER_SITE))
+    jsonData=json.loads(jsonString)
+    for x in jsonData['top']:
+        try:
+            name = str(x['game']['name'])
+            game = urllib.quote(name)
+            image = ''
+        except:
+            continue
+        try:
+            image = x['game']['images']['super']
+            image = image.replace("http://","",1)
+            image = urllib.quote(image)
+            image = 'http://' + image
+        except:
+            image = ''
+        addDir(name,game,'channel',image)
+    if len(jsonData['top']) >= ITEMS_PER_SITE:
+        addDir(translation(31001),'','games','',index+1)
+    xbmcplugin.endOfDirectory(thisPlugin)
+
 def search():
     keyboard = xbmc.Keyboard('', translation(30101))
     keyboard.doModal()
     if keyboard.isConfirmed() and keyboard.getText():
         search_string = urllib.quote_plus(keyboard.getText())
-        sdata = downloadWebData('http://api.swiftype.com/api/v1/public/engines/search.json?callback=jQuery1337&q='+search_string+'&engine_key=9NXQEpmQPwBEz43TM592&page=1&per_page=20')
+        sdata = downloadWebData('http://api.swiftype.com/api/v1/public/engines/search.json?callback=jQuery1337&q='+search_string+'&engine_key=9NXQEpmQPwBEz43TM592&page=1&per_page='+str(ITEMS_PER_SITE))
         sdata = sdata.replace('jQuery1337','');
         sdata = sdata[1:len(sdata)-1]
         jdata = json.loads(sdata)
@@ -82,21 +109,25 @@ def search():
             addLink(x['title'],x['user'],'play',x['thumbnail'],x['user'])
         xbmcplugin.endOfDirectory(thisPlugin)
 	
-def createListForGame(url, index=0):
-    htmlData=downloadWebData(url+ '&page=' + str(index+1))
-    if htmlData is None:
-        return
-    videoDiv=re.compile("(?<=<div class='video  clearfix).+?(?=</div>)", re.MULTILINE|re.DOTALL).findall(htmlData)
-    for x in videoDiv:
-        image = re.compile('(?<=http://)static-cdn.jtvnw.net/previews/.+?(?=")', re.MULTILINE|re.DOTALL).findall(x)[0]
-        image = urllib.quote(image)
-        image = 'http://' + image
-        nameAndLink = re.compile("(?<=<p class='title'>).+?(?=</a></p>)", re.MULTILINE|re.DOTALL).findall(x)[0]
-        name = re.compile('(?<=\>).+?\Z', re.MULTILINE|re.DOTALL).findall(nameAndLink)[0]
-        channelname = re.compile('(?<=<a href="/).+?(?=">)').findall(nameAndLink)[0]
+def createListForGame(gameName, index=0):
+    jsonString=downloadWebData(url='https://api.twitch.tv/kraken/streams?game='+gameName+'&limit='+str(ITEMS_PER_SITE)+'&offset='+str(index*ITEMS_PER_SITE))
+    jsonData=json.loads(jsonString)
+    for x in jsonData['streams']:
+        try:
+            image = x['channel']['logo']
+            image = image.replace("http://","",1)
+            image = urllib.quote(image)
+            image = 'http://' + image
+        except:
+            image = ""
+        name = x['channel']['status']
+        if name == '':
+            name = x['channel']['display_name']
+        channelname = x['channel']['name']
         addLink(name,'...','play',image,channelname)
-    addDir(translation(31001),url,'channel','',index+1)
-    xbmcplugin.endOfDirectory(thisPlugin)	
+    if len(jsonData['streams']) >= ITEMS_PER_SITE:
+        addDir(translation(31001),url,'channel','',index+1)
+    xbmcplugin.endOfDirectory(thisPlugin)
 	
 def addLink(name,url,mode,iconimage,channelname):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&channelname="+channelname
@@ -146,29 +177,66 @@ def parameters_string_to_dict(parameters):
         return paramDict
 		
 def getSwfUrl(channel_name):
-        """Helper method to grab the swf url, resolving HTTP 301/302 along the way"""
+        ''' Helper method to grab the swf url, resolving HTTP 301/302 along the way'''
         base_url = 'http://www.justin.tv/widgets/live_embed_player.swf?channel=%s' % channel_name
         headers = {'User-agent' : httpHeaderUserAgent,
                    'Referer' : 'http://www.justin.tv/'+channel_name}
         req = urllib2.Request(base_url, None, headers)
         response = urllib2.urlopen(req)
-        return response.geturl()		
+        return response.geturl()
 		
+def getBestJtvTokenPossible(name):
+        '''Helper method to find another jtv token'''
+        swf_url = getSwfUrl(name)
+        headers = {'User-agent' : httpHeaderUserAgent,
+                   'Referer' : swf_url}
+        url = 'http://usher.justin.tv/find/'+name+'.json?type=any&group='
+        data = json.loads(get_request(url,headers))
+        bestVideoHeight = -1
+        bestIndex = -1
+        index = 0
+        for x in data:
+            value = x.get('token', '')
+            videoHeight = int(x['video_height'])
+            if (value != '') and (videoHeight > bestVideoHeight):
+                bestVideoHeight = x['video_height']
+                bestIndex = index  
+            index = index + 1
+        return data[bestIndex]
+
 def playLive(name, play=False, password=None):
         swf_url = getSwfUrl(name)
         headers = {'User-agent' : httpHeaderUserAgent,
                    'Referer' : swf_url}
-        url = 'http://usher.justin.tv/find/'+name+'.json?type=live&group='
+        chosenQuality = settings.getSetting('video')
+        videoTypeName = 'any'
+        if chosenQuality == '0':
+            videoTypeName = 'any'
+        elif chosenQuality == '1':
+            videoTypeName = '720p'
+        elif chosenQuality == '2':
+            videoTypeName = '480p'
+        elif chosenQuality == '3':
+            videoTypeName = '360p'
+        url = 'http://usher.justin.tv/find/'+name+'.json?type='+videoTypeName+'&private_code=null&group='
         data = json.loads(get_request(url,headers))
+        tokenIndex = 0
         if data == []:
             xbmc.executebuiltin("XBMC.Notification("+translation(31000)+","+translation(32002)+")")
             return
         try:
-            token = ' jtv='+data[0]['token'].replace('\\','\\5c').replace(' ','\\20').replace('"','\\22')
+            '''trying to get a token in desired quality'''
+            token = ' jtv='+data[tokenIndex]['token'].replace('\\','\\5c').replace(' ','\\20').replace('"','\\22')
+            rtmp = data[tokenIndex]['connect']+'/'+data[tokenIndex]['play']
         except:
-            xbmc.executebuiltin("XBMC.Notification("+translation(31000)+","+translation(32004)+")")
-            return
-        rtmp = data[0]['connect']+'/'+data[0]['play']
+            xbmc.executebuiltin("XBMC.Notification("+translation(32005)+","+translation(32006)+")")
+            jtvtoken = getBestJtvTokenPossible(name)
+            if jtvtoken == '':
+                xbmc.executebuiltin("XBMC.Notification("+translation(31000)+","+translation(32004)+")")
+                return
+            token = ' jtv='+jtvtoken['token'].replace('\\','\\5c').replace(' ','\\20').replace('"','\\22')
+            rtmp = jtvtoken['connect']+'/'+jtvtoken['play']
+        
         swf = ' swfUrl=%s swfVfy=1 live=1' % swf_url
         Pageurl = ' Pageurl=http://www.justin.tv/'+name
         url = rtmp+token+swf+Pageurl
@@ -195,6 +263,8 @@ if type(url)==type(str()):
 	url=urllib.unquote_plus(url)
 if mode == 'games':
 	createListOfGames(index)  
+elif mode == 'featured':
+	createListOfFeaturedStreams()
 elif mode == 'channel':
 	createListForGame(url, index)
 elif mode == 'play':

4d1e6dc3f24aaeb84092788d679ddc8b6c6c9588
spiff <spiff@xbmc.org>
2012-09-12 08:43:34 +0200

    [plugin.video.tmz] updated to version 2.0.4

--- plugin.video.tmz/default.py
+++ plugin.video.tmz/default.py
@@ -1,12 +1,14 @@
 
-import xbmc, xbmcaddon, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback, time, datetime
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback, time, datetime, buggalo
 
 __plugin__ = 'TMZ'
 __author__ = 'stacked <stacked.xbmc@gmail.com>'
 __url__ = 'http://code.google.com/p/plugin/'
-__date__ = '07-26-2012'
-__version__ = '2.0.2'
+__date__ = '09-11-2012'
+__version__ = '2.0.4'
 __settings__ = xbmcaddon.Addon( id = 'plugin.video.tmz' )
+icon = os.path.join( __settings__.getAddonInfo( 'path' ), 'icon.png' )
+buggalo.SUBMIT_URL = 'http://www.xbmc.byethost17.com/submit.php'
 
 def open_url(url):
 	req = urllib2.Request(url)
@@ -30,7 +32,7 @@ def build_main_directory():
 		( __settings__.getLocalizedString( 30003 ) )
 		]
 	for name in main:
-		listitem = xbmcgui.ListItem( label = name, iconImage = "DefaultFolder.png", thumbnailImage = "thumbnailImage" )
+		listitem = xbmcgui.ListItem( label = name, iconImage = icon, thumbnailImage = icon )
 		u = sys.argv[0] + "?mode=0&name=" + urllib.quote_plus( name )
 		ok = xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = True )
 	xbmcplugin.addSortMethod( handle = int(sys.argv[1]), sortMethod = xbmcplugin.SORT_METHOD_NONE )
@@ -38,26 +40,33 @@ def build_main_directory():
 
 def build_video_directory( name ):
 	data = open_url( 'http://www.tmz.com/videos/' )
-	content = re.compile('carouselJsons\["Most Recent"\]\[\"' + name.upper() + '\"\](.+?)];\n', re.DOTALL).findall( data )
-	match = re.compile('\n{\n  (.+?)\n}', re.DOTALL).findall( content[0] )
+	content = re.compile('{ name: \'' + name.upper() + '\',( )?\n         allInitialJson: \[(.+?)\],\n', re.DOTALL).findall( data )
+	match = re.compile('\n{\n  (.+?)\n}', re.DOTALL).findall( content[0][1] )
 	for videos in match:
-		epsdata = re.compile('title": "(.+?)",\n  "duration": parseInt\("(.+?)", 10\),\n  "url": "(.+?)",\n  "videoUrl": "(.+?)",\n  "manualThumbnailUrl": "(.+?)",\n  "thumbnailUrl": "(.+?)",\n  "kalturaId": "(.+?)"', re.DOTALL).findall(videos)
+		epsdata = re.compile('title": "(.+?)",\n  "duration": "(.+?)",\n  "url": "(.+?)",\n  "videoUrl": "(.+?)",\n  "manualThumbnailUrl": "(.+?)",\n  "thumbnailUrl": "(.+?)",\n  "kalturaId": "(.+?)"', re.DOTALL).findall(videos)
 		title = clean(epsdata[0][0].replace("\\", ""))
 		duration = epsdata[0][1].replace("\\", "")
-		url = epsdata[0][3].replace("\\", "")
+		videoUrl = epsdata[0][3].replace("\\", "")
 		thumb = epsdata[0][5].replace("\\", "") + '/width/490/height/266/type/3'
-		if url.find('http://cdnbakmi.kaltura.com') == -1:
+		if videoUrl.find('http://cdnbakmi.kaltura.com') == -1:
+			if __settings__.getSetting("quality") == __settings__.getLocalizedString( 30005 ):
+				url = 'http://cdnapi.kaltura.com/p/' + thumb.split('/')[4] + '/sp/' + thumb.split('/')[6] + '/playManifest/entryId/0_' + videoUrl.split('_')[1]
+			else:
+				url = 'http://cdnapi.kaltura.com/p/' + thumb.split('/')[4] + '/sp/' + thumb.split('/')[6] + '/playManifest/entryId/0_' + videoUrl.split('_')[1] + '/flavorId/0_' + videoUrl.split('_')[3]
 			listitem = xbmcgui.ListItem( label = title, iconImage = thumb, thumbnailImage = thumb )
 			listitem.setInfo( type="Video", infoLabels={ "Title": title, "Director": "TMZ", "Studio": name, "Duration": str(datetime.timedelta(seconds=int(duration))) } )
+			listitem.setProperty('IsPlayable', 'true')
 			u = sys.argv[0] + "?mode=1&name=" + urllib.quote_plus( title ) + "&url=" + urllib.quote_plus( url ) + "&thumb=" + urllib.quote_plus( thumb ) + "&studio=" + urllib.quote_plus( name )
 			ok = xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = False )
 	xbmcplugin.addSortMethod( handle = int(sys.argv[1]), sortMethod = xbmcplugin.SORT_METHOD_NONE )
 	xbmcplugin.endOfDirectory( int( sys.argv[1] ) )
 		
 def play_video( name, url, thumb, studio ):
-	item = xbmcgui.ListItem( label = name, iconImage = "DefaultVideo.png", thumbnailImage = thumb )
-	item.setInfo( type="Video", infoLabels={ "Title": name , "Director": "TMZ", "Studio": studio } )
-	xbmc.Player( xbmc.PLAYER_CORE_DVDPLAYER ).play( url, item )
+	data = open_url( url )
+	url = re.compile('<media url=\"(.+?)\"').findall(data)[0]
+	listitem = xbmcgui.ListItem( label = name, iconImage = "DefaultVideo.png", thumbnailImage = thumb, path = url )
+	listitem.setInfo( type="Video", infoLabels={ "Title": name , "Director": "TMZ", "Studio": studio } )
+	xbmcplugin.setResolvedUrl( handle = int( sys.argv[1] ), succeeded = True, listitem = listitem )
 
 def get_params():
 	param = []
@@ -104,10 +113,13 @@ try:
 except:
         pass
 
-if mode == None:
-	build_main_directory()
-elif mode == 0:
-	build_video_directory( name )
-elif mode == 1:
-	play_video( name, url, thumb, studio )
+try:
+	if mode == None:
+		build_main_directory()
+	elif mode == 0:
+		build_video_directory( name )
+	elif mode == 1:
+		play_video( name, url, thumb, studio )
+except Exception:
+	buggalo.onExceptionRaised()
 	

dcf0adae1b7abc080bf0ea8795dd49b481f8b461
spiff <spiff@xbmc.org>
2012-10-03 10:25:21 +0200

    [plugin.audio.sky.fm] updated to version 2.0.0

--- plugin.audio.sky.fm/httpcomm.py
+++ plugin.audio.sky.fm/httpcomm.py
@@ -9,35 +9,17 @@ import socket
 import urllib2
 
 class HTTPComm :
-	def testStream( self, url ) :
-		timeout = 10
-		socket.setdefaulttimeout(timeout)
-		try :
-			# Set useragent, sites don't like to interact with scripts
-			headers = { 'User-Agent':'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.8) Gecko/20100723 Ubuntu/10.04 (lucid) Firefox/3.6.8','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Language':'en-us,en;q=0.5','Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.7'}
-			req = urllib2.Request(url=url, headers=headers)
-			f = urllib2.urlopen(req)
-			CHUNK = 16 * 1024
-			#CHUNK = 8 * 1024
-			chunk = f.read(CHUNK)
-			# if we actually got some data = we got a stream
-			if len(chunk) == CHUNK :
-				return True
-			else :
-				return False
-		except Exception:
-			print "Something went horribly wrong in testStream in HTTPComm"
-			return False
-
 	# Login
-	def post( self, url, postdata, cookiefile=None ) :
-		
+	
+	_cj = cookielib.CookieJar()
+	
+	def post( self, url, postdata ) :
 		timeout = 10
 		socket.setdefaulttimeout(timeout)
 
-		cj = cookielib.LWPCookieJar()
+		
 		# create an opener
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self._cj))
 
 		# Add useragent, sites don't like to interact with scripts
 		opener.addheaders = [
@@ -52,6 +34,7 @@ class HTTPComm :
 			]
 		
 		resp = opener.open(url, postdata)
+	
 		# Compressed (gzip) response...
 		if resp.headers.get( "content-encoding" ) == "gzip":
 			htmlGzippedData = resp.read()
@@ -60,29 +43,19 @@ class HTTPComm :
 			htmlData        = gzipper.read()
 		else :
 			htmlData = resp.read()
-		
-		if cookiefile != None :
-			cj.save(cookiefile, True)
+			
 		resp.close()
-
+		
 		# Return html
 		return htmlData
-		
+
 	# GET
-	def get( self, url, cookiefile=None ):
+	def get( self, url ):
 		timeout = 10
 		socket.setdefaulttimeout(timeout)
-		print "cookie" + str(cookiefile)
-		opener = urllib2.build_opener()
-		cj = cookielib.LWPCookieJar()
+
 		#create an opener
-		if cookiefile!=None :
-			print "using cookie"
-			cj = cookielib.LWPCookieJar(cookiefile)
-			cj.load(ignore_discard=True)
-			opener = urllib2.build_opener( urllib2.HTTPCookieProcessor(cj) )
-			urllib2.install_opener(opener)
-	
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self._cj))
 		#Add useragent, sites don't like to interact with scripts
 		opener.addheaders = [
 			('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.8) Gecko/20100723 Ubuntu/10.04 (lucid) Firefox/3.6.8'),

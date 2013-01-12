5f43b021543b63566f7d9def7c7fa8a3bdf5739c
beenje <beenje@xbmc.org>
2013-01-12 22:42:46 +0100

    [plugin.video.vgtv] updated to version 2.0.4

--- plugin.video.vgtv/addon.py
+++ plugin.video.vgtv/addon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2012 Espen Hovlandsdal
+# Copyright 2012-2013 Espen Hovlandsdal
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

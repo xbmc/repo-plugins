df11cf6b7c1ab9e0bee6ac723cb7024dd8702ae1
beenje <beenje@xbmc.org>
2013-02-04 21:50:22 +0100

    [plugin.video.theonion_com] updated to version 1.0.1

--- plugin.video.theonion_com/default.py
+++ plugin.video.theonion_com/default.py
@@ -4,62 +4,60 @@ import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon,base64,socket
 
 socket.setdefaulttimeout(30)
 pluginhandle = int(sys.argv[1])
-settings = xbmcaddon.Addon(id='plugin.video.theonion_com')
-translation = settings.getLocalizedString
+addon = xbmcaddon.Addon(id='plugin.video.theonion_com')
+translation = addon.getLocalizedString
 
-forceViewMode=settings.getSetting("forceViewMode")
+forceViewMode=addon.getSetting("forceViewMode")
 if forceViewMode=="true":
   forceViewMode=True
 else:
   forceViewMode=False
-viewMode=str(settings.getSetting("viewMode"))
+viewMode=str(addon.getSetting("viewMode"))
 
 def index():
-        addDir(translation(30002),"http://www.theonion.com/ajax/onn_playlist/mostrecent/1/",'listVideos',"")
-        content = getUrl("http://www.theonion.com/articles/latest/video/")
-        matchPage=re.compile('<a class="next_page" rel="next" href="(.+?)">', re.DOTALL).findall(content)
-        content = content[content.find('class="label">SHOWS</li>'):]
-        content = content[:content.find('<li rel="featured">')]
-        match=re.compile('<li rel="(.+?)"><a><span></span>(.+?)</a></li>', re.DOTALL).findall(content)
-        for id, title in match:
-          addDir(title.replace("<br>"," "),"http://www.theonion.com/ajax/onn_playlist/"+id+"/1/",'listVideos',"")
+        addDir(translation(30002),"http://www.theonion.com/playlists/recent-news/",'listVideos',"")
+        content = getUrl("http://www.theonion.com/video/")
+        match=re.compile('<header><h1>(.+?)</h1><a class="more" href="(.+?)"', re.DOTALL).findall(content)
+        for title, url in match:
+          addDir(title,"http://www.theonion.com"+url,"listVideos","")
+        addDir(translation(30003),"http://www.theonion.com/playlists/breaking-news/",'listVideos',"")
+        addDir(translation(30004),"http://www.theonion.com/playlists/biggest-stories-of-2012/",'listVideos',"")
+        addDir(translation(30005),"http://www.theonion.com/playlists/best-of-the-onion-review/",'listVideos',"")
         xbmcplugin.endOfDirectory(pluginhandle)
         if forceViewMode==True:
           xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
 def listVideos(url):
         content = getUrl(url)
-        spl=content.split('<li class="element_')
+        mainUrl=url[:url.find("?")]
+        spl=content.split('<li data-video-id=')
         for i in range(1,len(spl),1):
             entry=spl[i]
-            match=re.compile('video_(.+?)"', re.DOTALL).findall(entry)
-            id=match[0]
-            match=re.compile('<p class="title"><a href="(.+?)">(.+?)</a></p>', re.DOTALL).findall(entry)
-            title=match[0][1]
+            match=re.compile('data-url="(.+?)"', re.DOTALL).findall(entry)
+            url=match[0]
+            match=re.compile('class="title">(.+?)<', re.DOTALL).findall(entry)
+            title=match[0]
             title=cleanTitle(title)
-            match=re.compile('<p class="teaser">(.+?)</p>', re.DOTALL).findall(entry)
+            match=re.compile('<div class="desc" style="display: none;"><p>(.+?)</p></div>', re.DOTALL).findall(entry)
             desc=""
             if len(match)>0:
               desc=match[0]
               desc=cleanTitle(desc)
-            match=re.compile('<p class="info"><span class="showname">(.+?)</span> \\((.+?)\\)</p>', re.DOTALL).findall(entry)
-            length=match[0][1]
-            match=re.compile('src="(.+?)"', re.DOTALL).findall(entry)
+            match=re.compile('<span class="duration">(.+?)</span>', re.DOTALL).findall(entry)
+            length=match[0]
+            match=re.compile('data-src="(.+?)"', re.DOTALL).findall(entry)
             thumb=match[0]
-            addLink(title,id,'playVideo',thumb,length,desc)
-        match1=re.compile('<span id="nextpage">(.+?)</span>', re.DOTALL).findall(content)
-        match2=re.compile('<span id="slug">(.+?)</span>', re.DOTALL).findall(content)
-        if len(match1)>0:
-          urlNext="http://www.theonion.com/ajax/onn_playlist/"+match2[0]+"/"+match1[0]+"/"
+            addLink(title,url,'playVideo',thumb,length,desc)
+        match=re.compile('<li class="next"><a href="(.+?)">', re.DOTALL).findall(content)
+        if len(match)>0:
+          urlNext=mainUrl+match[0]
           addDir(translation(30001),urlNext,'listVideos',"")
         xbmcplugin.endOfDirectory(pluginhandle)
         if forceViewMode==True:
           xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
-def playVideo(id):
-        content = getUrl("http://www.theonion.com/videos/embed/"+id+".json")
-        match=re.compile('"video_url": "(.+?)"', re.DOTALL).findall(content)
-        listitem = xbmcgui.ListItem(path=match[0])
+def playVideo(url):
+        listitem = xbmcgui.ListItem(path=url)
         return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 
 def cleanTitle(title):

6a1b1d7da40442c82d19c4cd5025f84f2d6c9e9d
beenje <beenje@xbmc.org>
2013-03-16 23:04:34 +0100

    [plugin.video.dump] updated to version 2.0.0

--- plugin.video.dump/addon.py
+++ plugin.video.dump/addon.py
@@ -1,134 +1,130 @@
-import os
-from xbmcswift import Plugin, xbmcplugin, xbmcgui
-import resources.lib.scraper as scraper
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2013 Tristan Fischer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from xbmcswift2 import Plugin
+from resources.lib import scraper
+
+plugin = Plugin()
+
+STRINGS = {
+    'current': 30000,
+    'archive': 30002,
+    'search': 30003,
+    'network_error': 30020
+}
 
 
-class Plugin_adv(Plugin):
-
-    def add_items(self, iterable, sort_method_ids=[]):
-        items = []
-        urls = []
-        for i, li_info in enumerate(iterable):
-            items.append(self._make_listitem(**li_info))
-            if self._mode in ['crawl', 'interactive', 'test']:
-                print '[%d] %s%s%s (%s)' % (i + 1, '', li_info.get('label'),
-                                            '', li_info.get('url'))
-                urls.append(li_info.get('url'))
-        if self._mode is 'xbmc':
-            xbmcplugin.addDirectoryItems(self.handle, items, len(items))
-            for id in sort_method_ids:
-                xbmcplugin.addSortMethod(self.handle, id)
-            xbmcplugin.endOfDirectory(self.handle, cacheToDisc=False)
-        return urls
-
-    def set_resolved_url(self, url, title=None):
-        if self._mode in ['crawl', 'interactive', 'test']:
-            print 'ListItem resolved to %s' % url
-        li = xbmcgui.ListItem(path=url)
-        if title:
-            li.setLabel(title)
-        xbmcplugin.setResolvedUrl(self.handle, True, li)
-        if self._mode in ['interactive', 'crawl', 'test']:
-            return []
-
-
-plugin = Plugin_adv('Dump.com', 'plugin.video.dump', __file__)
-
-
-@plugin.route('/', default=True)
+@plugin.route('/')
 def show_root():
-    log('show_root started')
-    categories = [{'label': plugin.get_string(30000),
-                   'url': plugin.url_for('show_current')},
-                  {'label': plugin.get_string(30001),
-                   'url': plugin.url_for('watch_random'),
-                   'is_folder': False,
-                   'is_playable': True},
-                  {'label': plugin.get_string(30002),
-                   'url': plugin.url_for('show_archive')},
-                  {'label': plugin.get_string(30003),
-                   'url': plugin.url_for('search')}]
-    return plugin.add_items(categories)
+    items = [
+        {'label': _('current'),
+         'path': plugin.url_for('show_current')},
+        {'label': _('archive'),
+         'path': plugin.url_for('show_archive')},
+        {'label': _('search'),
+         'path': plugin.url_for('search')}
+    ]
+    return plugin.finish(items)
 
 
 @plugin.route('/current/')
 def show_current():
-    log('show_current started')
     videos = scraper.get_current_videos()
     return __add_videos(videos)
 
 
-@plugin.route('/watch/<video_id>/')
+@plugin.route('/watch/<video_id>')
 def watch_video(video_id):
-    log('watch_video started with video_id=%s' % video_id)
-    mobile = __mobile()
-    video_url, title = scraper.get_video_url(video_id, mobile=mobile)
-    log('watch_video finished with video_url=%s' % video_url)
+    video_url = scraper.get_video_url(video_id)
     return plugin.set_resolved_url(video_url)
-
-
-@plugin.route('/random/')
-def watch_random():
-    log('watch_random started')
-    mobile = __mobile()
-    video_url, title = scraper.get_random_video_url(mobile=mobile)
-    log('watch_random finished with video_url=%s' % video_url)
-    return plugin.set_resolved_url(video_url, title=title)
 
 
 @plugin.route('/archive/')
 def show_archive():
-    log('show_archive started')
     archive_dates = scraper.get_archive_dates()
-    items = [{'label': archive_date['title'],
-              'url': plugin.url_for('show_archived_videos',
-                                    archive_id=archive_date['archive_id']),
-             } for archive_date in archive_dates]
-    return plugin.add_items(items)
+    items = [{
+        'label': archive_date['title'],
+        'path': plugin.url_for(
+            'show_archived_videos',
+            archive_id=archive_date['archive_id'],
+        ),
+    } for archive_date in archive_dates]
+    items.reverse()
+    return plugin.finish(items)
 
 
 @plugin.route('/archive/<archive_id>/')
 def show_archived_videos(archive_id):
-    log('show_archived_videos started with archive_id: %s' % archive_id)
-    videos = scraper.get_videos_by_archive_date(archive_id)
+    videos = scraper.get_archived_videos(
+        archive_id=archive_id
+    )
     return __add_videos(videos)
 
 
 @plugin.route('/search/')
 def search():
-    log('search start')
-    search_string = None
-    keyboard = xbmc.Keyboard('', plugin.get_string(30003))
-    keyboard.doModal()
-    if keyboard.isConfirmed() and keyboard.getText():
-        search_string = keyboard.getText()
-        log('search gots a string: "%s"' % search_string)
-        videos = scraper.get_search_result(search_string)
-        return __add_videos(videos)
+    search_string = plugin.keyboard(heading=_('search'))
+    if search_string:
+        url = plugin.url_for(
+            'search_result',
+            search_string=search_string
+        )
+        plugin.redirect(url)
+
+
+@plugin.route('/search/<search_string>/')
+def search_result(search_string):
+    videos = scraper.get_search_result(search_string)
+    return __add_videos(videos)
 
 
 def __add_videos(videos):
-    p = plugin._plugin.getAddonInfo('path').decode('utf-8')
-    icon = os.path.join(xbmc.translatePath(p), 'icon.png')
-    items = [{'label': video['title'],
-              'thumbnail': icon,
-              'url': plugin.url_for('watch_video',
-                                    video_id=video['video_id']),
-              'info': {'date': video['date']},
-              'is_folder': False,
-              'is_playable': True,
-             } for video in videos]
-    sort_methods = [xbmcplugin.SORT_METHOD_DATE,
-                    xbmcplugin.SORT_METHOD_LABEL]
-    return plugin.add_items(items, sort_method_ids=sort_methods)
+    items = [{
+        'label': video['title'],
+        'thumbnail': video['thumb'],
+        'path': plugin.url_for(
+            'watch_video',
+            video_id=video['video_id']
+        ),
+        'info': {
+            'date': video['date']
+        },
+        'is_playable': True,
+    } for video in videos]
+    finish_kwargs = {
+        'sort_methods': ('DATE', )
+    }
+    if plugin.get_setting('force_viewmode', bool):
+        finish_kwargs['view_mode'] = 'thumbnail'
+    return plugin.finish(items, **finish_kwargs)
 
 
-def __mobile():
-    return plugin.get_setting('pref_video') == '1'
+def _(string_id):
+    if string_id in STRINGS:
+        return plugin.get_string(STRINGS[string_id])
+    else:
+        plugin.log.warning('String is missing: %s' % string_id)
+        return string_id
 
-
-def log(msg):
-    xbmc.log('%s addon: %s' % ('Dump.com', msg))
 
 if __name__ == '__main__':
-    plugin.run()
+    try:
+        plugin.run()
+    except scraper.NetworkError:
+        plugin.notify(msg=_('network_error'))
--- plugin.video.dump/resources/lib/scraper.py
+++ plugin.video.dump/resources/lib/scraper.py
@@ -1,28 +1,53 @@
-import urllib2
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2013 Tristan Fischer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from urllib import quote
+from urllib2 import urlopen, Request, HTTPError, URLError
 import re
 from datetime import datetime
 from base64 import b64decode
 from BeautifulSoup import BeautifulSoup
 
-MAIN_URL = 'http://m.dump.com/'
+MAIN_URL = 'http://dump.com/'
 
-MONTHS = {'Jan': 1,
-          'Feb': 2,
-          'Mar': 3,
-          'Apr': 4,
-          'May': 5,
-          'Jun': 6,
-          'Jul': 7,
-          'Aug': 8,
-          'Sep': 9,
-          'Oct': 10,
-          'Nov': 11,
-          'Dec': 12}
+MONTHS = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Aug': 8,
+    'Sep': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dec': 12
+}
+
+
+class NetworkError(Exception):
+    pass
 
 
 def get_current_videos():
-    tree = __get_tree(MAIN_URL)
-    return __get_videos(tree)
+    return __get_videos(MAIN_URL)
 
 
 def get_archive_dates():
@@ -32,103 +57,60 @@ def get_archive_dates():
     for entry in tree.findAll('a', {'class': 'b'}):
         title = entry.string.strip()
         archive_id = entry['href'].replace('/archives/', '').strip()
-        archive_dates.append({'title': title,
-                              'archive_id': archive_id})
-    return archive_dates
+        archive_dates.append({
+            'title': title,
+            'archive_id': archive_id
+        })
+    return reversed(archive_dates)
 
 
-def get_videos_by_archive_date(date):
-    url = MAIN_URL + 'archives/%s' % date
-    tree = __get_tree(url)
-    return __get_videos(tree)
+def get_archived_videos(archive_id):
+    return __get_videos(MAIN_URL + 'archives/%s' % archive_id)
 
 
 def get_search_result(query):
-    url = MAIN_URL + 'search?query=%s' % query
+    return __get_videos(MAIN_URL + 'search/%s' % quote(query))
+
+
+def get_video_url(video_id):
+    tree = __get_tree(MAIN_URL + video_id)
+    r_js = re.compile('lxUTILsign')
+    r_gc = re.compile('var googleCode = \'(.+?)\';')  # funny idea :-)
+    r_vurl = re.compile('"file","(.+?)"')
+    js_code = tree.find('script', text=r_js).string.strip()
+    js_params = b64decode(re.search(r_gc, js_code).group(1))
+    video_url = re.search(r_vurl, js_params).group(1)
+    return video_url
+
+
+def __get_videos(url):
     tree = __get_tree(url)
-    return __get_videos(tree)
-
-
-def get_random_video_url(mobile=False):
-    return get_video_url('random', mobile)
-
-
-def get_video_url(video_id, mobile=False):
-    log('get_video_url started with video_id=%s mobile: %s' % (video_id,
-                                                               mobile))
-    url = MAIN_URL + video_id
-
-    def _mobile(url):
-        log('get_video_url mobile mode')
-        tree = __get_tree(url, mobile=True)
-        title = tree.head.title.string.replace('[VIDEO]', '').strip()
-        video_url = tree.find('a', {'id': 'video'})['href']
-        return video_url, title
-
-    def _flv(url):
-        log('get_video_url flv mode')
-        tree = __get_tree(url, mobile=False)
-        title = tree.head.title.string.replace('[VIDEO]', '').strip()
-        r_js = re.compile('lxUTILsign')
-        r_gc = re.compile('var googleCode = \'(.+?)\';')  # funny idea :-)
-        r_vurl = re.compile('"file","(.+?)"')
-        try:
-            js_code = tree.find('script', text=r_js).string.strip()
-            js_params = b64decode(re.search(r_gc, js_code).group(1))
-            video_url = re.search(r_vurl, js_params).group(1)
-        except AttributeError:
-            log('get_video_url flv mode FAILED')
-            video_url = None
-        return video_url, title
-
-    if not mobile:
-        video_url, title = _flv(url) or _mobile(url)
-    else:
-        video_url, title = _mobile(url)
-    return video_url, title
-
-
-def __get_videos(tree):
     videos = []
     page_title = tree.head.title.string
-    year = None
+    y = None
     if page_title:
-        year = filter(lambda s: s.isdigit(), page_title)
-    if not year:
-        year = datetime.now().year
-    for video in tree.findAll('div', {'class': 'video-item'}):
-        title = video.find('a').string.strip()
-        video_id = video.find('a')['href'].replace(MAIN_URL, '').strip('/')
-        try:
-            date_str = video.find('span', {'class': 'video_date'}).b.string
-        except AttributeError:
-            date_str = video.find('span', {'class': 'video_date'}).string
-        month_str, day_str = date_str.split()
-        month = MONTHS.get(month_str)
-        date = '%02i.%02i.%s' % (int(day_str), month, year)
-        videos.append({'title': title,
-                       'video_id': video_id,
-                       'date': date})
+        y = filter(lambda s: s.isdigit(), page_title)
+    if not y:
+        y = datetime.now().year
+    for a in tree.findAll('a', {'class': 'b'}):
+        m, d = a.find('span', {'class': 'video_date'}).string.split()
+        thumb = a.find('img').get('data-original') or a.find('img')['src']
+        videos.append({
+            'title': a.find('span', {'class': 'video_title'}).string,
+            'video_id': a['href'].split('/')[-2],
+            'thumb': thumb,
+            'date': '%02i.%02i.%s' % (int(d), MONTHS[m], y)
+        })
     return videos
 
 
-def __get_tree(url, mobile=True):
-    if mobile:
-        if '?' in url:
-            url = url + '&nord'
-        else:
-            url = url + '?nord'
-    log('__get_tree opening url: %s' % url)
-    req = urllib2.Request(url)
-    req.add_header('Accept', ('text/html,application/xhtml+xml,'
-                              'application/xml;q=0.9,*/*;q=0.8'))
-    req.add_header('User-Agent', ('Mozilla/5.0 (X11; Linux i686) '
-                                  'AppleWebKit/535.21 (KHTML, like Gecko) '
-                                  'Chrome/19.0.1041.0 Safari/535.21'))
-    html = urllib2.urlopen(req).read()
-    tree = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
+def __get_tree(url):
+    req = Request(url)
+    try:
+        response = urlopen(req).read()
+    except HTTPError, error:
+        raise NetworkError(error)
+    except URLError, error:
+        raise NetworkError(error)
+    tree = BeautifulSoup(response, convertEntities=BeautifulSoup.HTML_ENTITIES)
     return tree
-
-
-def log(msg):
-    print('%s scraper: %s' % ('dump.com', msg))

cffa570b3e6dfa971f8bffbb22a2a4cbe2edacd3
beenje <beenje@xbmc.org>
2013-03-22 21:47:16 +0100

    [plugin.video.drnu] updated to version 3.0.0

--- plugin.video.drnu/nuapi.py
+++ plugin.video.drnu/nuapi.py
@@ -1,5 +1,5 @@
 #
-#      Copyright (C) 2011 Tommy Winther
+#      Copyright (C) 2013 Tommy Winther
 #      http://tommy.winther.nu
 #
 #  This Program is free software; you can redistribute it and/or modify
@@ -25,6 +25,7 @@ import urllib2
 
 API_URL = 'http://www.dr.dk/NU/api/%s'
 
+
 class DrNuApi(object):
     def __init__(self, cachePath, cacheMinutes):
         """
@@ -37,7 +38,7 @@ class DrNuApi(object):
         self.cachePath = cachePath
         self.cacheMinutes = cacheMinutes
 
-    def getProgramSeries(self, limitToSlugs = None, label = None):
+    def getProgramSeries(self, limitToSlugs=None, label=None):
         series = self._call_api('programseries', 'programseries.json')
         if limitToSlugs is not None:
             seriesWithSlug = list()
@@ -45,7 +46,7 @@ class DrNuApi(object):
                 for serie in series:
                     if slug == serie['slug']:
                         seriesWithSlug.append(serie)
-                        
+
             series = seriesWithSlug
 
         if label is not None:
@@ -77,7 +78,6 @@ class DrNuApi(object):
         list.sort(labels)
         return labels
 
-
     def getAllVideos(self):
         return self._call_api('videos/all', 'all.json') or list()
 
@@ -100,49 +100,52 @@ class DrNuApi(object):
         return self._call_api('videos/premiere', 'premiere.json') or list()
 
     def getProgramSeriesVideos(self, programSeriesSlug):
-        return self._call_api('programseries/%s/videos' % programSeriesSlug, 'programseries-%s.json' % programSeriesSlug) or list()
+        return self._call_api('programseries/%s/videos' % programSeriesSlug,
+                              'programseries-%s.json' % programSeriesSlug) or list()
 
-    def getVideoById(self, id):
-        response = self._call_api('videos/%s' % id, 'videobyid-%s.json' % id)
+    def getVideoById(self, videoId):
+        response = self._call_api('videos/%s' % videoId, 'videobyid-%s.json' % videoId)
         if type(response) in [str, unicode]:
-            print 'Video with ID %s not found' % id
+            print 'Video with ID %s not found' % videoId
             return None
         else:
             return response
 
-    def search(self, term, limit = 100):
+    def search(self, term, limit=100):
         if not term:
             return list()
         return self._call_api('search/%s?limit=%d' % (urllib.quote(term), limit)) or list()
 
-    def getProgramSeriesImageUrl(self, programSlug, width, height = None):
+    def getProgramSeriesImageUrl(self, programSlug, width, height=None):
         if height is None:
             height = width
         return API_URL % ('programseries/%s/images/%dx%d.jpg' % (programSlug, width, height))
 
-    def getVideoImageUrl(self, id, width, height = None):
+    def getVideoImageUrl(self, videoId, width, height=None):
         if height is None:
             height = width
-        return API_URL % ('videos/%s/images/%dx%d.jpg' % (id, width, height))
+        return API_URL % ('videos/%s/images/%dx%d.jpg' % (videoId, width, height))
 
-    def getChapterImageUrl(self, id, width, height = None):
+    def getChapterImageUrl(self, chapterId, width, height=None):
         if height is None:
             height = width
-        return API_URL % ('chapters/%s/images/%dx%d.jpg' % (id, width, height))
+        return API_URL % ('chapters/%s/images/%dx%d.jpg' % (chapterId, width, height))
 
-    def _call_api(self, path, cacheFilename = None):
-        print "Calling API: " + API_URL % path
+    def _call_api(self, url, cacheFilename=None):
+        if url[0:7] != 'http://':
+            url = API_URL % url
+        print "Calling API: " + url
 
         if cacheFilename:
             cachePath = os.path.join(self.cachePath, cacheFilename)
             try:
                 cachedOn = os.path.getmtime(cachePath)
-            except OSError: # File not found
+            except OSError:  # File not found
                 cachedOn = 0
 
             if not os.path.exists(cachePath) or time.time() - self.cacheMinutes * 60 >= cachedOn:
                 # Cache expired or miss
-                content = self._http_request(path)
+                content = self._http_request(url)
 
                 if content:
                     try:
@@ -150,7 +153,7 @@ class DrNuApi(object):
                         f.write(content)
                         f.close()
                     except Exception:
-                        pass # just too bad if file system is read-only
+                        pass  # just too bad if file system is read-only
 
             else:
                 f = open(cachePath)
@@ -158,7 +161,7 @@ class DrNuApi(object):
                 f.close()
 
         else:
-            content = self._http_request(path)
+            content = self._http_request(url)
 
         if content is not None:
             try:
@@ -168,9 +171,9 @@ class DrNuApi(object):
         else:
             return []
 
-    def _http_request(self, path):
+    def _http_request(self, url):
         try:
-            u = urllib2.urlopen(API_URL % path, timeout = 30)
+            u = urllib2.urlopen(url, timeout=30)
             content = u.read()
             u.close()
         except Exception as ex:
@@ -181,9 +184,10 @@ class DrNuApi(object):
 class DrNuException(Exception):
     pass
 
+
 if __name__ == '__main__':
     api = DrNuApi('/tmp', 0)
-#    json =  api.getProgramSeriesVideos('paa-skinner')
-    json =  api.getVideoById(16746)
+    #    json =  api.getProgramSeriesVideos('paa-skinner')
+    json = api.getVideoById(16746)
     s = simplejson.dumps(json, sort_keys=True, indent='    ')
-    print '\n'.join([l.rstrip() for l in  s.splitlines()])
+    print '\n'.join([l.rstrip() for l in s.splitlines()])

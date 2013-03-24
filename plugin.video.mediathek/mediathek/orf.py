5e172ffea7a21e9385c6dc98810c900760242056
beenje <beenje@xbmc.org>
2013-03-24 22:03:01 +0100

    [plugin.video.mediathek] updated to version 0.5.0

--- plugin.video.mediathek/mediathek/orf.py
+++ plugin.video.mediathek/mediathek/orf.py
@@ -6,121 +6,30 @@ from mediathek import *
 
 class ORFMediathek(Mediathek):
   def __init__(self, simpleXbmcGui):
-    self.gui = simpleXbmcGui;
-    self.menuTree = (
-      TreeNode("0","Startseite","http://tvthek.orf.at/",True),
-      TreeNode("1","Sendungen","",False,
-        (
-          TreeNode("1.0",u"Bundesländer","",False,
-            (
-              TreeNode("1.0.0",u"Burgenland heute","http://tvthek.orf.at/programs/70021-Burgenland-heute",True),
-              TreeNode("1.0.1",u"Kärnten heute","http://tvthek.orf.at/programs/70022-Kaernten-heute",True),
-              TreeNode("1.0.2",u"Niederösterreich heute","http://tvthek.orf.at/programs/70017-Niederoesterreich-heute",True),
-              TreeNode("1.0.3",u"Oberoesterreich heute","http://tvthek.orf.at/programs/70016-Oberoesterreich-heute",True),
-              TreeNode("1.0.4",u"Salzburg heute","http://tvthek.orf.at/programs/70019-Salzburg-heute",True),
-              TreeNode("1.0.5",u"Steiermark heute","http://tvthek.orf.at/programs/70020-Steiermark-heute",True),
-              TreeNode("1.0.6",u"Tirol heute","http://tvthek.orf.at/programs/70023-Tirol-heute",True),
-              TreeNode("1.0.6",u"Vorarlberg heute","http://tvthek.orf.at/programs/70024-Vorarlberg-heute",True),
-              TreeNode("1.0.6",u"Wien heute","http://tvthek.orf.at/programs/70018-Wien-heute",True),
-            )
-          ),
-          TreeNode("1.1",u"Dokumentationen","",False,
-            (
-              TreeNode("1.1.0",u"Erlebnis Österreich","http://tvthek.orf.at/programs/1200-Erlebnis-Oesterreich",True),
-              TreeNode("1.1.1",u"Menschen & Mächte","http://tvthek.orf.at/programs/170407-Menschen---Maechte",True),
-              TreeNode("1.1.2",u"Universum","http://tvthek.orf.at/programs/35429-Universum",True),
-            )
-          ),
-          TreeNode("1.2",u"Information","",False,
-            (
-              TreeNode("1.2.0",u"Club 2","http://tvthek.orf.at/programs/1283-Club-2",True),
-              TreeNode("1.2.1",u"Heute in Österreich","http://tvthek.orf.at/programs/1257-Heute-in-Oesterreich",True),
-              TreeNode("1.2.2",u"Hohes Haus","http://tvthek.orf.at/programs/1264-Hohes-Haus",True),
-              TreeNode("1.2.3",u"Im Zentrum","http://tvthek.orf.at/programs/1279-Im-Zentrum",True),
-              TreeNode("1.2.4",u"Österreich-Bild","http://tvthek.orf.at/programs/1296-Oesterreich-Bild",True),
-              TreeNode("1.2.5",u"Pressestunde","http://tvthek.orf.at/programs/1273-Pressestunde",True),
-              TreeNode("1.2.6",u"Runder Tisch","http://tvthek.orf.at/programs/70010-Runder-Tisch",True),
-              TreeNode("1.2.7",u"Südtirol heute","http://tvthek.orf.at/programs/1277675-Suedtirol-heute",True),
-              TreeNode("1.2.8",u"Wetter","http://tvthek.orf.at/programs/1250-Wetter",True),
-              TreeNode("1.2.9",u"Wetter (ÖGS)","http://tvthek.orf.at/programs/1786041-Wetter--OeGS-",True),
-              TreeNode("1.2.10",u"Wetter ZiB 20","http://tvthek.orf.at/programs/972117-Wetter-ZIB-20",True),
-              TreeNode("1.2.11",u"ZiB 9","http://tvthek.orf.at/programs/71256-ZiB-9",True),
-              TreeNode("1.2.12",u"ZiB 11","http://tvthek.orf.at/programs/71276-ZiB-11",True),
-              TreeNode("1.2.13",u"ZiB 13","http://tvthek.orf.at/programs/71280-ZiB-13",True),
-              TreeNode("1.2.14",u"ZiB 17","http://tvthek.orf.at/programs/71284-ZiB-17",True),
-              TreeNode("1.2.15",u"Zeit im Bild","http://tvthek.orf.at/programs/1203-Zeit-im-Bild",True),
-              TreeNode("1.2.16",u"Zeit im Bild (ÖGS)","http://tvthek.orf.at/programs/145302-Zeit-im-Bild--OeGS-",True),
-              TreeNode("1.2.17",u"ZiB 20","http://tvthek.orf.at/programs/1218-ZiB-20",True),
-              TreeNode("1.2.18",u"ZiB 2","http://tvthek.orf.at/programs/1211-ZiB-2",True),
-              TreeNode("1.2.19",u"Spät ZiB","http://tvthek.orf.at/programs/79134-Spaet-ZiB",True),
-              TreeNode("1.2.20",u"ZiB 24","http://tvthek.orf.at/programs/1225-ZiB-24",True),
-              TreeNode("1.2.21",u"ZiB Flash","http://tvthek.orf.at/programs/1232-ZiB-Flash",True),
-            )
-          ),
-          TreeNode("1.3",u"Magazine","",False,
-            (
-              TreeNode("1.3.0",u"Bewusst gesund - das Magazin","http://tvthek.orf.at/programs/1714463-Bewusst-gesund---das-Magazin",True),
-              TreeNode("1.3.1",u"Bürgeranwalt","http://tvthek.orf.at/programs/1339-Buergeranwalt",True),
-              TreeNode("1.3.2",u"Bürgerforum","http://tvthek.orf.at/programs/1343-Buergerforum",True),
-              TreeNode("1.3.3",u"Konkret","http://tvthek.orf.at/programs/1336-Konkret",True),
-              TreeNode("1.3.4",u"Land und Leute","http://tvthek.orf.at/programs/1369-Land-und-Leute",True),
-              TreeNode("1.3.5",u"Stöckl am Samstag","http://tvthek.orf.at/programs/1651-Stoeckl-am-Samstag",True),
-              TreeNode("1.3.6",u"Thema","http://tvthek.orf.at/programs/1319-Thema",True),
-              TreeNode("1.3.7",u"Vera exklusiv","http://tvthek.orf.at/programs/35440-Vera-exklusiv",True),
-              TreeNode("1.3.8",u"Weltjournal","http://tvthek.orf.at/programs/1328-Weltjournal",True),
-              TreeNode("1.3.9",u"Winterzeit","http://tvthek.orf.at/programs/1003023-Winterzeit",True),
-              TreeNode("1.3.10",u"€co","http://tvthek.orf.at/programs/1346-Eco",True),
-            )
-          ),
-          TreeNode("1.4",u"Kultur","",False,
-            (
-              TreeNode("1.4.0",u"a.viso","http://tvthek.orf.at/programs/1299-a-viso",True),
-              TreeNode("1.4.0",u"Kulturmontag","http://tvthek.orf.at/programs/1303-Kulturmontag",True),
-            )
-          ),
-          TreeNode("1.5",u"Sport","http://tvthek.orf.at/programs/1379-Sportbild",True,
-            (
-            )
-          ),
-          TreeNode("1.6",u"Religion","",False,
-            (
-              TreeNode("1.6.0",u"Kreuz & Quer","http://tvthek.orf.at/programs/1193-Kreuz---Quer",True),
-              TreeNode("1.6.1",u"Orientierung","http://tvthek.orf.at/programs/1366-Orientierung",True),
-              TreeNode("1.6.2",u"Religionen der Welt","http://tvthek.orf.at/programs/1656-Religionen-der-Welt",True),
-              TreeNode("1.6.3",u"Was ich glaube","http://tvthek.orf.at/programs/1287-Was-ich-glaube",True),
-            )
-          ),
-          TreeNode("1.7",u"Society","",False,
-            (
-              TreeNode("1.7.0",u"Chilli","http://tvthek.orf.at/programs/1504477-Chili",True),
-              TreeNode("1.7.1",u"Seitenblicke","http://tvthek.orf.at/programs/1360-Seitenblicke",True),
-            )
-          ),
-          TreeNode("1.8",u"Show","",False,
-            (
-              TreeNode("1.8.0",u"ARGE Talkshow","http://tvthek.orf.at/programs/1864371-ARGE-Talkshow",True),
-              TreeNode("1.8.1",u"Frisch gekocht","http://tvthek.orf.at/programs/1375-Frisch-gekocht",True),
-              TreeNode("1.8.2",u"Was gibt es Neues?","http://tvthek.orf.at/programs/1309553-Was-gibt-es-Neues-",True),
-              TreeNode("1.8.3",u"Willkommen Österreich","http://tvthek.orf.at/programs/1309549-Willkommen-Oesterreich",True),
-              TreeNode("1.8.4",u"Helden von Morgen - Die Show","http://tvthek.orf.at/programs/1679635-HvM---Die-Show",True),
-              TreeNode("1.8.5",u"Helden von Morgen - Die Entscheidung","http://tvthek.orf.at/programs/1710513-HvM---Die-Entscheidung",True),
-            )
-          ),
-          TreeNode("1.9",u"Volksgruppen","",False,
-            (
-              TreeNode("1.9.0",u"Heimat, fremde Heimat","http://tvthek.orf.at/programs/1357-Heimat--fremde-Heimat",True),
-              TreeNode("1.9.1",u"Dobar dan Hrvati","http://tvthek.orf.at/programs/85526-Dobar-dan-Hrvati",True),
-              TreeNode("1.9.2",u"Dober dan, Koroška","http://tvthek.orf.at/programs/85528-Dober-dan--Koroschka",True),
-              TreeNode("1.9.3",u"Adj' Isten magyarok","http://tvthek.orf.at/programs/85519-Adj--Isten-magyarok",True),
-              TreeNode("1.9.4",u"Servus Szia Zdravo Del tuha","http://tvthek.orf.at/programs/85512-Servus-Szia-Zdravo-Del-tuha",True),
-              TreeNode("1.9.5",u"České & Slovenské Ozveny","http://tvthek.orf.at/programs/181719-Tscheske---Slovenske-Ozveny",True),
-            )
-          ),          
-        )
-      )
-    );
-      
+    
     self.rootLink = "http://tvthek.orf.at"
+    self.gui = simpleXbmcGui;
+    self.menuTree = [];
+    self.menuTree.append(TreeNode("0","Startseite","http://tvthek.orf.at/",True));
+    
+    menuPage = self.loadPage(self.rootLink+"/programs");
+    
+    findMenuLink = re.compile("<li><a href=\"(/programs/.*?)\" title=\".*?\">(.*?)</a></li>");
+    findCategorie = re.compile("<h4>(.*?)</h4>\\s*?<ul>((\\s*?%s\\s*?)+)</ul>"%findMenuLink.pattern)
+    
+    categories = [];
+    
+    for categorieMatch in findCategorie.finditer(menuPage):
+      
+      title = categorieMatch.group(1);
+      
+      items = [];
+      for menuMatch in findMenuLink.finditer(categorieMatch.group(2)):
+        items.append(TreeNode("1.%d.%d"%(len(categories),len(items)), menuMatch.group(2),"%s%s"%(self.rootLink,menuMatch.group(1)),True));
+      
+      categories.append(TreeNode("1.%d"%len(categories), title,"",False,items));
+    
+    self.menuTree.append(TreeNode("1","Sendungen","",False,categories));
     
     videoLinkPage = "/programs/.*"
     imageLink = "http://tvthek.orf.at/assets/.*?.jpeg"

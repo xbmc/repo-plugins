09900f6474f71313d05ccd19019e985efee8c3b9
beenje <beenje@xbmc.org>
2013-04-05 22:16:21 +0200

    [plugin.video.news.tv2.dk] updated to version 2.0.0

--- plugin.video.news.tv2.dk/addon.py
+++ plugin.video.news.tv2.dk/addon.py
@@ -1,5 +1,5 @@
 #
-#      Copyright (C) 2012 Tommy Winther
+#      Copyright (C) 2013 Tommy Winther
 #      http://tommy.winther.nu
 #
 #  This Program is free software; you can redistribute it and/or modify
@@ -17,6 +17,7 @@
 #  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 #  http://www.gnu.org/copyleft/gpl.html
 #
+from htmlentitydefs import name2codepoint
 import os
 import sys
 import urlparse
@@ -26,64 +27,93 @@ import buggalo
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
-import random
+import simplejson
+import datetime
 
-VIDEO_LIST_URL = 'http://news.tv2.dk/js/video-list.js.php/video.js'
-PLAYLIST_URL = 'http://common-dyn.tv2.dk/flashplayer/playlist.xml.php/alias-player_news/autoplay-1/clipid-%s/keys-NEWS,PLAYER.xml'
+# http://nyhederne.tv2.dk/video/data/tag/nyheder/
+# http://nyhederne.tv2.dk/video/data/tag/nyh0600/day/0/staticjsonp/staticjsonp_148f9/
+
+TAGS = ['nyheder', 'most-viewed', 'nyh0600', 'nyh0900', 'nyh1700', 'nyh1900', 'nyh2200', 'station2', 'newsmagasiner']
+
+VIDEO_DATA_URL = "http://nyhederne.tv2.dk/video/data/tag/%s/"
+PLAYLIST_URL = 'http://common.tv2.dk/mpx/player.php/adtech_alias-player_nyhederne/adtech_group-441/autoplay-1/guid-%s/player_id-video_nyhederne.html'
+FLASH_PLAYLIST_URL = 'http://common.tv2.dk/flashplayer/playlist.xml.php/clipid-%s.xml'
+
 
 class TV2NewsAddon(object):
-    def listCategories(self):
-        data = self._loadJson()
-        if data:
-            m = re.match('.*sections: \[(.*?)\]', data, re.DOTALL)
-            for idx, m in enumerate(re.finditer('title: "([^"]+)"', m.group(1))):
-                title = m.group(1)
 
-                item = xbmcgui.ListItem(title, iconImage = ICON)
-                item.setProperty('Fanart_Image', FANART)
-                url = PATH + '?idx=' + str(idx)
-                xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder = True)
+    def listTags(self):
+        for idx, tag in enumerate(TAGS):
+            title = ADDON.getLocalizedString(30000 + idx)
 
-            xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_LABEL)
+            item = xbmcgui.ListItem(title, iconImage=ICON)
+            item.setProperty('Fanart_Image', FANART)
+            url = PATH + '?tag=' + tag
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder=True)
 
-        xbmcplugin.endOfDirectory(HANDLE, succeeded=data is not None)
+        xbmcplugin.endOfDirectory(HANDLE)
 
-    def listCategory(self, idx):
-        data = self._loadJson()
-        if data:
-            clips = list()
-            for m in re.finditer('sources: \[(.*?)\] \}', data, re.DOTALL):
-                clips.append(m.group(1))
+    def listClips(self, tag):
+        try:
+            u = urllib2.urlopen(VIDEO_DATA_URL % tag)
+            data = u.read()
+            u.close()
+            clips = simplejson.loads(data.decode('iso-8859-1'))
+        except Exception as ex:
+            heading = buggalo.getRandomHeading()
+            line1 = ADDON.getLocalizedString(30900)
+            line2 = ADDON.getLocalizedString(30901)
+            xbmcgui.Dialog().ok(heading, line1, line2, str(ex))
+            return
 
-            for m in re.finditer('id: ([0-9]+),\s+title:"(.*?)",\s+image:"([^"]+)",\s+description: "(.*?)",\s+.*?date: "([^"]+)"', clips[idx], re.DOTALL):
-                id = m.group(1)
-                title = m.group(2).replace("\\'", "'").replace('\\"', '"')
-                image = m.group(3)
-                description = m.group(4)
-                date = m.group(5)
+        for clip in clips:
+            if 'section' in clip:
+                title = self._decodeHtmlEntities('%s: %s' % (clip['section'], clip['title']))
+            else:
+                title = self._decodeHtmlEntities(clip['title'])
+            date = datetime.date.fromtimestamp(clip['created'])
 
-                item = xbmcgui.ListItem(title, iconImage = image)
-                item.setInfo('video', {
-                    'title' : title,
-                    'studio' : ADDON.getAddonInfo('name'),
-                    'plot' : description,
-                    'date' : date[6:].replace('-', '.')
-                })
-                item.setProperty('IsPlayable', 'true')
-                item.setProperty('Fanart_Image', FANART)
-                url = PATH + '?clip=' + str(id)
-                xbmcplugin.addDirectoryItem(HANDLE, url, item)
+            item = xbmcgui.ListItem(title, iconImage=clip['img'])
+            item.setInfo('video', {
+                'title': title,
+                'studio': ADDON.getAddonInfo('name'),
+                'plot': self._decodeHtmlEntities(clip['description']),
+                'date': date.strftime('%d.%m.%Y')
+            })
 
-            xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
-        xbmcplugin.endOfDirectory(HANDLE, succeeded=data is not None)
+            item.setProperty('IsPlayable', 'true')
+            item.setProperty('Fanart_Image', FANART)
+            url = PATH + '?id=' + str(clip['id'])
+            xbmcplugin.addDirectoryItem(HANDLE, url, item)
+
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_UNSORTED)
+        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
+        xbmcplugin.endOfDirectory(HANDLE)
 
     def playClip(self, clipId):
         try:
             u = urllib2.urlopen(PLAYLIST_URL % clipId)
-            playlist = u.read()
+            html = u.read()
             u.close()
+
+            m = re.match(".*var releaseUrl = '([^']+)'", html, re.DOTALL)
+            if m:  # new style playlist
+                u = urllib2.urlopen(m.group(1))
+                playlist = u.read()
+                u.close()
+
+                m = re.match('.*<video src="([^"]+)"', playlist, re.DOTALL)
+                url = m.group(1)
+            else:  # flash playlist
+                u = urllib2.urlopen(FLASH_PLAYLIST_URL % clipId)
+                xml = u.read()
+                u.close()
+
+                m = re.match('.*video="([^"]+)"', xml, re.DOTALL)
+                url = m.group(1)
+
         except Exception as ex:
-            heading = ADDON.getLocalizedString(random.randint(99980, 99985))
+            heading = buggalo.getRandomHeading()
             line1 = ADDON.getLocalizedString(30900)
             line2 = ADDON.getLocalizedString(30901)
             xbmcgui.Dialog().ok(heading, line1, line2, str(ex))
@@ -91,24 +121,45 @@ class TV2NewsAddon(object):
             xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
             return
 
-        m = re.match('.*video="([^"]+)" splash="([^"]+)"', playlist, re.DOTALL)
-        if m:
-            item = xbmcgui.ListItem(path = m.group(1), thumbnailImage=m.group(2))
-            xbmcplugin.setResolvedUrl(HANDLE, True, item)
+        item = xbmcgui.ListItem(thumbnailImage=ICON)
+        if url:
+            item.setPath(url)
 
-    def _loadJson(self):
-        try:
-            u = urllib2.urlopen(VIDEO_LIST_URL)
-            data = u.read()
-            u.close()
-            return data.decode('iso-8859-1')
-        except Exception as ex:
-            heading = ADDON.getLocalizedString(random.randint(99980, 99985))
-            line1 = ADDON.getLocalizedString(30900)
-            line2 = ADDON.getLocalizedString(30901)
-            xbmcgui.Dialog().ok(heading, line1, line2, str(ex))
+        xbmcplugin.setResolvedUrl(HANDLE, succeeded=url is not None, listitem=item)
 
-            return None
+    def _decodeHtmlEntities(self, string):
+        """Decodes the HTML entities found in the string and returns the modified string.
+
+        Both decimal (&#000;) and hexadecimal (&x00;) are supported as well as HTML entities,
+        such as &aelig;
+
+        Keyword arguments:
+        string -- the string with HTML entities
+
+        """
+        if type(string) not in [str, unicode]:
+            return string
+
+        def substituteEntity(match):
+            ent = match.group(3)
+            if match.group(1) == "#":
+                # decoding by number
+                if match.group(2) == '':
+                    # number is in decimal
+                    return unichr(int(ent))
+            elif match.group(2) == 'x':
+                # number is in hex
+                return unichr(int('0x'+ent, 16))
+            else:
+                # they were using a name
+                cp = name2codepoint.get(ent)
+                if cp:
+                    return unichr(cp)
+                else:
+                    return match.group()
+
+        entity_re = re.compile(r'&(#?)(x?)(\w+);')
+        return entity_re.subn(substituteEntity, string)[0]
 
 
 if __name__ == '__main__':
@@ -120,13 +171,14 @@ if __name__ == '__main__':
     FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
 
+    buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
     try:
         tv2News = TV2NewsAddon()
-        if PARAMS.has_key('idx'):
-            tv2News.listCategory(int(PARAMS['idx'][0]))
-        elif PARAMS.has_key('clip'):
-            tv2News.playClip(PARAMS['clip'][0])
+        if 'tag' in PARAMS:
+            tv2News.listClips(PARAMS['tag'][0])
+        elif 'id' in PARAMS:
+            tv2News.playClip(PARAMS['id'][0])
         else:
-            tv2News.listCategories()
+            tv2News.listTags()
     except Exception:
         buggalo.onExceptionRaised()

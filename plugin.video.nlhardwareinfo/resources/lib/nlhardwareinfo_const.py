05a874d113b499564407d5b9b698a49ed187ce8d
beenje <beenje@xbmc.org>
2013-04-10 21:52:59 +0200

    [plugin.video.nlhardwareinfo] initial version (1.0.0) thanks to Skipmode A1

---
+++ plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_const.py
@@ -0,0 +1,12 @@
+import os
+import xbmcaddon
+
+#
+# Constants
+# 
+__addon__       = "plugin.video.nlhardwareinfo"
+__settings__    = xbmcaddon.Addon(id=__addon__ )
+__language__    = __settings__.getLocalizedString
+__images_path__ = os.path.join( xbmcaddon.Addon(id=__addon__ ).getAddonInfo('path'), 'resources', 'images' )
+__date__        = "10 april 2013"
+__version__     = "1.0.0"
---
+++ plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_main.py
@@ -0,0 +1,44 @@
+#
+# Imports
+#
+from BeautifulSoup import BeautifulSoup
+from nlhardwareinfo_const import __addon__, __settings__, __language__, __images_path__, __date__, __version__
+from nlhardwareinfo_utils import HTTPCommunicator
+import os
+import re
+import sys
+import urllib, urllib2
+import urlparse
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+
+#
+# Main class
+#
+class Main:
+    def __init__( self ):
+        #
+        # Hardware.Info Tv
+        #
+        parameters = {"action" : "list", "plugin_category" : __language__(30000), "url" : "http://nl.hardware.info/tv?page=001", "next_page_possible": "True"}
+        url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+        listitem = xbmcgui.ListItem( __language__(30000), iconImage="DefaultFolder.png" )
+        folder = True
+        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        
+        #
+        # Losse Items
+        #
+        parameters = {"action" : "list", "plugin_category" : __language__(30001), "url" : "http://nl.hardware.info/tv/losse-items?page=001", "next_page_possible": "True"}
+        url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+        listitem = xbmcgui.ListItem( __language__(30001), iconImage="DefaultFolder.png" )
+        folder = True
+        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+                
+        # Disable sorting...
+        xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_NONE )
+		
+        # End of list...
+        xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )

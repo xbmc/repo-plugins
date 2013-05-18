ff0664cab75b2346249f93dc661ce998f007aa8c
beenje <beenje@xbmc.org>
2013-05-18 22:19:26 +0200

    [plugin.video.retrowaretv] updated to version 1.1.9

--- plugin.video.retrowaretv/default.py
+++ plugin.video.retrowaretv/default.py
@@ -7,7 +7,6 @@ import showEpisode
 
 #@author: dethfeet
 #@credits: Ricardo "Averre" Ocana Leal for the initial Version of the plugin
-#@version: 1.1.3
 
 addon = xbmcaddon.Addon(id='plugin.video.retrowaretv')
 
@@ -24,36 +23,34 @@ def mainPage():
     addDirectoryItem(addon.getLocalizedString(30003),{'action':"listArchive"})
         
 def listShows():
-    addDirectoryItem(addon.getLocalizedString(30101),{'action':"listVideos",'link' : baseLink+"/category/shows/16bitgems/"},baseLink+"/wp-content/uploads/2011/06/16bitsitebanner-300x84.png")
-    addDirectoryItem(addon.getLocalizedString(30102),{'action':"listVideos",'link' : baseLink+"/category/shows/the-game-chasers/"},baseLink+"/wp-content/uploads/2012/06/gamechasersbanner2-300x108.png")
-    addDirectoryItem(addon.getLocalizedString(30103),{'action':"listVideos",'link' : baseLink+"/category/shows/gamequickies/"},baseLink+"/wp-content/uploads/2011/06/gquickie.png")
-    addDirectoryItem(addon.getLocalizedString(30104),{'action':"listVideos",'link' : baseLink+"/category/shows/gaminghistorian/"},baseLink+"/wp-content/uploads/2011/06/gaming-historian-banner1-300x76.gif")
-    addDirectoryItem(addon.getLocalizedString(30105),{'action':"listVideos",'link' : baseLink+"/category/shows/hvgn/"},baseLink+"/wp-content/uploads/2011/06/HVGN-2.0-sizeb1-300x112.png")
-    addDirectoryItem(addon.getLocalizedString(30106),{'action':"listVideos",'link' : baseLink+"/category/shows/thehumansarecoming/"},baseLink+"/wp-content/uploads/2012/07/humans-are-coming-logo-3-gray-red-FINAL-300x287.png")
-    addDirectoryItem(addon.getLocalizedString(30119),{'action':"listVideos",'link' : baseLink+"/category/shows/jumblejunkie/"},baseLink+"/wp-content/uploads/2012/08/jumblejunkielogopng-600px.png")
-    addDirectoryItem(addon.getLocalizedString(30107),{'action':"listVideos",'link' : baseLink+"/category/shows/lazy-game-reviews/"},baseLink+"/wp-content/uploads/2012/06/LGRbg4b-300x168.png")
-    addDirectoryItem(addon.getLocalizedString(30108),{'action':"listVideos",'link' : baseLink+"/category/shows/letsget/"},baseLink+"/wp-content/uploads/2011/06/letsgetbanner-300x189.png")
-    addDirectoryItem(addon.getLocalizedString(30109),{'action':"listVideos",'link' : baseLink+"/category/shows/patnespunk/"},baseLink+"/wp-content/uploads/2011/06/PatNESLogo-300x196.jpg")
-    addDirectoryItem(addon.getLocalizedString(30110),{'action':"listVideos",'link' : baseLink+"/category/shows/pixelstoplastic/"},baseLink+"/wp-content/uploads/2011/06/P2P-Banner-300x128.gif")
-    addDirectoryItem(addon.getLocalizedString(30111),{'action':"listVideos",'link' : baseLink+"/category/shows/retroactive/"},baseLink+"/wp-content/uploads/2011/06/retroactiverwtvbanner-300x95.png")
-    addDirectoryItem(addon.getLocalizedString(30112),{'action':"listVideos",'link' : baseLink+"/category/shows/rwtvshow/"},baseLink+"/wp-content/uploads/2011/06/rwtvthe-show-logo.png")
-    addDirectoryItem(addon.getLocalizedString(30113),{'action':"listVideos",'link' : baseLink+"/category/shows/soldseparately/"},baseLink+"/wp-content/uploads/2011/06/sold-separately-banner.gif")
-    addDirectoryItem(addon.getLocalizedString(30118),{'action':"listVideos",'link' : baseLink+"/category/shows/strictlyarcade/"},"")
-    addDirectoryItem(addon.getLocalizedString(30114),{'action':"listVideos",'link' : baseLink+"/category/shows/vgto/"},baseLink+"/wp-content/uploads/2012/06/vgtobanner-300x72.png")
-    addDirectoryItem(addon.getLocalizedString(30120),{'action':"listVideos",'link' : baseLink+"/category/shows/videocade-commentaries/"},"")
-    addDirectoryItem(addon.getLocalizedString(30115),{'action':"listVideos",'link' : baseLink+"/category/shows/the-video-game-years/"},baseLink+"/wp-content/uploads/2012/02/tvgypagebanner.png")
-    addDirectoryItem(addon.getLocalizedString(30116),{'action':"listVideos",'link' : baseLink+"/category/shows/video-game-knowledge/"},baseLink+"/wp-content/uploads/2011/06/videogame-knowledge-banner.gif")
-    addDirectoryItem(addon.getLocalizedString(30117),{'action':"listVideos",'link' : baseLink+"/category/shows/you-can-play-this-2/"},baseLink+"/wp-content/uploads/2012/06/ycpt-banner-e1340208312357.png")
-
+    shows_page = LoadPage(baseLink)
+	
+    regex_extract_shows = re.compile("Shows</a>\n<ul class=\"sub-menu\">(.*?)</ul>\n</li>\n</ul>\n</li>",re.DOTALL)
+    shows_ul = regex_extract_shows.search(shows_page).group(1);
+    
+    regex_extract_show = re.compile("\n\t<li id=\".*?\" class=\".*?\"><a href=\"(.*?)\">(.*?)</a></li>");
+    shows_li = regex_extract_show.findall(shows_ul)
+    
+    for show in shows_li:
+        addDirectoryItem(show[1],{'action':"listVideos",'link' : show[0]},"")
 
 def ListArchive():
-    addDirectoryItem(addon.getLocalizedString(30201),{'action':"listVideos",'link' : baseLink+"/category/archives/boomstick/"},baseLink+"/wp-content/uploads/2011/06/Menu-Button-Boomstick.gif")
-    addDirectoryItem(addon.getLocalizedString(30202),{'action':"listVideos",'link' : baseLink+"/category/archives/jam-enslaver/"},baseLink+"/wp-content/uploads/2011/06/jamenslaver1.png")
-    addDirectoryItem(addon.getLocalizedString(30203),{'action':"listVideos",'link' : baseLink+"/category/archives/when-worlds-collide/"},"")
+    shows_page = LoadPage(baseLink)
+	
+    regex_extract_shows = re.compile("Archive</a>\n\t<ul class=\"sub-menu\">(.*?)</ul>\n</li>\n</ul>\n</li>",re.DOTALL)
+    shows_ul = regex_extract_shows.search(shows_page).group(1);
+    
+    regex_extract_show = re.compile("\n\t\t<li id=\".*?\" class=\".*?\"><a href=\"(.*?)\">(.*?)</a></li>");
+    shows_li = regex_extract_show.findall(shows_ul)
+    
+    for show in shows_li:
+        addDirectoryItem(show[1],{'action':"listVideos",'link' : show[0]},"")
+
 
 def listUserContent():
-    addDirectoryItem(addon.getLocalizedString(30301),{'action':"listVideos",'link' : baseLink+"/category/user-submissions/rdubspotlight/"},"")
-    addDirectoryItem(addon.getLocalizedString(30302),{'action':"listVideos",'link' : baseLink+"/category/user-submissions/"},"")
+    addDirectoryItem(addon.getLocalizedString(30301),{'action':"listVideos",'link' : baseLink+"/category/usercontent/user-submission-of-the-week/"},"")
+    addDirectoryItem(addon.getLocalizedString(30302),{'action':"listVideos",'link' : baseLink+"/category/usercontent/featured-user-content/"},"")
+    addDirectoryItem(addon.getLocalizedString(30303),{'action':"listVideos",'link' : baseLink+"/category/usercontent/"},"")
 
 def listVideos(url):
 	link = LoadPage(url)

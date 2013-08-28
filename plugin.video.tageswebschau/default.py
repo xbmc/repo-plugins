36157dbfaadb5283c179c82be4d1ce7c6ed3db2b
beenje <beenje@xbmc.org>
2013-08-28 22:23:41 +0200

    [plugin.video.tageswebschau] updated to version 1.0.5

--- plugin.video.tageswebschau/default.py
+++ plugin.video.tageswebschau/default.py
@@ -9,44 +9,55 @@ import xbmcaddon
 import xbmcplugin
 
 pluginhandle = int(sys.argv[1])
+xbox = xbmc.getCondVisibility("System.Platform.xbox")
 
 
 def index():
-    content = getUrl("http://www.radiobremen.de/extranet/tws/json/tws_toc.json?_=1367168129910")
-    spl = content.split('{\n  	"titel" : ')
+    content = getUrl("http://wochenwebschau.tumblr.com/")
+    spl = content.split('<div class="post-panel"')
     for i in range(1, len(spl), 1):
         entry = spl[i]
-        match = re.compile('"(.+?)"', re.DOTALL).findall(entry)
+        match = re.compile('<div class="copy">(.+?)</div>', re.DOTALL).findall(entry)
         title = match[0]
+        match = re.compile('<strong>(.+?)</strong>', re.DOTALL).findall(title)
+        if match:
+            title = match[0]
+        title = title.replace("<p>", "").replace("</p>", "").replace("<span>", "").replace("</span>", "")
+        if ">" in title:
+            title = title[title.find(">")+1:]
+            if ">" in title:
+                title = title[title.find(">")+1:]
+        if title[len(title)-1:]==":":
+            title = title[:-1]
         title = cleanTitle(title)
-        match = re.compile('"img" : "(.+?)"', re.DOTALL).findall(entry)
-        thumb = match[0]
-        title = cleanTitle(title)
-        match = re.compile('"jsonurl" : "(.+?)"', re.DOTALL).findall(entry)
-        url = "http://www.radiobremen.de"+match[0]
-        addLink(title, url, 'playVideo', thumb)
+        match = re.compile('src="http://www.youtube.com/embed/(.+?)\\?', re.DOTALL).findall(entry)
+        id = match[0]
+        thumb = "http://img.youtube.com/vi/"+id+"/0.jpg"
+        addLink(title, id, 'playVideo', thumb)
     xbmcplugin.endOfDirectory(pluginhandle)
 
 
-def playVideo(url):
-    content = getUrl(url)
-    match1 = re.compile('"url" : "http://(.+?)_L.mp4"', re.DOTALL).findall(content)
-    match2 = re.compile('"url":"http://(.+?)_L.mp4"', re.DOTALL).findall(content)
-    if match1:
-        url = "http://"+match1[0]+"_L.mp4"
-    elif match2:
-        url = "http://"+match2[0]+"_L.mp4"
-    listitem = xbmcgui.ListItem(path=url)
+def playVideo(id):
+    listitem = xbmcgui.ListItem(path=getYoutubePluginUrl(id))
     xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 
 
+def getYoutubePluginUrl(id):
+    if xbox:
+        return "plugin://video/YouTube/?path=/root/video&action=play_video&videoid=" + id
+    else:
+        return "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid=" + id
+
+
 def cleanTitle(title):
-    return title.replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&").replace("&#039;", "\\").replace("&quot;", "\"").strip()
+    title = title = title.replace("&#039;", "'").replace("&#8211;", "-").replace("&#8220;", "-").replace("&#8221;", "-").replace("&#8217;", "'").replace("&#8230;", "…")
+    title = title.replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&").replace("&#039;", "\\").replace("&quot;", "\"").strip()
+    return title
 
 
 def getUrl(url):
     req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0')
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20100101 Firefox/23.0')
     response = urllib2.urlopen(req)
     link = response.read()
     response.close()

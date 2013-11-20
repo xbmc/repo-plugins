eb0d06beb6bc4dd1ac10056a51b90061b118a683
beenje <beenje@xbmc.org>
2013-11-20 20:34:12 +0100

    [plugin.video.borsentv.dk] updated to version 1.0.5

--- plugin.video.borsentv.dk/addon.py
+++ plugin.video.borsentv.dk/addon.py
@@ -1,5 +1,5 @@
 #
-#      Copyright (C) 2012 Tommy Winther
+#      Copyright (C) 2013 Tommy Winther
 #      http://tommy.winther.nu
 #
 #  This Program is free software; you can redistribute it and/or modify
@@ -26,26 +26,15 @@ import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
-BASE_URL = 'http://borsen.dk/tv/'
 FEEDS = {
-    30100 : 'rss://borsen.dk/podcast/alle',
-    30101 : 'rss://borsen.dk/podcast/investor',
-    30102 : 'rss://borsen.dk/podcast/investor1000',
-    30103 : 'rss://borsen.dk/podcast/karriere',
-    30104 : 'rss://borsen.dk/podcast/politik',
-    30105 : 'rss://borsen.dk/podcast/privatokonomi',
-    30106 : 'rss://borsen.dk/podcast/okonomi',
-    30107 : 'rss://borsen.dk/podcast/finans'
+    30100: 'rss://borsen.dk/podcast/alle',
+    30101: 'rss://borsen.dk/podcast/investor',
+    30103: 'rss://borsen.dk/podcast/karriere',
+    30104: 'rss://borsen.dk/podcast/politik',
+    30105: 'rss://borsen.dk/podcast/privatokonomi',
+    30106: 'rss://borsen.dk/podcast/okonomi',
+    30107: 'rss://borsen.dk/podcast/finans'
 }
-
-class BorsenTVAddon(object):
-    def showCategories(self):
-        for id in FEEDS.keys():
-            item = xbmcgui.ListItem(ADDON.getLocalizedString(id), iconImage = ICON)
-            item.setProperty('Fanart_Image', FANART)
-            xbmcplugin.addDirectoryItem(HANDLE, FEEDS[id], item, isFolder = True)
-
-        xbmcplugin.endOfDirectory(HANDLE)
 
 if __name__ == '__main__':
     ADDON = xbmcaddon.Addon()
@@ -55,7 +44,11 @@ if __name__ == '__main__':
 
     buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
     try:
-        btv = BorsenTVAddon()
-        btv.showCategories()
+        for stringId in FEEDS.keys():
+            item = xbmcgui.ListItem(ADDON.getLocalizedString(stringId), iconImage=ICON)
+            item.setProperty('Fanart_Image', FANART)
+            xbmcplugin.addDirectoryItem(HANDLE, FEEDS[stringId], item, isFolder=True)
+
+        xbmcplugin.endOfDirectory(HANDLE)
     except Exception:
         buggalo.onExceptionRaised()

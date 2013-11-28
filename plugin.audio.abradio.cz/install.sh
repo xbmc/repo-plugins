d2bc85c58fcdca6ef4bd2027f7644e9b87d7a8da
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-11-28 18:34:36 +0100

    repo cleanup: remove unwanted files

--- plugin.audio.abradio.cz/install.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-# do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.xbmc/addons/plugin.audio.abradio.cz
-
-rm -rf ${DESTDIR}
-mkdir -p ${DESTDIR}
-cp -a * ${DESTDIR}

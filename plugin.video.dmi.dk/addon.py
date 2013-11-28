66cf4d5e42051f621970e20af7be1b1ec2fc9047
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-11-28 21:10:15 +0100

    [plugin.video.dmi.dk] 2.0.1

--- plugin.video.dmi.dk/addon.py
+++ plugin.video.dmi.dk/addon.py
@@ -1,29 +1,52 @@
+#
+#      Copyright (C) 2013 Tommy Winther
+#      http://tommy.winther.nu
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with XBMC; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+#
 import os
 import sys
-import simplejson
 import urllib2
 
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
 
+import buggalo
+
+try:
+    import json
+except:
+    import simplejson as json
 
 BASE_URL = 'http://tv.dmi.dk%s'
 JSON_URL = BASE_URL % '/js/photos?raw&'
 
+
 def showOverview():
     u = urllib2.urlopen(JSON_URL)
-    json = u.read()
+    data = u.read()
     u.close()
 
-    clips = simplejson.loads(json)
+    clips = json.loads(data)
     for clip in clips['photos']:
-        print os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
-
-        item = xbmcgui.ListItem(clip['content_text'], iconImage = BASE_URL % clip['portrait_download'] + '.jpg')
-        item.setProperty('Fanart_Image', os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg'))
-        item.setInfo(type = 'video', infoLabels = {
-            'title' : clip['content_text']
+        item = xbmcgui.ListItem(clip['content_text'], iconImage=BASE_URL % clip['portrait_download'] + '.jpg')
+        item.setProperty('Fanart_Image', FANART_IMAGE)
+        item.setInfo(type='video', infoLabels={
+            'title': clip['content_text']
         })
         item.setProperty('Fanart_Image', BASE_URL % clip['large_download'])
 
@@ -33,10 +56,14 @@ def showOverview():
     xbmcplugin.endOfDirectory(HANDLE)
 
 
-
 if __name__ == '__main__':
-    ADDON = xbmcaddon.Addon(id = 'plugin.video.dmi.dk')
+    ADDON = xbmcaddon.Addon()
     PATH = sys.argv[0]
     HANDLE = int(sys.argv[1])
+    FANART_IMAGE = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
 
-    showOverview()
+    buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
+    try:
+        showOverview()
+    except Exception:
+        buggalo.onExceptionRaised()

81d32745594dcdd55efc7c5964829cdc7069f43b
beenje <beenje@xbmc.org>
2014-01-31 13:56:42 +0100

    [plugin.audio.abcradionational] initial version (0.0.3) thanks to Damon Toumbourou

---
+++ plugin.audio.abcradionational/resources/lib/abcradionational.py
@@ -0,0 +1,85 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+import re
+
+ABC_URL= "http://abc.net.au/radionational"
+
+def get_podcasts(url_id):
+    """
+    returns playable podcasts links from ABC website
+    """
+    url = ABC_URL + url_id
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+    urls = soup.findAll('a', 'ico-download')
+    titles = soup.findAll('h3', 'title')
+    output = []
+    for i in range(len(titles)):
+		url = urls[i]['href']
+		title = titles[i].text
+		output.append({'url': url, 'title': title})
+    return output
+
+
+def podcasts_get(url):
+    """
+    returns playable podcasts depending on arg
+    """
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+    urls = soup.findAll('a', 'ico-download')
+    titles = soup.findAll('h3', 'title')
+    output = []
+    for i in range(len(titles)):
+        try:
+            url = urls[i]['href']
+            title = titles[i].text
+            output.append({'url': url, 'title': title})
+        except IndexError:
+            pass
+
+    return output
+
+
+def get_programs(url_id):
+    """
+    returns program info from ABC website
+    """
+    url = ABC_URL + url_id
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+    urls = soup.findAll(href=re.compile("/radionational/programs/"))
+    programs = []
+    for i in range(len(urls)):
+        path = urls[i]['href']
+        path_final = "http://www.abc.net.au" + path
+        title = urls[i].text
+        programs.append({'url': path_final, 'title': title})
+        program_final = programs[40:131]
+    return program_final
+
+def get_subjects(url_id):
+    """
+    returns subject info from ABC website
+    """
+    url = ABC_URL + url_id
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+    urls = soup.findAll(href=re.compile("/radionational/subjects/"))
+    programs = []
+    for i in range(len(urls)):
+        path = urls[i]['href']
+        path_final = "http://www.abc.net.au" + path
+        title = urls[i].text
+        programs.append({'url': path_final, 'title': title})
+        sorted_programs = sorted(programs, key=lambda item: item['title'])
+        programs_final = programs[10:30]
+    return programs_final
+
+
+programs = get_subjects("/podcasts/subjects")
+sorted_programs = sorted(programs, key=lambda item: item['title'])
+
+for program in programs:
+    print program
+

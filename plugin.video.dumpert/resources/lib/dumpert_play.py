d1be3916a67d51095ed4bdc6fb03f2cf5555a12d
sphere <sphere@dersphere.de>
2014-04-15 08:15:19 +0200

    [plugin.video.dumpert] updated to version 1.0.3

--- plugin.video.dumpert/resources/lib/dumpert_play.py
+++ plugin.video.dumpert/resources/lib/dumpert_play.py
@@ -6,6 +6,8 @@ from dumpert_const import __addon__, __settings__, __language__, __images_path__
 from dumpert_utils import HTTPCommunicator
 import os
 import re
+import base64
+import ast
 import sys
 import urllib, urllib2
 import urlparse
@@ -24,6 +26,7 @@ class Main:
 	def __init__( self ) :
 		# Get plugin settings
 		self.DEBUG = __settings__.getSetting('debug')
+		self.VIDEO = __settings__.getSetting('video')
 		
 		if (self.DEBUG) == 'true':
 			xbmc.log( "[ADDON] %s v%s (%s) debug mode, %s = %s, %s = %s" % ( __addon__, __version__, __date__, "ARGV", repr(sys.argv), "File", str(__file__) ), xbmc.LOGNOTICE )
@@ -78,15 +81,53 @@ class Main:
 			exit(1)
 
 		soup = BeautifulSoup(html_data)
-		
-		#<div class="video" id="item1" data-vidurl="http://media.dumpert.nl/flv/70a1ae93_At_Princess_Juliana_International_Airport.mp4.flv" style="width: 480px; height: 272px;">
+
+		#<div class="videoplayer" id="video1" data-files="eyJmbHYiOiJodHRwOlwvXC9tZWRpYS5kdW1wZXJ0Lm5sXC9mbHZcLzI4OTE2NWRhXzEwMjU1NzUyXzYzODMxODA4OTU1NDc2MV84MTk0MzU3MDVfbi5tcDQuZmx2IiwidGFibGV0IjoiaHR0cDpcL1wvbWVkaWEuZHVtcGVydC5ubFwvdGFibGV0XC8yODkxNjVkYV8xMDI1NTc1Ml82MzgzMTgwODk1NTQ3NjFfODE5NDM1NzA1X24ubXA0Lm1wNCIsIm1vYmlsZSI6Imh0dHA6XC9cL21lZGlhLmR1bXBlcnQubmxcL21vYmlsZVwvMjg5MTY1ZGFfMTAyNTU3NTJfNjM4MzE4MDg5NTU0NzYxXzgxOTQzNTcwNV9uLm1wNC5tcDQiLCJzdGlsbCI6Imh0dHA6XC9cL3N0YXRpYy5kdW1wZXJ0Lm5sXC9zdGlsbHNcLzY1OTM1MjRfMjg5MTY1ZGEuanBnIn0="></div></div>	
 		video_urls = soup.findAll('div', attrs={'class': re.compile("video")}, limit=1)
 		if len(video_urls) == 0:
 			no_url_found = True
 		else:
-			video_url = video_urls[0]['data-vidurl']
+			video_url_enc = video_urls[0]['data-files']
+			#base64 decode
+			video_url_dec = str(base64.b64decode(video_url_enc))
+			#{"flv":"http:\/\/media.dumpert.nl\/flv\/5770e490_Jumbo_KOOP_DAN__Remix.avi.flv","tablet":"http:\/\/media.dumpert.nl\/tablet\/5770e490_Jumbo_KOOP_DAN__Remix.avi.mp4","mobile":"http:\/\/media.dumpert.nl\/mobile\/5770e490_Jumbo_KOOP_DAN__Remix.avi.mp4","720p":"http:\/\/media.dumpert.nl\/720p\/5770e490_Jumbo_KOOP_DAN__Remix.avi.mp4","still":"http:\/\/static.dumpert.nl\/stills\/6593503_5770e490.jpg"}
+			
+			if (self.DEBUG) == 'true':
+				xbmc.log( "[ADDON] %s v%s (%s) debug mode, %s = %s" % ( __addon__, __version__, __date__, "video_url_dec", str(video_url_dec) ), xbmc.LOGNOTICE )
+
+			#convert string to dictionary
+			video_url_dec_dict = ast.literal_eval(video_url_dec)
+			
+			#matching the desired and available quality
+			if self.VIDEO == '0':
+				try:
+					video_url = str(video_url_dec_dict['mobile'])
+				except KeyError:
+					no_url_found = True
+			elif self.VIDEO == '1':
+ 				try:
+				 	video_url =  str(video_url_dec_dict['tablet'])
+ 				except KeyError:
+ 					try: 
+ 					 	video_url = str(video_url_dec_dict['mobile'])
+ 					except KeyError:
+ 						no_url_found = True
+ 			elif self.VIDEO == '2':
+ 				try:
+				 	video_url = str(video_url_dec_dict['720p'])
+ 			 	except KeyError:
+ 			 		try:
+ 			 		 	video_url =  str(video_url_dec_dict['tablet'])
+ 			 		except KeyError:	
+ 			 			try:
+ 			 			 	video_url =  str(video_url_dec_dict['mobile'])			 	
+ 			 			except KeyError:
+ 			 			 	no_url_found = True
+ 			 			 	
+ 			video_url = video_url.replace('\/','/')
 			if (self.DEBUG) == 'true':
 				xbmc.log( "[ADDON] %s v%s (%s) debug mode, %s = %s" % ( __addon__, __version__, __date__, "video_url", str(video_url) ), xbmc.LOGNOTICE )
+
 			if httpCommunicator.exists( video_url ):
 				have_valid_url = True
 			else:

184ce0cac9acabd6197b3e37d108e203a451aeb7
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-07-22 20:18:36 +0200

    [cleanup] remove broken plugins

--- plugin.video.m6groupe/README.rst
+++
@@ -1,5 +0,0 @@
-=====================
-plugin.video.m6groupe
-=====================
-
-XBMC addon to watch videos from french channels M6 and W9
--- plugin.video.vimeo/vimeo/complete_hooks.py
+++
@@ -1,3 +0,0 @@
-def progress_bar(chunk_info):
-    completed = (chunk_info["chunk_id"] + 1) * chunk_info["chunk_size"]
-    print completed // chunk_info["total_size"] * 100, "%"

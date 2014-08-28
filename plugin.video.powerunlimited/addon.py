457d953158b2bd5a9dff3aab13ce2defc1311908
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-08-28 18:25:51 +0200

    [plugin.video.powerunlimited] 1.0.0

---
+++ plugin.video.powerunlimited/addon.py
@@ -0,0 +1,64 @@
+##############################################################################
+#
+# PowerUnlimited - Addon for XBMC
+# http://www.pu.nl/
+#
+# Coding by Skipmode A1
+# 
+# Credits:
+#   * Dan Dar3                                   - Gametrailers xbmc plugin [http://dandar3.blogspot.com]
+#   * PowerUnlimited                                                        [http://www.pu.nl]
+#   * Team XBMC @ XBMC.org                                                  [http://xbmc.org/]
+#   * Leonard Richardson <leonardr@segfault.org> - BeautifulSoup            [http://www.crummy.com/software/BeautifulSoup/]
+#
+
+# 
+# Constants
+#
+#also in ..._const
+__addon__       = "plugin.video.powerunlimited"
+__date__        = "28 august 2014"
+__version__     = "1.0.0"
+
+#
+# Imports
+#
+import os
+import re
+import sys
+import urllib, urllib2
+import urlparse
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+
+LIB_DIR = xbmc.translatePath( os.path.join( xbmcaddon.Addon(id=__addon__).getAddonInfo('path'), 'resources', 'lib' ) )
+sys.path.append (LIB_DIR)
+
+#
+# Get plugin settings
+DEBUG = xbmcaddon.Addon(id='plugin.video.powerunlimited').getSetting('debug')
+
+# Parse parameters...
+if len(sys.argv[2]) == 0:
+    #
+    # Main menu
+    #
+    if (DEBUG) == 'true':
+        xbmc.log( "[ADDON] %s v%s (%s) is starting, ARGV = %s" % ( __addon__, __version__, __date__, repr(sys.argv) ), xbmc.LOGNOTICE )
+    import powerunlimited_main as plugin
+else:
+    action = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['action'][0]
+    #
+    # List
+    #
+    if action == 'list':
+        import powerunlimited_list as plugin
+    #
+    # Play
+    #
+    elif action == 'play':
+        import powerunlimited_play as plugin
+
+plugin.Main()
---
+++ plugin.video.powerunlimited/resources/lib/powerunlimited_main.py
@@ -0,0 +1,53 @@
+#
+# Imports
+#
+from BeautifulSoup import BeautifulSoup
+from powerunlimited_const import __addon__, __settings__, __language__, __images_path__, __date__, __version__
+from powerunlimited_utils import HTTPCommunicator
+import os
+import re
+import sys
+import urllib, urllib2
+import urlparse
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+
+#
+# Main class
+#
+class Main:
+    def __init__( self ):
+        #
+        # Alles
+        #
+        parameters = {"action" : "list", "plugin_category" : __language__(30000), "url" : "http://www.pu.nl/media/?page=001", "next_page_possible": "True"}
+        url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+        listitem = xbmcgui.ListItem( __language__(30000), iconImage="DefaultFolder.png" )
+        folder = True
+        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        
+        #
+        # PU-TV
+        #
+        parameters = {"action" : "list", "plugin_category" : __language__(30001), "url" : "http://www.pu.nl/media/pu-tv/?page=001", "next_page_possible": "True"}
+        url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+        listitem = xbmcgui.ListItem( __language__(30001), iconImage="DefaultFolder.png" )
+        folder = True
+        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        
+        #
+        # Trailers
+        #
+        parameters = {"action" : "list", "plugin_category" : __language__(30002), "url" : "http://www.pu.nl/media/trailer/?page=001", "next_page_possible": "True"}
+        url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+        listitem = xbmcgui.ListItem( __language__(30002), iconImage="DefaultFolder.png" )
+        folder = True
+        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+                
+        # Disable sorting...
+        xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_NONE )
+		
+        # End of list...
+        xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )

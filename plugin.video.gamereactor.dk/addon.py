2fb4b6d5000b67fbaca3e5df30c010bb22fa0cdc
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-09-03 19:35:25 +0200

    [plugin.video.gamereactor.dk] 2.0.0

--- plugin.video.gamereactor.dk/addon.py
+++ plugin.video.gamereactor.dk/addon.py
@@ -1,3 +1,22 @@
+#
+#      Copyright (C) 2013 Tommy Winther
+#      http://tommy.winther.nu
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this Program; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+#
 import os
 import sys
 import urlparse
@@ -6,11 +25,13 @@ import urllib2
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
+import buggalo
 
 BASE_URL = 'http://www.gamereactor.dk'
 GRTV_URL = BASE_URL + '/grtv/'
 PLAYLIST_URL = GRTV_URL + 'embed/sliderlist.php?id='
 USER_AGENT = 'Mozilla/5.0 (compatible, XBMC addon)'
+
 
 class GRTVAddon(object):
     def showCategories(self):
@@ -19,21 +40,21 @@ class GRTVAddon(object):
             slug = m.group(1)
             title = m.group(2)
 
-            item = xbmcgui.ListItem(title, iconImage = ICON)
+            item = xbmcgui.ListItem(title, iconImage=ICON)
             item.setProperty('Fanart_Image', FANART)
             item.setInfo(type='video', infoLabels={
                 'title': title
-                })
+            })
 
             url = PATH + '?slug=' + slug
-            xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder = True)
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, isFolder=True)
 
         xbmcplugin.endOfDirectory(HANDLE)
 
     def showCategory(self, slug):
-        html = self._downloadUrl(GRTV_URL + 'list_20latest.php?l=' + slug)
-        for m in re.finditer('<a href="\?id=([0-9]+)"><img src="([^\"]+)\" alt="(.*?)".*?class="grtvdate">([0-9]+)-([0-9]+)-([0-9]+)<.*?class="descr">(.*?)<', html, re.DOTALL):
-            id = m.group(1)
+        html = self._downloadUrl(GRTV_URL + 'list_20latest.php?l=' + slug.replace(' ', '%20'))
+        for m in re.finditer('<a href="\?id=([0-9]+).*?url\(\'([^\']+)\'\).*?<h4>(.*?)</h4><span class="publishDate">([0-9]+)-([0-9]+)-([0-9]+)</span><p>(.*?</p>)', html, re.DOTALL):
+            clipId = m.group(1)
             icon = BASE_URL + m.group(2).replace('_w184.jpg', '_w926.jpg')
             title = m.group(3)
             year = m.group(4)
@@ -41,28 +62,28 @@ class GRTVAddon(object):
             date = m.group(6)
             description = m.group(7)
 
-            item = xbmcgui.ListItem(title, iconImage = icon, thumbnailImage = icon)
+            item = xbmcgui.ListItem(title, iconImage=icon, thumbnailImage=icon)
             item.setProperty('Fanart_Image', icon)
             item.setProperty('IsPlayable', 'true')
             item.setInfo(type='video', infoLabels={
                 'title': title,
-                'plot' : description,
-                'studio' : ADDON.getAddonInfo('name'),
-                'date' : '%s.%s.%s' % (date, month, year),
-                'aired' : '%s-%s-%s' % (date, month, year),
-                'year' : int(year)
+                'plot': description,
+                'studio': ADDON.getAddonInfo('name'),
+                'date': '%s.%s.%s' % (date, month, year),
+                'aired': '%s-%s-%s' % (date, month, year),
+                'year': int(year)
             })
 
-            url = PATH + '?id=' + id
+            url = PATH + '?id=' + clipId
             xbmcplugin.addDirectoryItem(HANDLE, url, item)
 
         xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
         xbmcplugin.endOfDirectory(HANDLE)
 
-    def playVideo(self, id):
-        xml = self._downloadUrl(PLAYLIST_URL + id)
+    def playVideo(self, clipId):
+        xml = self._downloadUrl(PLAYLIST_URL + clipId)
         m = re.search('<location>([^<]+)</location>', xml)
-        item = xbmcgui.ListItem(path = m.group(1))
+        item = xbmcgui.ListItem(path=m.group(1))
         xbmcplugin.setResolvedUrl(HANDLE, True, item)
 
     def _downloadUrl(self, url):
@@ -74,8 +95,9 @@ class GRTVAddon(object):
 
         return contents
 
+
 if __name__ == '__main__':
-    ADDON = xbmcaddon.Addon(id = 'plugin.video.gamereactor.dk')
+    ADDON = xbmcaddon.Addon()
     PATH = sys.argv[0]
     HANDLE = int(sys.argv[1])
     PARAMS = urlparse.parse_qs(sys.argv[2][1:])
@@ -83,10 +105,14 @@ if __name__ == '__main__':
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
     FANART = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
 
-    grtv = GRTVAddon()
-    if PARAMS.has_key('id'):
-        grtv.playVideo(PARAMS['id'][0])
-    elif PARAMS.has_key('slug'):
-        grtv.showCategory(PARAMS['slug'][0])
-    else:
-        grtv.showCategories()
+    buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
+    try:
+        grtv = GRTVAddon()
+        if 'id' in PARAMS:
+            grtv.playVideo(PARAMS['id'][0])
+        elif 'slug' in PARAMS:
+            grtv.showCategory(PARAMS['slug'][0])
+        else:
+            grtv.showCategories()
+    except Exception:
+        buggalo.onExceptionRaised()

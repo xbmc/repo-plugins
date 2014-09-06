d85966da29dcbc1e2650e536d5fad139fa022051
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-09-06 09:29:40 +0200

    [plugin.video.botchamania] 1.0.1

--- plugin.video.botchamania/resources/lib/botchamania_const.py
+++ plugin.video.botchamania/resources/lib/botchamania_const.py
@@ -8,5 +8,5 @@ __addon__       = "plugin.video.botchamania"
 __settings__    = xbmcaddon.Addon(id=__addon__ )
 __language__    = __settings__.getLocalizedString
 __images_path__ = os.path.join( xbmcaddon.Addon(id=__addon__).getAddonInfo('path'), 'resources', 'images' )
-__date__        = "13 august 2014"
-__version__     = "1.0.0"
+__date__        = "06 september 2014"
+__version__     = "1.0.1"

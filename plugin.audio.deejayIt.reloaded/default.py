05d81f047b52cbbeed4aceab90e15db632fc0710
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-01-04 08:39:24 +0100

    [plugin.audio.deejayIt.reloaded] 1.2.0

---
+++ plugin.audio.deejayIt.reloaded/default.py
@@ -0,0 +1,132 @@
+"""
+Main module of Kodi (XBMC) addon to manage deejay.it's Reloaded shows.
+
+This is the main module that takes care of receiving the information from the
+parser and present it in Kodi creating the appropriate pages and feeding the
+URL of the media content to the player.
+"""
+import sys
+import urllib
+import urlparse
+import resources.lib.deejay_it_parser as deejay
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+
+
+__settings__ = xbmcaddon.Addon(id='plugin.audio.deejayIt.reloaded')
+__language__ = __settings__.getLocalizedString
+
+
+BASE_URL = sys.argv[0]
+ADDON_HANDLE = int(sys.argv[1])
+ARGS = urlparse.parse_qs(sys.argv[2][1:])
+xbmcplugin.setContent(ADDON_HANDLE, 'episodes')
+xbmcplugin.addSortMethod(ADDON_HANDLE,
+    sortMethod=xbmcplugin.SORT_METHOD_UNSORTED)
+xbmcplugin.addSortMethod(ADDON_HANDLE,
+    sortMethod=xbmcplugin.SORT_METHOD_DATE)
+xbmcplugin.addSortMethod(ADDON_HANDLE,
+    sortMethod=xbmcplugin.SORT_METHOD_LABEL)
+xbmcplugin.addSortMethod(ADDON_HANDLE,
+    sortMethod=xbmcplugin.SORT_METHOD_PROGRAM_COUNT)
+
+
+def build_url(query):
+    """
+    Taken from http://kodi.wiki/view/Audio/video_add-on_tutorial
+    """
+    return BASE_URL + '?' + urllib.urlencode(query)
+
+
+MODE = ARGS.get('mode', None)
+
+if MODE is None:
+    try:
+        LISTA = deejay.get_reloaded_list()
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
+        xbmcgui.Dialog().ok(
+            __language__(30002),
+            __language__(30003),
+            str(e_urllib2.reason))
+    else:
+        for idx, prog in enumerate(LISTA):
+            url = build_url({'mode': 'epList',
+                             'progName': prog[0],
+                             'lastReloadedUrl': prog[2],
+                             'showThumb': prog[1]})
+            #showThumb, carried by LISTA, must be used in 'play' mode. It is
+            #thus propagated through the calls
+            li = xbmcgui.ListItem(prog[0], iconImage=prog[1])
+            li.setInfo('music', {'date': prog[3], 'count': idx})
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
+                                        url=url,
+                                        listitem=li,
+                                        isFolder=True)
+elif MODE[0] == 'epList':
+    PROG_NAME = ARGS['progName'][0]
+    LAST_RELOADED_URL = ARGS['lastReloadedUrl'][0]
+    SHOW_THUMB = ARGS['showThumb'][0]
+    FAN_ART = ARGS.get('fanArt')
+    try:
+        EPISODI, NEXTPAGE, IMG = deejay.get_episodi(url=LAST_RELOADED_URL,
+            oldimg=FAN_ART)
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
+        xbmcgui.Dialog().ok(
+            __language__(30002),
+            __language__(30004),
+            str(e_urllib2.reason))
+    else:
+        for idx, ep in enumerate(EPISODI):
+            #('http://www.deejay.it/audio/20071120-2/278354/',
+            #   '20071120',
+            #   'Puntata del 20 Novembre 2007')
+            URL = build_url({'mode': 'play',
+                             'epUrl': ep[0],
+                             'showThumb': SHOW_THUMB,
+                             'title': ep[2],
+                             'progName': PROG_NAME})
+            LI = xbmcgui.ListItem(ep[2],
+                                  iconImage='DefaultAudio.png')
+            LI.setProperty('IsPlayable', 'true')
+            #Setting fanArt
+            #not using setArt to keep Frodo's compatibility
+            #LI.setArt({'fanart' : img})
+            LI.setProperty('fanart_image', IMG)
+            LI.setInfo('music', {'date': ep[1], 'count': idx})
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
+                                        url=URL,
+                                        listitem=LI)
+        if NEXTPAGE:
+            #Questo aggiunge la prossima pagina
+            URL = build_url({'mode': 'epList',
+                             'progName': PROG_NAME,
+                             'lastReloadedUrl': NEXTPAGE,
+                             'showThumb': SHOW_THUMB,
+                             'fanArt': IMG})
+            LI = xbmcgui.ListItem('>>> '+__language__(30001)+' >>>')
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
+                                        url=URL,
+                                        listitem=LI,
+                                        isFolder=True)
+
+elif MODE[0] == 'play':
+    try:
+        URL = deejay.get_epfile(ARGS['epUrl'][0])
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
+        xbmcgui.Dialog().ok(
+            __language__(30002),
+            __language__(30005),
+            str(e_urllib2.reason))
+    else:
+        LI = xbmcgui.ListItem(path=URL)
+        LI.setThumbnailImage(ARGS['showThumb'][0])
+        LI.setInfo('music', {'title': ARGS['title'][0],
+                               'album': ARGS['progName'][0],
+                               'artist': 'Radio Deejay'})
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, LI)
+# e chiudiamo la lista per tutti i modi
+xbmcplugin.endOfDirectory(ADDON_HANDLE)
---
+++ plugin.audio.deejayIt.reloaded/resources/lib/deejay_it_parser.py
@@ -0,0 +1,252 @@
+"""
+Parser of Kodi (XBMC) addon to manage deejay.it's Reloaded shows.
+
+This is the parser module that takes care of reading Radio Deejay's website,
+extracting the necessary information and return it to the main module.
+"""
+import re
+import urllib2
+from lxml import etree as ET
+import datetime
+
+NOW = datetime.datetime.now()
+ANNO = NOW.year
+MESE = NOW.month
+GIORNO = NOW.day
+
+def month_to_num(date):
+    """
+    Translate the month name (string) to its corresponding number.
+    Strings are written only in Italian since this module is used to parse
+    Radio Deejay's website (www.deejay.it).
+    Input
+        The month name, epxressed in Italian, e.g. Dicembre
+    Output
+        The corresponding month number, e.g. 12
+    """
+    return{
+    'Gennaio' : 1,
+    'Febbraio' : 2,
+    'Marzo' : 3,
+    'Aprile' : 4,
+    'Maggio' : 5,
+    'Giugno' : 6,
+    'Luglio' : 7,
+    'Agosto' : 8,
+    'Settembre' : 9,
+    'Ottobre' : 10,
+    'Novembre' : 11,
+    'Dicembre' : 12
+    }[date]
+
+
+def translate_date(ep_title):
+    """
+    Translate the date in the episode's title and return it in the format used
+    by Kodi.
+    Input
+        ep_title is the episode's title, such as Puntata del 3 Gennaio 2014.
+        But it could also be Puntata del 3 Gennaio
+    Output
+        translated_date formatted as dd.mm.YYYY
+    Fallback, in case the title is not understood, is 19.12.1982. This should
+    be the first day on which a DJ, Gerry Scotty in this case, was speaking.
+    """
+    #ep_title is, normally, Puntata del 3 Gennaio 2014
+    hit = re.findall(r"(Puntata*)*([0-9]{1,2}) (\S*)\s*([0-9]{4})*",
+        ep_title,
+        re.MULTILINE)
+    if hit:
+        mese = month_to_num(hit[0][2])
+        giorno = hit[0][1]
+        if hit[0][3]:
+            anno = hit[0][3]
+        else:
+            anno = ANNO
+        data_ep = str(anno)+str(mese).rjust(2, '0')+giorno.rjust(2, '0')
+        #Sometimes the year is not given, this part checks whether the returned
+        #date is in the future and eventually adjusts it.
+        #This works under the hypotesis that the website never returns a date
+        #in the future
+        today = str(ANNO)+str(MESE).rjust(2, '0')+str(GIORNO).rjust(2, '0')
+        if data_ep > today:
+            data_ep = str(int(data_ep[0:4])-1) + data_ep[4:]
+        translated_date = data_ep[6:8]+'.'+data_ep[4:6]+'.'+data_ep[0:4]
+    else:
+        #19 dicembre 1982: primo intervento del Gerry Scotti speaker
+        translated_date = '19.12.1982'
+        print "no hit"
+    return translated_date
+
+
+def get_reloaded_list_in_page(url, reloaded_list):
+    """
+    Return all the available reloaded shows from a single webpage.
+    The list is appended to an array of tuples carrying:
+    (Program name,
+        Thumbnail URL,
+        Last episode,
+        Date)
+    Input
+        url of the webpage, e.g.:
+        http://www.deejay.it/schede-reloaded/page/2/?section=radio
+        reloaded_list, an array of tuples carrying the list of shows returned
+        by another parsing operation
+    Output
+        The above-mentioned array of tuples.
+    Single element example:
+    ('Deejay chiama Italia',
+        http://www.deejay.it/wp-content/uploads/2013/05/DJCI-150x150.jpg',
+        'http://www.deejay.it/audio/20141212-4/412626/',
+        '19.12.1982')
+    """
+    root = ET.parse(urllib2.urlopen(url), ET.HTMLParser()).getroot()
+    prog_list = root.xpath(".//ul[@class='block-grid four-up mobile-two-up']/"
+        "li")
+    for prog in prog_list:
+        prog_name_url = prog.xpath("./a")[0].attrib
+        reloaded_list.append(
+            (prog_name_url['title'],
+                prog.xpath("./a/img")[0].attrib['src'],
+                prog_name_url['href'],
+                translate_date(prog.xpath("./hgroup/span")[0].text))
+            )
+    nextpage = root.xpath(".//a[@class='nextpostslink']")
+    if not nextpage:
+        nextpageurl = ''
+    else:
+        nextpageurl = nextpage[0].attrib['href']
+
+    return reloaded_list, nextpageurl
+
+
+def get_reloaded_list():
+    """
+    Crawl over all the pages to return the complete list of reloaded shows.
+    This returns an array of tuples containing the following info for all the
+    reloaded shows:
+    (Program name,
+        Thumbnail URL,
+        Last episode,
+        Date)
+    Input:
+        None
+    Output
+        The above-mentioned array
+    E.g.:
+    ('Deejay chiama Italia',
+        'http://www.deejay.it/wp-content/uploads/2013/05/DJCI-150x150.jpg',
+        'http://www.deejay.it/audio/20141212-4/412626/',
+        '12.12.2014')
+    """
+    url = "http://www.deejay.it/reloaded/radio/"
+    #hardcoded url
+    lista, nextpageurl = get_reloaded_list_in_page(url, [])
+    while nextpageurl:
+        lista, nextpageurl = get_reloaded_list_in_page(nextpageurl, lista)
+    return lista
+
+
+def get_episodi(url, oldimg):
+    """
+    Return all the available episodes of the selected reloaed show. A single
+    webpage is parsed.
+    Input
+        url is the site's page that lists all the available episodes. E.g.:
+        i) http://www.deejay.it/audio/20141215-10/412901/ or
+        ii) http://www.deejay.it/audio/page/13/?reloaded=dee-giallo
+        oldimg is a string carrying the path of the picture to be used as
+        fanArt. This must be extracted from type i) pages and passed when
+        parsing type ii) pages from which you can't retrieve such info.
+    Output
+    lista_episodi an array of tuples carrying the episode's details:
+    (link to the webpage where you play the episode to be used by get_epfile(),
+        Date,
+        Episode's title)
+    nextpageurl is the URL of the next page listing the older episodes of the
+    show, if any.
+    img, that is the fanArt URL to be used for every episode of the Reloaded.
+    """
+    root = ET.parse(urllib2.urlopen(url), ET.HTMLParser()).getroot()
+    #If the fanArt URL is already know there is no need to re-extract it since
+    #it is a show-wise property and not episode-specific.
+    if oldimg is not None:
+        img = oldimg[0]
+    else:
+        snippet = root.xpath(".//"
+            "article[@class='twelve columns video player audio']/script")
+        if snippet:
+            new_img = re.findall(".*addParam.*'param', "
+            "'image', '(http://www.deejay.it/.*)'.*",
+                snippet[0].text,
+                re.MULTILINE)
+            if new_img:
+                img = new_img[0]
+        else:
+            img = ''
+
+    new_url = root.xpath(".//span[@class='small-title']/a")
+    # This is as the user pressed on Archivio+
+    if new_url:
+        root = ET.parse(urllib2.urlopen(new_url[0].attrib['href']),
+            ET.HTMLParser()).getroot()
+    lista_episodi = []
+    episodi = root.xpath(".//ul[@class='lista']/li/a")
+
+    if episodi:
+        for episodio in episodi:
+            lista_episodi.append(
+                (
+                    episodio.attrib['href'],
+                    translate_date(episodio.attrib['title']),
+                    episodio.attrib['title'])
+                )
+
+    #Passo finale: aggiungi il link alla pagina successiva
+    nextpage = root.xpath(".//a[@class='nextpostslink']")
+    if not nextpage:
+        nextpageurl = ''
+    else:
+        nextpageurl = nextpage[0].attrib['href']
+    return lista_episodi, nextpageurl, img
+
+def get_epfile(url):
+    """
+    Return the file (mp3) URL to be read from the website to play the selected
+    reloaded episode.
+    Input
+        the webpage URL of the episode to be played.
+        E.g.: http://www.deejay.it/audio/20130526-4/269989/
+    Output
+        the URL of the mp3 (rarely a wma) file to be played to listen to the
+        selected episode. E.g.:
+        http://flv.kataweb.it/deejay/audio/dee_giallo/deegiallolosmemoratodicollegno.mp3
+        Returns an empty string if the file cannot be found.
+    """
+    root = ET.parse(urllib2.urlopen(url), ET.HTMLParser()).getroot()
+    fileurl = root.xpath(".//div[@id='playerCont']/p")
+    if not fileurl:
+        return ''
+    else:
+        return fileurl[0].text
+
+
+#    ---------------------------------------------------
+#PROGRAMMI = get_reloaded_list()
+#for p in PROGRAMMI:
+#    print p
+
+#p = PROGRAMMI[17][2]
+#print p
+
+#eps = get_episodi('http://www.deejay.it/audio/page/13/?reloaded=dee-giallo','')
+#eps = get_episodi('http://www.deejay.it/audio/20141215-10/412901/','')
+#eps = get_episodi('http://www.deejay.it/audio/20141223-2/414155/', 'pippo')
+#for e in eps:
+#    print e
+
+#fileurl = get_epfile('http://www.deejay.it/audio/20130527-3/269977/')
+#print fileurl
+
+#dataAstrale = translate_date('15 Dicembre')
+#print dataAstrale

61a6beff9bcde6b2be388e0c6ec206f2415274f3
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-02-08 15:57:40 +0100

    [plugin.video.jupiterbroadcasting] 3.2.1

--- plugin.video.jupiterbroadcasting/default.py
+++ plugin.video.jupiterbroadcasting/default.py
@@ -237,15 +237,19 @@ def categories():
     # List all the shows.
     shows = get_shows()
     quality = int(__settings__.getSetting("video_quality"))
-    scale = 'jblive.videocdn.scaleengine.net/'
 
-    # Jupiter Broadcasting Live via the HLS/RTMP stream
-    live_url = 'http://' + scale
-    live_url += 'jblive-iphone/live/jblive.stream/playlist.m3u8'
-    if quality == 1:
-        live_url = 'rtsp://' + scale + 'jblive/live/jblive.stream'
-    elif quality == 2:
+    # Add the Live Stream
+    livestream = int(__settings__.getSetting("live_stream"))
+    live_url = ''
+    if livestream == 0: # RTSP
+        live_url = 'rtsp://jblive.videocdn.scaleengine.net/jb-live/play/jblive.stream'
+    if livestream == 1: # RTMP
+        live_url = 'rtmp://jblive.videocdn.scaleengine.net/jb-live/play/jblive.stream'
+    elif livestream == 2: # HLS
+        live_url = 'http://jblive.videocdn.scaleengine.net/jb-live/play/jblive.stream/playlist.m3u8'
+    elif livestream == 3: # Audio
         live_url = 'http://jblive.fm'
+
     add_link(
         name=__language__(30010),
         url=live_url,

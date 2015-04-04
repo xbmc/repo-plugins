300f5ae083a3f42fb0d95658f5e299848dc76492
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-04-04 08:10:40 +0200

    [plugin.image.ilpiolatracker] 1.1.0

--- plugin.image.ilpiolatracker/addon.py
+++ plugin.image.ilpiolatracker/addon.py
@@ -8,17 +8,19 @@ import urllib2
 
 addon = xbmcaddon.Addon(id='plugin.image.ilpiolatracker')
 thisPlugin = int(sys.argv [1])
+xbmcplugin.setContent(thisPlugin, 'images')
 if (addon.getSetting('readkey')==''):
     dialog = xbmcgui.Dialog()
     dialog.ok(addon.getLocalizedString(id=30000 ), addon.getLocalizedString(id=30005))
 else:
-    u1='http://m.ilpiola.it/t/q.php?f=g&k='+addon.getSetting('readkey')
+    u1='http://m.ilpiola.it/t2/'+addon.getSetting('readkey') + '/g/'
     i=1
     while i <= 20:
-        u=u1+'&z='+str(i)
+        u=u1+str(i)+'.png'
         iconimage=os.path.join(addon.getAddonInfo('path'),"resources","media","zoom"+str(i)+".png")
         liz=xbmcgui.ListItem(unicode('Zoom='+str(i)), iconImage=iconimage,thumbnailImage=iconimage)
         liz.setInfo( type="Image", infoLabels={ "Title": 'Zoom='+str(i) })
         xbmcplugin.addDirectoryItem(handle=thisPlugin,url=u,listitem=liz,isFolder=False)
         i=i+1
-    xbmcplugin.endOfDirectory(handle=thisPlugin, succeeded=True, updateListing=False, cacheToDisc=True)
+    xbmcplugin.endOfDirectory(handle=thisPlugin, succeeded=True, updateListing=False, cacheToDisc=False)
+

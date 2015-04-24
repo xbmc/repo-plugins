1e926b3bd1949e11baa6630628ee0fe074c19e2f
bromix <bromix@gmx.net>
2015-04-24 21:40:13 +0200

    [plugin.video.break_com] 3.1.0

--- plugin.video.break_com/resources/lib/break_api/provider.py
+++ plugin.video.break_com/resources/lib/break_api/provider.py
@@ -1,6 +1,6 @@
 from resources.lib.break_api import Client
 
-from resources.lib.kodion.items import DirectoryItem, VideoItem, UriItem
+from resources.lib.kodion.items import DirectoryItem, VideoItem, UriItem, NextPageItem
 from resources.lib.kodion.utils import FunctionCache
 from resources.lib.kodion import constants
 
@@ -27,8 +27,10 @@ class Provider(kodion.AbstractProvider):
 
         data = json_data['data']['data']
         for item in data:
-            video_item = VideoItem(item['title'],
-                                   context.create_uri(['play', str(item['id'])]))
+            title = item['title']
+            video_id = str(item['id'])
+            video_item = VideoItem(title,
+                                   context.create_uri(['play'], {'video_id': video_id}))
             video_item.set_fanart(self.get_fanart(context))
             video_item.set_plot(item['description'])
             image = item.get('thumbnails', [])
@@ -41,13 +43,13 @@ class Provider(kodion.AbstractProvider):
 
         return result
 
-    @kodion.RegisterProviderPath('^/play/(?P<video_id>.*)/$')
+    @kodion.RegisterProviderPath('^/play/$')
     def _on_play(self, context, re_match):
         def _compare(item):
             vq = context.get_settings().get_video_quality()
             return vq - item['height']
 
-        video_id = re_match.group('video_id')
+        video_id = context.get_param('video_id')
         video_urls = self._client.get_video_urls(video_id)
         video_url = kodion.utils.find_best_fit(video_urls, _compare)
         uri_item = UriItem(video_url['url'])
@@ -78,21 +80,76 @@ class Provider(kodion.AbstractProvider):
 
         return result
 
+    def on_search(self, search_text, context, re_match):
+        context.set_content_type(kodion.constants.content_type.EPISODES)
+
+        result = []
+
+        import xml.etree.ElementTree as ET
+        page = int(context.get_param('page', '1'))
+        xml = self._client.search(search_text, page=page)
+        root = ET.fromstring(xml)
+        search_results = root.find('SearchResults')
+        if search_results is not None:
+            for search_result in search_results:
+                title = search_result.find('Title').text
+                video_id = search_result.find('ID').text
+                video_item = VideoItem(title,
+                                       context.create_uri(['play'], {'video_id': video_id}))
+                video_item.set_fanart(self.get_fanart(context))
+
+                description = search_result.find('Description')
+                if description is not None:
+                    video_item.set_plot(description.text)
+                    pass
+
+                image = search_result.find('Thumbnail')
+                if image is not None:
+                    video_item.set_image(image.text)
+                    pass
+                result.append(video_item)
+                pass
+            pass
+
+        # next page
+        total_pages = root.find('TotalPages')
+        if total_pages is not None:
+            total_pages = int(total_pages.text)
+            if page < total_pages:
+                next_page_item = NextPageItem(context, page)
+                next_page_item.set_fanart(self.get_fanart(context))
+                result.append(next_page_item)
+                pass
+            pass
+
+        return result
+
     def on_root(self, context, re_match):
         result = []
 
         json_data = context.get_function_cache().get(FunctionCache.ONE_DAY, self._client.get_home)
         collection = json_data['data']['data']['collection']
+        make_bold = True
         for item in collection:
             title = item['name']
             if title != u'Galleries':
                 item_id = str(item['id'])
+                if make_bold:
+                    title = '[B]%s[/B]' % title
+                    make_bold = False
+                    pass
                 feed_item = DirectoryItem(title,
                                           context.create_uri(['feed', item_id]))
                 feed_item.set_fanart(self.get_fanart(context))
                 result.append(feed_item)
                 pass
             pass
+
+        # search
+        search_item = kodion.items.SearchItem(context, image=context.create_resource_path('media', 'search.png'),
+                                              fanart=self.get_fanart(context))
+        search_item.set_name('[B]%s[/B]' % search_item.get_name())
+        result.append(search_item)
 
         return result
 

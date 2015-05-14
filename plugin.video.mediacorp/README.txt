038a9f54d21c505fb9e36492a16df89376d35e75
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-05-14 14:00:32 +0200

    [plugin.video.mediacorp] 1.4.3

--- plugin.video.mediacorp/README.txt
+++ plugin.video.mediacorp/README.txt
@@ -1,28 +1,10 @@
-plugin.video.mediacorp
+plugin.video.mcorp
 ================
 
-Version 1.3.0 Added proxy support for Channel 8, bug fixes
 
-v0.1.0
-XBMC Addon for Mediacorp TV Singapore
+Kodi Addon for Mediacorp (toggle.sg)
 
-Plays Tv catch-up for channels 5,8,Channel U, Suria, Okto and Vasantham. To display chinese titles correctly, choose Arial Font under Settings/Appearance/Skin/Font
+Version 1.4.3 Added all channels tv-catchup
+Version 1.4.2 Fix for Telemovies
+Version 1.4.1 initial toggle.sg release
 
-v0.1.1
-
-Removed UNSORTED View to allow other sorts to be viewed
-Set mimetype in addurl - supposed to speed things up
-
-v0.2.0
-version 0.2.0 added required fields in addon.xml, translatable strings separated
-
-v0.2.2
-minor change to addon.xml
-
-Version 1.0.2 rewrite for web site changes
-
-Version 1.0.3 cleanup of folder name and settings xml
-
-Version 1.1.4 significant website change
-
-Version 1.1.5 Added Brightcove support
--- plugin.video.mediacorp/default.py
+++ plugin.video.mediacorp/default.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
-# Mediacorp Broadcove
+# Kodi Mediacorp (toggle.sg) Addon
 
+import sys
+import httplib
 
-import urllib
-import urllib2
-import cookielib
-import datetime
-import time
-import re
-import os
-import xbmcplugin
-import xbmcgui
-import xbmcaddon
-import xbmcvfs
-import cgi
-import httplib, sys
-from StringIO import StringIO
+import urllib, urllib2, cookielib, datetime, time, re, os, string
+import xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, xbmc
 import cgi, gzip
+from StringIO import StringIO
+import json
 
 
 UTF8          = 'utf-8'
@@ -25,30 +17,17 @@ addon         = xbmcaddon.Addon('plugin.video.mediacorp')
 __addonname__ = addon.getAddonInfo('name')
 __language__  = addon.getLocalizedString
 
-home          = addon.getAddonInfo('path').decode('utf-8')
-media         = xbmc.translatePath(os.path.join(home, 'resources', 'media'))
-icon_next     = xbmc.translatePath(os.path.join(media, 'next.png'))
-icon_channel5  = xbmc.translatePath(os.path.join(media, 'icon_channel5.png'))
-icon_channel8  = xbmc.translatePath(os.path.join(media, 'icon_channel8.png'))
-icon_channelu  = xbmc.translatePath(os.path.join(media, 'icon_channelu.png'))
-icon_okto      = xbmc.translatePath(os.path.join(media, 'icon_okto.png'))
-icon_suria     = xbmc.translatePath(os.path.join(media, 'icon_suria.png'))
-icon_vasantham = xbmc.translatePath(os.path.join(media, 'icon_vasantham.png'))
+qp  = urllib.quote_plus
+uqp = urllib.unquote_plus
 
+home          = addon.getAddonInfo('path').decode(UTF8)
 icon          = xbmc.translatePath(os.path.join(home, 'icon.png'))
 addonfanart   = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
 
 
-qp  = urllib.quote_plus
-uqp = urllib.unquote_plus
-
 def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
-
-def cleanname(name):    
-    return name.replace('&#39;',"'").replace('&amp;','&')
-
 
 def demunge(munge):
         try:
@@ -57,13 +36,21 @@ def demunge(munge):
             pass
         return munge
 
+def deuni(a):
+    a = a.replace('&amp;#039;',"'")
+    a = a.replace('&amp','&')
+    a = a.replace('&#039;',"'")
+    return a
+
+
+
 USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36'
 defaultHeaders = {'User-Agent':USER_AGENT, 
                  'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
                  'Accept-Encoding':'gzip,deflate,sdch',
                  'Accept-Language':'en-US,en;q=0.8'} 
 
-def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, donotuseproxy=False):
+def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, donotuseproxy=True):
 
               log("getRequest URL:"+str(url))
               if (donotuseproxy==False) and (addon.getSetting('us_proxy_enable') == 'true'):
@@ -105,113 +92,101 @@ def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, donot
               return(link1)
 
 
-
-def getSources():
-              dolist = [('channel5', 30002, icon_channel5),('channel8', 30003, icon_channel8),('channelu', 30004, icon_channelu), 
-                        ('okto', 30007, icon_okto), ('suria', 30008, icon_suria), ('vasantham', 30009, icon_vasantham)]
-
-              for url, gstr, img in dolist:
-                  name = __language__(gstr)
-                  liz  = xbmcgui.ListItem(name,'',img,img)
-                  liz.setProperty( "Fanart_Image", addonfanart )
-                  xbmcplugin.addDirectoryItem(int(sys.argv[1]), '%s?url=%s&name=%s&iconimage=%s&mode=%s' % (sys.argv[0],qp(url),qp(name), qp(img), 'GC'), liz, True)
-
-def getChannel(url,iconimage,channame):
-     iconimage = uqp(iconimage)
-     channame  = uqp(channame)
-     html  = getRequest('http://xin.msn.com/en-sg/video/catchup/', donotuseproxy=True)
-     blob  = re.compile('class="section tabsection horizontal".+?data-section-id="%s"(.+?)</ul>' % (url)).search(html).group(1)
-     blobs = re.compile('<li tabindex="0" data-tabid="(.+?)".+?>(.+?)</li>').findall(blob)
-     ilist = []
-     for curl, name in blobs:
-         urx = '%s#%s' % (url, curl)
-         name = cleanname(name).strip()
-         plot = name
-         u    = "%s?url=%s&name=%s&channame=%s&mode=%s" % (sys.argv[0], urx, qp(name), qp(channame), 'GS')
-         liz=xbmcgui.ListItem(name, '','DefaultFolder.png', iconimage)
-         liz.setProperty( "Fanart_Image", addonfanart )
-         liz.setInfo( 'Video', { "Studio": channame, "Title": name, "Plot": plot })
-         ilist.append((u, liz, True))
-     xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+def getSources(fanart):
+              ilist=[]
+              html = getRequest('http://video.toggle.sg/en/tvshows/mediacorpcollection')
+              cu   = re.compile('<h5 class="collapsible__title">.+?href="(.+?)">(.+?)<').findall(html)
+              html = getRequest('http://video.toggle.sg/en/catchup-listing')
+              cats = re.compile('<h5 class="collapsible__title">.+?href="(.+?)">(.+?)<').findall(html)
+              cats.extend(cu)
+              for url, name in cats:
+                  name = name.strip()
+                  plot = name
+                  mode = 'GS'
+                  u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
+                  liz=xbmcgui.ListItem(name, '',icon, icon)
+                  liz.setInfo( 'Video', { "Title": name, "Plot": plot })
+                  liz.setProperty('fanart_image', addonfanart)
+                  ilist.append((u, liz, True))
+              xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
 
 
-def getShows(url, channame):
- channame = uqp(channame)
- try:
-  url, caturl = url.split('#')
-  html = getRequest('http://xin.msn.com/en-sg/video/catchup/', donotuseproxy=True)
-  blob  = re.compile('class="section tabsection horizontal".+?data-section-id="%s".+?<div data-tabkey="%s"(.+?)</ul>' % (url, caturl)).search(html).group(1)
-  blobs = re.compile('<li.+?href="(.+?)".+?:&quot;(.+?)&quot.+?<h4>(.+?)</h4>.+?"duration">(.+?)<.+?</li>').findall(blob)
+def getShows(cat_url):
+              ilist=[] 
+              cat_url = uqp(cat_url)       
+              html = getRequest(cat_url)
+              cats = re.compile('<div class="tg-teaser-item">.+?href="(.+?)".+?src="(.+?)".+?<h6 class.+?href=.+?">(.+?)<.+?</div').findall(html)
+              for url, img, name in cats:
+                 if '/movies/' in url: mode = 'GV'
+                 else: mode = 'GE'
+                 name = name.strip()
+                 plot = name
+                 u = '%s?url=%s&mode=%s&name=%s' % (sys.argv[0],qp(url), mode, qp(name))
+                 liz=xbmcgui.ListItem(name, '',img, img)
+                 liz.setInfo( 'Video', { "Title": name, "Plot": plot })
+                 liz.setProperty('fanart_image', addonfanart)
+                 if '/movies' in url:
+                    liz.setProperty('IsPlayable', 'true')
+                    ilist.append((u, liz, False))
+                 else:
+                    ilist.append((u, liz, True))
+              xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+                 
 
-  ilist = []
-  for url, img, name, dur in blobs:
-     url  = 'http://xin.msn.com'+url
-     dur  = dur.strip()
-     name = cleanname(name).strip()
-     plot = name
-     img  = 'http:'+img.replace('&amp;','&')
-     u    = "%s?url=%s&name=%s&mode=%s" % (sys.argv[0],qp(url), qp(name), 'GV')
-     liz=xbmcgui.ListItem(name, '',img, img)
-     liz.setProperty( "Fanart_Image", addonfanart )
-     liz.setInfo( 'Video', { "Studio": channame, "Title": name, "Plot": plot })
-     liz.setProperty('IsPlayable', 'true')
-     ilist.append((u, liz, False))
-  xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
-
- except:
-     return
-
-
-def getVideo(url):
-  retry = 2
-  dontuseproxy = True
-  while (retry > 0):
-    html = getRequest(url, donotuseproxy = dontuseproxy)
-    html=html.replace('\r','').replace("&#39;","'").replace('&quot;','"')
-    try:
-       vidurl = re.compile('"formatCode":"103","url":"(.+?)"').search(html).group(1)
-       retry = 0
-    except:
-       try:
-          vidurl = re.compile('"formatCode":"101","url":"(.+?)"').search(html).group(1)
-          retry = 0
-
-       except:
-          try:
-            videoId = re.compile('"providerId":"(.+?)"').search(html).group(1)
-            vidurl = getBrightCoveUrl(videoId)
-            retry = 0
-
-          except:
-            retry -= 1
-            if (retry != 0) and (addon.getSetting('us_proxy_enable') == 'true'):
-               dontuseproxy = False
-               continue
-            else:
-               dialog = xbmcgui.Dialog()
-               dialog.ok(__language__(30000), '',__language__(30001))
-               return
-
-    vidurl = vidurl.encode(UTF8)
-    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=vidurl))
+def getEpis(epurl, showname):
+            xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
+            ilist=[] 
+            epurl = uqp(epurl)
+            html = getRequest(epurl)
+            epis = re.compile('<li class="fixed-height__item.+?href="(.+?)".+?data-src="(.+?)".+?href=.+?">(.+?)<.+?</div').findall(html)
+            for url, img, name in epis:
+                 if not (img.startswith('http')): img = 'http://video.toggle.sg'+img
+                 mode = 'GV'
+                 name = name.strip()
+                 plot = name
+                 u = '%s?url=%s&mode=%s' % (sys.argv[0],qp(url), mode)
+                 liz=xbmcgui.ListItem(name, '',img, img)
+                 liz.setInfo( 'Video', { "Title": name, "Studio":showname, "Plot": showname })
+                 liz.setProperty('fanart_image', addonfanart)
+                 liz.setProperty('IsPlayable', 'true')
+                 ilist.append((u, liz, False))
+            xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
 
 
-def getBrightCoveUrl(video_content_id):
-      html = getRequest('http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=%s&pubId=1678873430001' % (video_content_id))
-      html = html+'#'
-      vids = re.compile('BANDWIDTH=([0-9]*).+?http(.+?)#').findall(html)
-      b = 0
-      furl = ''
-      if (addon.getSetting('vid_res') == "1"):
-        for bwidth, url in vids:
-          if (int(bwidth) > b):
-              b = int(bwidth)
-              furl = url
-      else: furl = vids[0][1]
 
-      if (furl == ''): raise Exception('No Video')
-      return 'http'+furl
+def getVid(vurl):
+            vurl = uqp(vurl)
+            vid = vurl.rsplit('/',1)[1]
+            url = 'http://toggleplayer-1410100339.ap-southeast-1.elb.amazonaws.com/v0.30/mwEmbed/mwEmbedFrame.php?&wid=_27017&uiconf_id=8413350&entry_id='+vid+'&flashvars[ks]=0&flashvars[logo]=undefined&flashvars[toggle.sgPlus]=false&flashvars[vast]=%7B%22htmlCompanions%22%3A%22video-companion-ad-320-100-in-flash%3A320%3A100%22%7D&flashvars[multiDrm]=%7B%22plugin%22%3Atrue%2C%22isClear%22%3Atrue%7D&flashvars[localizationCode]=en&flashvars[autoPlay]=true&flashvars[proxyData]=%7B%22initObj%22%3A%7B%22Locale%22%3A%7B%22LocaleLanguage%22%3A%22%22%2C%22LocaleCountry%22%3A%22%22%2C%22LocaleDevice%22%3A%22%22%2C%22LocaleUserState%22%3A0%7D%2C%22Platform%22%3A0%2C%22SiteGuid%22%3A0%2C%22DomainID%22%3A%220%22%2C%22UDID%22%3A%22%22%2C%22ApiUser%22%3A%22tvpapi_147%22%2C%22ApiPass%22%3A%2211111%22%7D%2C%22MediaID%22%3A%22'+vid+'%22%2C%22iMediaID%22%3A%22'+vid+'%22%2C%22picSize%22%3A%22640X360%22%7D&playerId=SilverlightContainer&forceMobileHTML5=true&urid=2.29.1.10&callback='
+            html = getRequest(url)
+            html = re.compile('kalturaIframePackageData = (.+?)};').search(html).group(1)
+            html = html+'}'
+            html = html.replace('\\','')
+            a = json.loads(html)
+            a = a['entryResult']['meta']['partnerData']['Files']
+            u =''
+            if addon.getSetting('vid_res') == '1':
+              for b in a:
+                vtype = 'STB Main'
+                if b['Format'] == vtype:
+                  u = b['URL']
+                  break
+              req = urllib2.Request(u, None, defaultHeaders)
+              try:
+                response = urllib2.urlopen(req, timeout=40)
+              except:
+                u = ''
+            if u == '':
+                for b in a:
+                  vtype = 'Android'
+                  if b['Format'] == vtype:
+                    u = b['URL']
+                    break
+            if ( u == '' or u.endswith('.wvm')):
+                 u=''
+                 xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, __language__(30001) , 10000) )
 
+            xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = u))
 
 
 # MAIN EVENT PROCESSING STARTS HERE
@@ -228,12 +203,12 @@ except:
 
 p = parms.get
 
-mode = p('mode', None)
+mode = p('mode',None)
 
-if mode==  None:  getSources()
-elif mode=='GV':  getVideo(p('url'))
-elif mode=='GS':  getShows(p('url'),p('channame'))
-elif mode=='GC':  getChannel(p('url'),p('iconimage'),p('name'))
+if mode==  None:  getSources(p('fanart'))
+elif mode=='GS':  getShows(p('url'))
+elif mode=='GE':  getEpis(p('url'), p('name'))
+elif mode=='GV':  getVid(p('url'))
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
 

55fe6d9c5c93be39945152fd662ab1525731f4c5
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-05-18 20:57:18 +0200

    [plugin.video.tweakers] 1.0.4

--- plugin.video.tweakers/resources/lib/tweakers_utils.py
+++ plugin.video.tweakers/resources/lib/tweakers_utils.py
@@ -10,24 +10,24 @@ import urlparse
 #
 #
 class HTTPCommunicator :
-    # 
+    #
     # POST
     #
     def post( self, host, url, params ):
         parameters  = urllib.urlencode( params )
-        headers     = { "Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Accept-Encoding" : "gzip" }
+        headers     = { "Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Accept-Encoding" : "gzip", "Cookie": "TnetID=1lmx3gb83nsK7j82d8f_OJp0uH7sjsE4" }
         connection  = httplib.HTTPConnection("%s:80" % host)
         
         connection.request( "POST", url, parameters, headers )
         response = connection.getresponse()
         
-        # Compressed (gzip) response
+        # Compressed (gzip) response...
         if response.getheader( "content-encoding" ) == "gzip" :
             htmlGzippedData = response.read()
             stringIO       = StringIO.StringIO( htmlGzippedData )
             gzipper        = gzip.GzipFile( fileobj = stringIO )
             htmlData       = gzipper.read()
-        # Plain text response
+        # Plain text response...
         else :
             htmlData = response.read()
 
@@ -41,13 +41,15 @@ class HTTPCommunicator :
     # GET
     #
     def get( self, url ):
-        h = urllib2.HTTPHandler(debuglevel=0)
-        #use a mobile device browser header, so we get the mobile version of the site        
-        headers = { "Accept-Encoding": "gzip", "User-agent": "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36" }
-        request = urllib2.Request( url, None, headers ) 
-        opener = urllib2.build_opener(h)
-        f = opener.open(request)
-
+        user_agent = "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36"
+        values = {}
+        headers = { 'User-Agent'      : user_agent , 
+                    'Accept-Encoding' : 'gzip',
+                    "Cookie"          : 'TnetID=1lmx3gb83nsK7j82d8f_OJp0uH7sjsE4' }
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data, headers)
+        f = urllib2.urlopen(req)
+        
         # Compressed (gzip) response
         if f.headers.get( "content-encoding" ) == "gzip" :
             htmlGzippedData = f.read()

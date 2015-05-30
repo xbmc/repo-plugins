7aae32ba403fee077ed34be6e610a7789a6ed58e
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-05-30 09:07:42 +0200

    [plugin.video.travel] 1.0.3

--- plugin.video.travel/README.txt
+++ plugin.video.travel/README.txt
@@ -3,6 +3,7 @@ plugin.video.travel
 
 XBMC Addon for Travel Network Video website
 
+Version 1.0.3 Added subtitles, metadata, views
 Version 1.0.2 website change
 Version 1.0.1 initial release
 
--- plugin.video.travel/default.py
+++ plugin.video.travel/default.py
@@ -1,45 +1,28 @@
 # -*- coding: utf-8 -*-
 # Travel Channel Kodi Addon
 
-import sys
-import httplib, socket
-
+import sys,httplib
 import urllib, urllib2, cookielib, datetime, time, re, os, string
 import xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, xbmc
-import cgi, gzip
-from StringIO import StringIO
-import json
+import zlib,json,HTMLParser
+h = HTMLParser.HTMLParser()
+qp  = urllib.quote_plus
+uqp = urllib.unquote_plus
 
-
-UTF8          = 'utf-8'
+UTF8     = 'utf-8'
 
 addon         = xbmcaddon.Addon('plugin.video.travel')
 __addonname__ = addon.getAddonInfo('name')
 __language__  = addon.getLocalizedString
-
 
 home          = addon.getAddonInfo('path').decode(UTF8)
 icon          = xbmc.translatePath(os.path.join(home, 'icon.png'))
 addonfanart   = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
 
 
-qp  = urllib.quote_plus
-uqp = urllib.unquote_plus
-
 def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
-
-def cleanname(name):
-    return name.replace('&apos;',"'").replace('&#8217;',"'").replace('&amp;','&').replace('&#39;',"'").replace('&quot;','"').replace('&#039;',"'")
-
-def demunge(munge):
-        try:
-            munge = urllib.unquote_plus(munge).decode(UTF8)
-        except:
-            pass
-        return munge
-
 
 USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36'
 defaultHeaders = {'User-Agent':USER_AGENT, 
@@ -49,129 +32,188 @@ defaultHeaders = {'User-Agent':USER_AGENT,
 
 def getRequest(url, user_data=None, headers = defaultHeaders , alert=True):
 
-              log("getRequest URL:"+str(url))
-              if addon.getSetting('us_proxy_enable') == 'true':
-                  us_proxy = 'http://%s:%s' % (addon.getSetting('us_proxy'), addon.getSetting('us_proxy_port'))
-                  proxy_handler = urllib2.ProxyHandler({'http':us_proxy})
-                  if addon.getSetting('us_proxy_pass') <> '' and addon.getSetting('us_proxy_user') <> '':
-                      log('Using authenticated proxy: ' + us_proxy)
-                      password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-                      password_mgr.add_password(None, us_proxy, addon.getSetting('us_proxy_user'), addon.getSetting('us_proxy_pass'))
-                      proxy_auth_handler = urllib2.ProxyBasicAuthHandler(password_mgr)
-                      opener = urllib2.build_opener(proxy_handler, proxy_auth_handler)
-                  else:
-                      log('Using proxy: ' + us_proxy)
-                      opener = urllib2.build_opener(proxy_handler)
-              else:   
-                  opener = urllib2.build_opener()
-              urllib2.install_opener(opener)
+    if addon.getSetting('us_proxy_enable') == 'true':
+        us_proxy = 'http://%s:%s' % (addon.getSetting('us_proxy'), addon.getSetting('us_proxy_port'))
+        proxy_handler = urllib2.ProxyHandler({'http':us_proxy})
+        if addon.getSetting('us_proxy_pass') <> '' and addon.getSetting('us_proxy_user') <> '':
+            log('Using authenticated proxy: ' + us_proxy)
+            password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            password_mgr.add_password(None, us_proxy, addon.getSetting('us_proxy_user'), addon.getSetting('us_proxy_pass'))
+            proxy_auth_handler = urllib2.ProxyBasicAuthHandler(password_mgr)
+            opener = urllib2.build_opener(proxy_handler, proxy_auth_handler)
+        else:
+            log('Using proxy: ' + us_proxy)
+            opener = urllib2.build_opener(proxy_handler)
+    else:   
+        opener = urllib2.build_opener()
+    urllib2.install_opener(opener)
 
-              log("getRequest URL:"+str(url))
-              req = urllib2.Request(url.encode(UTF8), user_data, headers)
+    log("getRequest URL:"+str(url))
+    req = urllib2.Request(url.encode(UTF8), user_data, headers)
 
-              retries = 2
-              while ( retries > 0):
-                try:
-                   response = urllib2.urlopen(req, timeout=30)
-                   retries = 0
-                except urllib2.URLError, e:
-                   retries -= 1
-                except socket.timeout:
-                   retries -= 1
-           
+    try:
+       response = urllib2.urlopen(req, timeout=30)
+       page = response.read()
+       if response.info().getheader('Content-Encoding') == 'gzip':
+           log("Content Encoding == gzip")
+           page = zlib.decompress(page, zlib.MAX_WBITS + 16)
 
-              try:
-                 if response.info().getheader('Content-Encoding') == 'gzip':
-                    log("Content Encoding == gzip")
-                    buf = StringIO( response.read())
-                    f = gzip.GzipFile(fileobj=buf)
-                    link1 = f.read()
-                 else:
-                    link1=response.read()
+    except urllib2.URLError, e:
+       if alert:
+           xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, e , 5000) )
+       page = ""
 
-              except:
-                 if alert:
-                     xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, e , 5000) )
-                 link1 = ""
-
-              if not (str(url).endswith('.zip')):
-                 link1 = str(link1).replace('\n','')
-              return(link1)
+    return(page)
 
 
-def getSources(fanart):
-        ilist = []
-        html = getRequest('http://www.travelchannel.com/shows/whats-new-on-travel-channel/articles/full-episodes')
-        blob = re.compile('<article(.+?)</article').search(html).group(1)
-        a = re.compile('<section class="topn-wrapper">.+?<a href="(.+?)".+?blank">(.+?)<.+?src="(.+?)".+?<p>(.+?)<.+?</section>').findall(blob)
-        a[(len(a)-1)] = ('/video/p/1?wcmmode=disabled',__language__(30021),icon,__language__(30021))
-        for url,name,img,plot in a:
-              mode = 'GS'
-              name = cleanname(name.decode(UTF8))
-              plot = cleanname(plot.decode(UTF8))
-              u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
-              liz=xbmcgui.ListItem(name, '','DefaultFolder.png', img)
-              liz.setInfo( 'Video', { "Title": name, "Plot": plot })
-              liz.setProperty('fanart_image', addonfanart)
-              ilist.append((u, liz, True))
-        xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
-           
+def getShows():
+   xbmcplugin.setContent(int(sys.argv[1]), 'files')
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_UNSORTED)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_TITLE)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_EPISODE)
 
-def getQuery(cat_url):
-        keyb = xbmc.Keyboard('', __addonname__)
-        keyb.doModal()
-        if (keyb.isConfirmed()):
-              qurl = qp('/search/?q=%s' % (keyb.getText()))
-              getCats(qurl, '')
-
-
-def getShows(gsurl,catname):
-        ilist = []
-        html  = getRequest('http://www.travelchannel.com%s' % gsurl)
-        c     = re.compile("data\-videoplaylist\-data='(.+?)'>").findall(html)
-        for a in c:
-              b = json.loads(a)
-              name = cleanname(b["title"].decode(UTF8))
-              url  = b["releaseUrl"]
-              plot = cleanname(b["description"].decode(UTF8))
-              img  = 'http://www.travelchannel.com%s' % b["thumbnailUrl"]
-              dur  = b["length"]
-              mode = 'GL'
-              u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
-              liz=xbmcgui.ListItem(name, '','DefaultFolder.png', img)
-              liz.setInfo( 'Video', { "Title": name, "Studio":catname, "Plot": plot })
-              liz.setProperty('fanart_image', addonfanart)
-              liz.setProperty('IsPlayable', 'true')
-              ilist.append((u, liz, False))
-        if len(ilist) != 0:
-          if 'wcmmode=disabled' in gsurl:
-              name = '[COLOR red]%s[/COLOR]' % __language__(30012)
-              mode = 'GS'
-              url = '/video/p/%s?wcmmode=disabled' % str(int(gsurl[9])+1)
-              u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
-              liz=xbmcgui.ListItem(name, '','DefaultFolder.png', icon)
-              liz.setInfo( 'Video', { "Title": name, "Studio":catname, "Plot": name })
-              liz.setProperty('fanart_image', addonfanart)
-              ilist.append((u, liz, True))
-                
-          xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+   ilist=[]
+   html = getRequest('http://www.travelchannel.com/shows/whats-new-on-travel-channel/articles/full-episodes')
+   m = re.compile('<article(.+?)</article',re.DOTALL).search(html)
+   a = re.compile('<section class="topn-wrapper">.+?<a href="(.+?)".+?blank">(.+?)<.+?src="(.+?)".+?<p>(.+?)<.+?</section>', re.DOTALL).findall(html,m.start(1),m.end(1))
+   a[(len(a)-1)] = ('/video/p/1?wcmmode=disabled',__language__(30021),icon,__language__(30021))
+   for url,name,img,plot in a:
+       html  = getRequest('http://www.travelchannel.com%s' % url, alert=False)
+       c     = re.compile("data\-videoplaylist\-data='(.+?)'>", re.DOTALL).findall(html)
+       if len(c) == 0: continue
+       else: vidcnt = len(c)
+       name=name.strip()
+       fanart = img
+       infoList = {}
+       infoList['TVShowTitle'] = name
+       infoList['Title']       = name
+       infoList['Studio']      = __language__(30010)
+       infoList['Genre']       = ''
+       try:    infoList['Episode'] = int(vidcnt)
+       except: infoList['Episode'] = 0
+       infoList['Plot'] = h.unescape(plot)
+       mode = 'GE'
+       u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
+       liz=xbmcgui.ListItem(name, '',None, img)
+       liz.setInfo( 'Video', infoList)
+       liz.setProperty('fanart_image', fanart)
+       ilist.append((u, liz, True))
+   xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+   if addon.getSetting('enable_views') == 'true':
+      xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('default_view'))
+   xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def getLink(url,vidname):
-            html = getRequest(url)
-            u = re.compile('<video src="(.+?)"').search(html).group(1)
-            xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=u))
+def getEpisodes(geurl, showName):
+   xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_UNSORTED)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_TITLE)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_EPISODE)
+
+   ilist=[]
+   html  = getRequest('http://www.travelchannel.com%s' % uqp(geurl))
+   c     = re.compile("data\-videoplaylist\-data='(.+?)'>", re.DOTALL).findall(html)
+   mode = 'GV'
+   for a in c:
+      b = json.loads(a)
+      url     = b['releaseUrl']
+      html = getRequest(url)
+      name    = h.unescape(b['title'])
+      fanart  = 'http://www.travelchannel.com%s' % b['thumbnailUrl']
+      thumb   = 'http://www.travelchannel.com%s' % b['thumbnailUrl']
+
+      infoList = {}
+      infoList['Duration']    = b['length']
+      infoList['Title']       = name
+      infoList['Studio']      = b['publisherId']
+      months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+      try:
+         dstr = (re.compile('"premierDate" value="(.+?)"',re.DOTALL).search(html).group(1)).split(' ')
+         dt   = '%s-%s-%s' % (dstr[5],months[dstr[1]],dstr[2])
+         infoList['Date']        = dt
+         infoList['Aired']       = infoList['Date']
+      except: pass
+      try:    infoList['MPAA'] = re.compile('ratings="(.+?)"',re.DOTALL).search(html).group(1).split(':',1)[1]
+      except: infoList['MPAA'] = None
+      try:    infoList['Episode'] = int(re.compile('"episodeNumber" value=".(.+?)H"',re.DOTALL).search(html).group(1).replace('Z',''), 16)
+      except: infoList['Episode'] = None
+      try:    infoList['Season']  = int(re.compile('"episodeNumber" value="(.+?)H"',re.DOTALL).search(html).group(1).replace('Z',''),16)/256
+      except: infoList['Season']  = 1
+      infoList['Plot']        = h.unescape(b["description"])
+      infoList['TVShowTitle'] = showName
+      u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
+      liz=xbmcgui.ListItem(name, '',icon, thumb)
+      liz.setInfo( 'Video', infoList)
+      liz.addStreamInfo('video', { 'codec': 'avc1', 
+                                   'width' : 1280, 
+                                   'height' : 720, 
+                                   'aspect' : 1.78 })
+      liz.addStreamInfo('audio', { 'codec': 'aac', 'language' : 'en', 'channels': 2})
+      liz.addStreamInfo('subtitle', { 'language' : 'en'})
+      liz.setProperty('fanart_image', fanart)
+      liz.setProperty('IsPlayable', 'true')
+      ilist.append((u, liz, False))
+
+   if len(ilist) != 0:
+      if 'wcmmode=disabled' in geurl:
+        name = '[COLOR red]%s[/COLOR]' % __language__(30012)
+        mode = 'GE'
+        url = '/video/p/%s?wcmmode=disabled' % str(int(geurl[9])+1)
+        u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
+        liz=xbmcgui.ListItem(name, '','DefaultFolder.png', icon)
+        liz.setInfo( 'Video', { "Title": name, "Studio":showName, "Plot": name })
+        liz.setProperty('fanart_image', addonfanart)
+        ilist.append((u, liz, True))
+
+   xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+   if addon.getSetting('enable_views') == 'true':
+      xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('episode_view'))
+   xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+def getVideo(url, show_name):
+   html   = getRequest(uqp(url))
+   try:    
+           subs = re.compile('<textstream src="(.+?)"',re.DOTALL).findall(html)
+           suburl =''
+           for st in subs:
+             if '.srt' in st:
+                suburl = st
+                break
+   except: pass
+   try:
+     url   = re.compile('<video src="(.+?)"',re.DOTALL).search(html).group(1)
+     if int(addon.getSetting('vid_res')) == 0: url = url.replace('_6.','_3.')
+   except:
+     url, msg   = re.compile('<ref src="(.+?)".+?abstract="(.+?)"',re.DOTALL).search(html).groups()
+     xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, msg , 5000) )
+   xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=url))
+
+   if (addon.getSetting('sub_enable') == "true") and (suburl != ''):
+      profile = addon.getAddonInfo('profile').decode(UTF8)
+      subfile = xbmc.translatePath(os.path.join(profile, 'DIYSubtitles.srt'))
+      prodir  = xbmc.translatePath(os.path.join(profile))
+      if not os.path.isdir(prodir):
+         os.makedirs(prodir)
+
+      pg = getRequest(suburl)
+      if pg != "":
+        ofile = open(subfile, 'w+')
+        ofile.write(pg)
+        ofile.close()
+        xbmc.sleep(2000)
+        xbmc.Player().setSubtitles(subfile)
 
 
 # MAIN EVENT PROCESSING STARTS HERE
-
-xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
 
 parms = {}
 try:
     parms = dict( arg.split( "=" ) for arg in ((sys.argv[2][1:]).split( "&" )) )
     for key in parms:
-       parms[key] = demunge(parms[key])
+      try:    parms[key] = urllib.unquote_plus(parms[key]).decode(UTF8)
+      except: pass
 except:
     parms = {}
 
@@ -179,10 +221,6 @@ p = parms.get
 
 mode = p('mode',None)
 
-if mode==  None:  getSources(p('fanart'))
-elif mode=='GQ':  getQuery(p('url'))
-elif mode=='GS':  getShows(p('url'),p('name'))
-elif mode=='GL':  getLink(p('url'),p('name'))
-
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
+if mode==  None:  getShows()
+elif mode=='GE':  getEpisodes(p('url'), p('name'))
+elif mode=='GV':  getVideo(p('url'), p('name'))

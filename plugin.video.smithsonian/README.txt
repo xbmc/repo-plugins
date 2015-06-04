0b9619f5a9d55938c7ae3919194aa39e035320f1
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-06-04 21:15:58 +0200

    [plugin.video.smithsonian] 1.0.7

--- plugin.video.smithsonian/README.txt
+++ plugin.video.smithsonian/README.txt
@@ -1,9 +1,10 @@
 plugin.video.smithsonian
 ================
 
-
 XBMC Addon for Smithsonian Channel website
 
+Version 1.0.7 Added select content type
+Version 1.0.6 Added metadata and views
 Version 1.0.4 Fix for 1080 and audio only choices (website change)
 version 1.0.3 Added subtitle and proxy support
 version 1.0.1 initial release
--- plugin.video.smithsonian/default.py
+++ plugin.video.smithsonian/default.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
-# Smithsonian Channel XBMC Addon
+# Smithsonian Channel Kodi Addon
 
-import sys
-import httplib
-
+import sys,httplib
 import urllib, urllib2, cookielib, datetime, time, re, os, string
 import xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, xbmc
-import cgi, gzip
-from StringIO import StringIO
-import json
+import zlib,json,HTMLParser
+h = HTMLParser.HTMLParser()
+qp  = urllib.quote_plus
+uqp = urllib.unquote_plus
 
-
-USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
-GENRE_TV  = "TV"
-UTF8          = 'utf-8'
-MAX_PER_PAGE  = 25
+UTF8     = 'utf-8'
 
 addon         = xbmcaddon.Addon('plugin.video.smithsonian')
 __addonname__ = addon.getAddonInfo('name')
 __language__  = addon.getLocalizedString
-
 
 home          = addon.getAddonInfo('path').decode(UTF8)
 icon          = xbmc.translatePath(os.path.join(home, 'icon.png'))
@@ -30,173 +24,122 @@ def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
 
-def cleanname(name):    
-    return name.replace('&apos;',"'").replace('&#8217;',"'")
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
+defaultHeaders = {'User-Agent':USER_AGENT, 'Accept':"text/html", 'Accept-Encoding':'gzip,deflate,sdch', 'Accept-Language':'en-US,en;q=0.8'} 
 
-def demunge(munge):
-        try:
-            munge = urllib.unquote_plus(munge).decode(UTF8)
-        except:
-            pass
-        return munge
-
-USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36'
-defaultHeaders = {'User-Agent':USER_AGENT, 
-                 'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
-                 'Accept-Encoding':'gzip,deflate,sdch',
-                 'Accept-Language':'en-US,en;q=0.8'} 
-
-def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, deleteCR=True):
-
-              log("getRequest URL:"+str(url))
-              if addon.getSetting('us_proxy_enable') == 'true':
-                  us_proxy = 'http://%s:%s' % (addon.getSetting('us_proxy'), addon.getSetting('us_proxy_port'))
-                  proxy_handler = urllib2.ProxyHandler({'http':us_proxy})
-                  if addon.getSetting('us_proxy_pass') <> '' and addon.getSetting('us_proxy_user') <> '':
-                      log('Using authenticated proxy: ' + us_proxy)
-                      password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-                      password_mgr.add_password(None, us_proxy, addon.getSetting('us_proxy_user'), addon.getSetting('us_proxy_pass'))
-                      proxy_auth_handler = urllib2.ProxyBasicAuthHandler(password_mgr)
-                      opener = urllib2.build_opener(proxy_handler, proxy_auth_handler)
-                  else:
-                      log('Using proxy: ' + us_proxy)
-                      opener = urllib2.build_opener(proxy_handler)
-              else:   
-                  opener = urllib2.build_opener()
-              urllib2.install_opener(opener)
-
-              log("getRequest URL:"+str(url))
-              req = urllib2.Request(url.encode(UTF8), user_data, headers)
-
-              try:
-                 response = urllib2.urlopen(req)
-                 if response.info().getheader('Content-Encoding') == 'gzip':
-                    log("Content Encoding == gzip")
-                    buf = StringIO( response.read())
-                    f = gzip.GzipFile(fileobj=buf)
-                    link1 = f.read()
-                 else:
-                    link1=response.read()
-
-              except urllib2.URLError, e:
-                 if alert:
-                     xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, e , 10000) )
-                 link1 = ""
-
-              if not (str(url).endswith('.zip')):
-                if (deleteCR):
-                 link1 = str(link1).replace('\n','')
-              return(link1)
+def getRequest(url, headers = defaultHeaders):
+   log("getRequest URL:"+str(url))
+   req = urllib2.Request(url.encode(UTF8), None, headers)
+   try:
+      response = urllib2.urlopen(req)
+      page = response.read()
+      if response.info().getheader('Content-Encoding') == 'gzip':
+         log("Content Encoding == gzip")
+         page = zlib.decompress(page, zlib.MAX_WBITS + 16)
+   except:
+      page = ""
+   return(page)
 
 
 
-def getSources(fanart):
-              pg = getRequest('http://www.smithsonianchannel.com/full-episodes')
-              cats = re.compile('data-premium=".+?href="(.+?)".+?srcset="(.+?)".+?show-name">(.+?)<.+?</li>').findall(pg)
-              for showurl, showimg, showname in cats:
-                   showname = cleanname(showname)
-                   showurl = '%s?mode=GS&url=%s' %(sys.argv[0], urllib.quote_plus(showurl))
-                   addLink(showurl.encode(UTF8),showname,showimg,addonfanart,showname,GENRE_TV,'')
+def getMovies():
+   ct = ("files", "movies","episodes")
+   xbmcplugin.setContent(int(sys.argv[1]), ct[int(addon.getSetting('content_type'))])
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_UNSORTED)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_TITLE)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+   xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_DURATION)
 
-def getShow(cat_url):
-              pg = getRequest('http://www.smithsonianchannel.com'+urllib.unquote_plus(cat_url))
-              (suburl, vidID) = re.compile('data-vttfile="(.+?)".+?data-bcid="(.+?)"').search(pg).groups()
-              pg = getRequest('http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=%s&pubId=1466806621001' % (vidID))
-              pg = str(pg)+'#'
-              urls = re.compile('http(.+?)#').findall(pg)
-              i = int(addon.getSetting('vid_res'))
-              try:
-                url = 'http'+urls[i]
-              except:
-                url = 'http'+urls[len(urls)-1]
-              url = url.strip()
-              xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = url))
+   ilist=[]
+   html = getRequest('http://www.smithsonianchannel.com/full-episodes')
+   vids = re.compile('data-premium=".+?href="(.+?)".+?srcset="(.+?)".+?"timecode">(.+?)<.+?</li>',re.DOTALL).findall(html)
+   mode = 'GV'
+   for url, thumb, dur in vids:
+      html = getRequest('http://www.smithsonianchannel.com%s' % url)
+      m = re.compile('property="og:title" content="(.+?)"',re.DOTALL).search(html)
+      title = m.group(1)
+      m = re.compile('property="og:image" content="(.+?)"',re.DOTALL).search(html, m.end(1))
+      iconimg = m.group(1)
+      m = re.compile('"og:site_name" content="(.+?)"', re.DOTALL).search(html, m.end(1))
+      studio = m.group(1)
+      m = re.compile('"twitter:image" content="(.+?)"',re.DOTALL).search(html, m.end(1))
+      fanart = m.group(1)
+      plot = re.compile('<div class="accordion-content-mobile">.+?"description">(.+?)</p',re.DOTALL).search(html,m.end(1)).group(1)
+      infoList = {}
+      length = 0
+      dur = dur.replace('|','').split(':')
+      for d in dur: length = length*60 + int(d.strip())
 
-              if (suburl != "") and (addon.getSetting('sub_enable') == "true"):
-                 profile = addon.getAddonInfo('profile').decode(UTF8)
-                 subfile = xbmc.translatePath(os.path.join(profile, 'SCSubtitles.srt'))
-                 prodir  = xbmc.translatePath(os.path.join(profile))
-                 if not os.path.isdir(prodir):
-                    os.makedirs(prodir)
+      infoList['Duration']    = length
 
-                 cc = getRequest('http://www.smithsonianchannel.com'+suburl, deleteCR=False, alert=False)
-                 if cc != "":
-                   ofile = open(subfile, 'w+')
-                   ofile.write(cc)
-                   ofile.close()
-                   xbmc.sleep(5000)
-                   xbmc.Player().setSubtitles(subfile)
+      infoList['MPAA']        = 'tv-pg'
+      infoList['Title']       = h.unescape(title)
+      infoList['Studio']      = studio
+      infoList['Genre']       = 'Documentary'
+      infoList['Plot']        = h.unescape(plot)
+      name  = infoList['Title']
+      u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
+      liz=xbmcgui.ListItem(name, '',iconimg, thumb)
+      liz.setInfo( 'Video', infoList)
+      liz.addStreamInfo('video', { 'codec': 'h264', 
+                                   'width' : 1920, 
+                                   'height' : 1080, 
+                                   'aspect' : 1.78 })
+      liz.addStreamInfo('audio', { 'codec': 'aac', 'language' : 'en'})
+      liz.addStreamInfo('subtitle', { 'language' : 'en'})
+      liz.setProperty('fanart_image', fanart)
+      liz.setProperty('IsPlayable', 'true')
+      ilist.append((u, liz, False))
+   xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+   if addon.getSetting('enable_views') == 'true':
+      xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('default_view'))
+   xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def play_playlist(name, list):
-        playlist = xbmc.PlayList(1)
-        playlist.clear()
-        item = 0
-        for i in list:
-            item += 1
-            info = xbmcgui.ListItem('%s) %s' %(str(item),name))
-            playlist.add(i, info)
-        xbmc.executebuiltin('playlist.playoffset(video,0)')
+def getVideo(url):
+   html = getRequest('http://www.smithsonianchannel.com%s' % uqp(url))
+   (suburl, vidID) = re.compile('data-vttfile="(.+?)".+?data-bcid="(.+?)"', re.DOTALL).search(html).groups()
+   html = getRequest('http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=%s&pubId=1466806621001' % (vidID))
+   html = str(html)+'#'
+   urls = re.compile('http(.+?)#',re.DOTALL).findall(html)
+   i = int(addon.getSetting('vid_res'))
+   try:
+     url = 'http'+urls[i]
+   except:
+     url = 'http'+urls[len(urls)-1]
+   url = url.strip()
+   xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = url))
 
+   if (suburl != "") and (addon.getSetting('sub_enable') == "true"):
+      profile = addon.getAddonInfo('profile').decode(UTF8)
+      subfile = xbmc.translatePath(os.path.join(profile, 'SCSubtitles.srt'))
+      prodir  = xbmc.translatePath(os.path.join(profile))
+      if not os.path.isdir(prodir):
+         os.makedirs(prodir)
 
-def addDir(name,url,mode,iconimage,fanart,description,genre,date,showcontext=True,playlist=None,autoplay=False):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+mode
-        dir_playable = False
-        cm = []
-
-        if mode != 'SR':
-            u += "&name="+urllib.quote_plus(name)
-            if (fanart is None) or fanart == '': fanart = addonfanart
-            u += "&fanart="+urllib.quote_plus(fanart)
-            dir_image = "DefaultFolder.png"
-            dir_folder = True
-        else:
-            dir_image = "DefaultVideo.png"
-            dir_folder = False
-            dir_playable = True
-
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage=dir_image, thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description, "Genre": genre, "Year": date } )
-        liz.setProperty( "Fanart_Image", fanart )
-
-        if dir_playable == True:
-         liz.setProperty('IsPlayable', 'true')
-        if not playlist is None:
-            playlist_name = name.split(') ')[1]
-            cm.append(('Play '+playlist_name+' PlayList','XBMC.RunPlugin(%s?mode=PP&name=%s&playlist=%s)' %(sys.argv[0], playlist_name, urllib.quote_plus(str(playlist).replace(',','|')))))
-        liz.addContextMenuItems(cm)
-        return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=dir_folder)
-
-def addLink(url,name,iconimage,fanart,description,genre,date,showcontext=True,playlist=None, autoplay=False):
-        return addDir(name,url,'SR',iconimage,fanart,description,genre,date,showcontext,playlist,autoplay)
-
+      cc = getRequest('http://www.smithsonianchannel.com%s'% suburl)
+      if cc != "":
+        ofile = open(subfile, 'w+')
+        ofile.write(cc)
+        ofile.close()
+        xbmc.sleep(2000)
+        xbmc.Player().setSubtitles(subfile)
 
 
 # MAIN EVENT PROCESSING STARTS HERE
-
-xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 
 parms = {}
 try:
     parms = dict( arg.split( "=" ) for arg in ((sys.argv[2][1:]).split( "&" )) )
     for key in parms:
-       parms[key] = demunge(parms[key])
+      try:    parms[key] = urllib.unquote_plus(parms[key]).decode(UTF8)
+      except: pass
 except:
     parms = {}
 
 p = parms.get
 
-try:
-    mode = p('mode')
-except:
-    mode = None
+mode = p('mode',None)
 
-if mode==  None:  getSources(p('fanart'))
-elif mode=='SR':  xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=p('url')))
-elif mode=='PP':  play_playlist(p('name'), p('playlist'))
-elif mode=='GS':  getShow(p('url'))
-
-
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
+if mode==  None:  getMovies()
+elif mode=='GV':  getVideo(p('url'))

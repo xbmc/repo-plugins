426277b3c1c9ba857df8afdf144e2d6be6ec42dd
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-06-11 19:54:41 +0200

    [plugin.video.npr] 1.0.5

--- plugin.video.npr/README.txt
+++ plugin.video.npr/README.txt
@@ -1,15 +1,12 @@
 plugin.video.npr
 ================
 
-
-XBMC Addon for NPR Music website
+Kodi Addon for NPR Music website
 
 version 1.0.1 initial release
-
 version 1.0.2 clean-up of genre
-
 Version 1.0.3 - website changes
-
 Version 1.0.4 - website changes
+Version 1.0.5 website changes, added views
 
 
--- plugin.video.npr/default.py
+++ plugin.video.npr/default.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
-# NPR Music XBMC Addon
+# NPR Music Kodi Video Addon
 
-import sys
-import httplib
-
+import sys, httplib
 import urllib, urllib2, cookielib, datetime, time, re, os, string
 import xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, xbmc
-import cgi, gzip
-from StringIO import StringIO
+import zlib,json,HTMLParser
+h = HTMLParser.HTMLParser()
+qp  = urllib.quote_plus
+uqp = urllib.unquote_plus
 
-
-USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
-GENRE      = "Music Video"
 UTF8          = 'utf-8'
-MAX_PER_PAGE  = 25
 NPRBASE = 'http://www.npr.org%s'
 
 addon         = xbmcaddon.Addon('plugin.video.npr')
@@ -23,6 +19,7 @@ __language__  = addon.getLocalizedString
 
 home          = addon.getAddonInfo('path').decode(UTF8)
 icon          = xbmc.translatePath(os.path.join(home, 'icon.png'))
+iconNext      = xbmc.translatePath(os.path.join(home, 'next.png'))
 addonfanart   = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
 
 
@@ -30,88 +27,130 @@ def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
 
-def cleanfilename(name):    
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-    return ''.join(c for c in name if c in valid_chars)
 
-def demunge(munge):
-        try:
-            munge = urllib.unquote_plus(munge).decode(UTF8)
-        except:
-            pass
-        return munge
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
+#USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36'
+defaultHeaders = {'User-Agent':USER_AGENT, 
+                 'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+                 'Accept-Encoding':'gzip,deflate,sdch',
+                 'Accept-Language':'en-US,en;q=0.8'} 
 
-def deuni(a):
-    a = a.replace('&amp;#039;',"'")
-    a = a.replace('&amp','&')
-    a = a.replace('&;','&')
-    a = a.replace('&quot;',"'")
-    a = a.replace('&#039;',"'")
-    return a
+def getRequest(url, user_data=None, headers = defaultHeaders , alert=True):
 
-def getRequest(url):
-              log("getRequest URL:"+str(url))
-              headers = {'User-Agent':USER_AGENT, 'Accept':"text/html", 'Accept-Encoding':'gzip,deflate,sdch', 'Accept-Language':'en-US,en;q=0.8'} 
-              req = urllib2.Request(url.encode(UTF8), None, headers)
+    opener = urllib2.build_opener()
+    urllib2.install_opener(opener)
 
-              try:
-                 response = urllib2.urlopen(req)
-                 if response.info().getheader('Content-Encoding') == 'gzip':
-                    log("Content Encoding == gzip")
-                    buf = StringIO( response.read())
-                    f = gzip.GzipFile(fileobj=buf)
-                    link1 = f.read()
-                 else:
-                    link1=response.read()
-              except:
-                 link1 = ""
+    log("getRequest URL:"+str(url))
+    req = urllib2.Request(url.encode(UTF8), user_data, headers)
 
-              link1 = str(link1).replace('\n','')
-              return(link1)
+    try:
+       response = urllib2.urlopen(req, timeout=30)
+       page = response.read()
+       if response.info().getheader('Content-Encoding') == 'gzip':
+           log("Content Encoding == gzip")
+           page = zlib.decompress(page, zlib.MAX_WBITS + 16)
+
+    except urllib2.URLError, e:
+       if alert:
+           xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, e , 5000) )
+       page = ""
+
+    return(page)
 
 
-def getSources(fanart):
+def getSources():
+              ilist = []
               urlbase   = NPRBASE % ('/sections/music-videos/')
-              pg = getRequest(urlbase)
-              catname = __language__(30000)
+              html = getRequest(urlbase)
+              name = __language__(30000)
               urlbase = NPRBASE % ('/series/15667984/favorite-sessions')
-              addDir(catname,urlbase.encode(UTF8),'GC',icon,addonfanart,catname,GENRE,'',False)
-              blob = re.compile('<div class="subtopics">(.+?)</ul>').findall(pg)[0]
-              cats = re.compile('<a href="(.+?)">(.+?)<').findall(blob)
-              for caturl, catname in cats:
-                  caturl = NPRBASE % (caturl)
-                  addDir(catname,caturl.encode(UTF8),'GC',icon,addonfanart,catname,GENRE,'',False)
+              u = '%s?url=%s&mode=GC' % (sys.argv[0],urlbase.encode(UTF8))
+              liz=xbmcgui.ListItem(name, '',icon, None)
+              liz.setProperty('fanart_image', addonfanart)
+              ilist.append((u, liz, True))
 
-def getCats(cat_url):
-             pg = getRequest(cat_url)
-             blobs = re.compile('<article class(.+?)</article>').findall(pg)
+              m = re.compile('<div class="subtopics">(.+?)</ul>',re.DOTALL).search(html)
+              cats = re.compile('<a href="(.+?)">(.+?)<',re.DOTALL).findall(html,m.start(1),m.end(1))
+              for url, name in cats:
+                  url = NPRBASE % (url)
+                  u = '%s?url=%s&mode=GC' % (sys.argv[0],url)
+                  liz=xbmcgui.ListItem(name, '',icon, None)
+                  liz.setProperty('fanart_image', addonfanart)
+                  ilist.append((u, liz, True))
+              xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+              if addon.getSetting('enable_views') == 'true':
+                 xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('default_view'))
+              xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+def getCats(url):
+             xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
+             xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_UNSORTED)
+             xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_TITLE)
+             xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+             xbmcplugin.addSortMethod(int(sys.argv[1]),xbmcplugin.SORT_METHOD_EPISODE)
+
+             ilist =[]
+             html = getRequest(url)
+             nexturl = url
+             blobs = re.compile('<article class(.+?)</article>',re.DOTALL).findall(html)
              curlink  = 0
              nextlink = 1
              for blob in blobs:
                nextlink = nextlink+1
                if ('article-video' in blob) or ('type-video' in blob):
-                 (showurl, showimg, showname) = re.compile('<a href="(.+?)".+?src="(.+?)".+?title="(.+?)"').findall(blob)[0]
-                 showurl = "%s?url=%s&name=%s&mode=GS" %(sys.argv[0], urllib.quote_plus(showurl), urllib.quote_plus(showname))
-                 addLink(showurl.encode(UTF8),deuni(showname),showimg,addonfanart,deuni(showname),GENRE,'')
+                 try:
+                    (url, thumb, name, dt, plot) = re.compile('<a href="(.+?)".+?src="(.+?)".+?title="(.+?)".+?<time datetime="(.+?)".+?</time>(.+?)</p',re.DOTALL).search(blob).groups()
+                 except:
+                    (url, thumb, name) = re.compile('<a href="(.+?)".+?src="(.+?)".+?title="(.+?)"',re.DOTALL).search(blob).groups()
+                    plot = name
+                    dt   = ''
+                 name = h.unescape(name)
+                 plot = h.unescape(plot.strip().decode('utf-8'))
+                 infoList ={}
+                 infoList['Title'] = name
+                 infoList['date']  = dt
+                 infoList['Plot']  = plot
+                 try: infoList['Year'] = int(dt.split('-',1)[0])
+                 except: pass
+                 u = "%s?url=%s&mode=GV" %(sys.argv[0], qp(url))
+                 liz=xbmcgui.ListItem(name, '',None, thumb)
+                 liz.setInfo( 'Video', infoList)
+                 liz.addStreamInfo('video', { 'codec': 'avc1', 
+                                   'width' : 1280, 
+                                   'height' : 720, 
+                                   'aspect' : 1.78 })
+                 liz.addStreamInfo('audio', { 'codec': 'aac', 'language' : 'en', 'channels': 2})
+                 liz.addStreamInfo('subtitle', { 'language' : 'en'})
+                 liz.setProperty('fanart_image', addonfanart)
+                 liz.setProperty('IsPlayable', 'true')
+                 ilist.append((u, liz, False))
 
+             curlink=0
              nextstr = '/archive?start='
-             nexturl = cat_url
-             if (nextstr in cat_url):
+             if (nextstr in nexturl):
                 (nexturl,curlink) = nexturl.split(nextstr,1)
                 curlink = int(curlink)
              nextlink = str(nextlink+curlink+1)
-             nexturl = nexturl+nextstr+nextlink
-             catname = '[COLOR red]%s[/COLOR]' % (__language__(30001))
-             addDir(catname,nexturl.encode(UTF8),'GC','',addonfanart,catname,GENRE,'',False)
+             url = nexturl+nextstr+nextlink
+             name = '[COLOR red]%s[/COLOR]' % (__language__(30001))
+             u = '%s?url=%s&mode=GC' % (sys.argv[0],url)
+             liz=xbmcgui.ListItem(name, '',iconNext, None)
+             liz.setProperty('fanart_image', addonfanart)
+             ilist.append((u, liz, True))
+             xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+             if addon.getSetting('enable_views') == 'true':
+                xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('episode_view'))
+             xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def getShow(show_url, show_name):
-            pg = getRequest(show_url)
+def getVideo(url):
+            html = getRequest(uqp(url))
             try:
-                finalurl = re.compile("'SD'.+?file:'(.+?)'").findall(pg)[0]
+                finalurl = re.compile("'SD'.+?file:'(.+?)'",re.DOTALL).search(html).group(1)
             except:
                 try:
-                    videoid = re.compile('<div class="video-wrap">.+?src="http://www\.youtube\.com/embed/(.+?)\?').findall(pg)[0]
+                    videoid = re.compile('<div class="video-wrap">.+?src="http://www\.youtube\.com/embed/(.+?)\?',re.DOTALL).search(html).group(1)
                     finalurl = 'plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid=%s' % (videoid)
                 except:
                     dialog = xbmcgui.Dialog()
@@ -120,72 +159,22 @@ def getShow(show_url, show_name):
             xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path = finalurl))
 
 
-def play_playlist(name, list):
-        playlist = xbmc.PlayList(1)
-        playlist.clear()
-        item = 0
-        for i in list:
-            item += 1
-            info = xbmcgui.ListItem('%s) %s' %(str(item),name))
-            playlist.add(i, info)
-        xbmc.executebuiltin('playlist.playoffset(video,0)')
-
-
-def addDir(name,url,mode,iconimage,fanart,description,genre,date,showcontext=True,playlist=None,autoplay=False):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+mode
-        dir_playable = False
-        cm = []
-
-        if mode != 'SR':
-            u += "&name="+urllib.quote_plus(name)
-            if (fanart is None) or fanart == '': fanart = addonfanart
-            u += "&fanart="+urllib.quote_plus(fanart)
-            dir_image = "DefaultFolder.png"
-            dir_folder = True
-        else:
-            dir_image = "DefaultVideo.png"
-            dir_folder = False
-            dir_playable = True
-
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage=dir_image, thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description, "Genre": genre, "Year": date } )
-        liz.setProperty( "Fanart_Image", fanart )
-
-        if dir_playable == True:
-         liz.setProperty('IsPlayable', 'true')
-        if not playlist is None:
-            playlist_name = name.split(') ')[1]
-            cm.append(('Play '+playlist_name+' PlayList','XBMC.RunPlugin(%s?mode=PP&name=%s&playlist=%s)' %(sys.argv[0], playlist_name, urllib.quote_plus(str(playlist).replace(',','|')))))
-        liz.addContextMenuItems(cm)
-        return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=dir_folder)
-
-def addLink(url,name,iconimage,fanart,description,genre,date,showcontext=True,playlist=None, autoplay=False):
-        return addDir(name,url,'SR',iconimage,fanart,description,genre,date,showcontext,playlist,autoplay)
-
-
-
 # MAIN EVENT PROCESSING STARTS HERE
-
-xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 
 parms = {}
 try:
     parms = dict( arg.split( "=" ) for arg in ((sys.argv[2][1:]).split( "&" )) )
     for key in parms:
-       parms[key] = demunge(parms[key])
+      try:    parms[key] = urllib.unquote_plus(parms[key]).decode(UTF8)
+      except: pass
 except:
     parms = {}
 
 p = parms.get
-
 mode = p('mode',None)
 
-if mode==  None:  getSources(p('fanart'))
-elif mode=='SR':  xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=p('url')))
-elif mode=='PP':  play_playlist(p('name'), p('playlist'))
+if mode==  None:  getSources()
 elif mode=='GC':  getCats(p('url'))
-elif mode=='GS':  getShow(p('url'), p('name'))
+elif mode=='GV':  getVideo(p('url'))
 
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
 

3f31c20c3d833d4b7e00fa66f56db3cffca681b2
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-06-25 20:25:01 +0200

    [plugin.video.tweakers] 1.0.6

--- plugin.video.tweakers/resources/lib/tweakers_const.py
+++ plugin.video.tweakers/resources/lib/tweakers_const.py
@@ -8,5 +8,5 @@ __addon__       = "plugin.video.tweakers"
 __settings__    = xbmcaddon.Addon(id=__addon__ )
 __language__    = __settings__.getLocalizedString
 __images_path__ = os.path.join( xbmcaddon.Addon(id=__addon__ ).getAddonInfo('path'), 'resources', 'images' )
-__date__        = "16 june 2015"
-__version__     = "1.0.5"
+__date__        = "23 june 2015"
+__version__     = "1.0.6"

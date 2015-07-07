926c9392d13d243da98f84fe76b1ffa7bf51229b
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-07-07 20:25:44 +0200

    [plugin.video.tubitv] 1.0.7

--- plugin.video.tubitv/README.txt
+++ plugin.video.tubitv/README.txt
@@ -3,6 +3,7 @@ plugin.video.tubitv
 
 XBMC Addon for TUBI TV website
 
+Version 1.0.7 website change, added views
 Version 1.0.6 website change
 Version 1.0.5 website change
 Version 1.0.4 website change
--- plugin.video.tubitv/default.py
+++ plugin.video.tubitv/default.py
@@ -7,8 +7,7 @@ import httplib, socket
 import urllib, urllib2, cookielib, datetime, time, re, os, string
 import xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, xbmc
 import cgi, gzip
-from StringIO import StringIO
-import json
+import zlib,json
 
 
 UTF8          = 'utf-8'
@@ -30,16 +29,6 @@ def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
 
-def cleanname(name):
-    return name.replace('&apos;',"'").replace('&#8217;',"'").replace('&amp;','&').replace('&#39;',"'").replace('&quot;','"').replace('&#039;',"'")
-
-def demunge(munge):
-        try:
-            munge = urllib.unquote_plus(munge).decode(UTF8)
-        except:
-            pass
-        return munge
-
 
 USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36'
 defaultHeaders = {'User-Agent':USER_AGENT, 
@@ -47,7 +36,7 @@ defaultHeaders = {'User-Agent':USER_AGENT,
                  'Accept-Encoding':'gzip,deflate,sdch',
                  'Accept-Language':'en-US,en;q=0.8'} 
 
-def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, striplf = True):
+def getRequest(url, user_data=None, headers = defaultHeaders , alert=True):
 
               log("getRequest URL:"+str(url))
               if addon.getSetting('us_proxy_enable') == 'true':
@@ -79,39 +68,35 @@ def getRequest(url, user_data=None, headers = defaultHeaders , alert=True, strip
                 except socket.timeout:
                    retries -= 1
            
-
               try:
+                 page = response.read()
                  if response.info().getheader('Content-Encoding') == 'gzip':
                     log("Content Encoding == gzip")
-                    buf = StringIO( response.read())
-                    f = gzip.GzipFile(fileobj=buf)
-                    link1 = f.read()
-                 else:
-                    link1=response.read()
+                    page = zlib.decompress(page, zlib.MAX_WBITS + 16)
 
               except:
                  if alert:
                      xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, e , 5000) )
-                 link1 = ""
+                 page = ""
 
-              if (not (str(url).endswith('.zip'))) and (striplf == True):
-                 link1 = str(link1).replace('\n','')
-              return(link1)
+              return(page)
 
 
 def getSources(fanart):
         ilist = []
         html  = getRequest('http://tubitv.com')
-        c     = re.compile('<li.+?class=".+?href="(.+?)">(.+?)<').findall(html)
+        c     = re.compile('<li.+?class=".+?href="(.+?)">(.+?)<',re.DOTALL).findall(html)
         for url, name in c:
               mode = 'GS'
               u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
               liz=xbmcgui.ListItem(name, '','DefaultFolder.png', icon)
-              liz.setInfo( 'Video', { "Title": name })
               liz.setProperty('fanart_image', addonfanart)
               ilist.append((u, liz, True))
                 
         xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+        if addon.getSetting('enable_views') == 'true':
+          xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('default_view'))
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 def getQuery(cat_url):
@@ -126,7 +111,7 @@ def getShows(gsurl,catname):
         ilist = []
         gsurl = uqp(gsurl)
         html  = getRequest('http://tubitv.com%s' % gsurl)
-        c     = re.compile("<A class='imageBox' href='(.+?)'.+?src='(.+?)'.+?title'>(.+?)<.+?description'>(.+?)<.+?</A").findall(html)
+        c     = re.compile("<A class='img_box' href='(.+?)'.+?src='(.+?)'.+?title'>(.+?)<.+?description'>(.+?)<.+?</A",re.DOTALL).findall(html)
         for url,img,name,plot in c:
           try: 
               season, epi = re.compile('S([0-9]*)\:E([0-9]*)').search(url).groups()
@@ -149,21 +134,30 @@ def getShows(gsurl,catname):
           liz.setProperty('IsPlayable', 'true')
           ilist.append((u, liz, False))
         xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+        if addon.getSetting('enable_views') == 'true':
+           xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('movie_view'))
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
 
 def getEpis(gsurl,catname):
+        xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
         ilist = []
         gsurl = uqp(gsurl)
+        catname = uqp(catname)
         html  = getRequest('http://tubitv.com%s' % gsurl)
-        c     = re.compile("<A class='imageBox' href='(.+?)'.+?src='(.+?)'.+?Title'>(.+?)<.+?</A").findall(html)
+        c     = re.compile("<A class='img_box' href='(.+?)'.+?src='(.+?)'.+?Title'>(.+?)<.+?</A", re.DOTALL).findall(html)
         for url,img,name in c:
           mode = 'GL'
           u = '%s?url=%s&name=%s&mode=%s' % (sys.argv[0],qp(url), qp(name), mode)
           liz=xbmcgui.ListItem(name, '','DefaultFolder.png', img)
-          liz.setInfo( 'Video', { "Title": name })
+          liz.setInfo( 'Video', { "Title": name, "TVShowTitle":catname })
           liz.setProperty('fanart_image', addonfanart)
           liz.setProperty('IsPlayable', 'true')
           ilist.append((u, liz, False))
         xbmcplugin.addDirectoryItems(int(sys.argv[1]), ilist, len(ilist))
+        if addon.getSetting('enable_views') == 'true':
+           xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting('episode_view'))
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 def getLink(url,vidname):
@@ -209,7 +203,7 @@ def getLink(url,vidname):
            html =''
 
         try:
-            a = re.compile("<script>apu='(.+?)'").search(html).group(1)
+            a = re.compile("<script>apu='(.+?)'",re.DOTALL).search(html).group(1)
         except:
             xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( __addonname__, __language__(30018) , 5000) )
             return
@@ -227,8 +221,7 @@ def getLink(url,vidname):
         try:
           vres = int(addon.getSetting('vid_res'))
           data = getRequest(u)
-          data = data+'#'
-          urls = re.compile('BANDWIDTH=[0-9]*(.+?)#').findall(data)
+          urls = re.compile('BANDWIDTH=[0-9]*(.+?)#', re.DOTALL).findall(data)
           if vres !=0: vres = len(urls)-1
           u = '%s/%s' % (u.rsplit('/',1)[0], urls[vres].strip())
         except: pass
@@ -236,7 +229,7 @@ def getLink(url,vidname):
         xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=u))
 
         try:
-           suburl = re.compile("sbt=(.+?);").search(html).group(1)
+           suburl = re.compile("sbt=(.+?);", re.DOTALL).search(html).group(1)
            if suburl != "undefined":
               suburl = json.loads(suburl)
               suburl = suburl[0]["subtitle"]["url"]
@@ -250,7 +243,7 @@ def getLink(url,vidname):
                  if not os.path.isdir(prodir):
                     os.makedirs(prodir)
 
-                 captions = getRequest(suburl, striplf = False)
+                 captions = getRequest(suburl)
                  if captions != "":
                    ofile = open(subfile, 'w+')
                    ofile.write(captions)
@@ -261,13 +254,12 @@ def getLink(url,vidname):
 
 # MAIN EVENT PROCESSING STARTS HERE
 
-xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
-
 parms = {}
 try:
     parms = dict( arg.split( "=" ) for arg in ((sys.argv[2][1:]).split( "&" )) )
     for key in parms:
-       parms[key] = demunge(parms[key])
+      try:    parms[key] = urllib.unquote_plus(parms[key]).decode(UTF8)
+      except: pass
 except:
     parms = {}
 
@@ -280,6 +272,4 @@ elif mode=='GQ':  getQuery(p('url'))
 elif mode=='GS':  getShows(p('url'),p('name'))
 elif mode=='GE':  getEpis(p('url'),p('name'))
 elif mode=='GL':  getLink(p('url'),p('name'))
-
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
 

4a4c6c0c3660e60d174f90494fd5106149572808
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-08-19 19:13:43 +0200

    [plugin.video.docu] 1.0.1

--- plugin.video.docu/default.py
+++ plugin.video.docu/default.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------
-# http://www.youtube.com/user/vice
+# TRUTH VIDEOS Addon by L33TGUY
 #------------------------------------------------------------
 # License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
 # Based on code from youtube addon
+#
+# Author: L33TGUY
+# Twitter:  @truth_videos
 #------------------------------------------------------------
 
 import os
@@ -17,7 +20,20 @@ addon = Addon(addonID, sys.argv)
 local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
-YOUTUBE_CHANNEL_ID = "ufotvpresents"
+YOUTUBE_CHANNEL_ID_1 = "UFOTVstudios"
+YOUTUBE_CHANNEL_ID_2 = "vice"
+YOUTUBE_CHANNEL_ID_3 = "journeymanpictures"
+YOUTUBE_CHANNEL_ID_4 = "RTDocumentaries"
+YOUTUBE_CHANNEL_ID_5 = "davidicke"
+YOUTUBE_CHANNEL_ID_6 = "TheAlexJonesChannel"
+YOUTUBE_CHANNEL_ID_7 = "MisterMachine1977"
+YOUTUBE_CHANNEL_ID_8 = "thirdphaseofmoon"
+YOUTUBE_CHANNEL_ID_9 = "corbettreport"
+YOUTUBE_CHANNEL_ID_10 = "StormCloudsGathering"
+YOUTUBE_CHANNEL_ID_11 = "PowerfulJRE"
+YOUTUBE_CHANNEL_ID_12 = "wearechange"
+YOUTUBE_CHANNEL_ID_13 = "GrahamHancockDotCom"
+YOUTUBE_CHANNEL_ID_14 = "TZMOfficialChannel"
 
 # Entry point
 def run():
@@ -40,9 +56,100 @@ def main_list(params):
 
     plugintools.add_item( 
         #action="", 
-        title="Documentary",
-        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID+"/",
+        title="UFOTV Studios",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_1+"/",
         thumbnail=icon,
         folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Vice",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_2+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Journeyman Pictures",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_3+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Russia Today Documentaries",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_4+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="David Icke",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_5+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Alex Jones",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_6+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Mister Enigma",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_7+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="ThirdPhaseOfMoon",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_8+"/",
+        thumbnail=icon,
+        folder=True )
+        
+    plugintools.add_item( 
+        #action="", 
+        title="The Corbett Report",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_9+"/",
+        thumbnail=icon,
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Storm Clouds Gathering",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_10+"/",
+        thumbnail=icon,
+        folder=True )                
+
+    plugintools.add_item( 
+        #action="", 
+        title="The Joe Rogan Experience",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_11+"/",
+        thumbnail=icon,
+        folder=True )    
+
+    plugintools.add_item( 
+        #action="", 
+        title="We Are Change",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_12+"/",
+        thumbnail=icon,
+        folder=True )  
+
+    plugintools.add_item( 
+        #action="", 
+        title="Graham Hancock",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_13+"/",
+        thumbnail=icon,
+        folder=True )  
+
+    plugintools.add_item( 
+        #action="", 
+        title="The Zeitgeist Movement",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_14+"/",
+        thumbnail=icon,
+        folder=True ) 
 
 run()

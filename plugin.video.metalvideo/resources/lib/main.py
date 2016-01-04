beb2a2601d60544f91faaff3a72ee6f8c7411e1b
Martijn Kaijser <mcm.kaijser@gmail.com>
2016-01-04 20:16:23 +0100

    [plugin.video.metalvideo] 2.0.1

--- plugin.video.metalvideo/resources/lib/main.py
+++ plugin.video.metalvideo/resources/lib/main.py
@@ -194,49 +194,13 @@ class VideoList(listitem.VirtualFS):
 			urlString = {u"0":u"%sdate.html", u"1":u"%sartist.html", u"2":u"%srating.html", u"3":u"%sviews.html"}[_plugin.getSetting("sort")]
 			url = urlString % _plugin["url"]
 		
-		# Fetch SourceCode
-		sourceCode = urlhandler.urlread(url, 14400) # TTL = 4 Hours
-		
 		# Set Content Properties
 		self.set_sort_methods(self.sort_method_unsorted)
 		
-		# Fetch and Return VideoItems
-		return self.regex_scraper(sourceCode)
-	
-	def regex_scraper(self, sourceCode):
-		# Create Speed vars
-		localListitem = listitem.ListItem
-		import CommonFunctions
-		
-		# Add Next Page if Exists   
-		nextUrl = re.findall('<a href="(\S+?)">next \xbb</a>', sourceCode)
-		if nextUrl: self.add_next_page(url={"url":u"http://www.metalvideo.com/%s" % nextUrl[0]})
-		
-		# Iterate the list of videos
-		searchUrl = re.compile('<a href="(\S+?)">')
-		searchImg = re.compile('<img src="(\S+?)"')
-		searchSong = re.compile('<span class="song_name">(.+?)</span>')
-		searchArtist = re.compile('<span class="artist_name">(.+?)</span>')
-		for htmlSegment in CommonFunctions.parseDOM(sourceCode, u"li", {u"class":u"video"}):
-			# Fetch artist and url
-			artist = searchArtist.findall(htmlSegment)[0]
-			url = searchUrl.findall(htmlSegment)[0]
-			
-			# Create listitem of Data
-			item = localListitem()
-			item.setLabel(u"%s - %s" % (artist, searchSong.findall(htmlSegment)[0]))
-			item.setInfoDict(artist=[artist])
-			item.setParamDict(action="PlayVideo", url=url)
-			
-			# Set Thumbnail Image
-			image = searchImg.findall(htmlSegment)
-			if image: item.setThumb(image[0])
-			
-			# Add Context item to link to related videos
-			item.addRelatedContext(url=url[url.rfind(u"_")+1:url.rfind(u".")])
-			
-			# Store Listitem data
-			yield item.getListitemTuple(True)
+		# Fetch SourceObj
+		import parsers
+		with urlhandler.urlopen(url, 14400) as sourceObj: # TTL = 4 Hours
+			return parsers.VideosParser().parse(sourceObj)
 
 class PlayVideo(listitem.PlaySource):
 	@plugin.error_handler
@@ -247,6 +211,7 @@ class PlayVideo(listitem.PlaySource):
 			# Add Current path to playlist
 			playlist = _plugin.xbmc.PlayList(1)
 			playlist.add(_plugin.handleThree)
+			
 			# Return video url untouched
 			return self.find_video(0) # TTL = 1 Week
 		
@@ -255,6 +220,7 @@ class PlayVideo(listitem.PlaySource):
 			# Clear Playlist first
 			playlist = _plugin.xbmc.PlayList(1)
 			playlist.clear()
+			
 			# Return Video Player url Twice to start party mode playlist
 			return {"url":[self.find_video(0), _plugin.handleThree+"partymode=true"]}
 		
@@ -271,6 +237,5 @@ class PlayVideo(listitem.PlaySource):
 		try: videoId = [part for part in re.findall('src="(http://www.youtube.com/embed/\S+?)"|file:\s+\'(\S+?)\'', sourceCode)[0] if part][0]
 		except: return None
 		else:
-			print 
 			if u"metalvideo.com" in videoId: return videoId
 			elif u"youtube.com" in videoId: return self.sourceType(videoId, "youtube_com")
--- plugin.video.metalvideo/resources/lib/parsers.py
+++ plugin.video.metalvideo/resources/lib/parsers.py
@@ -38,7 +38,7 @@ class WatchingParser(HTMLParser.HTMLParser):
 		try:
 			if isinstance(html, unicode): self.feed(html)
 			else: self.feed(html.decode(encoding))
-		except plugin.ParserError: pass
+		except EOFError: pass
 		
 		# Return Results
 		return results
@@ -95,4 +95,121 @@ class WatchingParser(HTMLParser.HTMLParser):
 		
 		# Check for the full end ul tag
 		elif tag == u"ul" and self.section == 1:
-			raise plugin.ParserError
+			raise EOFError
+
+class VideosParser(HTMLParser.HTMLParser):
+	""" Parses video from http://metalvideo.com/browse-black_metal-videos-1-date.html """
+	def parse(self, urlobject, encoding="utf8"):
+		return self.fromstring(urlobject.read(), encoding)
+	
+	def fromstring(self, html, encoding="utf8"):
+		""" Parses SourceCode and Scrape Categorys """
+
+		# Class Vars
+		self.divcount = None
+		self.section = None
+		self.nextSearch = False
+		self.pageUrl = None
+		
+		# Proceed with parsing
+		results = []
+		self.reset_lists()
+		self.append = results.append
+		try:
+			if isinstance(html, unicode): self.feed(html)
+			else: self.feed(html.decode(encoding))
+		except EOFError: pass
+		
+		# Return Results
+		return results
+	
+	def reset_lists(self):
+		# Reset List for Next Run
+		self.item = listitem.ListItem()
+		self.item.urlParams["action"] = "PlayVideo"
+		self.artist = self.song = ""
+	
+	def handle_starttag(self, tag, attrs):
+		# Check for required section
+		if tag == u"div":
+			if self.divcount is not None:
+				self.divcount += 1
+				if self.nextSearch is True:
+					for key, value in attrs:
+						if key == u"class" and value == u"pagination":
+							self.nextSearch = 2
+							break
+			else:
+				for key, value in attrs:
+					if key == u"id" and value == u"browse_main":
+						self.divcount = 1
+						break
+		
+		# Fetch Next Page if Available
+		elif tag == u"a" and self.nextSearch == 2:
+			for key, value in attrs:
+				if key == u"href" and value:
+					self.pageUrl = value
+					break
+		
+		# Fetch Required Data
+		elif self.divcount == 3:
+			# Fetch Image
+			if tag == u"img":
+				for key, value in attrs:
+					if key == u"src" and value:
+						self.item.setThumb(value)
+						break
+			
+			# Fetch url
+			elif tag == u"a":
+				for key, value in attrs:
+					if key == u"href" and value and not "url" in self.item.urlParams:
+						# Set Url Param
+						self.item.urlParams["url"] = value
+						
+						# Add Context item to link to related videos
+						self.item.addRelatedContext(url=value[value.rfind(u"_")+1:value.rfind(u".")])
+						break
+			
+			# Fetch Artist & Song Name
+			elif tag == u"span":
+				for key, value in attrs:
+					if key == u"class" and value == u"artist_name":
+						self.section = 101
+						break
+					elif key == u"class" and value == u"song_name":
+						self.section = 102
+						break
+	
+	def handle_data(self, data):
+		# Fetch Artist
+		if self.section == 101:
+			self.artist = data
+			self.item.setInfoDict(artist=[data])
+			self.section = 0
+		
+		# Fetch Song
+		elif self.section == 102:
+			self.song = data
+			self.section = 0
+		
+		# Fetch Next Page
+		elif self.pageUrl:
+			if data.startswith(u"next"): self.item.add_next_page(url={"url":u"http://www.metalvideo.com/%s" % self.pageUrl})
+			self.pageUrl = None
+	
+	def handle_endtag(self, tag):
+		# Decrement div count on div end tag
+		if tag == "div" and self.divcount is not None:
+			self.divcount -= 1
+			if self.divcount == 2:
+				self.item.setLabel(u"%s - %s" % (self.artist, self.song))
+				self.append(self.item.getListitemTuple(True))
+				self.reset_lists()
+			elif self.divcount == 0:
+				raise EOFError
+		
+		# Start Searching for next apge after the ul end tag
+		elif tag == u"ul" and self.section is not None:
+			self.nextSearch = True

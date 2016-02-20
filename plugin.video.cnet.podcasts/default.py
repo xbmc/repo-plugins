7a33ca59ee6bf7ad6ae96320853fb54c71ee8fa2
Martijn Kaijser <mcm.kaijser@gmail.com>
2016-02-20 10:32:43 +0100

    [plugin.video.cnet.podcasts] 1.1.2

--- plugin.video.cnet.podcasts/default.py
+++ plugin.video.cnet.podcasts/default.py
@@ -1,17 +1,17 @@
-﻿import urllib
-import urllib2
-import time
-import re
-from datetime import datetime
-from urlparse import urlparse, parse_qs
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-import xmltodict
 import StorageServer
-from bs4 import BeautifulSoup
-
-import xbmcplugin
-import xbmcgui
+import os
+import re
+import urllib
+import urllib2
 import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import xmltodict
+from bs4 import BeautifulSoup
+from urlparse import parse_qs
 
 cache = StorageServer.StorageServer("cnetpodcasts", 6)
 addon = xbmcaddon.Addon()
@@ -27,19 +27,18 @@ def addon_log(string):
         log_message = string.encode('utf-8', 'ignore')
     except:
         log_message = 'addonException: addon_log'
-    xbmc.log("[%s-%s]: %s" %(addon_id, addon_version, log_message),level=xbmc.LOGDEBUG)
+    xbmc.log("[%s-%s]: %s" % (addon_id, addon_version, log_message), level=xbmc.LOGDEBUG)
 
 
 def make_request(url, post_data=None):
-    addon_log('Request URL: %s' %url)
+    addon_log('Request URL: %s' % url)
     headers = {
         'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
         'Referer': 'http://www.cnet.com'
-        }
+    }
     try:
         req = urllib2.Request(url, post_data, headers)
         response = urllib2.urlopen(req)
-        response_url = urllib.unquote_plus(response.geturl())
         data = response.read()
         response.close()
         return data
@@ -47,32 +46,32 @@ def make_request(url, post_data=None):
         addon_log('We failed to open "%s".' % url)
         if hasattr(e, 'reason'):
             addon_log('We failed to reach a server.')
-            addon_log('Reason: %s' %e.reason)
+            addon_log('Reason: %s' % e.reason)
         if hasattr(e, 'code'):
             addon_log('We failed with error code - %s.' % e.code)
 
 
 def cache_categories():
-    #url = 'http://www.cnet.com/podcasts/'
+    # url = 'http://www.cnet.com/podcasts/'
     url = 'http://www.cnet.com/cnet-podcasts/'
     soup = BeautifulSoup(make_request(url), 'html.parser')
-    items = soup.find_all('a', attrs={'href': re.compile("hd.xml$")} )
+    items = soup.find_all('a', attrs={'href': re.compile("hd.xml$")})
     cats = [{'thumb': '',
              'name': i['href'],
              'desc': '',
              'links': i['href']} for
-             i in items]
+            i in items]
     return cats
 
 
 def display_categories():
     cats = cache.cacheFunction(cache_categories)
     previous_name = ''
-    
-    #add a category
+
+    # add a category
     name = 'Latest Videos'
     add_dir(name, latest_videos_href, 'category', '', {'Plot': ''})
-    
+
     for i in cats:
         name = str(i['name'])
         name = name.replace("http://feed.cnet.com/feed/podcast/", "")
@@ -80,9 +79,9 @@ def display_categories():
         name = name.replace("/", " ")
         name = name.replace("hd.xml", "")
         name = name.capitalize()
-        
-        #skip name if it is the same as the previous name
-        if name == previous_name: 
+
+        # skip name if it is the same as the previous name
+        if name == previous_name:
             pass
         else:
             previous_name = name
@@ -94,22 +93,22 @@ def display_category(links_list):
 
     soup = BeautifulSoup(make_request(url), 'html.parser')
 
-    #latest videos isn't a real category in CNET, therefore this hardcoded stuff was needed
+    # latest videos isn't a real category in CNET, therefore this hardcoded stuff was needed
     if url == latest_videos_href:
         urls = soup.find_all('a', attrs={'href': re.compile("^http://feedproxy.google.com/")})
-        #<a href="http://feedproxy.google.com/~r/cnet/allhdpodcast/~3/4RHUa95GiUM/14n041814_walkingpalua_740.mp4">A walk among hidden graves and WWII bombs</a>
+        # <a href="http://feedproxy.google.com/~r/cnet/allhdpodcast/~3/4RHUa95GiUM/14n041814_walkingpalua_740.mp4">A walk among hidden graves and WWII bombs</a>
         for url in urls:
             title = str(url)
-            title = title.replace('</a>','')
-            title = title.replace("&#039;","'")
+            title = title.replace('</a>', '')
+            title = title.replace("&#039;", "'")
             pos_last_greater_than_sign = title.rfind('>')
             title = title[pos_last_greater_than_sign + 1:]
-            
+
             meta = {'Plot': title,
                     'Duration': '',
                     'Date': '',
                     'Premiered': ''}
-             
+
             add_dir(title, url['href'], 'resolve', 'Defaultvideo.png', meta, False)
     else:
         #     <item>
@@ -132,9 +131,9 @@ def display_category(links_list):
         #           <category>Technology</category>
         #           <pubDate>Fri, 21 Feb 2014 19:49:08 PST</pubDate>
         #     </item>
-        urls = soup.find_all('enclosure', attrs={'url': re.compile("^http")} )
-     
-        #skip 2 titles
+        urls = soup.find_all('enclosure', attrs={'url': re.compile("^http")})
+
+        # skip 2 titles
         title_index = 2
 
         for url in urls:
@@ -143,43 +142,52 @@ def display_category(links_list):
             title_index = title_index + 1
             title = title.replace("<title><![CDATA[", "")
             title = title.replace("]]></title>", "")
-            title = title.replace("&#039;","'")
+            title = title.replace("&#039;", "'")
 
             meta = {'Plot': title,
                     'Duration': '',
                     'Date': '',
                     'Premiered': ''}
-        
+
             add_dir(title, url['url'], 'resolve', 'Defaultvideo.png', meta, False)
 
+
 def resolve_url(video_id):
-    params = {
+    parameters = {
         'iod': 'images,videoMedia,relatedLink,breadcrumb,relatedAssets,broadcast,lowcache',
         'partTag': 'cntv',
         'players': 'Download,RTMP',
         'showBroadcast': 'true',
         'videoIds': video_id,
         'videoMediaType': 'preferred'
-        }
-    data = make_request('http://api.cnet.com/restApi/v1.0/videoSearch?' + urllib.urlencode(params))
+    }
+    data = make_request('http://api.cnet.com/restApi/v1.0/videoSearch?' + urllib.urlencode(parameters))
     pod_dict = xmltodict.parse(data)
-    item = pod_dict['CNETResponse']['Videos']['Video']
-    thumb = [i['ImageURL'] for i in item['Images']['Image'] if i['@width'] == '360'][0]
-    media_items = item['VideoMedias']['VideoMedia']
+    media_item = pod_dict['CNETResponse']['Videos']['Video']
+    # thumb = [i['ImageURL'] for i in media_item['Images']['Image'] if i['@width'] == '360'][0]
+    media_items = media_item['VideoMedias']['VideoMedia']
     stream_urls = [i['DeliveryUrl'] for i in media_items if i['Height'] == '720']
     if stream_urls:
         return stream_urls[0]
 
 
-def add_dir(name, url, mode, iconimage, meta={}, isfolder=True):
-    params = {'name': name, 'url': url, 'mode': mode}
-    url = '%s?%s' %(sys.argv[0], urllib.urlencode(params))
-    listitem = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    if not isfolder:
-        listitem.setProperty('IsPlayable', 'true')
-        listitem.setProperty('Fanart_Image', iconimage)
-    xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, isfolder)
-   
+def add_dir(name, url, modus, iconimage, meta=None, isfolder=True):
+    if meta is None:
+        meta = {}
+    parameters = {'name': name, 'url': url, 'mode': modus}
+    url = '%s?%s' % (sys.argv[0], urllib.urlencode(parameters))
+    list_item = xbmcgui.ListItem(name, thumbnailImage=iconimage)
+    if isfolder:
+        list_item.setProperty('IsPlayable', 'false')
+    else:
+        list_item.setProperty('IsPlayable', 'true')
+    ADDON = "plugin.video.cnet.podcasts"
+    IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
+    list_item.setProperty('Fanart_Image', os.path.join(IMAGES_PATH, 'fanart-blur.jpg'))
+    # Add refresh option to context menu
+    list_item.addContextMenuItems([('Refresh', 'Container.Refresh')])
+    xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, list_item, isfolder)
+
 
 def get_params():
     p = parse_qs(sys.argv[2][1:])
@@ -196,11 +204,11 @@ def set_view_mode():
         '3': '504',
         '4': '503',
         '5': '515'
-        }
+    }
     view_mode = addon.getSetting('view_mode')
     if view_mode == '6':
         return
-    xbmc.executebuiltin('Container.SetViewMode(%s)' %view_modes[view_mode])
+    xbmc.executebuiltin('Container.SetViewMode(%s)' % view_modes[view_mode])
 
 
 params = get_params()

846e8177067138ebd7a29cac150aba3105805544
Martijn Kaijser <mcm.kaijser@gmail.com>
2016-02-28 20:05:00 +0100

    [plugin.video.ted.talks] 4.2.7

--- plugin.video.ted.talks/resources/lib/menu_util_test.py
+++ plugin.video.ted.talks/resources/lib/menu_util_test.py
@@ -7,4 +7,7 @@ class TestNewTalksRss(unittest.TestCase):
 
     def test_create_context_menu(self):
         context_menu = menu_util.create_context_menu(getLS)
-        self.assertEquals([(30097, 'Action(queue)')], context_menu)
+        self.assertEquals([
+            (30097, 'Action(queue)'),
+            ('Toggle watched', 'Action(ToggleWatched)'),
+            ], context_menu)

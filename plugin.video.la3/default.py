2e08d0edbac66df0d5b001dc9601e14e738a39a5
NeverWise <NeverWise@users.noreply.github.com>
2016-05-15 09:31:44 +0200

    Update default.py

--- plugin.video.la3/default.py
+++ plugin.video.la3/default.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-import os, requests
+import os, requests, xbmcplugin
 from neverwise import Util
+
+_handle = int(sys.argv[1])
+xbmcplugin.endOfDirectory(_handle)
 
 li = Util.createListItem(Util._addonName, thumbnailImage = '{0}/icon.png'.format(os.path.dirname(os.path.abspath(__file__))), streamtype = 'video', infolabels = { 'title' : Util._addonName })
 url1 = 'http://h3ghdchan102-i.akamaihd.net/hls/live/217154/Virtual_Channel102/master.m3u8'
@@ -13,3 +16,5 @@ else:
   request = requests.get(url2)
   if request.status_code == 200:
     xbmc.Player().play(url2, li)
+
+xbmc.executebuiltin('ActivateWindow(Videos, addons://sources/video/)')

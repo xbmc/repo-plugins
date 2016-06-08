893171485f8133f064649c1801e17b2c6da28651
skipmodea1 <skipmodea1@outlook.com>
2016-06-08 18:58:23 +0200

    [plugin.video.gamekings] 1.2.3

--- plugin.video.tweakers/addon.py
+++ plugin.video.tweakers/addon.py
@@ -10,41 +10,40 @@ import urlparse
 import xbmc
 import xbmcaddon
 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 LIB_DIR = xbmc.translatePath(
-    os.path.join(xbmcaddon.Addon(id="plugin.video.tweakers").getAddonInfo('path'), 'resources', 'lib'))
+    os.path.join(xbmcaddon.Addon(id="plugin.video.gamekings").getAddonInfo('path'), 'resources', 'lib'))
 sys.path.append(LIB_DIR)
 
-from tweakers_const import ADDON, DATE, VERSION
-
-# Get plugin settings
-DEBUG = xbmcaddon.Addon(id=ADDON).getSetting('debug')
+from gamekings_const import ADDON, SETTINGS, LANGUAGE, IMAGES_PATH, DATE, VERSION
 
 # Parse parameters
 if len(sys.argv[2]) == 0:
     #
     # Main menu
     #
-    if DEBUG == 'true':
-        xbmc.log("[ADDON] %s, Python Version %s" % (ADDON, str(sys.version)), xbmc.LOGNOTICE)
-        xbmc.log("[ADDON] %s v%s (%s) is starting, ARGV = %s" % (ADDON, VERSION, DATE, repr(sys.argv)), xbmc.LOGNOTICE)
-
-    import tweakers_list as plugin
+    xbmc.log("[ADDON] %s, Python Version %s" % (ADDON, str(sys.version)), xbmc.LOGDEBUG)
+    xbmc.log("[ADDON] %s v%s (%s) is starting, ARGV = %s" % (ADDON, VERSION, DATE, repr(sys.argv)),
+                 xbmc.LOGDEBUG)
+    import gamekings_main as plugin
 else:
     action = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['action'][0]
     #
     # List
     #
     if action == 'list':
-        import tweakers_list as plugin
+        import gamekings_list as plugin
     #
     # Play
     #
     elif action == 'play':
-        import tweakers_play as plugin
+        import gamekings_play as plugin
     #
-    # Search
+    # Play
     #
     elif action == 'search':
-        import tweakers_search as plugin
+        import gamekings_search as plugin
 
 plugin.Main()

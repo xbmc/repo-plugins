dce398bb35bf3d731c9484500d876b5d66b009f6
LS80 <github@lee-smith.me.uk>
2016-07-31 12:33:45 +0100

    [plugin.video.spurs-tv] 2.9.0

--- plugin.video.spurs-tv/addon.py
+++ plugin.video.spurs-tv/addon.py
@@ -61,6 +61,9 @@ PAGINATION_FMT = "Pagination1${}"
 
 SEARCH_NAV_FMT = FIELD_NAME_ROOT_FMT.format(0) + PAGINATION_FMT
 
+STADIUM_CAMS = ('0_74tzyqju', '0_4asu087t', '0_4et6qkjl')
+STADIUM_THUMB = HOST + ("/uploadedImages/Shared_Assets/Images/News_images/SEASON_16-17/"
+                        "July_2016/NDP_update/west_elevation_instory.jpg")
 
 plugin = Plugin()
 
@@ -87,9 +90,12 @@ def get_viewstate(soup):
     return soup.find('input', id='__VIEWSTATE')['value']
 
 def get_media_url(entry_id):
-    hls_url = 'hlsvariant://' + HLS_URL_FMT.format(entry_id)
+    hls_url = HLS_URL_FMT.format(entry_id)
+    livestreamer_url = 'hlsvariant://' + hls_url
+    log("Retrieving streams from {}".format(hls_url))
     resolution = plugin.get_setting('resolution')
-    streams = livestreamer.streams(hls_url, sorting_excludes=[">{}".format(resolution)])
+    streams = livestreamer.streams(livestreamer_url, sorting_excludes=[">{}".format(resolution)])
+    log("Available streams: {}".format(' '.join(streams)))
 
     media_url = streams['best'].url
     log("Playing URL {}".format(media_url))
@@ -190,13 +196,23 @@ def get_search_result_videos(soup, query):
         yield video_item(entry_id, title, date_str, date_format="%d %b %Y")
         
     form_data['viewstate'] = get_viewstate(soup)
+
+def get_stadium_index():
+    for cam_num, entry_id in enumerate(STADIUM_CAMS, 1):
+        yield {'label': "{} {}".format(plugin.get_string(30018), cam_num),
+               'path': plugin.url_for('play_video', entry_id=entry_id),
+               'is_playable': True}
+
+    yield {'label': "Stadium TV",
+           'path': plugin.url_for('show_playlist', playlist_id='0_n8hezta2')}
         
 def get_categories(path):
     yield {'label': "[B]{}[/B]".format(plugin.get_string(30010)),
            'path': plugin.url_for('show_video_list', path=path)}
 
-    yield {'label': "Stadium TV",
-           'path': plugin.url_for('show_playlist', playlist_id='0_n8hezta2')}
+    yield {'label': plugin.get_string(30017),
+           'path': plugin.url_for('show_stadium_index'),
+           'thumbnail': STADIUM_THUMB}
 
     url = urljoin(HOST, path)
     soup = get_soup(url)
@@ -208,9 +224,7 @@ def get_categories(path):
 
         href = a['href'].strip('/')
         playable = False
-        if href.endswith("spurs-tv"):
-            plugin_path = plugin.url_for('show_categories', path=href)
-        elif title == "Ledley King Testimonial":
+        if title == "Ledley King Testimonial":
             plugin_path = plugin.url_for('show_playlist', playlist_id='0_2nmzot3u')
         elif title == "The Vault":
             plugin_path = plugin.url_for('show_playlist', playlist_id='0_32nxk7s7')
@@ -299,6 +313,10 @@ def show_categories(path):
 @plugin.cached_route('/path/<path>/subcategories')
 def show_subcategories(path):
     return list(get_subcategories(path))
+
+@plugin.cached_route('/stadium')
+def show_stadium_index():
+    return list(get_stadium_index())
     
 @plugin.route('/videos/path/<path>')
 def show_video_list(path):
@@ -395,9 +413,8 @@ def youtube_search_result(query):
 
 if __name__ == '__main__':
     rollbar.init('45541e2cb1e24f95b9c6311c2e931a11')
-
     try:
         plugin.run()
     except Exception:
-        rollbar.report_exc_info()
+        rollbar.report_exc_info(extra_data={'url': plugin.request.url})
         raise

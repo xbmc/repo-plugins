9dbb135e012cd84359aef1e3adf7faa0f5de6f6c
skipmodea1 <skipmodea1@outlook.com>
2016-09-22 18:04:19 +0200

    [plugin.video.worldstarhiphop] 1.0.7

--- plugin.video.worldstarhiphop/resources/lib/worldstarhiphop_play.py
+++ plugin.video.worldstarhiphop/resources/lib/worldstarhiphop_play.py
@@ -12,6 +12,8 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 import YDStreamExtractor
+import re
+from BeautifulSoup import BeautifulSoup
 
 
 #
@@ -29,13 +31,10 @@ class Main:
         self.plugin_handle = int(sys.argv[1])
 
         # Get plugin settings
-        self.DEBUG = SETTINGS.getSetting('debug')
         self.VIDEO = SETTINGS.getSetting('video')
 
-        if (self.DEBUG) == 'true':
-            print 'Python Version: ' + sys.version
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s, %s = %s" % (
-            ADDON, VERSION, DATE, "ARGV", repr(sys.argv), "File", str(__file__)), xbmc.LOGNOTICE)
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s, %s = %s" % (
+            ADDON, VERSION, DATE, "ARGV", repr(sys.argv), "File", str(__file__)), xbmc.LOGDEBUG)
 
         # Parse parameters...
         self.video_page_url = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['video_page_url'][0]
@@ -43,9 +42,8 @@ class Main:
         self.title = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['title'][0]
         self.title = str(self.title)
 
-        if (self.DEBUG) == 'true':
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-            ADDON, VERSION, DATE, "self.video_page_url", str(self.video_page_url)), xbmc.LOGNOTICE)
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "self.video_page_url", str(self.video_page_url)), xbmc.LOGDEBUG)
 
         #
         # Play video...
@@ -98,24 +96,58 @@ class Main:
             # A bit of a dirty hack, but let's try it anyway...
             # so.addVariable("file","http://hw-videos.worldstarhiphop.com/u/vid/2015/09/SAWGSqGpaohk.mp4");
             if str(html_source).find("file") >= 0:
-                #
                 # Seems like it's an 18+ video
                 begin_pos_video_file = str(html_source).find("http", str(html_source).find("file"))
                 end_pos_video_file = str(html_source).find('"', begin_pos_video_file)
                 video_url = html_source[begin_pos_video_file:end_pos_video_file]
                 have_valid_url = True
             else:
-                unplayable_media_file = True
+                # Maybe it's a youtube video then ?!
+                if str(html_source).find("www.youtube.com/embed") >= 0:
+                    # Seems like it's an youtube video
+                    # <iframe src="http://www.youtube.com/embed/1xcxn7pOYCg?autoplay=1" width="640" height="390" frameborder="0"></iframe>
+                    soup = BeautifulSoup(html_source)
+                    # look for http youtube
+                    video_urls = soup.findAll('iframe', attrs={'src': re.compile("^http://www.youtube.com/embed")}, limit=1)
+                    if len(video_urls) == 0:
+                        # look for https youtube
+                        video_urls = soup.findAll('iframe', attrs={'src': re.compile("^https://www.youtube.com/embed")}, limit=1)
+                        if len(video_urls) == 0:
+                            unplayable_media_file = True
+                        else:
+                            video_url = video_urls[0]['src']
+                            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "video_url", str(video_url)), xbmc.LOGDEBUG)
+                            # make youtube plugin url
+                            pos_of_last_question_mark = video_url.rfind("?")
+                            video_url = video_url[0: pos_of_last_question_mark]
+                            video_url_len = len(video_url)
+                            youtubeID = video_url[len("https://www.youtube.com/embed/"):video_url_len]
+                            youtube_url = 'plugin://plugin.video.youtube/play/?video_id=%s' % youtubeID
+                            have_valid_url = True
+                            video_url = youtube_url
+                    else:
+                        video_url = video_urls[0]['src']
+                        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "video_url", str(video_url)), xbmc.LOGDEBUG)
+                        # make youtube plugin url
+                        pos_of_last_question_mark = video_url.rfind("?")
+                        video_url = video_url[0: pos_of_last_question_mark]
+                        video_url_len = len(video_url)
+                        youtubeID = video_url[len("http://www.youtube.com/embed/"):video_url_len]
+                        youtube_url = 'plugin://plugin.video.youtube/play/?video_id=%s' % youtubeID
+                        have_valid_url = True
+                        video_url = youtube_url
+                else:
+                    unplayable_media_file = True
 
-        if (self.DEBUG) == 'true':
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-            ADDON, VERSION, DATE, "have_valid_url", str(have_valid_url)), xbmc.LOGNOTICE)
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-            ADDON, VERSION, DATE, "video_url", str(video_url)), xbmc.LOGNOTICE)
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "have_valid_url", str(have_valid_url)), xbmc.LOGDEBUG)
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "video_url", str(video_url)), xbmc.LOGDEBUG)
 
         if have_valid_url:
             list_item = xbmcgui.ListItem(path=video_url)
             xbmcplugin.setResolvedUrl(self.plugin_handle, True, list_item)
+
         #
         # Alert user
         #

b91d535f6dbaaafb2251e4903b56745e4c61f196
Petr Kutalek <petr.kutalek@forers.com>
2016-10-19 23:35:24 +0200

    [plugin.video.aktualnetv] 1.2.1

---
+++ plugin.video.aktualnetv/resources/data/setup.js
@@ -0,0 +1,259 @@
+{
+	"topics": [
+		{
+			"href": "auto-moto/l~d4c2abb46f0d11e49b26002590604f2e",
+			"label": "Auto moto"
+		}, 
+		{
+			"href": "byznys/l~1cc84c7868b811e3af700025900fea04",
+			"label": "Byznys"
+		},
+		{
+			"href": "cestovani/l~i:keyword:413",
+			"label": "Cestování"
+		},
+		{
+			"href": "jidlo/l~i:keyword:178",
+			"label": "Jídlo"
+		},
+		{
+			"href": "kultura/l~i:keyword:349",
+			"label": "Kultura"
+		},
+		{
+			"href": "politika/l~i:keyword:109",
+			"label": "Politika"
+		},
+		{
+			"href": "spolecnost/l~i:keyword:2464",
+			"label": "Společnost"
+		},
+		{
+			"href": "ze-sportu/l~b2f4fe58233411e48e74002590604f2e",
+			"label": "Sport"
+		},
+		{
+			"href": "styl/l~i:keyword:3345",
+			"label": "Styl"
+		},
+		{
+			"href": "technologie/l~i:keyword:2194",
+			"label": "Technologie"
+		},
+		{
+			"href": "veda/l~i:keyword:162",
+			"label": "Věda"
+		},
+		{
+			"href": "zahranici/l~i:keyword:18",
+			"label": "Zahraničí"
+		},
+		{
+			"href": "zdravi/l~i:keyword:94",
+			"label": "Zdraví"
+		}
+	],
+
+	"shows": [
+		{
+			"href": "dvtv",
+			"label": "DVTV",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-dvtv-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-dvtv-siroky.jpg"
+				}
+		},
+		{
+			"href": "rallye-dakar-z-prvni-ruky",
+			"label": "Rallye Dakar: Z první ruky",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-rallye-dakar-z-prvni-ruky-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-rallye-dakar-z-prvni-ruky-siroky.jpg"
+				}
+		},
+		{
+			"href": "akademie-ved",
+			"label": "Akademie věd",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-akademie-ved-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-akademie-ved-siroky.jpg"
+			}
+		},
+		{
+			"href": "aktualne-pro-byznys",
+			"label": "Aktuálně probyznys",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-aktualne-pro-byznys-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-aktualne-pro-byznys-siroky.jpg"
+			}
+		},
+		{
+			"href": "auto-report",
+			"label": "Auto report",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-auto-report-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-auto-report-siroky.jpg"
+			}
+		},
+		{
+			"href": "ego-night",
+			"label": "EGO NIGHT",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-egonight-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-egonight-siroky.jpg"
+			}
+		},
+		{
+			"href": "face-to-face",
+			"label": "Face to Face (KPMG, ČBA)",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-face-to-face-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-face-to-face-siroky.jpg"
+			}
+		},
+		{
+			"href": "jak-na-to",
+			"label": "Jak na to",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-jak-na-to-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-jak-na-to-siroky.jpg"
+			}
+		},
+		{
+			"href": "komentar-za-minutu",
+			"label": "Komentář za minutu (HN)",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-komentar-za-minutu-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-komentar-za-minutu-siroky.jpg"
+			}
+		},
+		{
+			"href": "aktualne-s-tm",
+			"label": "Matonoha TV",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-aktualne-s-tm-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-aktualne-s-tm-siroky.jpg"
+				}
+		},
+		{
+			"href": "objektivem-jana-sibika",
+			"label": "Objektivem Jana Šibíka",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-objektivem-jana-sibika-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-objektivem-jana-sibika-siroky.jpg"
+			}
+		},
+		{
+			"href": "petr-ctvrtnicek-tv",
+			"label": "Petr Čtvrtníček TV",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-petr-ctvrtnicek-tv-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-petr-ctvrtnicek-tv-siroky.jpg"
+				}
+		},
+		{
+			"href": "pravni-radce",
+			"label": "Právní rádce",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-pravni-radce-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-pravni-radce-siroky.jpg"
+			}
+		},
+		{
+			"href": "respektovani",
+			"label": "Respektování (Respekt)",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-respektovani-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-respektovani-siroky.jpg"
+			}
+		},
+		{
+			"href": "site",
+			"label": "Sítě Miloše Čermáka",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-site-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-site-siroky.jpg"
+			}
+		},
+		{
+			"href": "start-upy",
+			"label": "Start-upy u tabule",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-start-upy-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-start-upy-siroky.jpg"
+			}
+		},
+		{
+			"href": "strih",
+			"label": "Střih",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-strih-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-strih-siroky.jpg"
+			}
+		},
+		{
+			"href": "svet-na-hrane",
+			"label": "Svět na hraně (RFE)",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-svet-na-hrane-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-svet-na-hrane-siroky.jpg"
+			}
+		},
+		{
+			"href": "sefkucharovy-recepty",
+			"label": "Šefkuchařovy recepty",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-sefkucharovy-recepty-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-sefkucharovy-recepty-siroky.jpg"
+			}
+		},
+		{
+			"href": "tech-news",
+			"label": "Tech News",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-tech-news-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-tech-news-siroky.jpg"
+			}
+		},
+		{
+			"href": "tyden-v-kine",
+			"label": "Týden v kině",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-tyden-v-kine-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-tyden-v-kine-siroky.jpg"
+			}
+		},
+		{
+			"href": "ze-sveta",
+			"label": "Ze světa",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-zesveta-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-zesveta-siroky.jpg"
+			}
+		},
+		{
+			"href": "zabijak-kamila-fily",
+			"label": "(Za)biják Kamila Fily",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-zabijak-kamila-fily-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-zabijak-kamila-fily-siroky.jpg"
+			}
+		},
+		{
+			"href": "zkrotte-sve-penize",
+			"label": "Zkroťte své peníze (Partners)",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-zkrotte-sve-penize-uzky.png",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-zkrotte-sve-penize-siroky.png"
+			}
+		},
+		{
+			"href": "48-hodin-v",
+			"label": "48 hodin v…",
+			"cover": {
+				"thumb": "http://i0.cz/bbx/video/img/video/porad-d-hp-48-hodin-uzky.jpg",
+				"fanart": "http://i0.cz/bbx/video/img/video/porad-d-hp-48-hodin-siroky.jpg"
+			}
+		}
+	]
+}

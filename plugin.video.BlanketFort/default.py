03f27df98573370ed8b30b2a695cdd70a61b43b2
BF Media <blanketfortlife@gmail.com>
2017-01-04 15:32:25 -0500

    [plugin.video.BlanketFort] v1.0.0

---
+++ plugin.video.BlanketFort/default.py
@@ -0,0 +1,366 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib2
+import datetime
+import re
+import os
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
+import xbmcvfs
+from BeautifulSoup import BeautifulStoneSoup, BeautifulSoup, BeautifulSOAP
+try:
+    import json
+except:
+    import simplejson as json
+
+addon = xbmcaddon.Addon(id='plugin.video.BlanketFort')
+profile = addon.getAddonInfo('profile').decode('utf-8')
+home = addon.getAddonInfo('path').decode('utf-8')
+icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
+fanart = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
+
+
+def makeRequest(url):
+
+        try:
+            req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+            response = urllib2.urlopen(req)
+            data = response.read()
+            response.close()
+            return data
+        except urllib2.URLError, e:
+            if hasattr(e, 'code'):
+                xbmc.log('We failed with error code - %s.' % e.code)
+                xbmc.executebuiltin("XBMC.Notification(BlanketFort,We failed with error code - "+str(e.code)+",10000,"+icon+")")
+            elif hasattr(e, 'reason'):
+                xbmc.log('We failed to reach a server.')
+                xbmc.log('Reason: ', e.reason)
+                xbmc.executebuiltin("XBMC.Notification(BlanketFort,We failed to reach a server. - "+str(e.reason)+",10000,"+icon+")")
+
+
+def getSoup(url):
+        if url.startswith('http://'):
+            data = makeRequest(url)
+        soup = BeautifulSOAP(data, convertEntities=BeautifulStoneSoup.XML_ENTITIES)
+        return soup
+
+
+def getData(url,fanart):
+        soup = getSoup(url)
+        if len(soup('channels')) > 0:
+            channels = soup('channel')
+            for channel in channels:
+                name = channel('name')[0].string
+                thumbnail = channel('thumbnail')[0].string
+
+                if thumbnail == None:
+                    thumbnail = ''
+				
+				
+                try:
+                    if not channel('fanart'):
+                        if addon.getSetting('use_thumb') == "true":
+                            fanArt = thumbnail
+                        else:
+                            fanArt = fanart
+                    else:
+                        fanArt = channel('fanart')[0].string
+                    if fanArt == None:
+                        raise
+                except:
+                    fanArt = fanart
+
+                try:
+                    desc = channel('info')[0].string
+                    if desc == None:
+                        raise
+                except:
+                    desc = ''
+
+                try:
+                    genre = channel('genre')[0].string
+                    if genre == None:
+                        raise
+                except:
+                    genre = ''
+
+                try:
+                    date = channel('date')[0].string
+                    if date == None:
+                        raise
+                except:
+                    date = ''
+                try:
+                    addDir(name.encode('utf-8', 'ignore'),url.encode('utf-8'),2,thumbnail,fanArt,desc,genre,date)
+                except:
+                    xbmc.log('There was a problem adding directory from getData(): '+name.encode('utf-8', 'ignore'))
+        else:
+            getItems(soup('item'),fanart)
+
+
+def getChannelItems(name,url,fanart):
+        soup = getSoup(url)
+        channel_list = soup.find('channel', attrs={'name' : name})
+        items = channel_list('item')
+        try:
+            fanArt = channel_list('fanart')[0].string
+            if fanArt == None:
+                raise
+        except:
+            fanArt = fanart
+        for channel in channel_list('subchannel'):
+            name = channel('name')[0].string
+            try:
+                thumbnail = channel('thumbnail')[0].string
+                if thumbnail == None:
+                    raise
+            except:
+                thumbnail = ''
+            try:
+                if not channel('fanart'):
+                    if addon.getSetting('use_thumb') == "true":
+                        fanArt = thumbnail
+                else:
+                    fanArt = channel('fanart')[0].string
+                if fanArt == None:
+                    raise
+            except:
+                pass
+            try:
+                desc = channel('info')[0].string
+                if desc == None:
+                    raise
+            except:
+                desc = ''
+
+            try:
+                genre = channel('genre')[0].string
+                if genre == None:
+                    raise
+            except:
+                genre = ''
+
+            try:
+                date = channel('date')[0].string
+                if date == None:
+                    raise
+            except:
+                date = ''
+            try:
+                addDir(name.encode('utf-8', 'ignore'),url.encode('utf-8'),3,thumbnail,fanArt,desc,genre,date)
+            except:
+                xbmc.log('There was a problem adding directory - '+name.encode('utf-8', 'ignore'))
+        getItems(items,fanArt)
+
+
+def getSubChannelItems(name,url,fanart):
+        soup = getSoup(url)
+        channel_list = soup.find('subchannel', attrs={'name' : name})
+        items = channel_list('subitem')
+        getItems(items,fanart)
+
+
+def getItems(items,fanart):
+        for item in items:
+            try:
+                name = item('title')[0].string
+            except:
+                xbmc.log('-----Name Error----')
+                name = ''
+            try:
+                if item('epg'):
+                    if item('epg')[0].string > 1:
+                        name += getepg(item('epg')[0].string)
+                else:
+                    pass
+            except:
+                xbmc.log('----- EPG Error ----')
+
+            try:
+                url = []
+                for i in item('link'):
+                    url.append(i.string)
+            except:
+                xbmc.log('---- URL Error Passing ----'+name)
+                continue
+
+            try:
+                thumbnail = item('thumbnail')[0].string
+                if thumbnail == None:
+                    raise
+            except:
+                thumbnail = ''
+            try:
+                if not item('fanart'):
+                    if addon.getSetting('use_thumb') == "true":
+                        fanArt = thumbnail
+                    else:
+                        fanArt = fanart
+                else:
+                    fanArt = item('fanart')[0].string
+                if fanArt == None:
+                    raise
+            except:
+                fanArt = fanart
+            try:
+                desc = item('info')[0].string
+                if desc == None:
+                    raise
+            except:
+                desc = ''
+
+            try:
+                genre = item('genre')[0].string
+                if genre == None:
+                    raise
+            except:
+                genre = ''
+
+            try:
+                date = item('date')[0].string
+                if date == None:
+                    raise
+            except:
+                date = ''
+            try:
+                if len(url) > 1:
+                    alt = 0
+                    playlist = []
+                    for i in url:
+                        playlist.append(i)
+                    for i in url:
+                        alt += 1
+                        addLink(i,'%s) %s' %(str(alt), name.encode('utf-8', 'ignore')),thumbnail,fanArt,desc,genre,date,True,playlist)
+                else:
+                    addLink(url[0],name.encode('utf-8', 'ignore'),thumbnail,fanArt,desc,genre,date,True)
+            except:
+                xbmc.log('There was a problem adding link - '+name.encode('utf-8', 'ignore'))
+
+
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+            params=sys.argv[2]
+            cleanedparams=params.replace('?','')
+            if (params[len(params)-1]=='/'):
+                params=params[0:len(params)-2]
+            pairsofparams=cleanedparams.split('&')
+            param={}
+            for i in range(len(pairsofparams)):
+                splitparams={}
+                splitparams=pairsofparams[i].split('=')
+                if (len(splitparams))==2:
+                    param[splitparams[0]]=splitparams[1]
+        return param
+
+def addDir(name,url,mode,iconimage,fanart,description,genre,date,showcontext=True):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&fanart="+urllib.quote_plus(fanart)
+        ok=True
+        liz=xbmcgui.ListItem(name)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        liz.setArt({"thumb": iconimage, "icon": "DefaultFolder.png", "fanart": fanart})
+		
+		
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+
+def addLink(url,name,iconimage,fanart,description,genre,date,showcontext=True,playlist=None):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode=12"
+        ok=True
+        liz=xbmcgui.ListItem(name)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        liz.setArt({"thumb": iconimage, "icon": "DefaultFolder.png", "fanart": fanart})
+        liz.setProperty('IsPlayable', 'true')
+		
+
+        if not playlist is None:
+            playlist_name = name.split(') ')[1]
+            contextMenu_ = [('Play '+playlist_name+' PlayList','XBMC.RunPlugin(%s?mode=13&name=%s&playlist=%s)' %(sys.argv[0], urllib.quote_plus(playlist_name), urllib.quote_plus(str(playlist).replace(',','|'))))]
+            liz.addContextMenuItems(contextMenu_)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        return ok
+
+
+
+params=get_params()
+
+url=None
+name=None
+mode=None
+playlist=None
+
+
+try:
+    url=urllib.unquote_plus(params["url"]).decode('utf-8')
+except:
+    pass
+try:
+    name=urllib.unquote_plus(params["name"])
+except:
+    pass
+try:
+    iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+    pass
+try:
+    fanart=urllib.unquote_plus(params["fanart"])
+except:
+    pass
+try:
+    mode=int(params["mode"])
+except:
+    pass
+try:
+    playlist=eval(urllib.unquote_plus(params["playlist"]).replace('|',','))
+except:
+    pass
+
+xbmc.log("Mode: "+str(mode))
+if not url is None:
+    xbmc.log("URL: "+str(url.encode('utf-8')))
+xbmc.log("Name: "+str(name))
+
+
+if mode==None:
+    xbmc.log("getData")
+    data = 'http://blanketfortlife.com/kodi/bftv.xml' 
+    getData(data, fanart)
+
+elif mode==2:
+    xbmc.log("getChannelItems")
+    getChannelItems(name,url,fanart)
+
+elif mode==3:
+    xbmc.log("")
+    getSubChannelItems(name,url,fanart)
+
+
+elif mode==5:
+    xbmc.log("")
+    try:
+        name = name.split('\\ ')[1]
+    except:
+        pass
+    try:
+        name = name.split('  - ')[0]
+    except:
+        pass
+    addFavorite(name,url,iconimage,fanart)
+
+
+
+
+elif mode==12:
+    xbmc.log("setResolvedUrl")
+    item = xbmcgui.ListItem(path=url)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+elif mode==13:
+    xbmc.log("play_playlist")
+    play_playlist(name, playlist)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))

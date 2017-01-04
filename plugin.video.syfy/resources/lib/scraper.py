14c404af70868e23b6e0036e7863b019f23efbfe
learningit <scottyroscoe13@gmail.com>
2017-01-03 19:33:23 -0500

    [plugin.video.syfy] 3.0.3

--- plugin.video.syfy/resources/lib/scraper.py
+++ plugin.video.syfy/resources/lib/scraper.py
@@ -22,6 +22,7 @@ UTF8 = 'utf-8'
 class myAddon(t1mAddon):
 
  def getAddonMenu(self,url,ilist):
+     xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
      epiHTML = self.getRequest('http://www.syfy.com/episodes')
      posterHTML = self.getRequest('http://www.syfy.com/shows')
      posters = re.compile('<div class="grid-image-above">.+?<img  srcset="(.+?)".+?class="title">.+?>(.+?)<',re.DOTALL).findall(posterHTML)
@@ -39,25 +40,34 @@ class myAddon(t1mAddon):
          infoList['Studio'] = 'Syfy'
          url = name
          contextMenu = [('Add To Library','XBMC.RunPlugin(%s?mode=DF&url=AL%s)' % (sys.argv[0], url))]
+         url = name.replace(" ","")
+         url = 'http://www.syfy.com/%s/episodes?page=0' % url
          ilist = self.addMenuItem(name,'GE', ilist, url, poster, self.addonFanart, infoList, isFolder=True, cm=contextMenu)
+     ilist = self.addMenuItem('Movies','GM', ilist, 'movies', self.addonIcon, self.addonFanart, [], isFolder=True)
      return(ilist)
 
 
 
+
  def getAddonEpisodes(self,url,ilist, getFileData = False):
-     sname = url
      self.defaultVidStream['width'] = 1920
      self.defaultVidStream['height']= 1080
-     html = self.getRequest('http://www.syfy.com/episodes')
-     m = re.compile('<div class="show id.+?<h3>'+sname+'</h3>(.+?)<div class="view-footer">',re.DOTALL).search(html)
-     if m is None:
-         return(ilist)
-     fd = re.compile('href="(.+?)/videos').search(html,m.start(1),m.end(1)).group(1)
-     epis = re.compile('href="'+fd+'(.+?)"',re.DOTALL).findall(html,m.start(1),m.end(1))
+     html = self.getRequest(url)
+     epis = re.compile('<div class="views-row.+?href="(.+?)"',re.DOTALL).findall(html)
+     nextPrompt = None
+     nextUrl = re.compile('class="pager-next first last">(.+?)</li>', re.DOTALL).search(html)
+     if not nextUrl is None:
+         nextUrl = re.compile('href="(.+?)">(.+?)<', re.DOTALL).search(nextUrl.group(1))
+         if not nextUrl is None:
+             nextUrl, nextPrompt = nextUrl.groups()
+             nextUrl = 'http://www.syfy.com'+nextUrl 
      for shurl in epis:
-         url = 'http://www.syfy.com%s' % fd+shurl
+         url = shurl
          html = self.getRequest(url)
-         purl = re.compile('data-src="(.+?)"',re.DOTALL).search(html).group(1)
+         purl = re.compile('data-src="(.+?)"',re.DOTALL).search(html)
+         if purl is None:
+             continue
+         purl = purl.group(1)
          purl = 'http:'+purl.replace('&amp;','&')
          html = self.getRequest(purl)
          purl = re.compile('<link rel="alternate" href=".+?<link rel="alternate" href="(.+?)"',re.DOTALL).search(html).group(1)
@@ -72,7 +82,7 @@ class myAddon(t1mAddon):
          infoList['Aired'] = infoList['Date']
          infoList['Duration'] = str(int(a['duration']/1000))
          infoList['MPAA'] = a['ratings'][0]['rating']
-         infoList['TVShowTitle'] = sname
+         infoList['TVShowTitle'] = a['categories'][0]['name'].replace('Series/','',1)
          infoList['Title'] = a['title']
          infoList['Studio'] = a['provider']
          infoList['Genre'] = (a['nbcu$advertisingGenre']).replace('and','/')
@@ -82,15 +92,64 @@ class myAddon(t1mAddon):
          if infoList['Plot'] in [None, '']:
              infoList['Plot'] = a.get('abstract')
          infoList['Plot'] = h.unescape(infoList['Plot'])
+         infoList['mediatype'] = 'episode'
          url = a["captions"][0]["src"]
          url = url.split('/caption/',1)[1]
          url = url.split('.',1)[0]
          if getFileData == False:
-             ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart, infoList, isFolder=False)
+             ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart , infoList, isFolder=False)
          else:
              if not 'Spanish_Subtitles' in url:
                  ilist.append((infoList.get('Season',''), infoList.get('Episode',''), url))
+     if not nextUrl is None:
+         ilist = self.addMenuItem('[COLOR blue]'+nextPrompt+'[/COLOR]','GE', ilist, nextUrl, self.addonIcon, self.addonFanart, [], isFolder=True)
+
      return(ilist)
+
+ def getAddonMovies(self,url,ilist):
+     self.defaultVidStream['width'] = 1920
+     self.defaultVidStream['height']= 1080
+     html = self.getRequest('http://www.syfy.com/movies')
+     html = re.compile('<div class="row-3">(.+?)<div class="row-4">', re.DOTALL).search(html).group(1)
+     epis = re.compile('class="title"><a href="(.+?)"',re.DOTALL).findall(html)
+     for shurl in epis:
+         url = 'http://www.syfy.com'+shurl
+         html = self.getRequest(url)
+         purl = re.compile('data-src="(.+?)"',re.DOTALL).search(html)
+         if purl is None:
+             continue
+         purl = purl.group(1)
+         purl = 'http:'+purl.replace('&amp;','&')
+         html = self.getRequest(purl)
+         purl = re.compile('<link rel="alternate" href=".+?<link rel="alternate" href="(.+?)"',re.DOTALL).search(html).group(1)
+         purl += '&format=Script&height=576&width=1024'
+         html = self.getRequest(purl)
+         a = json.loads(html)
+         name = a['title']
+         fanart = a['defaultThumbnailUrl']
+         thumb = a['defaultThumbnailUrl']
+         infoList = {}
+         infoList['Date'] = datetime.datetime.fromtimestamp(a['pubDate']/1000).strftime('%Y-%m-%d')
+         infoList['Aired'] = infoList['Date']
+         infoList['Duration'] = str(int(a['duration']/1000))
+         infoList['MPAA'] = a['ratings'][0]['rating']
+         infoList['TVShowTitle'] = a['categories'][0]['name'].replace('Series/','',1)
+         infoList['Title'] = a['title']
+         infoList['Studio'] = a['provider']
+         infoList['Genre'] = (a['nbcu$advertisingGenre']).replace('and','/')
+         infoList['Episode'] = a.get('pl1$episodeNumber')
+         infoList['Season'] = a.get('pl1$seasonNumber')
+         infoList['Plot'] = a.get('description')
+         if infoList['Plot'] in [None, '']:
+             infoList['Plot'] = a.get('abstract')
+         infoList['Plot'] = h.unescape(infoList['Plot'])
+         infoList['mediatype'] = 'movie'
+         url = a["captions"][0]["src"]
+         url = url.split('/caption/',1)[1]
+         url = url.split('.',1)[0]
+         ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart , infoList, isFolder=False)
+     return(ilist)
+
 
  def doFunction(self, url):
      func = url[0:2]
@@ -121,9 +180,11 @@ class myAddon(t1mAddon):
      u = gvu1 % (url, str(unow), str(unow+60))
      liz = xbmcgui.ListItem(path = u)
      infoList ={}
+     infoList['mediatype'] = xbmc.getInfoLabel('ListItem.DBTYPE')
      infoList['Title'] = xbmc.getInfoLabel('ListItem.Title')
      infoList['TVShowTitle'] = xbmc.getInfoLabel('ListItem.TVShowTitle')
      infoList['Year'] = xbmc.getInfoLabel('ListItem.Year')
+     infoList['Premiered'] = xbmc.getInfoLabel('Premiered')
      infoList['Plot'] = xbmc.getInfoLabel('ListItem.Plot')
      infoList['Studio'] = xbmc.getInfoLabel('ListItem.Studio')
      infoList['Genre'] = xbmc.getInfoLabel('ListItem.Genre')

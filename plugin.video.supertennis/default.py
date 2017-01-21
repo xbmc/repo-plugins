091e7b6693452c495dceda113096665bc7aefbbd
Andrea Musuruane <musuruan@gmail.com>
2017-01-21 18:06:31 +0100

    [plugin.video.supertennis] 1.2.1

--- plugin.video.supertennis/default.py
+++ plugin.video.supertennis/default.py
@@ -5,7 +5,8 @@ import xbmcgui
 import xbmcplugin
 import xbmcaddon
 import urllib
-from resources.lib.supertennis import SuperTennis
+import urlparse
+from resources.lib.live import Live
 
 # plugin constants
 __plugin__ = "plugin.video.supertennis"
@@ -17,9 +18,41 @@ Icon = os.path.join(Addon.getAddonInfo('path'), 'icon.png')
 # plugin handle
 handle = int(sys.argv[1])
 
-supertennis = SuperTennis()
-url = supertennis.getUrl()
+# utility functions
+def parameters_string_to_dict(parameters):
+    ''' Convert parameters encoded in a URL to a dict. '''
+    paramDict = dict(urlparse.parse_qsl(parameters[1:]))
+    return paramDict
+ 
+def addDirectoryItem(parameters, li):
+    url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+    return xbmcplugin.addDirectoryItem(handle=handle, url=url, 
+        listitem=li, isFolder=True)
 
-item=xbmcgui.ListItem("SuperTennis", thumbnailImage=Icon)
-item.setInfo(type="Video", infoLabels={"Title": "SuperTennis"})
-xbmc.Player().play(url, item)
+def addLinkItem(parameters, li):
+    url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+    return xbmcplugin.addDirectoryItem(handle=handle, url=url, 
+        listitem=li, isFolder=False)
+
+# UI builder functions
+def show_root_menu():
+    liStyle=xbmcgui.ListItem("Live", thumbnailImage=Icon)
+    liStyle.setProperty('IsPlayable', 'true')
+    addLinkItem({"mode": "live"}, liStyle)
+    xbmcplugin.endOfDirectory(handle=handle, succeeded=True)
+
+def play_live():
+    live = Live()
+    url = live.getUrl()
+    liStyle=xbmcgui.ListItem(path=url)
+    xbmcplugin.setResolvedUrl(handle=handle, succeeded=True, listitem=liStyle)
+
+# parameter values
+params = parameters_string_to_dict(sys.argv[2])
+mode = str(params.get("mode", ""))
+url = str(params.get("url", ""))
+
+if mode == "":
+    show_root_menu()
+elif mode == "live":
+    play_live()

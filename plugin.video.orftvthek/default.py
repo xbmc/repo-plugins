e8128787f1ea573ffb8ff0d3e84a9735b7af0e5f
s0faking <sofaking@gettingmoney.at>
2017-02-08 23:27:18 +0100

    [plugin.video.orftvthek] 0.7.1 (#954)

--- plugin.video.orftvthek/default.py
+++ plugin.video.orftvthek/default.py
@@ -18,7 +18,7 @@ except:
 socket.setdefaulttimeout(30)
 cache = StorageServer.StorageServer("plugin.video.orftvthek", 999999)
 
-version = "0.7.0"
+version = "0.7.1"
 plugin = "ORF-TVthek-" + version
 author = "sofaking,Rechi"
 

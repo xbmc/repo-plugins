75f0c8b81a564b6f15d40e95720c956293ce8e0f
skipmodea1 <skipmodea1@outlook.com>
2017-03-04 10:30:09 +0100

    [plugin.video.botchamania] 1.1.5

--- plugin.video.botchamania/resources/lib/botchamania_const.py
+++ plugin.video.botchamania/resources/lib/botchamania_const.py
@@ -11,6 +11,6 @@ ADDON = "plugin.video.botchamania"
 SETTINGS = xbmcaddon.Addon(id=ADDON)
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
-DATE = "2016-08-20"
-VERSION = "1.1.4"
+DATE = "2017-03-03"
+VERSION = "1.1.5"
 

06dfa1eec727dae6b8a9a498f0a6eb68b8445334
skipmodea1 <skipmodea1@outlook.com>
2017-03-18 17:52:10 +0100

    [plugin.video.gamekings] 1.2.6 (#1058)

--- plugin.video.gamekings/resources/lib/gamekings_const.py
+++ plugin.video.gamekings/resources/lib/gamekings_const.py
@@ -12,5 +12,5 @@ SETTINGS = xbmcaddon.Addon(id=ADDON)
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
 BASE_URL_GAMEKINGS_TV = "http://www.gamekings.tv/"
-DATE = "2017-03-10"
-VERSION = "1.2.5"
+DATE = "2017-03-12"
+VERSION = "1.2.6"

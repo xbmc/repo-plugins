44767c974a2d9d9af8ae7070653331292c298e5a
skipmodea1 <skipmodea1@outlook.com>
2017-03-19 07:10:16 +0100

    [plugin.video.nlhardwareinfo] 1.0.6

--- plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_const.py
+++ plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_const.py
@@ -11,5 +11,5 @@ ADDON = "plugin.video.nlhardwareinfo"
 SETTINGS = xbmcaddon.Addon(id=ADDON)
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
-DATE = "2016-02-12"
-VERSION = "1.0.5"
+DATE = "2017-03-12"
+VERSION = "1.0.6"

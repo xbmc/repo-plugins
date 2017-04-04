901b4238c571b184e2652c590e47ce7a4b0adda4
NextStepUnione <nextstep@nextstep.it>
2017-04-04 10:31:27 +0200

    [plugin.video.videolina] 1.0.0

---
+++ addon.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import urllib
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import logging
+from operator import itemgetter
+
+listitem =xbmcgui.ListItem ('Videolina')
+listitem.setInfo('video', {'Title': 'Videolina', 'Genre': ''})
+xbmc.Player().play('http://178.33.229.111/live/Videolina/playlist.m3u8', listitem, False)

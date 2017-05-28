d276a2965a8f8d6e8a86b1d844ef2d9b83e0f6a5
learningit <scottyroscoe13@gmail.com>
2017-05-28 10:34:33 -0400

    [plugin.video.foodnetwork.canada] 3.0.3

--- plugin.video.foodnetwork.canada/resources/lib/scraper.py
+++ plugin.video.foodnetwork.canada/resources/lib/scraper.py
@@ -17,50 +17,65 @@ import sys
 h = HTMLParser.HTMLParser()
 qp  = urllib.quote_plus
 uqp = urllib.unquote_plus
-UTF8     = 'utf-8'
+UTF8  = 'utf-8'
+
 
 class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
-     html = self.getRequest('http://www.foodnetwork.ca/video/')
-     a = re.compile('ShawVideoDrawer\(.+?drawerTitle: "(.+?)".+?categories: "(.+?)".+?\);',re.DOTALL).findall(html)
-     fanart = self.addonFanart
-     for i, (name, xurl) in list(enumerate(a[1:], start=1)):
-         name = name.decode(UTF8)
-         url = xurl
-         thumb  = self.addonIcon
-         fanart = self.addonFanart
-         infoList = {}
-         ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True)
+     html = self.getRequest('http://www.foodnetwork.ca/shows/')
+     html = re.compile('<section class="top-shows">(.+?)</ul>', re.DOTALL).search(html).group(1)
+     a = re.compile('href="(.+?)".+?data-src="(.+?)".+?<h3>(.+?)<(.+?)</li>', re.DOTALL).findall(html)
+     infoList = {}
+     infoList['mediatype'] = 'tvshow'
+     for url, thumb, name, extracrap in a:
+        if 'Full Episodes' in extracrap:
+           name = name.decode(UTF8)
+           name = h.unescape(name)
+           thumb = thumb.replace(' ','%20') #really? who the fuck puts spaces in urls?
+           fanart = thumb
+           ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True)
      return(ilist)
 
 
   def getAddonEpisodes(self,url,ilist):
     self.defaultVidStream['width']  = 960
     self.defaultVidStream['height'] = 540
-    geurl = uqp(url).replace(' ','%20')
-    url = 'http://feed.theplatform.com/f/dtjsEC/hoR6lzBdUZI9?byCategories=%s&form=cjson&count=true&startIndex=1&endIndex=150&fields=id,content,defaultThumbnailUrl,title,description,:show,:season,:episode,pubdate,availableDate,:clipType,restrictionID,:allowHTML5&sort=pubDate|desc&callback=' % geurl
-    html  = self.getRequest(url)
+    if not url.endswith('/video/'):
+        if url.endswith('/'):
+           url += 'video/'
+        else:
+           url += '/video/'
+    if not url.startswith('http:'):
+        url = 'http://www.foodnetwork.ca'+url
+    html = self.getRequest(url)
+    endpoint, geurl = re.compile('endpoint: "(.+?)".+?categories: "(.+?)"', re.DOTALL).search(html).groups()
+    url = 'http:%s?byCategories=%s&form=cjson&count=true&startIndex=1&endIndex=150&fields=id,content,defaultThumbnailUrl,title,description,:show,:season,:episode,pubdate,availableDate,:clipType,restrictionID,:allowHTML5&sort=pubDate|desc&callback=' % (endpoint, geurl)
+
+    html = self.getRequest(url)
     a = json.loads(html)['entries']
     for b in a:
-      url     = b['content'][0]['url']
-      url    += '&manifest=m3u'
-      name    = h.unescape(b['title']).encode(UTF8)
-      fanart  = b['defaultThumbnailUrl'].rsplit('_',2)[0]+'.png'
-      thumb   = fanart
+      url = b['content'][0]['url']
+      url += '&manifest=m3u'
+      name = h.unescape(b['title']).encode(UTF8)
+      fanart = b['defaultThumbnailUrl']
+      thumb = fanart
       infoList = {}
-      infoList['Duration']    = int(b['content'][0]['duration'])
-      infoList['Title']       = name
-      infoList['Studio']      = b.get('pl1$network')
-      infoList['Date']        = datetime.datetime.fromtimestamp(b['pubDate']/1000).strftime('%Y-%m-%d')
-      infoList['Aired']       = infoList['Date']
-      infoList['Year']        = int(infoList['Date'].split('-',1)[0])
+      infoList['Duration'] = int(b['content'][0]['duration'])
+      infoList['Title'] = name
+      infoList['Studio'] = b.get('pl1$network')
+      infoList['Date'] = datetime.datetime.fromtimestamp(b['pubDate']/1000).strftime('%Y-%m-%d')
+      infoList['Aired'] = infoList['Date']
+      infoList['Year'] = int(infoList['Date'].split('-',1)[0])
       episode = b.get('pl1$episode')
-      if episode is not None: infoList['Episode'] = int(episode)
+      if episode is not None and episode.isdigit():
+          infoList['Episode'] = int(episode)
       season = b.get('pl1$season')
-      if season is not None: infoList['Season']  = int(season)
-      else: infoList['Season']  = 1
-      infoList['Plot']        = h.unescape(b["description"])
+      if season is not None and season.isdigit():
+          infoList['Season']  = int(season)
+      else:
+          infoList['Season'] = 1
+      infoList['Plot'] = h.unescape(b["description"])
       infoList['TVShowTitle'] = b['pl1$show']
       infoList['mediatype'] = 'episode'
       ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart, infoList, isFolder=False)
@@ -72,16 +87,13 @@ class myAddon(t1mAddon):
      url = re.compile('video src="(.+?)"', re.DOTALL).search(html).group(1)
      m3ranges = re.compile('_,(.+?),_', re.DOTALL).search(url)
      if m3ranges is not None:
-       m3ranges = m3ranges.group(1)
-       url = url.replace(m3ranges, 'highest,medium', 1)
+         m3ranges = m3ranges.group(1)
+         url = url.replace(m3ranges, 'highest,medium', 1)
      suburl = re.compile('textstream src="(.+?)"', re.DOTALL).search(html)
      html = self.getRequest(url) #needs proxy
      url = re.compile('http(.+?)\n', re.DOTALL).search(html).group(0).strip()
      liz = xbmcgui.ListItem(path = url)
      if suburl is not None: 
-        suburl=suburl.group(1).replace('.ttml','.vtt')
-        liz.setSubtitles([suburl])
+         suburl=suburl.group(1).replace('.ttml','.vtt')
+         liz.setSubtitles([suburl])
      xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
-
-
-

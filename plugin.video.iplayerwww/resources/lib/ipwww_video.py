822b97e0f35abae2a46d436338955e4be24b0625
CaptainT <CaptainTK@users.noreply.github.com>
2017-05-30 19:03:21 +0200

    [plugin.video.iplayerwww] 2.5.16

--- plugin.video.iplayerwww/resources/lib/ipwww_video.py
+++ plugin.video.iplayerwww/resources/lib/ipwww_video.py
@@ -1561,6 +1561,7 @@ def ScrapeAvailableStreams(url):
         for stream in json_data['episode']['versions']:
             if ((stream['kind'] == 'original') or
                (stream['kind'] == 'iplayer-version') or
+               (stream['kind'] == 'technical-replacement') or
                (stream['kind'] == 'editorial')):
                 stream_id_st = stream['id']
             elif ((stream['kind'] == 'signed') and

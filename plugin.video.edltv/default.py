b3bc711b0fc36aed0f9e7a6db84f6f795be3fdb2
Kritsana-Punyaporn <s55160483@gmail.com>
2017-06-12 10:29:52 +0700

    [plugin.video.edltv] 1.0.0

--- plugin.video.edltv/default.py
+++ plugin.video.edltv/default.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Module: default
-# Author: Roman V. M.
-# Created on: 28.11.2014
+# Author: Kritsana Punyaporn
+# Created on: 01.03.2017
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-import sqlite3
+
 import json
 import sys
 import os
@@ -14,54 +14,68 @@ from urlparse import parse_qsl
 import xbmcgui
 import xbmcplugin
 import xbmcvfs
+import xbmcaddon
 
 _url = sys.argv[0]
 _handle = int(sys.argv[1])
+cloud_path = ""
 string = ""
 word_list = []
+study_time = []
+addon         = xbmcaddon.Addon('plugin.video.edltv')
+__language__  = addon.getLocalizedString
 
 def check_internet_connected () :
     for timeout in [1,5,10,15]:
         try:
-            print "checking internet connection.."
+            xbmc.log("checking internet connection..")
             socket.setdefaulttimeout(timeout)
             host = socket.gethostbyname("www.google.com")
             s = socket.create_connection((host, 80), 2)
             s.close()
             return True
         except Exception,e:
-            print e
+            xbmc.log(str(e))
     return False
 
 def set_period(code) :
-    rows = c.execute('SELECT * FROM edltv WHERE parent LIKE ?',(code,))
-    for row in rows :
-        period.append(row[0])
-    if period :
-        maxperiod = str(max(period))[7:8]
-        if maxperiod == "0":
-            list_videos(str(max(period))[0:8])
+    with open(database_path) as database:
+        data = json.load(database)
+        my_list = list(filter(lambda d: code.decode('utf8') == d['parent'], data['edltv']))
+        for item in my_list :
+            study_time.append(item['code'])
+        if study_time :
+            maxstudy_time = study_time[-1][8:9]
+            if maxstudy_time == "0" :
+                list_videos(study_time[-1][0:9])
+            else :
+                for i in range(1,int(maxstudy_time)+1) :
+                    data = __language__(33040).encode('utf8') + str(i)
+                    class_code = code + str(i)
+                    list_item = xbmcgui.ListItem(label=data)
+                    list_item.setInfo('videos', {'mediatype' : 'video'})
+                    url = get_url(action='listing', category=class_code, hasfolder='no')
+                    is_folder = True
+                    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+                xbmcplugin.endOfDirectory(_handle)
         else :
-            for i in range(1, int(maxperiod)+1) :
-                data = 'คาบที่ '+str(i)
-                period_code = code+str(i)
-                list_item = xbmcgui.ListItem(label=data)
-                url = get_url(action='listing', category=period_code, hasfolder='no')
-                is_folder = True
-                xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-            xbmcplugin.endOfDirectory(_handle)
-    else :
-        dialog = xbmcgui.Dialog()
-        ok = dialog.ok('eDLTV', 'ขออภัย หัวข้อนี้ไม่มีวิดีโอ')
+            dialog = xbmcgui.Dialog()
+            ok = dialog.ok('eDLTV', __language__(33031).encode('utf8'))
 
 def set_class(code=None):
-    category = ['ค้นหา',
-                'ชั้นมัธยมศึกษาปีที่ 1', 'ชั้นมัธยมศึกษาปีที่ 2',
-                'ชั้นมัธยมศึกษาปีที่ 3', 'ชั้นมัธยมศึกษาปีที่ 4',
-                'ชั้นมัธยมศึกษาปีที่ 5', 'ชั้นมัธยมศึกษาปีที่ 6']
+    category = [__language__(33007).encode('utf8'), __language__(33008).encode('utf8'),
+                __language__(33009).encode('utf8'), __language__(33010).encode('utf8'),
+                __language__(33011).encode('utf8'), __language__(33012).encode('utf8')]
     if code == None:
+        list_item = xbmcgui.ListItem(label=__language__(33000).encode('utf8'))
+        list_item.setInfo('videos', {'mediatype' : 'video'})
+        url = get_url(action='search', hasfolder='yes')
+        is_folder = True
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
         for data in category :
             list_item = xbmcgui.ListItem(label=data)
+            list_item.setInfo('videos', {'mediatype' : 'video'})
             url = get_url(action='listing', category=data, hasfolder='yes')
             is_folder = True
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
@@ -71,69 +85,80 @@ def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 def get_data(code,head=None):
-    if head != None:
-        rows = c.execute('SELECT * FROM edltv WHERE LENGTH(code) <= 3 AND code LIKE ?',("%"+code,))
-        rows = c.fetchall()
-        return rows
-    else:
-        if len(code) == 8 :
-            rows = c.execute('SELECT * FROM edltv WHERE code LIKE ?',(code+'%',))
-            rows = c.fetchall()
-            return rows
-        else :
-            rows = c.execute('SELECT * FROM edltv WHERE parent LIKE ?',(code,))
-            rows = c.fetchall()
-            return rows
+    print "code data"
+    print code
+    with open(database_path) as database:
+        data = json.load(database)
+        if head != None:
+            my_list = list(filter(lambda d: code.decode('utf8') in d['description'] and d['parent'] == 'NULL', data['edltv']))
+            return my_list
+        else:
+            if len(code) == 9 :
+                my_list = list(filter(lambda d: code.decode('utf8') in d['code'], data['edltv']))
+                return my_list
+            else :
+                my_list = list(filter(lambda d: code.decode('utf8') == d['parent'], data['edltv']))
+                return my_list
 
 def list_categories(category):
-    if len(category) > 10 :
-        classes = get_data(category[-1:],0)
+    header = False
+    item_code = 0
+    for i in range(33001,33012) :
+        if category == __language__(i).encode('utf8') :
+            header = True
+            break;
+
+    if header :
+        classes = get_data(category,0)
         for data in classes:
-            list_item = xbmcgui.ListItem(label=data[1])
-            url = get_url(action='listing', category=data[0], hasfolder='yes')
+            list_item = xbmcgui.ListItem(label=data['name'])
+            list_item.setInfo('videos', {'mediatype' : 'video'})
+            url = get_url(action='listing', category=data['code'], hasfolder='yes')
             is_folder = True
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         xbmcplugin.endOfDirectory(_handle)
-    elif len(category) == 7 :
-        set_period(category)
-    else :
-        lessons = get_data(category,None)
-        for data in lessons:
-            list_item = xbmcgui.ListItem(label=data[1])
-            url = get_url(action='listing', category=data[0], hasfolder='yes')
-            is_folder = True
-            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-        xbmcplugin.endOfDirectory(_handle)
+    else :    
+        if len(category) == 8 :
+            set_period(category)
+        else :
+            lessons = get_data(category,None)
+            for data in lessons:
+                list_item = xbmcgui.ListItem(label=data['name'])
+                list_item.setInfo('videos', {'mediatype' : 'video'})
+                url = get_url(action='listing', category=data['code'], hasfolder='yes')
+                is_folder = True
+                xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+            xbmcplugin.endOfDirectory(_handle)
 
 def list_videos(category) :
     cloud_path = get_socket_data ()
-    if cloud_path :
-        cloud_path = cloud_path + '/Public/edltv/eDLTVA/htdocs/'
-        host_path = "smb://"
+    cloud_path = "smb://" + str(cloud_path) + "/Public/edltv/eDLTVA/htdocs/"
     lessons = get_data(category,None)
-    if cloud_path :
+    print "test data"
+    print lessons
+    if xbmcvfs.exists(cloud_path) :
         for data in lessons :
-            list_item = xbmcgui.ListItem(label=data[1])
-            list_item.setInfo('video', {'title': data[1]})
+            list_item = xbmcgui.ListItem(label=data['name'])
+            list_item.setInfo('video', {'title': data['name'],'mediatype' : 'video'})
             list_item.setProperty('IsPlayable', 'true')
-            url = get_url(action='play', video=host_path+cloud_path+data[4])
+            url = get_url(action='play', video=cloud_path+data['location'].encode('utf8'))
             is_folder = False
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         xbmcplugin.endOfDirectory(_handle)
     elif check_internet_connected() :
         dialog = xbmcgui.Dialog()
-        ok = dialog.ok('eDLTV', 'ขั้นตอนนี้อาจมีค่าใช้บริการเมื่อต้องการเล่นวิดีโอ เนื่องจากมีการเชื่อมต่ออินเทอร์เน็ต')
+        ok = dialog.ok('eDLTV', __language__(33033).encode('utf8'))
         for data in lessons :
-            list_item = xbmcgui.ListItem(label=data[1])
-            list_item.setInfo('video', {'title': data[1]})
+            list_item = xbmcgui.ListItem(label=data['name'])
+            list_item.setInfo('video', {'title': data['name'],'mediatype' : 'video'})
             list_item.setProperty('IsPlayable', 'true')
-            url = get_url(action='play', video=data[7])
+            url = get_url(action='play', video=data['url'].encode('utf8'))
             is_folder = False
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         xbmcplugin.endOfDirectory(_handle)
     else :
         dialog = xbmcgui.Dialog()
-        ok = dialog.ok('eDLTV', 'ไม่สามารถติดต่อกับเซิฟเวอร์ได้เนื่องจากไม่ได้เชื่อมต่อเครือข่ายหรือไม่มีการเชื่อมต่ออินเทอร์เน็ต')
+        ok = dialog.ok('eDLTV', __language__(33030).encode('utf8'))
 
 def play_video(path):
     play_item = xbmcgui.ListItem(path=path)
@@ -141,53 +166,47 @@ def play_video(path):
 
 def set_search_list (l) :
     cloud_path = get_socket_data ()
-    if cloud_path :
-        cloud_path = cloud_path + '/Public/edltv/eDLTVA/htdocs/'
-        host_path = "smb://"
-    
-    if cloud_path :
+    cloud_path = "smb://" + str(cloud_path) + "/Public/edltv/eDLTVA/htdocs/"
+    if xbmcvfs.exists(cloud_path) :
         for data in l :
-            list_item = xbmcgui.ListItem(label=data[1])
-            list_item.setInfo('video', {'title': data[1]})
+            list_item = xbmcgui.ListItem(label=data['name'].encode('utf8'))
+            list_item.setInfo('video', {'title': data['name'].encode('utf8'),'mediatype' : 'video'})
             list_item.setProperty('IsPlayable', 'true')
-            url = get_url(action='play', video=host_path+cloud_path+data[3])
+            url = get_url(action='play', video=cloud_path+data['location'].encode('utf8'))
             is_folder = False
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         xbmcplugin.endOfDirectory(_handle)
     elif check_internet_connected() :
         dialog = xbmcgui.Dialog()
-        ok = dialog.ok('eDLTV', 'ขั้นตอนนี้อาจมีค่าใช้บริการเมื่อต้องการเล่นวิดีโอ เนื่องจากมีการเชื่อมต่ออินเทอร์เน็ต')
+        ok = dialog.ok('eDLTV', __language__(33033).encode('utf8'))
         for data in l :
-            list_item = xbmcgui.ListItem(label=data[1])
-            list_item.setInfo('video', {'title': data[1]})
+
+            list_item = xbmcgui.ListItem(label=data['name'].encode('utf8'))
+            list_item.setInfo('video', {'title': data['name'].encode('utf8'),'mediatype' : 'video'})
             list_item.setProperty('IsPlayable', 'true')
-            url = get_url(action='play', video=data[4])
+            url = get_url(action='play', video=data['url'].encode('utf8'))
             is_folder = False
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         xbmcplugin.endOfDirectory(_handle)
     else :
         dialog = xbmcgui.Dialog()
-        ok = dialog.ok('eDLTV', 'ไม่สามารถติดต่อกับเซิฟเวอร์ได้เนื่องจากไม่ได้เชื่อมต่อเครือข่ายหรือไม่มีการเชื่อมต่ออินเทอร์เน็ต')
+        ok = dialog.ok('eDLTV', __language__(33030).encode('utf8'))
 
 def query_data (l) :
-    check_except = ''
-    l.sort()
-    l.reverse()
-    sql = 'SELECT code, name, parent, location, url FROM edltv WHERE'
-    for i in l :
-        if i[0] != '-' :
-            sql = sql + ' name like "%' + i + '%" OR'
-        else :
-            if not check_except :
-                sql = sql.rstrip('OR')
-                sql = sql + 'EXCEPT SELECT code, name, parent, location, url FROM edltv WHERE filename IS NULL'
-                check_except = True
-            sql = sql + ' OR name like "%' + i[1:] + '%"'
-    sql = sql.rstrip(' OR')
-    sql = sql + 'EXCEPT SELECT code, name, parent, location, url FROM edltv where length(code) < 10'
-    rows = c.execute(sql)
-    rows = c.fetchall()
-    return rows
+    my_list = []
+    buffer_list = []
+    remove_list = filter(lambda d: "-" in d[0], l)
+    select_list = [x for x in l if '-' not in x[0]]
+    with open(database_path) as database:
+        data = json.load(database)
+        for item in select_list :
+            my_list = my_list + list(filter(lambda d: item.decode('utf-8').lower() in d["name"].lower() and d['filename'] != "NULL" and d not in my_list, data['edltv']))
+        for item in remove_list :
+            buffer_list = buffer_list + list(filter(lambda d: item[1:].decode('utf-8').lower() in d["name"].lower() and d['filename'] != "NULL" and d not in buffer_list, data['edltv']))
+        for item in buffer_list :
+            if item in my_list :
+                my_list.remove(item)
+    return my_list
 
 def remove_minus (l) :
     sql = ''
@@ -199,36 +218,31 @@ def remove_minus (l) :
         else :
             buffer_l[buffer_l.index(i)] = i.rstrip('-')
     buffer_l = filter(None, buffer_l)
+    
     return query_data (buffer_l)
 
 def check_one_word (l) :
-    sql = ''
-    if len(l[0]) > 1 :        
-        if l[0][0] != '-' :
-            sql = 'SELECT code, name, parent, location, url FROM edltv WHERE name like "%'+l[0]+'%" AND filename IS NOT NULL'
-    elif len(l[0]) == 1 :
-        if l[0][0] == '-' or l[0][0] == '+' :
-            sql = 'SELECT code, name, parent, location, url FROM edltv WHERE name like "%'+l[0]+'%" AND filename IS NOT NULL'
-    sql = sql + ' EXCEPT SELECT code, name, parent, location, url FROM edltv where length(code) < 10 ORDER BY code'
-    rows = c.execute(sql)
-    rows = c.fetchall()
-    return rows
+    with open(database_path) as database:
+        data = json.load(database)
+        if len(l[0]) > 1 :        
+            if l[0][0] != '-' :
+                data_list = filter(lambda d: l[0].decode('utf8').lower() in d["name"].lower() and d['filename'] != "NULL", data['edltv'])
+        elif len(l[0]) == 1 :
+            if l[0][0] == '-' or l[0][0] == '+' :
+                data_list = filter(lambda d: l[0].decode('utf8').lower() in d["name"].lower() and d['filename'] != "NULL", data['edltv'])
+    return data_list
 
 def set_word_list (l) :
-    global word_list
-    word_list = l
+    globals()['word_list'] = l
 
 def get_word_list () :
-    global word_list
-    return word_list
+    return globals()['word_list']
 
 def set_string (s) :
-    global string
-    string = s
+    globals()['string'] = s
 
 def get_string () :
-    global string
-    return string
+    return globals()['string']
 
 def replace_and_split (string,symbol='+',to_symbol=' ') :
     buffer_word_list = []
@@ -256,29 +270,29 @@ def word_function (data) :
     return False
 
 def search_function () :
-    kb = xbmc.Keyboard(None,'Search')
+    kb = xbmc.Keyboard(None,__language__(33000).encode('utf8'))
     kb.doModal()
     if (kb.isConfirmed()):
         error = word_function (kb.getText())
         if error :
-            ok = dialog.ok('eDLTV Error', 'ไม่พบข้อมูลที่ต้องการค้นหา')
+            dialog = xbmcgui.Dialog()
+            ok = dialog.ok('eDLTV', __language__(33032).encode('utf8'))
 
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
     if params:
         if params['action'] == 'listing':
-            if params['category'] == 'ค้นหา':
-                search_function()
-            elif params['hasfolder'] == 'yes':
+            if params['hasfolder'] == 'yes':
                 list_categories(params['category'])
             else :
                 list_videos(params['category'])
+        elif params['action'] == 'search':
+            search_function()
         elif params['action'] == 'play':
             play_video(params['video'])
         else:
            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
-        get_socket_data()
         set_class()
 
 def get_socket_data () :
@@ -297,21 +311,17 @@ def get_socket_data () :
             data, addr = s.recvfrom(65507)
             return addr[0]
     except socket.timeout,e:
-        print e
+        xbmc.log(str(e))
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    #database_path = dir_path[:dir_path.find("addons")] + "userdata/Database/edltv.db"
-    folder_path = dir_path[:dir_path.find("addons")] + "userdata/addon_data/plugin.video.edltv/"
-    database_path = folder_path + "edltv.db"
-    db_path = dir_path+'/edltv.db'
+    folder_path = addon.getAddonInfo('profile')
+    database_path = xbmc.translatePath(os.path.join(folder_path,"database.json"))
+    db_path = xbmc.translatePath(os.path.join(dir_path,"database.json"))
     if not xbmcvfs.exists(folder_path) :
         xbmcvfs.mkdir(folder_path)
     if not xbmcvfs.exists(database_path) :
         xbmcvfs.copy(db_path, database_path)
     elif not filecmp.cmp(db_path,database_path) :
         xbmcvfs.copy(db_path, database_path)
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    period = []
     router(sys.argv[2][1:])

2d815e92ef677a959adb21117dd627d540b4d69c
Andras Nemes <andras.nemes@gmail.com>
2017-07-02 01:00:17 +1000

    + changed xbmcswift2 to kodiswift + updated addon.xml xbmc.python to 2.25.0 + changed language files from deprecated xml to .po files + removed unused lib folder + added title info to playable menu items

--- plugins.audio.pbsfm/addon.py
+++ plugins.audio.pbsfm/addon.py
@@ -1,22 +1,21 @@
-from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import pbsfm
+from kodiswift import Plugin, xbmcgui
 import urllib2
 import json
 
 plugin = Plugin()
 pbsapi = 'http://pulse.emit.com/api/stations/pbs'
 
-@plugin.route('/')
+@plugin.cached_route('/')
 def main_menu():
     items = [
-        {'label': plugin.get_string(30000),'path': "http://eno.emit.com:8000/pbsfm_live_64.mp3",'is_playable': True},
+        {'label': plugin.get_string(30000),'path': "http://eno.emit.com:8000/pbsfm_live_64.mp3",'is_playable': True, 'info':{'title': plugin.get_string(30000)}},
         {'label': plugin.get_string(30001), 'path': plugin.url_for('shows')},
         ]
 
     return items
 
 
-@plugin.route('/shows/')
+@plugin.cached_route('/shows/')
 def shows():
 
     shows = fetch_shows()
@@ -30,7 +29,7 @@ def shows():
     return items
 
 
-@plugin.route('/episodes/<slug>/')
+@plugin.cached_route('/episodes/<slug>/')
 def list_episodes(slug):
 
     episodes = fetch_episodes(slug)
@@ -38,30 +37,30 @@ def list_episodes(slug):
     if episodes:
         for episode in episodes:
             if episode['streams']:
-                items.append({'label': episode['name'], 'path': episode['streams'][0]['href'], 'is_playable': True})
+                items.append({'label': episode['name'], 'path': episode['streams'][0]['href'], 'is_playable': True, 'info':{'title': episode['name']}})
     else:
         last = fetch_last(slug)
         if last['lastEpisode']:
-            items.append({'label': last['lastEpisode']['name'], 'path': last['lastEpisode']['streams'][0]['href'], 'is_playable': True})
+            items.append({'label': last['lastEpisode']['name'], 'path': last['lastEpisode']['streams'][0]['href'], 'is_playable': True, 'info':{'title': episode['name']}})
         else:
             items.append({'label': 'Nothing Found!', 'path': None})
     return items
 
-@plugin.cached(TTL=60*24*7)
+@plugin.cached(ttl=60*24*7)
 def fetch_shows():
     response = urllib2.urlopen(pbsapi)
     home = json.load(response)
     shows = json.load(urllib2.urlopen(home['$shows']))
     return shows
 
-@plugin.cached(TTL=60*24)
+@plugin.cached(ttl=60*24)
 def fetch_episodes(slug):
     episode_list = pbsapi+'/shows/'+slug+'/episodes'
     episodes = json.load(urllib2.urlopen(episode_list))
 
     return episodes
 
-@plugin.cached(TTL=60*24)
+@plugin.cached(ttl=60*24)
 def fetch_last(slug):
     show_url = pbsapi+'/shows/'+slug
     show = json.load(urllib2.urlopen(show_url))

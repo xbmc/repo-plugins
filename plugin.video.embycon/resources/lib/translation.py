49d6dd033fc083c4ee6f69947861c0d923144041
Shaun <shaun@bluebit.com.au>
2017-07-16 00:09:51 +1000

    [plugin.video.embycon] 1.3.17 (#1309)
    [plugin.video.embycon] 1.3.17

---
+++ plugin.video.embycon/resources/lib/translation.py
@@ -0,0 +1,98 @@
+import xbmcaddon
+from simple_logging import SimpleLogging
+
+log = SimpleLogging(__name__)
+addon = xbmcaddon.Addon(id='plugin.video.embycon')
+
+
+def i18n(string_id):
+    try:
+        return addon.getLocalizedString(STRINGS[string_id]).encode('utf-8', 'ignore')
+    except Exception as e:
+        log.error('Failed String Lookup: %s (%s)' % (string_id, e))
+        return string_id
+
+
+STRINGS = {
+    'server_port:': 30001,
+    'username:': 30005,
+    'password:': 30006,
+    'incorrect_user_pass': 30044,
+    'username_not_found': 30045,
+    'deleting': 30052,
+    'waiting_server_delete': 30053,
+    'username_secured': 30060,
+    'username_userdefined': 30061,
+    'username_userinput': 30062,
+    'n/a': 30063,
+    'confirm_file_delete': 30091,
+    'file_delete_confirm': 30092,
+    'loading_content': 30112,
+    'retrieving_data': 30113,
+    'done': 30125,
+    'processing_item:': 30126,
+    'error': 30135,
+    'service_not_running': 30136,
+    'restart_kodi': 30137,
+    'no_media_type': 30139,
+    'select_server': 30166,
+    'server_detect_succeeded': 30167,
+    'found_server': 30168,
+    'address:': 30169,
+    'select_user': 30180,
+    'url_error_': 30200,
+    'unable_connect_server': 30201,
+    'tvshows': 30229,
+    'default_view': 30230,
+    'movies': 30231,
+    'boxsets': 30232,
+    'series': 30233,
+    'seasons': 30234,
+    'episodes': 30235,
+    'save': 30236,
+    'start_from_beginning': 30237,
+    'default_sort': 30238,
+    'next_page': 30245,
+    'search': 30246,
+    'widgets': 30247,
+    'emby_movies': 30248,
+    'emby_tvshows': 30249,
+    'unknown': 30250,
+    'movies_genre': 30251,
+    'movies_az': 30252,
+    'change_user': 30253,
+    'show_settings': 30254,
+    'movies_all': 30256,
+    'movies_recently_added': 30257,
+    'movies_in_progress': 30258,
+    'movies_favorites': 30259,
+    'movies_boxsets': 30260,
+    'tvshows_all': 30261,
+    'tvshows_favorites': 30262,
+    'episodes_recently_added': 30263,
+    'episodes_in_progress': 30264,
+    'episodes_up_next': 30265,
+    'upcoming_tv': 30266,
+    '_in_progress': 30267,
+    '_recently_added': 30268,
+    'movies_random': 30269,
+    'emby_mark_watched': 30270,
+    'emby_mark_unwatched': 30271,
+    'emby_set_favorite': 30272,
+    'emby_unset_favorite': 30273,
+    'emby_delete': 30274,
+    'missing_title': 30280,
+    'emby_force_transcode': 30275,
+    'extra_prompt': 30276,
+    'turn_on_auto_resume?': 30277,
+    'skin_not_supported': 30281,
+    'no_server_detected': 30282,
+    'play_next_title': 30283,
+    'play_next_question': 30284,
+    '_next_up': 30278,
+    'detect_server': 30011,
+    'show_clients': 30017,
+    'tvshows_unwatched': 30279,
+    '_unwatched': 30285,
+    'movies_unwatched': 30286
+}
---
+++ plugin.video.embycon/resources/lib/utils.py
@@ -0,0 +1,191 @@
+# Gnu General Public License - see LICENSE.TXT
+import xbmcaddon
+
+import re
+
+from downloadutils import DownloadUtils
+from simple_logging import SimpleLogging
+from clientinfo import ClientInformation
+
+# define our global download utils
+downloadUtils = DownloadUtils()
+log = SimpleLogging(__name__)
+
+
+###########################################################################
+class PlayUtils():
+    def getPlayUrl(self, id, result, force_transcode):
+        log.debug("getPlayUrl")
+        addonSettings = xbmcaddon.Addon(id='plugin.video.embycon')
+        playback_type = addonSettings.getSetting("playback_type")
+        server = downloadUtils.getServer()
+        log.debug("playback_type: " + playback_type)
+        if force_transcode:
+            log.debug("playback_type: FORCED_TRANSCODE")
+        playurl = None
+
+        # transcode
+        if playback_type == "2" or force_transcode:
+
+            playback_bitrate = addonSettings.getSetting("playback_bitrate")
+            log.debug("playback_bitrate: " + playback_bitrate)
+
+            width_options = ["640", "720", "1024", "1280", "1440", "1600", "1920", "2600", "4096"]
+            playback_max_width = width_options[int(addonSettings.getSetting("playback_max_width"))]
+            playback_video_force_8 = addonSettings.getSetting("playback_video_force_8") == "true"
+
+            clientInfo = ClientInformation()
+            deviceId = clientInfo.getDeviceId()
+            bitrate = int(playback_bitrate) * 1000
+            user_token = downloadUtils.authenticate()
+
+            playurl = (
+                "%s/emby/Videos/%s/master.m3u8?MediaSourceId=%s&VideoCodec=h264&AudioCodec=ac3&MaxAudioChannels=6&deviceId=%s&VideoBitrate=%s"
+                % (server, id, id, deviceId, bitrate))
+
+            playurl = playurl + "&maxWidth=" + playback_max_width
+
+            if playback_video_force_8:
+                playurl = playurl + "&MaxVideoBitDepth=8"
+
+            playurl = playurl + "&api_key=" + user_token
+
+        # do direct path playback
+        elif playback_type == "0":
+            playurl = result.get("Path")
+
+            # handle DVD structure
+            if (result.get("VideoType") == "Dvd"):
+                playurl = playurl + "/VIDEO_TS/VIDEO_TS.IFO"
+            elif (result.get("VideoType") == "BluRay"):
+                playurl = playurl + "/BDMV/index.bdmv"
+
+            smb_username = addonSettings.getSetting('smbusername')
+            smb_password = addonSettings.getSetting('smbpassword')
+
+            # add smb creds
+            if smb_username == '':
+                playurl = playurl.replace("\\\\", "smb://")
+            else:
+                playurl = playurl.replace("\\\\", "smb://" + smb_username + ':' + smb_password + '@')
+
+            playurl = playurl.replace("\\", "/")
+
+        # do direct http streaming playback
+        elif playback_type == "1":
+            playurl = "%s/emby/Videos/%s/stream?static=true" % (server, id)
+            user_token = downloadUtils.authenticate()
+            playurl = playurl + "&api_key=" + user_token
+
+        log.debug("Playback URL: " + playurl)
+        return playurl.encode('utf-8')
+
+    def getStrmDetails(self, result):
+        playurl = None
+        listitem_props = []
+
+        source = result['MediaSources'][0]
+        contents = source.get('Path').encode('utf-8')  # contains contents of strm file with linebreaks
+
+        line_break = '\r'
+        if '\r\n' in contents:
+            line_break += '\n'
+
+        lines = contents.split(line_break)
+        for line in lines:
+            line = line.strip()
+            if line.startswith('#KODIPROP:'):
+                match = re.search('#KODIPROP:(?P<item_property>[^=]+?)=(?P<property_value>.+)', line)
+                if match:
+                    listitem_props.append((match.group('item_property'), match.group('property_value')))
+            elif line != '':
+                playurl = line
+
+        log.debug("Playback URL: " + playurl + " ListItem Properties: " + str(listitem_props))
+        return playurl, listitem_props
+
+
+def getDetailsString():
+
+    addonSettings = xbmcaddon.Addon(id='plugin.video.embycon')
+    include_media = addonSettings.getSetting("include_media") == "true"
+    include_people = addonSettings.getSetting("include_people") == "true"
+    include_overview = addonSettings.getSetting("include_overview") == "true"
+
+    detailsString = "EpisodeCount,SeasonCount,Path,Genres,Studios,CumulativeRunTimeTicks,Etag"
+
+    if include_media:
+        detailsString += ",MediaStreams"
+
+    if include_people:
+        detailsString += ",People"
+
+    if include_overview:
+        detailsString += ",Overview"
+
+    return detailsString
+
+
+def getChecksum(item):
+    userdata = item['UserData']
+    checksum = "%s_%s_%s_%s_%s_%s_%s" % (
+        item['Etag'],
+        userdata['Played'],
+        userdata['IsFavorite'],
+        userdata.get('Likes', "-"),
+        userdata['PlaybackPositionTicks'],
+        userdata.get('UnplayedItemCount', "-"),
+        userdata.get("PlayedPercentage", "-")
+    )
+
+    return checksum
+
+
+def getArt(item, server, widget=False):
+    art = {
+        'thumb': '',
+        'fanart': '',
+        'poster': '',
+        'banner': '',
+        'clearlogo': '',
+        'clearart': '',
+        'discart': '',
+        'landscape': '',
+        'tvshow.poster': ''
+    }
+    item_id = item.get("Id")
+
+    image_id = item_id
+    imageTags = item.get("ImageTags")
+    if (imageTags is not None) and (imageTags.get("Primary") is not None):
+        image_tag = imageTags.get("Primary")
+        if widget:
+            art['thumb'] = downloadUtils.imageUrl(image_id, "Primary", 0, 400, 400, image_tag, server=server)
+        else:
+            art['thumb'] = downloadUtils.getArtwork(item, "Primary", server=server)
+
+    if item.get("Type") == "Episode":
+        art['thumb'] = art['thumb'] if art['thumb'] else downloadUtils.getArtwork(item, "Thumb", server=server)
+        art['landscape'] = art['thumb'] if art['thumb'] else downloadUtils.getArtwork(item, "Thumb", parent=True, server=server)
+        art['tvshow.poster'] = downloadUtils.getArtwork(item, "Primary", parent=True, server=server)
+    else:
+        art['poster'] = art['thumb']
+
+    art['fanart'] = downloadUtils.getArtwork(item, "Backdrop", server=server)
+    if not art['fanart']:
+        art['fanart'] = downloadUtils.getArtwork(item, "Backdrop", parent=True, server=server)
+
+    if not art['landscape']:
+        art['landscape'] = downloadUtils.getArtwork(item, "Thumb", server=server)
+        if not art['landscape']:
+            art['landscape'] = art['fanart']
+
+    if not art['thumb']:
+        art['thumb'] = art['landscape']
+
+    art['banner'] = downloadUtils.getArtwork(item, "Banner", server=server)
+    art['clearlogo'] = downloadUtils.getArtwork(item, "Logo", server=server)
+    art['clearart'] = downloadUtils.getArtwork(item, "Art", server=server)
+    art['discart'] = downloadUtils.getArtwork(item, "Disc", server=server)
+
+    return art

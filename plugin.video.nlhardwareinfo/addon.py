5cef7fa49677dfd93441037a621fc813ec756cc6
skipmodea1 <skipmodea1@outlook.com>
2017-07-16 18:52:04 +0200

    [plugin.video.nlhardwareinfo] 1.0.7

--- plugin.video.nlhardwareinfo/addon.py
+++ plugin.video.nlhardwareinfo/addon.py
@@ -4,24 +4,19 @@
 #
 # Imports
 #
-import os
 import sys
 import xbmc
-import xbmcaddon
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-LIB_DIR = xbmc.translatePath(
-    os.path.join(xbmcaddon.Addon(id="plugin.video.nlhardwareinfo").getAddonInfo('path'), 'resources', 'lib'))
-sys.path.append(LIB_DIR)
 
-from nlhardwareinfo_const import ADDON, SETTINGS, LANGUAGE, IMAGES_PATH, DATE, VERSION
+from resources.lib.nlhardwareinfo_const import ADDON, DATE, VERSION
 
 xbmc.log("[ADDON] %s, Python Version %s" % (ADDON, str(sys.version)), xbmc.LOGDEBUG)
 xbmc.log("[ADDON] %s v%s (%s) is starting, ARGV = %s" % (ADDON, VERSION, DATE, repr(sys.argv)),
              xbmc.LOGDEBUG)
 
-import nlhardwareinfo_list_play as plugin
+from resources.lib import nlhardwareinfo_list_play as plugin
 
 plugin.Main()

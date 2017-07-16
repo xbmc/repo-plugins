e565019c7df13e00b90d7b32a086b66f2a2194a8
skipmodea1 <skipmodea1@outlook.com>
2017-07-16 06:12:21 +0200

    [plugin.video.cnet.podcasts] 1.1.4

--- plugin.video.cnet.podcasts/default.py
+++ plugin.video.cnet.podcasts/default.py
@@ -5,56 +5,30 @@ import StorageServer
 import os
 import re
 import urllib
-import urllib2
+import requests
+from time import strptime
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
-import xmltodict
+import xbmc
+import sys
 from bs4 import BeautifulSoup
 from urlparse import parse_qs
 
-cache = StorageServer.StorageServer("cnetpodcasts", 6)
-addon = xbmcaddon.Addon()
-addon_version = addon.getAddonInfo('version')
-addon_id = addon.getAddonInfo('id')
-icon = addon.getAddonInfo('icon')
-language = addon.getLocalizedString
-latest_videos_href = 'http://feeds2.feedburner.com/cnet/allhdpodcast'
-
-
-def addon_log(string):
-    try:
-        log_message = string.encode('utf-8', 'ignore')
-    except:
-        log_message = 'addonException: addon_log'
-    xbmc.log("[%s-%s]: %s" % (addon_id, addon_version, log_message), level=xbmc.LOGDEBUG)
-
-
-def make_request(url, post_data=None):
-    addon_log('Request URL: %s' % url)
-    headers = {
-        'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-        'Referer': 'http://www.cnet.com'
-    }
-    try:
-        req = urllib2.Request(url, post_data, headers)
-        response = urllib2.urlopen(req)
-        data = response.read()
-        response.close()
-        return data
-    except urllib2.URLError, e:
-        addon_log('We failed to open "%s".' % url)
-        if hasattr(e, 'reason'):
-            addon_log('We failed to reach a server.')
-            addon_log('Reason: %s' % e.reason)
-        if hasattr(e, 'code'):
-            addon_log('We failed with error code - %s.' % e.code)
+ADDON = "plugin.video.cnet.podcasts"
+SETTINGS = xbmcaddon.Addon()
+LANGUAGE = SETTINGS.getLocalizedString
+IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'images')
+HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0','Referer': 'http://www.cnet.com'}
+CACHE = StorageServer.StorageServer("cnetpodcasts", 6)
+BASE_URL = 'http://www.cnet.com/cnet-podcasts/'
+DATE = "2017-07-12"
+VERSION = "1.0.4"
 
 
 def cache_categories():
-    # url = 'http://www.cnet.com/podcasts/'
-    url = 'http://www.cnet.com/cnet-podcasts/'
-    soup = BeautifulSoup(make_request(url), 'html.parser')
+    html_source = requests.get(BASE_URL, headers=HEADERS).text
+    soup = BeautifulSoup(html_source)
     items = soup.find_all('a', attrs={'href': re.compile("hd.xml$")})
     cats = [{'thumb': '',
              'name': i['href'],
@@ -65,14 +39,14 @@ def cache_categories():
 
 
 def display_categories():
-    cats = cache.cacheFunction(cache_categories)
+    cats = CACHE.cacheFunction(cache_categories)
     previous_name = ''
 
-    # add a category
-    name = 'Latest Videos'
-    add_dir(name, latest_videos_href, 'category', '', {'Plot': ''})
-
     for i in cats:
+
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "cat", str(i)), xbmc.LOGDEBUG)
+
         name = str(i['name'])
         name = name.replace("http://feed.cnet.com/feed/podcast/", "")
         name = name.replace("-", " ")
@@ -91,87 +65,170 @@ def display_categories():
 def display_category(links_list):
     url = links_list
 
-    soup = BeautifulSoup(make_request(url), 'html.parser')
+    xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+        ADDON, VERSION, DATE, "url", str(url)), xbmc.LOGDEBUG)
 
-    # latest videos isn't a real category in CNET, therefore this hardcoded stuff was needed
-    if url == latest_videos_href:
-        urls = soup.find_all('a', attrs={'href': re.compile("^http://feedproxy.google.com/")})
-        # <a href="http://feedproxy.google.com/~r/cnet/allhdpodcast/~3/4RHUa95GiUM/14n041814_walkingpalua_740.mp4">A walk among hidden graves and WWII bombs</a>
-        for url in urls:
-            title = str(url)
-            title = title.replace('</a>', '')
-            title = title.replace("&#039;", "'")
-            pos_last_greater_than_sign = title.rfind('>')
-            title = title[pos_last_greater_than_sign + 1:]
+    html_source = requests.get(url, headers=HEADERS).text
+    soup = BeautifulSoup(html_source)
 
-            meta = {'Plot': title,
-                    'Duration': '',
-                    'Date': '',
-                    'Premiered': ''}
+    # xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+    #     ADDON, VERSION, DATE, "soup", str(soup)), xbmc.LOGDEBUG)
 
-            add_dir(title, url['href'], 'resolve', 'Defaultvideo.png', meta, False)
-    else:
-        #     <item>
-        #           <title><![CDATA[Inside Scoop: Will acquiring Nokia devices give Microsoft an edge?]]></title>
-        #           <link>http://www.podtrac.com/pts/redirect.mp4/dw.cbsi.com/redir/13n0903_MicrosoftScoop_740.m4v?destUrl=http://download.cnettv.com.edgesuite.net/21923/2013/09/03/13n0903_MicrosoftScoop_740.m4v</link>
-        #           <author>feedback-cnettv@cnet.com (CNETTV)</author>
-        #           <description><![CDATA[Details of the Microsoft and Nokia deal are now finalized. CNET's Josh Lowensohn discusses the effects the merger could have on customers, Microsoft's sagging market share, and the selection of a new Microsoft CEO.]]></description>
-        #           <itunes:subtitle><![CDATA[Details of the Microsoft and Nokia deal are now finalized. CNET's Josh Lowensohn discusses the effects the merger could have on customers, Microsoft's sagging market share, and the selection of a new Microsoft CEO.]]></itunes:subtitle>
-        #           <itunes:summary><![CDATA[Details of the Microsoft and Nokia deal are now finalized. CNET's Josh Lowensohn discusses the effects the merger could have on customers, Microsoft's sagging market share, and the selection of a new Microsoft CEO.]]></itunes:summary>
-        #           <itunes:explicit>no</itunes:explicit>
-        #           <itunes:author>CNET.com</itunes:author>
-        #           <guid isPermaLink="false">35ffbba2-67e4-11e3-a665-14feb5ca9861</guid>
-        #           <itunes:duration></itunes:duration>
-        #           <itunes:keywords>
-        #               CNET
-        #                CNETTV
-        #             Tech Industry
-        #           </itunes:keywords>
-        #           <enclosure url="http://www.podtrac.com/pts/redirect.mp4/dw.cbsi.com/redir/13n0903_MicrosoftScoop_740.m4v?destUrl=http://download.cnettv.com.edgesuite.net/21923/2013/09/03/13n0903_MicrosoftScoop_740.m4v" length="0" type="video/mp4"/>
-        #           <category>Technology</category>
-        #           <pubDate>Fri, 21 Feb 2014 19:49:08 PST</pubDate>
-        #     </item>
-        urls = soup.find_all('enclosure', attrs={'url': re.compile("^http")})
+    items = soup.find_all('item')
 
-        # skip 2 titles
-        title_index = 2
+    xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+        ADDON, VERSION, DATE, "len(items)", str(len(items))), xbmc.LOGDEBUG)
 
-        for url in urls:
-            titles = soup.find_all('title')
-            title = str(titles[title_index])
-            title_index = title_index + 1
-            title = title.replace("<title><![CDATA[", "")
-            title = title.replace("]]></title>", "")
-            title = title.replace("&#039;", "'")
+    # <item>
+    # <title><![CDATA[This TV has Amazon Alexa built-in, but it's not what you think]]></title>
+    # <link>http://dw.cbsi.com/redir/FL_amazon_tv_1353763_2696.mp4?destUrl=http://download.cnettv.com.edgesuite.net/21923/mpx/2017/07/12/994479171622/FL_amazon_tv_1353763_2696.mp4</link>
+    # <author>feedback-cnettv@cnet.com (CNETTV)</author>
+    # <description><![CDATA[The Element Amazon Fire TV Edition features thousands of apps and Alexa Voice control. So what's the catch?]]></description>
+    # <itunes:subtitle><![CDATA[The Element Amazon Fire TV Edition features thousands of apps and Alexa Voice control. So what's the catch?]]></itunes:subtitle>
+    # <itunes:summary><![CDATA[The Element Amazon Fire TV Edition features thousands of apps and Alexa Voice control. So what's the catch?]]></itunes:summary>
+    # <itunes:explicit>no</itunes:explicit>
+    # <itunes:author>CNET.com</itunes:author>
+    # <guid ispermalink="false">a18291b5-2122-48a8-a42c-9cde4c6d3951</guid>
+    # <itunes:duration></itunes:duration>
+    # <itunes:keywords>
+    #                     CNET
+    #                     CNETTV
+    #                     TVs
+    #                 </itunes:keywords>
+    # <enclosure length="0" type="video/mp4" url="http://dw.cbsi.com/redir/FL_amazon_tv_1353763_2696.mp4?destUrl=http://download.cnettv.com.edgesuite.net/21923/mpx/2017/07/12/994479171622/FL_amazon_tv_1353763_2696.mp4"></enclosure>
+    # <category>Technology</category>
+    # <pubdate>Fri, 14 Jul 2017 14:57:27 PDT</pubdate>
+    # </item>
 
-            meta = {'Plot': title,
-                    'Duration': '',
-                    'Date': '',
-                    'Premiered': ''}
+    for item in items:
 
-            add_dir(title, url['url'], 'resolve', 'Defaultvideo.png', meta, False)
+        # xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+        #     ADDON, VERSION, DATE, "item", str(item)), xbmc.LOGDEBUG)
 
+        # <enclosure url="http://dw.cbsi.com/redir/AB445_1164934_2696.mp4?destUrl=http://download.cnettv.com.edgesuite.net/21923/mpx/2017/07/12/994592835973/AB445_1164934_2696.mp4" length="0" type="video/mp4"/>
+        # get the url of the video
+        url = item.link.string
 
-def resolve_url(video_id):
-    parameters = {
-        'iod': 'images,videoMedia,relatedLink,breadcrumb,relatedAssets,broadcast,lowcache',
-        'partTag': 'cntv',
-        'players': 'Download,RTMP',
-        'showBroadcast': 'true',
-        'videoIds': video_id,
-        'videoMediaType': 'preferred'
-    }
-    data = make_request('http://api.cnet.com/restApi/v1.0/videoSearch?' + urllib.urlencode(parameters))
-    pod_dict = xmltodict.parse(data)
-    media_item = pod_dict['CNETResponse']['Videos']['Video']
-    # thumb = [i['ImageURL'] for i in media_item['Images']['Image'] if i['@width'] == '360'][0]
-    media_items = media_item['VideoMedias']['VideoMedia']
-    stream_urls = [i['DeliveryUrl'] for i in media_items if i['Height'] == '720']
-    if stream_urls:
-        return stream_urls[0]
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "url", str(url)), xbmc.LOGDEBUG)
+
+        # Get title
+        title = item.title.string
+
+        try:
+            title = title.encode('utf-8')
+        except:
+            pass
+
+        title = title.replace('-', ' ')
+        title = title.replace('/', ' ')
+        title = title.replace(' i ', ' I ')
+        title = title.replace(' ii ', ' II ')
+        title = title.replace(' iii ', ' III ')
+        title = title.replace(' iv ', ' IV ')
+        title = title.replace(' v ', ' V ')
+        title = title.replace(' vi ', ' VI ')
+        title = title.replace(' vii ', ' VII ')
+        title = title.replace(' viii ', ' VIII ')
+        title = title.replace(' ix ', ' IX ')
+        title = title.replace(' x ', ' X ')
+        title = title.replace(' xi ', ' XI ')
+        title = title.replace(' xii ', ' XII ')
+        title = title.replace(' xiii ', ' XIII ')
+        title = title.replace(' xiv ', ' XIV ')
+        title = title.replace(' xv ', ' XV ')
+        title = title.replace(' xvi ', ' XVI ')
+        title = title.replace(' xvii ', ' XVII ')
+        title = title.replace(' xviii ', ' XVIII ')
+        title = title.replace(' xix ', ' XIX ')
+        title = title.replace(' xx ', ' XXX ')
+        title = title.replace(' xxi ', ' XXI ')
+        title = title.replace(' xxii ', ' XXII ')
+        title = title.replace(' xxiii ', ' XXIII ')
+        title = title.replace(' xxiv ', ' XXIV ')
+        title = title.replace(' xxv ', ' XXV ')
+        title = title.replace(' xxvi ', ' XXVI ')
+        title = title.replace(' xxvii ', ' XXVII ')
+        title = title.replace(' xxviii ', ' XXVIII ')
+        title = title.replace(' xxix ', ' XXIX ')
+        title = title.replace(' xxx ', ' XXX ')
+        title = title.replace('  ', ' ')
+        # in the title of the item a single-quote "'" is represented as "&#039;" for some reason . Therefore this replace is needed to fix that.
+        title = title.replace("&#039;", "'")
+
+        xbmc.log(
+            "[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "title", str(title)),
+            xbmc.LOGDEBUG)
+
+        context_menu_items = []
+        # Add refresh option to context menu
+        context_menu_items.append((LANGUAGE(30104), 'Container.Refresh'))
+        # Add episode  info to context menu
+        context_menu_items.append((LANGUAGE(30105), 'XBMC.Action(Info)'))
+
+        # Get description
+        plot_start_pos = str(item).find("itunes:summary><![CDATA[") + len("itunes:summary><![CDATA[")
+        plot_end_pos = str(item).find("]", plot_start_pos)
+        plot = str(item)[plot_start_pos:plot_end_pos]
+
+        xbmc.log(
+            "[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "plot", str(plot)),
+            xbmc.LOGDEBUG)
+
+        # in the title of the item a single-quote "'" is represented as "&#039;" for some reason . Therefore this replace is needed to fix that.
+        try:
+            plot = plot.replace("&#039;", "'")
+        except:
+            plot = title
+
+        # Get pubdate
+        pubdate = item.pubdate.string
+        # Extract date time fields
+        day = pubdate[len('Fri, '):len('Fri, ') + 2]
+        month_name = pubdate[len('Fri, 01 '):len('Fri, 01 ') + 3]
+        year = pubdate[len('Fri, 01 Mar '):len('Fri, 01 Mar ') + 4]
+        hour = pubdate[len('Fri, 01 Mar 2016 '):len('Fri, 01 Mar 2016 ') + 2]
+        minute = pubdate[len('Fri, 01 Mar 2016 17:'):len('Fri, 01 Mar 2016 17:') + 2]
+        second = pubdate[len('Fri, 01 Mar 2016 17:40:'):len('Fri, 01 Mar 2016 17:40:') + 2]
+
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+            ADDON, VERSION, DATE, "extracted pubdate",
+            str(day + '/' + month_name + '/' + year + '/' + hour + '/' + minute + '/' + second)), xbmc.LOGDEBUG)
+
+        month_numeric = strptime(month_name, '%b').tm_mon
+
+        xbmc.log(
+            "[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "month_numeric", str(month_numeric)),
+            xbmc.LOGDEBUG)
+
+        if len(str(month_numeric)) == 1:
+            month = '0' + str(month_numeric)
+        else:
+            month = str(month_numeric)
+
+        # Dateadded has this form: 2009-04-05 23:16:04
+        dateadded = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second
+
+        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "dateadded", str(dateadded)),
+                 xbmc.LOGDEBUG)
+
+        meta = {'plot': plot,
+                'duration': '',
+                'year': year,
+                'dateadded': dateadded}
+
+        add_dir(title, url, 'resolve', 'Defaultvideo.png', meta, False)
 
 
 def add_dir(name, url, modus, iconimage, meta=None, isfolder=True):
+    add_sort_methods()
+
+    context_menu_items = []
+    # Add refresh option to context menu
+    context_menu_items.append((LANGUAGE(30104), 'Container.Refresh'))
+    # Add episode  info to context menu
+    context_menu_items.append((LANGUAGE(30105), 'XBMC.Action(Info)'))
+
     if meta is None:
         meta = {}
     parameters = {'name': name, 'url': url, 'mode': modus}
@@ -181,12 +238,19 @@ def add_dir(name, url, modus, iconimage, meta=None, isfolder=True):
         list_item.setProperty('IsPlayable', 'false')
     else:
         list_item.setProperty('IsPlayable', 'true')
-    ADDON = "plugin.video.cnet.podcasts"
-    IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
     list_item.setProperty('Fanart_Image', os.path.join(IMAGES_PATH, 'fanart-blur.jpg'))
-    # Add refresh option to context menu
-    list_item.addContextMenuItems([('Refresh', 'Container.Refresh')])
+    list_item.setInfo("mediatype", "video")
+    list_item.setInfo("video", meta)
+    # Adding context menu items to context menu
+    list_item.addContextMenuItems(context_menu_items, replaceItems=False)
+    # Add our item to directory
     xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, list_item, isfolder)
+
+
+def add_sort_methods():
+    sort_methods = [xbmcplugin.SORT_METHOD_UNSORTED,xbmcplugin.SORT_METHOD_LABEL,xbmcplugin.SORT_METHOD_DATE,xbmcplugin.SORT_METHOD_DURATION,xbmcplugin.SORT_METHOD_EPISODE]
+    for method in sort_methods:
+        xbmcplugin.addSortMethod(int(sys.argv[1]), sortMethod=method)
 
 
 def get_params():
@@ -205,11 +269,10 @@ def set_view_mode():
         '4': '503',
         '5': '515'
     }
-    view_mode = addon.getSetting('view_mode')
+    view_mode = SETTINGS.getSetting('view_mode')
     if view_mode == '6':
         return
     xbmc.executebuiltin('Container.SetViewMode(%s)' % view_modes[view_mode])
-
 
 params = get_params()
 
@@ -217,8 +280,11 @@ try:
     mode = params['mode']
 except:
     mode = None
+    xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s, %s = %s" % (
+        ADDON, VERSION, DATE, "ARGV", repr(sys.argv), "File", str(__file__)), xbmc.LOGDEBUG)
 
-addon_log(repr(params))
+xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+    ADDON, VERSION, DATE, "params", str(params)), xbmc.LOGDEBUG)
 
 if not mode:
     display_categories()

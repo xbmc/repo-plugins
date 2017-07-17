12bae2a1030c595f6cc186207adb59e7c8f85916
skipmodea1 <skipmodea1@outlook.com>
2017-07-17 18:39:13 +0200

    [plugin.video.gamegurumania] 1.0.6

--- plugin.video.gamegurumania/resources/lib/gamegurumania_list_play.py
+++ plugin.video.gamegurumania/resources/lib/gamegurumania_list_play.py
@@ -4,14 +4,13 @@ import os
 import sys
 import urllib
 import urlparse
+import requests
 import xbmc
 import xbmcgui
 import xbmcplugin
 from BeautifulSoup import BeautifulSoup
 
-from gamegurumania_const import SETTINGS, LANGUAGE, IMAGES_PATH, ADDON, DATE, VERSION
-from gamegurumania_utils import HTTPCommunicator
-
+from gamegurumania_const import LANGUAGE, IMAGES_PATH, ADDON, DATE, VERSION, HEADERS
 
 #
 # Main class
@@ -74,129 +73,137 @@ class Main:
         video_page_url_napdis = ''
         video_page_url_iframe = ''
         video_page_url_youtube = ''
+        thumbnail_url = ''
         nadpis_found = False
         iframe_found = False
         youtube_found = False
-        thumbnail_url = ''
-        list_item = ''
-        is_folder = False
         # Create a list for our items.
         listing = []
 
-        # 
-        # Get HTML page...
         #
-        html_source = HTTPCommunicator().get(self.video_list_page_url)
+        # Get HTML page
+        #
+        response = requests.get(self.video_list_page_url, headers=HEADERS)
+        html_source = response.text
+        html_source = html_source.encode('utf-8', 'ignore')
 
-        # Parse response...
+        # Parse response
         soup = BeautifulSoup(html_source)
 
-        # find a title and directly after it a video-link. Some title don't have a video-link and must be skipped. 
-        # Find Title
-        # <a class="nadpis" name="shadowrun-returns-alpha-gameplay-video-34776" href="http://www.ggmania.com/?smsid=shadowrun-returns-alpha-gameplay-video-34776">Shadowrun Returns - Alpha Gameplay Video</a>
-        # Find youtubeID
-        # <iframe width="560" height="315" src="http://www.youtube.com/embed/9MiMjQwd2VE" frameborder="0" allowfullscreen="allowfullscreen"></iframe>		
-        # <div class="youtube" id="fDZF-jIhhbk" style="width: 640px; height: 360px;">
+        # <b><a class="nadpis" name="middle-earth-shadow-of-war-gameplay-43762" href="http://www.ggmania.com/?smsid=middle-earth-shadow-of-war-gameplay-43762">Middle-earth: Shadow of War Gameplay</a></b></font><b><font color="black" size="-1" face="arial,helvetica"> - movie</font></b><br />
+        # <font style="color:#000080;text-decoration:none" face="arial,helvetica"><small>(hx) 09:46 AM EDT - Jul,17 2017
+        #
+        # </small></font><a class="koment" href="http://www.ggmania.com/cf.php3?show=43762">Post a comment</a><br /><font class="textik"> Warner Bros and Monolith have shared a video, showing 40 minutes of new gameplay footage from Middle-earth: Shadow of War. This video showcases some of the new skills, abilities and upgrades, as well as the games enhanced combat mechanics and its improved environments. Middle Earth: Shadow of War is currently planned for an October 10th.
+        # <br /> <br />
+        # <center>
+        # <iframe src="https://player.twitch.tv/?video=v159159060&amp;autoplay=false" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/monolithlive?tt_medium=live_embed&amp;tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from monolithlive on www.twitch.tv</a>
+        # </center> </font></td></tr>
 
-        video_page_urls = soup.findAll(["a", "iframe", "div"])
-
+        items = soup.findAll("tr")
         xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-            ADDON, VERSION, DATE, "len(video_page_urls)", str(len(video_page_urls))), xbmc.LOGDEBUG)
+            ADDON, VERSION, DATE, "len(items)", str(len(items))), xbmc.LOGDEBUG)
 
-        for video_page_url in video_page_urls:
+        for item in items:
+
+            # Skip these items
+            if str(item).find('youtube') < 0:
+                xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+                    ADDON, VERSION, DATE, "skipped item without youtube", str(item)), xbmc.LOGDEBUG)
+                continue
 
             xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                ADDON, VERSION, DATE, "video_page_url", str(video_page_url)), xbmc.LOGDEBUG)
+                ADDON, VERSION, DATE, "item", str(item)), xbmc.LOGDEBUG)
 
-            video_page_url_str = str(video_page_url)
-            if video_page_url_str.startswith('<a class="nadpis"'):
-                nadpis_found = True
-                youtube_found = False
-                video_page_url_napdis = video_page_url
-            if video_page_url_str.startswith('<iframe'):
-                iframe_found = True
-                video_page_url_iframe = video_page_url
-            if video_page_url_str.startswith('<div class="youtube"'):
-                youtube_found = True
-                video_page_url_youtube = video_page_url
-            if (nadpis_found == True and iframe_found == True) or (nadpis_found == True and youtube_found == True):
-                # Find Title
-                # <a class="nadpis" name="shadowrun-returns-alpha-gameplay-video-34776" href="http://www.ggmania.com/?smsid=shadowrun-returns-alpha-gameplay-video-34776">Shadowrun Returns - Alpha Gameplay Video</a>
+            youtubeID = item.div['id']
 
-                video_page_url_napdis_str = str(video_page_url_napdis)
-                start_pos_title = video_page_url_napdis_str.find(">") + 1
-                title = video_page_url_napdis_str[start_pos_title:]
-                title = title.replace("</a>", "")
+            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+                ADDON, VERSION, DATE, "youtubeID", str(youtubeID)), xbmc.LOGDEBUG)
 
-                title = title.capitalize()
-                title = title.replace('/', ' ')
-                title = title.replace('&amp;', '&')
-                title = title.replace(' i ', ' I ')
-                title = title.replace(' amp ', ' & ')
-                title = title.replace(' ii ', ' II ')
-                title = title.replace(' iii ', ' III ')
-                title = title.replace(' iv ', ' IV ')
-                title = title.replace(' v ', ' V ')
-                title = title.replace(' vi ', ' VI ')
-                title = title.replace(' vii ', ' VII ')
-                title = title.replace(' viii ', ' VIII ')
-                title = title.replace(' ix ', ' IX ')
-                title = title.replace(' x ', ' X ')
-                title = title.replace(' xi ', ' XI ')
-                title = title.replace(' xii ', ' XII ')
-                title = title.replace(' xiii ', ' XIII ')
-                title = title.replace(' xiv ', ' XIV ')
-                title = title.replace(' xv ', ' XV ')
-                title = title.replace(' xvi ', ' XVI ')
-                title = title.replace(' xvii ', ' XVII ')
-                title = title.replace(' xviii ', ' XVIII ')
-                title = title.replace(' xix ', ' XIX ')
-                title = title.replace(' xx ', ' XXX ')
-                title = title.replace(' xxi ', ' XXI ')
-                title = title.replace(' xxii ', ' XXII ')
-                title = title.replace(' xxiii ', ' XXIII ')
-                title = title.replace(' xxiv ', ' XXIV ')
-                title = title.replace(' xxv ', ' XXV ')
-                title = title.replace(' xxvi ', ' XXVI ')
-                title = title.replace(' xxvii ', ' XXVII ')
-                title = title.replace(' xxviii ', ' XXVIII ')
-                title = title.replace(' xxix ', ' XXIX ')
-                title = title.replace(' xxx ', ' XXX ')
+            title = item.a.string
 
-                xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "title", str(title)), xbmc.LOGDEBUG)
+            title = title.capitalize()
+            title = title.replace('/', ' ')
+            title = title.replace('&amp;', '&')
+            title = title.replace(' i ', ' I ')
+            title = title.replace(' amp ', ' & ')
+            title = title.replace(' ii ', ' II ')
+            title = title.replace(' iii ', ' III ')
+            title = title.replace(' iv ', ' IV ')
+            title = title.replace(' v ', ' V ')
+            title = title.replace(' vi ', ' VI ')
+            title = title.replace(' vii ', ' VII ')
+            title = title.replace(' viii ', ' VIII ')
+            title = title.replace(' ix ', ' IX ')
+            title = title.replace(' x ', ' X ')
+            title = title.replace(' xi ', ' XI ')
+            title = title.replace(' xii ', ' XII ')
+            title = title.replace(' xiii ', ' XIII ')
+            title = title.replace(' xiv ', ' XIV ')
+            title = title.replace(' xv ', ' XV ')
+            title = title.replace(' xvi ', ' XVI ')
+            title = title.replace(' xvii ', ' XVII ')
+            title = title.replace(' xviii ', ' XVIII ')
+            title = title.replace(' xix ', ' XIX ')
+            title = title.replace(' xx ', ' XXX ')
+            title = title.replace(' xxi ', ' XXI ')
+            title = title.replace(' xxii ', ' XXII ')
+            title = title.replace(' xxiii ', ' XXIII ')
+            title = title.replace(' xxiv ', ' XXIV ')
+            title = title.replace(' xxv ', ' XXV ')
+            title = title.replace(' xxvi ', ' XXVI ')
+            title = title.replace(' xxvii ', ' XXVII ')
+            title = title.replace(' xxviii ', ' XXVIII ')
+            title = title.replace(' xxix ', ' XXIX ')
+            title = title.replace(' xxx ', ' XXX ')
 
-                # Find youtubeID
-                # <iframe width="560" height="315" src="http://www.youtube.com/embed/9MiMjQwd2VE" frameborder="0" allowfullscreen="allowfullscreen"></iframe>				
-                # <div class="youtube" id="fDZF-jIhhbk" style="width: 640px; height: 360px;">
-                if iframe_found == True:
-                    youtubeID = str(video_page_url_iframe['src'])
-                    youtubeID = youtubeID.replace("//www.youtube.com/embed/", '')
-                if youtube_found == True:
-                    youtubeID = str(video_page_url_youtube['id'])
+            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+                ADDON, VERSION, DATE, "title", str(title)), xbmc.LOGDEBUG)
 
-                youtube_url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % youtubeID
+            youtube_url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % youtubeID
 
-                xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "youtube_url", str(youtube_url)), xbmc.LOGDEBUG)
+            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+                ADDON, VERSION, DATE, "youtube_url", str(youtube_url)), xbmc.LOGDEBUG)
 
-                # Add to list...
-                list_item = xbmcgui.ListItem(title, thumbnailImage=thumbnail_url)
-                list_item.setArt({'fanart': os.path.join(IMAGES_PATH, 'fanart-blur.jpg')})
-                list_item.setProperty('IsPlayable', 'true')
-                is_folder = False
-                url = youtube_url
-                # Add refresh option to context menu
-                list_item.addContextMenuItems([('Refresh', 'Container.Refresh')])
-                # Add our item to the listing as a 3-element tuple.
-                listing.append((url, list_item, is_folder))
+            item_string = str(item)
+            start_pos_of_plot = item_string.find('"textik"> ', 0) + len('"textik"> ')
+            end_pos_of_plot = item_string.find('<', start_pos_of_plot)
+            plot = item_string[start_pos_of_plot:end_pos_of_plot]
 
-                nadpis_found = False
-                iframe_found = False
-                youtube_found = False
+            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
+                ADDON, VERSION, DATE, "plot", str(plot)), xbmc.LOGDEBUG)
+
+            meta = {'plot': plot,
+                    'duration': '',
+                    'year': '',
+                    'dateadded': ''}
+
+            add_sort_methods()
+
+            context_menu_items = []
+            # Add refresh option to context menu
+            context_menu_items.append((LANGUAGE(30104), 'Container.Refresh'))
+            # Add episode  info to context menu
+            context_menu_items.append((LANGUAGE(30105), 'XBMC.Action(Info)'))
+
+            # Add to list...
+            list_item = xbmcgui.ListItem(title, thumbnailImage=thumbnail_url)
+            list_item.setArt({'fanart': os.path.join(IMAGES_PATH, 'fanart-blur.jpg')})
+            list_item.setProperty('IsPlayable', 'true')
+            is_folder = False
+            url = youtube_url
+            list_item.setInfo("mediatype", "video")
+            list_item.setInfo("video", meta)
+            # Adding context menu items to context menu
+            list_item.addContextMenuItems(context_menu_items, replaceItems=False)
+            # Add our item to the listing as a 3-element tuple.
+            listing.append((url, list_item, is_folder))
 
         # Next page entry...
         if self.next_page_possible == 'True':
+            context_menu_items = []
+            # Add refresh option to context menu
+            context_menu_items.append((LANGUAGE(30104), 'Container.Refresh'))
+
             list_item = xbmcgui.ListItem(LANGUAGE(30503), thumbnailImage=os.path.join(IMAGES_PATH, 'next-page.png'))
             list_item.setArt({'fanart': os.path.join(IMAGES_PATH, 'fanart-blur.jpg')})
             list_item.setProperty('IsPlayable', 'false')
@@ -204,8 +211,8 @@ class Main:
                           "next_page_possible": self.next_page_possible}
             url = self.plugin_url + '?' + urllib.urlencode(parameters)
             is_folder = True
-            # Add refresh option to context menu
-            list_item.addContextMenuItems([('Refresh', 'Container.Refresh')])
+            # Adding context menu items to context menu
+            list_item.addContextMenuItems(context_menu_items, replaceItems=False)
             # Add our item to the listing as a 3-element tuple.
             listing.append((url, list_item, is_folder))
 
@@ -217,3 +224,8 @@ class Main:
         xbmcplugin.addSortMethod(handle=self.plugin_handle, sortMethod=xbmcplugin.SORT_METHOD_NONE)
         # Finish creating a virtual folder.
         xbmcplugin.endOfDirectory(self.plugin_handle)
+
+def add_sort_methods():
+    sort_methods = [xbmcplugin.SORT_METHOD_UNSORTED,xbmcplugin.SORT_METHOD_LABEL,xbmcplugin.SORT_METHOD_DATE,xbmcplugin.SORT_METHOD_DURATION,xbmcplugin.SORT_METHOD_EPISODE]
+    for method in sort_methods:
+        xbmcplugin.addSortMethod(int(sys.argv[1]), sortMethod=method)

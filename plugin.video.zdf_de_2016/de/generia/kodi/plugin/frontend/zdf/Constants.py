b434a6d942d6865ab5329dbc2b8f265b3c4f965b
generia <alex@generia.de>
2017-07-28 23:55:18 +0200

    [plugin.video.zdf_de_2016] 1.0.5 (#1347)

--- plugin.video.zdf_de_2016/de/generia/kodi/plugin/frontend/zdf/Constants.py
+++ plugin.video.zdf_de_2016/de/generia/kodi/plugin/frontend/zdf/Constants.py
@@ -3,3 +3,4 @@ class Constants(object):
     baseUrl = 'https://www.zdf.de'
     apiBaseUrl = 'https://api.zdf.de'
     configUrl = baseUrl + '/ZDFplayer/configs/zdf/zdf2016/configuration.json'
+    apiToken = 'd2726b6c8c655e42b68b0db26131b15b22bd1a32' 

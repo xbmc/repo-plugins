5e264d6c8d4570409db218f364b531f52497beea
Martijn Moreel <martijn.moreel@gmail.com>
2017-08-06 22:44:24 +0200

    [plugin.video.vrt.nu] 0.0.6

--- plugin.video.vrt.nu/addon.py
+++ plugin.video.vrt.nu/addon.py
@@ -1,12 +1,10 @@
 import sys
 import xbmcaddon
-import os
-import requests
 from urlparse import parse_qsl
 from resources.lib.vrtplayer import vrtplayer
 from resources.lib.kodiwrappers import kodiwrapper
 from resources.lib.vrtplayer import actions
-from resources.lib.helperobjects import helperobjects
+from resources.lib.kodiwrappers import sortmethod
 
 _addon_ = xbmcaddon.Addon()
 _url = sys.argv[0]
@@ -19,21 +17,21 @@ def router(params_string):
     params = dict(parse_qsl(params_string))
     if params:
         if params['action'] == actions.LISTING_AZ:
-            kodi_wrapper.show_listing(vrt_player.get_az_menu_items())
+            kodi_wrapper.show_listing(vrt_player.get_az_menu_items(), sortmethod.ALPHABET)
         elif params['action'] == actions.LISTING_CATEGORIES:
-            kodi_wrapper.show_listing(vrt_player.get_category_menu_items())
+            kodi_wrapper.show_listing(vrt_player.get_category_menu_items(), sortmethod.ALPHABET)
         elif params['action'] == actions.LISTING_LIVE:
-            kodi_wrapper.show_listing(vrt_player.get_livestream_items())
+            kodi_wrapper.show_listing(vrt_player.get_livestream_items(), sortmethod.ALPHABET)
         elif params['action'] == actions.LISTING_VIDEOS:
             kodi_wrapper.show_listing(vrt_player.get_videos(params['video']))
         elif params['action'] == actions.LISTING_CATEGORY_VIDEOS:
-            kodi_wrapper.show_listing(vrt_player.get_video_category_episodes(params['video']))
+            kodi_wrapper.show_listing(vrt_player.get_video_category_episodes(params['video']), sortmethod.ALPHABET)
         elif params['action'] == actions.PLAY:
             kodi_wrapper.play_video(params['video'])
         elif params['action'] == actions.PLAY_LIVE:
             kodi_wrapper.play_livestream(params['video'])
     else:
-        kodi_wrapper.show_listing(vrt_player.get_main_menu_items())
+        kodi_wrapper.show_listing(vrt_player.get_main_menu_items(), sortmethod.ALPHABET)
 
 if __name__ == '__main__':
     router(sys.argv[2][1:])
---
+++ plugin.video.vrt.nu/resources/lib/kodiwrappers/sortmethod.py
@@ -0,0 +1,1 @@
+ALPHABET = 1

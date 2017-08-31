5cc6c128403179c7a5db2895959ddda5226e151e
Babak <babak@habibis.net>
2017-08-31 12:53:10 -0700

    [plugin.video.manoto] 2.0.1

--- plugin.video.manoto/default.py
+++ plugin.video.manoto/default.py
@@ -7,5 +7,5 @@ li = xbmcgui.ListItem ('manoto')
 li.setArt({ 'thumb': icon })
 li.setInfo('video', {'plot': 'Live Stream'})
 
-url = 'http://manotolive-i.akamaihd.net/hls/live/251779/livexhq/master_Layer1.m3u8'
+url = 'https://d1v7e0o3q8z5j0.cloudfront.net/Live.m3u8'
 xbmc.Player().play(url, li, False)

7392ea23faf8e8fcb353a1d725f25429663e2dde
b-jesch <birger.jesch@gmail.com>
2017-09-25 13:11:29 +0200

    [plugin.service.gto] v2.0.23

---
+++ plugin.service.gto/resources/lib/klack_de.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+
+import re
+import urllib2
+import datetime
+from dateutil import parser
+
+RSS_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+class Scraper():
+
+    def __init__(self):
+
+        # Properties
+
+        self.enabled = True
+        self.baseurl = 'http://www.klack.de'
+        self.rssurl = 'http://www.klack.de/xml/tippsRSS.xml'
+        self.friendlyname = 'klack.de - TV Highlights'
+        self.shortname = 'klack.de'
+        self.icon = 'klack.png'
+        self.selector = '<item>'
+        self.detailselector = '<table id="content">'
+        self.err404 = 'klackde_dummy.jpg'
+
+
+    def reset(self):
+
+        # Items
+
+        self.channel = ''
+        self.title = ''
+        self.thumb = False
+        self.detailURL = ''
+        self.startdate = ''
+        self.enddate = ''
+        self.runtime = '0'
+        self.genre = ''
+        self.plot = ''
+        self.cast = ''
+        self.rating = ''
+
+
+    def checkResource(self, resource, fallback):
+        if not resource: return fallback
+        _req = urllib2.Request(resource)
+        try:
+            _res = urllib2.urlopen(_req, timeout=5)
+        except urllib2.HTTPError as e:
+            if e.code == '404': return fallback
+        except urllib2.URLError as e:
+            return fallback
+        else:
+            return resource
+        return fallback
+
+    def scrapeRSS(self, content):
+
+        self.reset()
+
+        try:
+            self.channel = re.compile('<title>(.+?)</title>', re.DOTALL).findall(content)[0].split(': ')[0]
+            self.detailURL = re.compile('<link>(.+?)</link>', re.DOTALL).findall(content)[0]
+            self.title = re.compile('<title>(.+?)</title>', re.DOTALL).findall(content)[0].split(': ')[1]
+            self.thumb = re.compile('<img align="left" src="(.+?)"', re.DOTALL).findall(content)[0].replace('150x100.jpg', '500x333.jpg')
+        except IndexError:
+            pass
+
+        self.thumb = self.checkResource(self.thumb, self.err404)
+        try:
+            self.plot = re.compile('<description>(.+?)</description>', re.DOTALL).findall(content)[0].split('</a>')[1][:-3]
+        except IndexError:
+            pass
+
+        try:
+            self.startdate = (re.compile('<dc:date>(.+?)</dc:date>', re.DOTALL).findall(content)[0][0:19]).replace('T', ' ')
+        except IndexError:
+            pass
+
+    def scrapeDetailPage(self, content, contentID):
+
+        try:
+            if contentID in content:
+
+                container = content.split(contentID)
+                container.pop(0)
+                content = container[0]
+
+                # Broadcast Info (stop)
+
+                _start = parser.parse(self.startdate)
+                try:
+                    _s = re.compile('<span style="color: #d10159!important">(.+?)</span>', re.DOTALL).findall(content)[0].split()[2]
+                    _stop = _start.replace(hour=int(_s[0:2]), minute=int(_s[3:5]))
+                except IndexError:
+                    _stop = _start
+
+                if _start > _stop: _stop += datetime.timedelta(days=1)
+                self.enddate = datetime.datetime.strftime(_stop, RSS_TIME_FORMAT)
+                self.runtime = str((_stop - _start).seconds / 60)
+
+                # Genre
+                try:
+                    self.genre = re.compile('<span>(.+?)</span>', re.DOTALL).findall(content)[4].strip()
+
+                    # Cast
+                    self.cast = ', '.join(re.compile('<td class="actor">(.+?)</td>', re.DOTALL).findall(content))
+                except IndexError:
+                    pass
+
+        except TypeError:
+            pass
+
---
+++ plugin.service.gto/resources/lib/rtv_de.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+import re
+import urllib2
+
+class Scraper():
+
+    def __init__(self):
+
+        # Properties
+
+        self.enabled = False
+        self.baseurl = 'http://www.rtv.de'
+        self.rssurl = 'http://www.rtv.de/rss/filmtipps.xml'
+        self.friendlyname = 'rtv Highlights'
+        self.shortname = 'rtv'
+        self.icon = 'rtv.png'
+        self.selector = '<item><pubDate>'
+        self.detailselector = '<div id="details">'
+        self.err404 = 'broadcastDummy.jpg'
+
+    def reset(self):
+
+        # Items
+
+        self.channel = ''
+        self.title = ''
+        self.thumb = False
+        self.detailURL = ''
+        self.starttime = '00:00'
+        self.runtime = '0'
+        self.genre = ''
+        self.extrainfos = ''
+        self.cast = ''
+        self.rating = ''
+
+        self.endtime = '00:00'
+
+
+    def checkResource(self, resource, fallback):
+        if not resource: return fallback
+        _req = urllib2.Request(resource)
+        try:
+            _res = urllib2.urlopen(_req, timeout=5)
+        except urllib2.HTTPError as e:
+            if e.code == '404': return fallback
+        except urllib2.URLError as e:
+            return fallback
+        else:
+            return resource
+        return fallback
+
+    def scrapeRSS(self, content):
+
+        self.reset()
+
+        try:
+            self.channel = re.compile('<description>(.+?),', re.DOTALL).findall(content)[0]
+            self.thumb = re.compile('<media:content url="(.+?)" type="image/jpeg"/>', re.DOTALL).findall(content)[0]
+            self.thumb = self.checkResource(self.thumb, self.err404)
+            self.detailURL = re.compile('<link>(.+?)</link>', re.DOTALL).findall(content)[0]
+            self.title = ', '.join(re.compile('<title>(.+?)</title>', re.DOTALL).findall(content)[0].split(' - ')[:-1])
+        except IndexError:
+            pass
+
+        try:
+            self.extrainfos = re.compile('<description>(.+?)</description>', re.DOTALL).findall(content)[0].split('&lt;br/&gt;&lt;br/&gt;')[1]
+            self.genre = re.compile('Uhr&lt;br/&gt;(.+?),', re.DOTALL).findall(content)[0]
+            self.cast = re.compile('<description>(.+?)</description>', re.DOTALL).findall(content)[0].split('&lt;br/&gt;&lt;br/&gt;')[2][5:]
+        except IndexError:
+            pass
+
+        try:
+            self.date = re.compile('<title>(.+?)</title>', re.DOTALL).findall(content)[0].split(',')[-2].strip()
+            self.starttime = re.compile('<title>(.+?)</title>', re.DOTALL).findall(content)[0].split(',')[-1].strip().replace(' Uhr', '')
+        except IndexError:
+            pass
+
+    def scrapeDetailPage(self, content, contentID):
+
+        try:
+            if contentID in content:
+
+                container = content.split(contentID)
+                container.pop(0)
+                content = container[0]
+
+
+                # Thumbnail
+
+                if not self.thumb:
+                    try:
+                        self.thumb = re.compile('style="background-image:(.+?);">', re.DOTALL).findall(content)[0].split("'")[1]
+                    except IndexError:
+                        try:
+                            self.thumb = re.compile('<img class="kalooga_12730" src="(.+?)"', re.DOTALL).findall(content)[0]
+                        except IndexError:
+                            self.thumb = 'image://%s' % (self.err404)
+                    self.thumb = self.checkResource(self.thumb, self.err404)
+
+                # Broadcast Info (stop)
+                try:
+                    self.endtime = re.compile('<div class="time">(.+?)</div>', re.DOTALL).findall(content)[0].split()[2]
+                except IndexError:
+                    pass
+                
+                # Rating
+                try:
+                    self.rating = re.compile('<div class="rating editorialRating" onmouseout="Pit.StarRating.reset(this,(.+?))">', re.DOTALL).findall(content)[0].replace("'","")
+                except IndexError:
+                    pass
+        except TypeError:
+            pass
+

4f762d5a031e2905209f8ac4d83edd21480e0d13
Babak Habibi <babak@habibis.net>
2017-10-31 17:46:01 -0700

    [plugin.video.manoto] 2.1.0 (#1485)

--- plugin.video.manoto/default.py
+++ plugin.video.manoto/default.py
@@ -1,11 +1,30 @@
-import xbmc, xbmcgui, os, xbmcaddon
+import xbmc, xbmcgui, os, xbmcaddon, urllib2
+from BeautifulSoup import BeautifulSoup
 
 __settings__ = xbmcaddon.Addon()
+__language__ = __settings__.getLocalizedString
 home = __settings__.getAddonInfo('path')
+addonname = __settings__.getAddonInfo('name')
 icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
 li = xbmcgui.ListItem ('manoto')
 li.setArt({ 'thumb': icon })
 li.setInfo('video', {'plot': 'Live Stream'})
 
-url = 'https://d1v7e0o3q8z5j0.cloudfront.net/Live.m3u8'
-xbmc.Player().play(url, li, False)
+livePageUrl = "https://www.manototv.com/live"
+
+request = urllib2.Request(livePageUrl, headers={'User-Agent' : "Kodi"})
+try:
+    response = urllib2.urlopen(request)
+    html = response.read()
+
+    soup = BeautifulSoup(html)
+    stream = soup.find('source', type='application/x-mpegURL')
+
+    if stream is not None and stream['src'] is not None:
+            xbmc.Player().play(stream['src'], li, False)
+    else:
+        xbmcgui.Dialog().ok(addonname,  __language__(30002), __language__(30010))
+except urllib2.HTTPError, err:
+        xbmcgui.Dialog().ok(addonname,  __language__(30000) % err.code, __language__(30010))
+except:
+        xbmcgui.Dialog().ok(addonname,  __language__(30001), __language__(30010))

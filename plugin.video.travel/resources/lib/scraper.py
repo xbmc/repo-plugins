a837009fa77902db2148be41fbeae8050c14e571
learningit <scottyroscoe13@gmail.com>
2017-11-10 18:49:10 -0500

    [plugin.video.travel] 3.0.6

--- plugin.video.travel/resources/lib/scraper.py
+++ plugin.video.travel/resources/lib/scraper.py
@@ -20,22 +20,26 @@ UTF8 = 'utf-8'
 class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
-   html = self.getRequest('http://www.travelchannel.com/video/full-episodes')
-   a = re.compile('MediaBlock\-\-playlist.+?<a href="(.+?)".+?src="(.+?)".+?HeadlineText.+?>(.+?)<', re.DOTALL).findall(html)
-   for url,thumb,name in a:
-       plot = name
-       fanart = thumb
-       infoList = {}
-       infoList['TVShowTitle'] = name
-       infoList['Title'] = name
-       infoList['Studio'] = 'Travel Channel'
-       infoList['Plot'] = h.unescape(plot)
-       infoList['mediatype'] = 'tvshow'
-       ilist = self.addMenuItem(name,'GE', ilist, url, thumb, self.addonFanart, infoList, isFolder=True)
-   return(ilist)
+      html = self.getRequest('http://www.travelchannel.com/shows/video/full-episodes')
+      a = re.compile('MediaBlock\-\-playlist.+?<a href="(.+?)".+?src="(.+?)".+?HeadlineText.+?>(.+?)<', re.DOTALL).findall(html)
+      for url,thumb,name in a:
+          plot = name
+          if not thumb.startswith('http'):
+              thumb = 'http:'+thumb
+          fanart = thumb
+          infoList = {}
+          infoList['TVShowTitle'] = name
+          infoList['Title'] = name
+          infoList['Studio'] = 'Travel Channel'
+          infoList['Plot'] = h.unescape(plot)
+          infoList['mediatype'] = 'tvshow'
+          ilist = self.addMenuItem(name,'GE', ilist, url, thumb, self.addonFanart, infoList, isFolder=True)
+      return(ilist)
 
 
   def getAddonEpisodes(self,url,ilist):
+      if not url.startswith('http:'):
+          url = 'http:'+url
       html  = self.getRequest(url)
       c = re.compile('class="m\-VideoPlayer__a\-Container".+?x\-config">(.+?)</script>', re.DOTALL).search(html).group(1)
       c = json.loads(c)
@@ -66,5 +70,6 @@ class myAddon(t1mAddon):
                break
       url   = re.compile('<video src="(.+?)"',re.DOTALL).search(html).group(1)
       liz = xbmcgui.ListItem(path=url)
-      if suburl != "" : liz.setSubtitles([suburl])
+      if suburl != "" :
+          liz.setSubtitles([suburl])
       xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)

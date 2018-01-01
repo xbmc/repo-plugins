12491519c15d6962f3514c9ac225b3e7b4d73379
Erik Lindqvist <eriksmejl@gmail.com>
2018-01-01 13:01:38 +0100

    [plugin.video.svtplay] 4.1.0

--- plugin.video.svtplay/default.py
+++ plugin.video.svtplay/default.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# system imports
 import re
 import os
 import sys
@@ -8,6 +9,7 @@ import xbmc
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
+# own imports
 import CommonFunctions as common
 import resources.lib.helper as helper
 import resources.lib.svt as svt
@@ -30,14 +32,15 @@ MODE_VIEW_TITLES = "view_titles"
 MODE_VIEW_EPISODES = "view_episodes"
 MODE_VIEW_CLIPS = "view_clips"
 
+# settings keys
 S_DEBUG = "debug"
 S_HIDE_SIGN_LANGUAGE = "hidesignlanguage"
 S_SHOW_SUBTITLES = "showsubtitles"
 S_USE_ALPHA_CATEGORIES = "alpha"
 S_HIDE_RESTRICTED_TO_SWEDEN = "hideonlysweden"
 
+# plugin setup
 PLUGIN_HANDLE = int(sys.argv[1])
-
 addon = xbmcaddon.Addon("plugin.video.svtplay")
 localize = addon.getLocalizedString
 xbmcplugin.setContent(PLUGIN_HANDLE, "tvshows")
@@ -48,25 +51,36 @@ DEFAULT_FANART = os.path.join(
   xbmc.translatePath(addon.getAddonInfo("path")+"/resources/images/").decode("utf-8"),
   "background.png")
 
-common.plugin = addon.getAddonInfo('name') + ' ' + addon.getAddonInfo('version')
+common.plugin = "%s %s" % (addon.getAddonInfo('name'), addon.getAddonInfo('version'))
 common.dbg = helper.getSetting(S_DEBUG)
 
+def view_start():
+  __add_directory_item(localize(30009), {"mode": MODE_POPULAR})
+  __add_directory_item(localize(30003), {"mode": MODE_LATEST})
+  __add_directory_item(localize(30004), {"mode": MODE_LATEST_NEWS})
+  __add_directory_item(localize(30010), {"mode": MODE_LAST_CHANCE})
+  __add_directory_item(localize(30002), {"mode": MODE_LIVE_PROGRAMS})
+  __add_directory_item(localize(30008), {"mode": MODE_CHANNELS})
+  __add_directory_item(localize(30000), {"mode": MODE_A_TO_O})
+  __add_directory_item(localize(30001), {"mode": MODE_CATEGORIES})
+  __add_directory_item(localize(30006), {"mode": MODE_SEARCH})
 
-def viewStart():
-
-  addDirectoryItem(localize(30009), {"mode": MODE_POPULAR})
-  addDirectoryItem(localize(30003), {"mode": MODE_LATEST})
-  addDirectoryItem(localize(30004), {"mode": MODE_LATEST_NEWS})
-  addDirectoryItem(localize(30010), {"mode": MODE_LAST_CHANCE})
-  addDirectoryItem(localize(30002), {"mode": MODE_LIVE_PROGRAMS})
-  addDirectoryItem(localize(30008), {"mode": MODE_CHANNELS})
-  addDirectoryItem(localize(30000), {"mode": MODE_A_TO_O})
-  addDirectoryItem(localize(30001), {"mode": MODE_CATEGORIES})
-  addDirectoryItem(localize(30006), {"mode": MODE_SEARCH})
-
-def viewAtoO():
+def view_a_to_z():
   programs = svt.getAtoO()
+  __program_listing(programs)
 
+def view_alpha_directories():
+  alphas = svt.getAlphas()
+  if not alphas:
+    return
+  for alpha in alphas:
+    __add_directory_item(alpha, {"mode": MODE_LETTER, "letter": alpha})
+
+def view_programs_by_letter(letter):
+  programs = svt.getProgramsByLetter(letter)
+  __program_listing(programs)
+
+def __program_listing(programs):
   for program in programs:
     if program["onlyAvailableInSweden"] and \
         helper.getSetting(S_HIDE_RESTRICTED_TO_SWEDEN):
@@ -76,34 +90,17 @@ def viewAtoO():
     if program["type"] == "video":
       mode = MODE_VIDEO
       folder = False
-    addDirectoryItem(program["title"],
+    __add_directory_item(program["title"],
                 {"mode": mode, "url": program["url"]},
                 thumbnail=program["thumbnail"], folder=folder)
 
-def viewCategories():
+def view_categories():
   categories = svt.getCategories()
-
   for category in categories:
-    addDirectoryItem(category["title"],
+    __add_directory_item(category["title"],
                 {"mode": MODE_CATEGORY, "url": category["genre"]})
 
-def viewAlphaDirectories():
-  alphas = svt.getAlphas()
-  if not alphas:
-    return
-  for alpha in alphas:
-    addDirectoryItem(alpha, {"mode": MODE_LETTER, "letter": alpha})
-
-def viewProgramsByLetter(letter):
-  programs = svt.getProgramsByLetter(letter)
-
-  for program in programs:
-    if program["onlyAvailableInSweden"] and \
-        helper.getSetting(S_HIDE_RESTRICTED_TO_SWEDEN):
-      continue
-    addDirectoryItem(program["title"], {"mode": MODE_PROGRAM, "url": program["url"]}, thumbnail=program["thumbnail"])
-
-def viewSection(section, page):
+def view_section(section, page):
   (items, more_items) = svt.getItems(section, page)
   if not items:
     return
@@ -111,25 +108,25 @@ def viewSection(section, page):
     mode = MODE_VIDEO
     if item["type"] == "program":
       mode = MODE_PROGRAM
-    createDirItem(item, mode)
+    __create_dir_item(item, mode)
   if more_items:
-    addNextPageItem(page+1, section)
+    __add_next_page_item(page+1, section)
 
-def viewChannels():
+def view_channels():
   channels = svt.getChannels()
   if not channels:
     return
   for channel in channels:
-    createDirItem(channel, MODE_VIDEO)
+    __create_dir_item(channel, MODE_VIDEO)
 
-def viewLatestNews():
+def view_latest_news():
     items = svt.getLatestNews()
     if not items:
       return
     for item in items:
-      createDirItem(item, MODE_VIDEO)
+      __create_dir_item(item, MODE_VIDEO)
 
-def viewCategory(genre):
+def view_category(genre):
   programs = svt.getProgramsForGenre(genre)
   if not programs:
     return
@@ -137,59 +134,58 @@ def viewCategory(genre):
     mode = MODE_PROGRAM
     if program["type"] == "video":
       mode = MODE_VIDEO
-    createDirItem(program, mode)
+    __create_dir_item(program, mode)
 
-def viewEpisodes(url):
+def view_episodes(url):
   """
-  Displays the episodes for a program with URL 'url'.
+  Displays the episodes for a program
   """
-  episodes = svt.getEpisodes(url)
+  common.log("View episodes for %s" % url)
+  episodes = svt.getEpisodes(url.split("/")[-1])
   if episodes is None:
     helper.errorMsg("No episodes found!")
     return
-
   for episode in episodes:
-    createDirItem(episode, MODE_VIDEO)
+    __create_dir_item(episode, MODE_VIDEO)
 
-def addClipDirItem(url):
+def __add_clip_dir_item(url):
   """
   Adds the "Clips" directory item to a program listing.
   """
   params = {}
   params["mode"] = MODE_CLIPS
   params["url"] = url
-  addDirectoryItem(localize(30108), params)
+  __add_directory_item(localize(30108), params)
 
-def viewClips(url):
+def view_clips(url):
   """
   Displays the latest clips for a program
   """
-  clips = svt.getClips(url)
+  common.log("View clips for %s" % url)
+  clips = svt.getClips(url.split("/")[-1])
   if not clips:
     helper.errorMsg("No clips found!")
     return
-
   for clip in clips:
-    createDirItem(clip, MODE_VIDEO)
+    __create_dir_item(clip, MODE_VIDEO)
 
-def viewSearch():
+def view_search():
   keyword = common.getUserInput(localize(30102))
   if keyword == "" or not keyword:
-    viewStart()
+    view_start()
     return
   keyword = urllib.quote(keyword)
   helper.infoMsg("Search string: " + keyword)
-
   keyword = re.sub(r" ", "+", keyword)
-
+  keyword = keyword.strip()
   results = svt.getSearchResults(keyword)
   for result in results:
     mode = MODE_VIDEO
     if result["type"] == "program":
       mode = MODE_PROGRAM
-    createDirItem(result["item"], mode)
+    __create_dir_item(result["item"], mode)
 
-def createDirItem(article, mode):
+def __create_dir_item(article, mode):
   """
   Given an article and a mode; create directory item
   for the article.
@@ -201,10 +197,8 @@ def createDirItem(article, mode):
     params["mode"] = mode
     params["url"] = article["url"]
     folder = False
-
     if mode == MODE_PROGRAM:
       folder = True
-
     info = None
     if "info" in article.keys():
       if "onlyAvailableInSweden" in article["info"] and \
@@ -213,71 +207,57 @@ def createDirItem(article, mode):
         # Do not show geo restricted items
         return
       info = article["info"]
-    addDirectoryItem(article["title"], params, article["thumbnail"], folder, False, info)
+    __add_directory_item(article["title"], params, article["thumbnail"], folder, False, info)
 
-def addNextPageItem(next_page, section):
-  addDirectoryItem("Next page",
+def __add_next_page_item(next_page, section):
+  __add_directory_item("Next page",
                    {"page": next_page,
                     "mode": section})
 
-def startVideo(url):
-  if "m3u8" not in url:
-    video_json = svt.getVideoJSON(url)
-    if video_json is None:
-      common.log("ERROR: Could not get video JSON")
-      return
-    try:
-      show_obj = helper.resolveShowJSON(video_json)
-    except ValueError:
-      common.log("Could not decode JSON for "+url)
-      return
-  else:
-    show_obj = {"videoUrl": url, "subtitleUrl": ""}
+def start_video(video_id):
+  video_json = svt.getVideoJSON(video_id)
+  if video_json is None:
+    common.log("ERROR: Could not get video JSON")
+    return
+  try:
+    show_obj = helper.resolveShowJSON(video_json)
+  except ValueError:
+    common.log("Could not decode JSON for "+video_id)
+    return
   if show_obj["videoUrl"]:
-    playVideo(show_obj)
+    play_video(show_obj)
   else:
     dialog = xbmcgui.Dialog()
     dialog.ok("SVT Play", localize(30100))
 
-def playVideo(show_obj):
+def play_video(show_obj):
   player = xbmc.Player()
   start_time = time.time()
-
   xbmcplugin.setResolvedUrl(PLUGIN_HANDLE, True, xbmcgui.ListItem(path=show_obj["videoUrl"]))
-
   if show_obj["subtitleUrl"]:
     while not player.isPlaying() and time.time() - start_time < 10:
       time.sleep(1.)
-
     player.setSubtitles(show_obj["subtitleUrl"])
-
     if not helper.getSetting(S_SHOW_SUBTITLES):
       player.showSubtitles(False)
 
-
-def addDirectoryItem(title, params, thumbnail=None, folder=True, live=False, info=None):
-
+def __add_directory_item(title, params, thumbnail="", folder=True, live=False, info=None):
   list_item = xbmcgui.ListItem(title)
-
   if thumbnail:
     list_item.setThumbnailImage(thumbnail)
-
   if live:
     list_item.setProperty("IsLive", "true")
-
-  if not folder:
-    if params["mode"] == MODE_VIDEO:
+  if not folder and params["mode"] == MODE_VIDEO:
       list_item.setProperty("IsPlayable", "true")
-      if not thumbnail:
-        thumbnail = ""
-
-  fanart = DEFAULT_FANART
+  fanart = info.get("fanart", "") if info else DEFAULT_FANART
+  poster = info.get("poster", "") if info else ""
   if info:
     list_item.setInfo("Video", info)
-    if "fanart" in info:
-      fanart = info["fanart"]
-
-  list_item.setArt({"fanart": fanart})
+  list_item.setArt({
+    "fanart": fanart,
+    "poster": poster,
+    "thumbnail": thumbnail
+  })
   xbmcplugin.addDirectoryItem(PLUGIN_HANDLE, sys.argv[0] + '?' + urllib.urlencode(params), list_item, folder)
 
 # Main segment of script
@@ -290,35 +270,35 @@ if not ARG_PAGE:
   ARG_PAGE = "1"
 
 if not ARG_MODE:
-  viewStart()
+  view_start()
 elif ARG_MODE == MODE_A_TO_O:
   if helper.getSetting(S_USE_ALPHA_CATEGORIES):
-    viewAlphaDirectories()
+    view_alpha_directories()
   else:
-    viewAtoO()
+    view_a_to_z()
 elif ARG_MODE == MODE_CATEGORIES:
-  viewCategories()
+  view_categories()
 elif ARG_MODE == MODE_CATEGORY:
-  viewCategory(ARG_URL)
+  view_category(ARG_URL)
 elif ARG_MODE == MODE_PROGRAM:
-  viewEpisodes(ARG_URL)
-  addClipDirItem(ARG_URL)
+  view_episodes(ARG_URL)
+  __add_clip_dir_item(ARG_URL)
 elif ARG_MODE == MODE_CLIPS:
-  viewClips(ARG_URL)
+  view_clips(ARG_URL)
 elif ARG_MODE == MODE_VIDEO:
-  startVideo(ARG_URL)
+  start_video(ARG_URL)
 elif ARG_MODE == MODE_POPULAR or \
      ARG_MODE == MODE_LATEST or \
      ARG_MODE == MODE_LAST_CHANCE or \
      ARG_MODE == MODE_LIVE_PROGRAMS:
-  viewSection(ARG_MODE, int(ARG_PAGE))
+  view_section(ARG_MODE, int(ARG_PAGE))
 elif ARG_MODE == MODE_LATEST_NEWS:
-  viewLatestNews()
+  view_latest_news()
 elif ARG_MODE == MODE_CHANNELS:
-  viewChannels()
+  view_channels()
 elif ARG_MODE == MODE_LETTER:
-  viewProgramsByLetter(ARG_PARAMS.get("letter"))
+  view_programs_by_letter(ARG_PARAMS.get("letter"))
 elif ARG_MODE == MODE_SEARCH:
-  viewSearch()
+  view_search()
 
 xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
--- plugin.video.svtplay/resources/lib/helper.py
+++ plugin.video.svtplay/resources/lib/helper.py
@@ -4,7 +4,6 @@ import json
 import re
 import urllib
 import urlparse
-
 import xbmcaddon
 
 import CommonFunctions as common
@@ -15,141 +14,19 @@ THUMB_SIZE = "extralarge"
 # Available bandwidths
 BANDWIDTH = [300, 500, 900, 1600, 2500, 5000]
 
-def getPage(url):
-  if not url.startswith("/") and not url.startswith("http://"):
-    url = "/" + url
-
-  result = common.fetchPage({ "link": url })
-
-  if result["status"] == 200:
-    return result["content"]
-
-  if result["status"] == 500:
-    common.log("redirect url: %s" %result["new_url"])
-    common.log("header: %s" %result["header"])
-    common.log("content: %s" %result["content"])
-    return None
-
-
-def convertChar(char):
-  if char == "&Aring;":
-    return "Å"
-  elif char == "&Auml;":
-    return "Ä"
-  elif char == "&Ouml;":
-    return "Ö"
-  else:
-    return char
-
-
-def convertDuration(duration):
-  """
-  Converts SVT's duration format to XBMC friendly format (minutes).
-
-  SVT has the following format on their duration strings:
-  1 h 30 min
-  1 min 30 sek
-  1 min
-  """
-
-  match = re.match(r'(^(\d+)\sh)*(\s*(\d+)\smin)*(\s*(\d+)\ssek)*', duration)
-
-  dhours = 0
-  dminutes = 0
-  dseconds = 0
-
-  if match.group(1):
-    dhours = int(match.group(2)) * 60
-
-  if match.group(3):
-    dminutes = int(match.group(4))
-
-  if match.group(5):
-    dseconds = int(match.group(6)) / 60
-
-  return str(dhours + dminutes + dseconds)
-
-def convertDate(date):
-  """
-  Converts a SVT date string to a XBMC date string
-
-  Examples:
-  From "lör 21 mar" to "2014-03-21"
-  From "idag 18.00" to "2014-03-24"
-  From "igår 20.00" to "2014-03-23"
-  """
-  if not date:
-    return ""
-
-  months = {
-      "jan": 1,
-      "feb": 2,
-      "mar": 3,
-      "apr": 4,
-      "maj": 5,
-      "jun": 6,
-      "jul": 7,
-      "aug": 8,
-      "sep": 9,
-      "okt": 10,
-      "nov": 11,
-      "dec": 12
-      }
-  today = datetime.date.today()
-  one_day = datetime.timedelta(days=1)
-  return_date = today
-
-  match = re.match(r"(.+)\s(\d+)\s(\w+)", date)
-  if match:
-    # "lör 21 mar" match
-    month = months[match.group(3)]
-    day = int(match.group(2))
-    return_date = today.replace(day=day, month=month)
-  else:
-    match = re.match(r"(.+)\s.+", date)
-    if match:
-      if match.group(1) == "ig&aring;r":
-        # "igår 18.00" match
-        return_date = today - one_day
-  return return_date.isoformat()
-
 def getUrlParameters(arguments):
   """
   Return URL parameters as a dict from a query string
   """
   params = {}
-
   if arguments:
-
       start = arguments.find("?") + 1
       pairs = arguments[start:].split("&")
-
       for pair in pairs:
-
         split = pair.split("=")
-
         if len(split) == 2:
           params[split[0]] = split[1]
-
   return params
-
-
-def tabExists(html, tabname):
-  """
-  Check if a specific tab exists in the DOM.
-  """
-  return elementExists(html, "div", { "data-tabname": tabname})
-
-
-def elementExists(html, etype, attrs):
-  """
-  Check if a specific element exists in the DOM.
-  """
-
-  htmlelement = common.parseDOM(html, etype, attrs = attrs)
-
-  return len(htmlelement) > 0
-
 
 def prepareImgUrl(url, baseUrl):
   if url.startswith("//"):
@@ -157,6 +34,8 @@ def prepareImgUrl(url, baseUrl):
     url = "http://" + url
   elif not (url.startswith("http://") or url.startswith("https://")) and baseUrl:
     url = baseUrl + url
+  # Kodi has issues fetching images over SSL
+  url = url.replace("https", "http")
   return url
 
 def prepareThumb(thumbUrl, baseUrl):
@@ -167,8 +46,6 @@ def prepareThumb(thumbUrl, baseUrl):
     return ""
   thumbUrl = prepareImgUrl(thumbUrl, baseUrl)
   thumbUrl = re.sub(r"\{format\}|small|medium|large|extralarge", THUMB_SIZE, thumbUrl)
-  # Kodi has issues fetching images over SSL
-  thumbUrl = thumbUrl.replace("https", "http")
   return thumbUrl
 
 def prepareFanart(fanartUrl, baseUrl):
@@ -179,46 +56,7 @@ def prepareFanart(fanartUrl, baseUrl):
     return ""
   fanartUrl = prepareImgUrl(fanartUrl, baseUrl)
   fanartUrl = re.sub(r"\{format\}|small|medium|large|extralarge", "extralarge_imax", fanartUrl)
-  # Kodi has issues fetching images over SSL
-  fanartUrl = fanartUrl.replace("https", "http")
   return fanartUrl
-
-
-def mp4Handler(jsonObj):
-  """
-  Returns a mp4 stream URL.
-
-  If there are several mp4 streams in the JSON object:
-  pick the one with the highest bandwidth.
-
-  Some programs are available with multiple mp4 streams
-  for different bitrates. This function ensures that the one
-  with the highest bitrate is chosen.
-
-  Can possibly be extended to support some kind of quality
-  setting in the plugin.
-  """
-  videos = []
-
-  # Find all mp4 videos
-  for video in jsonObj["video"]["videoReferences"]:
-    if video["url"].endswith(".mp4"):
-      videos.append(video)
-
-  if len(videos) == 1:
-    return videos[0]["url"]
-
-  bitrate = 0
-  url = ""
-
-  # Find the video with the highest bitrate
-  for video in videos:
-    if video["bitrate"] > bitrate:
-      bitrate = video["bitrate"]
-      url = video["url"]
-
-  common.log("Info: bitrate="+str(bitrate)+" url="+url)
-  return url
 
 def getStreamForBW(url):
   """
@@ -227,14 +65,11 @@ def getStreamForBW(url):
   """
   low_bandwidth  = int(float(addon.getSetting("bandwidth")))
   high_bandwidth = getHighBw(low_bandwidth)
-
   f = urllib.urlopen(url)
   lines = f.readlines()
-
   hls_url = ""
   marker = "#EXT-X-STREAM-INF"
   found = False
-
   for line in lines:
     if found:
       # The stream url is on the line proceeding the header
@@ -246,19 +81,15 @@ def getStreamForBW(url):
         if low_bandwidth < int(match.group(1)) < high_bandwidth:
           common.log("Found stream with bandwidth " + match.group(1) + " for selected bandwidth " + str(low_bandwidth))
           found = True
-
   f.close()
-
   if found:
     hls_url = hls_url.rstrip()
     return_url = urlparse.urljoin(url, hls_url)
     common.log("Returned stream url: " + return_url)
     return (return_url, '')
-  else:
-    error_msg = "No stream found for bandwidth setting " + str(low_bandwidth)
-    common.log(error_msg)
-    return (None, error_msg)
-
+  error_msg = "No stream found for bandwidth setting " + str(low_bandwidth)
+  errorMsg(error_msg)
+  return (None, error_msg)
 
 def getHighBw(low):
   """
@@ -266,16 +97,6 @@ def getHighBw(low):
   """
   i = BANDWIDTH.index(low)
   return BANDWIDTH[i+1]
-
-def getJSONObj(show_url):
-  """
-  Returns a SVT JSON object from a show URL
-  """
-  url_obj = urllib.urlopen(show_url)
-  json_obj = json.load(url_obj)
-  url_obj.close()
-  return json_obj
-
 
 def getVideoURL(json_obj):
   """
@@ -289,7 +110,6 @@ def getVideoURL(json_obj):
         video_url = video["url"]
       else:
         video_url = alt
-
   return video_url
 
 def getAltUrl(video_url):
@@ -326,13 +146,11 @@ def resolveShowJSON(json_obj):
   """
   video_url = None
   subtitle_url = None
-
   video_url = getVideoURL(json_obj)
   if video_url:
     subtitle_url = getSubtitleUrl(json_obj)
     extension = getVideoExtension(video_url)
     errormsg = None
-
     if extension == "HLS":
       if getSetting("bwselect"):
         (video_url, errormsg) = getStreamForBW(video_url)
@@ -370,14 +188,11 @@ def getVideoExtension(video_url):
   """
   # Remove all query strings
   url = video_url.split("?")[0]
-  extension = None
   if url.endswith(".m3u8"):
-    extension = "HLS"
+    return "HLS"
   elif url.endswith(".mp4"):
-    extension = "MP4"
-
-  return extension
-
+    return "MP4"
+  return None
 
 def getSetting(setting):
   return True if addon.getSetting(setting) == "true" else False

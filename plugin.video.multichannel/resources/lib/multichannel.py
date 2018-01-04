6bfc6e18350174dca74ca58b54fc47c2b1d5f0db
Lunatixz <Kgraer@gmail.com>
2018-01-04 16:17:19 -0500

    [plugin.video.multichannel] 1.0.0

---
+++ plugin.video.multichannel/resources/lib/multichannel.py
@@ -0,0 +1,169 @@
+#   Copyright (C) 2017 Lunatixz
+#
+#
+# This file is part of multichannel.
+#
+# multichannel is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multichannel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with multichannel.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+import sys, time, datetime, re, traceback
+import urlparse, urllib, urllib2, socket, json
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon
+
+from bs4 import BeautifulSoup
+from YDStreamExtractor import getVideoInfo
+from simplecache import SimpleCache, use_cache
+
+# Plugin Info
+ADDON_ID      = 'plugin.video.multichannel'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME    = REAL_SETTINGS.getAddonInfo('name')
+SETTINGS_LOC  = REAL_SETTINGS.getAddonInfo('profile')
+ADDON_PATH    = REAL_SETTINGS.getAddonInfo('path').decode('utf-8')
+ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
+ICON          = REAL_SETTINGS.getAddonInfo('icon')
+FANART        = REAL_SETTINGS.getAddonInfo('fanart')
+LANGUAGE      = REAL_SETTINGS.getLocalizedString
+
+## GLOBALS ##
+TIMEOUT       = 15
+CONTENT_TYPE  = 'files'
+DEBUG         = REAL_SETTINGS.getSetting('Enable_Debugging') == 'true'
+QUALITY       = int(REAL_SETTINGS.getSetting('Quality'))
+BASE_URL      = 'http://multichannel.com/'
+BASE_VID      = BASE_URL + 'video'
+
+def log(msg, level=xbmc.LOGDEBUG):
+    if DEBUG == False and level != xbmc.LOGERROR: return
+    if level == xbmc.LOGERROR: msg += ' ,' + traceback.format_exc()
+    xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + msg, level)
+    
+def getParams():
+    return dict(urlparse.parse_qsl(sys.argv[2][1:]))
+                 
+socket.setdefaulttimeout(TIMEOUT)
+class MultiChannel(object):
+    def __init__(self):
+        log('__init__')
+        self.cache = SimpleCache()
+           
+           
+    def openURL(self, url):
+        log('openURL, url = ' + str(url))
+        try:
+            cacheresponse = self.cache.get(ADDON_NAME + '.openURL, url = %s'%url)
+            if not cacheresponse:
+                request = urllib2.Request(url)
+                request.add_header('User-Agent','Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)')
+                response = urllib2.urlopen(request, timeout = TIMEOUT).read()
+                self.cache.set(ADDON_NAME + '.openURL, url = %s'%url, response, expiration=datetime.timedelta(hours=1))
+            return self.cache.get(ADDON_NAME + '.openURL, url = %s'%url)
+        except Exception as e:
+            log("openURL Failed! " + str(e), xbmc.LOGERROR)
+            xbmcgui.Dialog().notification(ADDON_NAME, LANGUAGE(30001), ICON, 4000)
+            return ''
+         
+         
+    def buildMenu(self):
+        self.addDir('Browse', BASE_VID, 1)
+        self.addYoutube("Browse Youtube" , 'plugin://plugin.video.youtube/channel/UC0VOh1nD6Tlq_5gjkpSecWA/')
+            
+            
+    def browse(self, url):
+        log('browse')
+        soup   = BeautifulSoup(self.openURL(url), "html.parser")
+        videos = soup('div', {'class': 'screen'})
+        for video in videos:
+            try: thumb = (video('a', {'class': 'thumb'})[0].find('img').attrs['src'])
+            except: thumb = ICON
+            vid_url = BASE_URL + video.find_all('a')[1].attrs['href']
+            plot   = video.find_all('a')[1].get_text()
+            label  = plot.split(' -- ')[1]
+            try: aired = (datetime.datetime.strptime(video.find('time').get_text(), '%m/%d/%Y')).strftime('%Y-%m-%d')
+            except: aired = (datetime.datetime.now()).strftime('%Y-%m-%d')
+            infoLabels = {"mediatype":"episode","label":label,"title":label,"plot":plot,"genre":'News',"aired":aired}
+            infoArt    = {"thumb":thumb,"poster":thumb,"fanart":FANART,"icon":ICON,"logo":ICON}
+            self.addLink(label, vid_url, 9, infoLabels, infoArt, len(videos))
+        next = soup('li', {'class': 'pager-next'})
+        if len(next) == 0: return
+        next_url   = BASE_URL + next[0].find('a').attrs['href']
+        next_label = (next[0].find('a').attrs['title'] or next[0].get_text())
+        self.addDir(next_label, next_url, 1)
+            
+            
+    def playVideo(self, name, url, liz=None):
+        log('playVideo')
+        info = getVideoInfo(url,QUALITY,True)
+        if info is None: return
+        info = info.streams()
+        url  = info[0]['xbmc_url']
+        liz  = xbmcgui.ListItem(name, path=url)
+        if 'subtitles' in info[0]['ytdl_format']: liz.setSubtitles([x['url'] for x in info[0]['ytdl_format']['subtitles'].get('en','') if 'url' in x])
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
+        
+        
+    def addYoutube(self, name, url):
+        liz=xbmcgui.ListItem(name)
+        liz.setProperty('IsPlayable', 'false')
+        liz.setInfo(type="Video", infoLabels={"label":name,"title":name} )
+        liz.setArt({'thumb':ICON,'fanart':FANART})
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=True)
+        
+           
+    def addLink(self, name, u, mode, infoList=False, infoArt=False, total=0):
+        name = name.encode("utf-8")
+        log('addLink, name = ' + name)
+        liz=xbmcgui.ListItem(name)
+        liz.setProperty('IsPlayable', 'true')
+        if infoList == False: liz.setInfo(type="Video", infoLabels={"mediatype":"video","label":name,"title":name})
+        else: liz.setInfo(type="Video", infoLabels=infoList)
+        if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
+        else: liz.setArt(infoArt)
+        u=sys.argv[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,totalItems=total)
+
+
+    def addDir(self, name, u, mode, infoList=False, infoArt=False):
+        name = name.encode("utf-8")
+        log('addDir, name = ' + name)
+        liz=xbmcgui.ListItem(name)
+        liz.setProperty('IsPlayable', 'false')
+        if infoList == False: liz.setInfo(type="Video", infoLabels={"mediatype":"video","label":name,"title":name})
+        else: liz.setInfo(type="Video", infoLabels=infoList)
+        if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
+        else: liz.setArt(infoArt)
+        u=sys.argv[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+
+params=getParams()
+try: url=urllib.unquote_plus(params["url"])
+except: url=None
+try: name=urllib.unquote_plus(params["name"])
+except: name=None
+try: mode=int(params["mode"])
+except: mode=None
+log("Mode: "+str(mode))
+log("URL : "+str(url))
+log("Name: "+str(name))
+
+if mode==None:  MultiChannel().buildMenu()
+elif mode == 1: MultiChannel().browse(url)
+elif mode == 9: MultiChannel().playVideo(name, url)
+
+xbmcplugin.setContent(int(sys.argv[1])    , CONTENT_TYPE)
+xbmcplugin.addSortMethod(int(sys.argv[1]) , xbmcplugin.SORT_METHOD_UNSORTED)
+xbmcplugin.addSortMethod(int(sys.argv[1]) , xbmcplugin.SORT_METHOD_NONE)
+xbmcplugin.addSortMethod(int(sys.argv[1]) , xbmcplugin.SORT_METHOD_LABEL)
+xbmcplugin.addSortMethod(int(sys.argv[1]) , xbmcplugin.SORT_METHOD_TITLE)
+xbmcplugin.endOfDirectory(int(sys.argv[1]), cacheToDisc=True)

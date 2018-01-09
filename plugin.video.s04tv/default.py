a01fa3c6b90f3f9ec54c4470399752bb617ac263
Malte Löpmann <maloep@googlemail.com>
2018-01-09 20:48:22 +0100

    [plugin.video.s04tv] 3.1.3
    - fix issues found by codacy
    - don't use variable names with underscores

--- plugin.video.s04tv/default.py
+++ plugin.video.s04tv/default.py
@@ -2,32 +2,31 @@
 
 # Copyright (C) 2015 Malte Loepmann (maloep@googlemail.com)
 #
-# This program is free software; you can redistribute it and/or modify it under the terms 
-# of the GNU General Public License as published by the Free Software Foundation; 
+# This program is free software; you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation;
 # either version 2 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program; 
+# You should have received a copy of the GNU General Public License along with this program;
 # if not, see <http://www.gnu.org/licenses/>.
 
 import xbmc, xbmcplugin, xbmcgui, xbmcaddon
-import os, sys, re, json, string
-import urllib, urllib2
+import os, sys, re, json
+import urllib
 from urlparse import *
 #import xml.etree.ElementTree as ET
 import cookielib
 try:
-# Python 2.6-2.7 
+    # Python 2.6-2.7
     from HTMLParser import HTMLParser
 except ImportError:
     # Python 3
     from html.parser import HTMLParser
 
 from BeautifulSoup import BeautifulSoup
-from BeautifulSoup import Tag
 import mechanize
 
 PLUGINNAME = 'S04tv'
@@ -36,15 +35,15 @@ BASE_URL = 'https://schalke04.de/tv/videos/'
 
 # Shared resources
 addonPath = ''
-__addon__ = xbmcaddon.Addon(id='plugin.video.s04tv')
-addonPath = __addon__.getAddonInfo('path')
-        
+addon = xbmcaddon.Addon(id='plugin.video.s04tv')
+addonPath = addon.getAddonInfo('path')
+
 BASE_RESOURCE_PATH = os.path.join(addonPath, "resources" )
 sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib" ) )
 sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib", "BeautifulSoup" ) )
 
 
-__language__ = __addon__.getLocalizedString
+language = addon.getLocalizedString
 thisPlugin = int(sys.argv[1])
 browser = mechanize.Browser()
 
@@ -52,7 +51,7 @@ missingelementtext = "Missing element '%s'. Maybe the site structure has changed
 
 videoquality = 'hd720'
 
-quality = __addon__.getSetting('videoquality')
+quality = addon.getSetting('videoquality')
 if quality == 'low':
     videoquality = 'small'
 elif quality == 'mid':
@@ -64,16 +63,15 @@ elif quality == 'hd':
 
 
 
-def buildVideoDir(url, doc):    
-    
+def buildVideoDir(url, doc):
+
     #allow sorting of video titles
-    xbmcplugin.addSortMethod(thisPlugin, xbmcplugin.SORT_METHOD_UNSORTED)    
+    xbmcplugin.addSortMethod(thisPlugin, xbmcplugin.SORT_METHOD_UNSORTED)
     #xbmcplugin.addSortMethod(thisPlugin, xbmcplugin.SORT_METHOD_DATE)
     xbmcplugin.addSortMethod(thisPlugin, xbmcplugin.SORT_METHOD_TITLE)
-    
-    hideexclusive = __addon__.getSetting('hideexclusivevideos').upper() == 'TRUE'
-    hideflag = __addon__.getSetting('hidefreeexclflag').upper() == 'TRUE'
-    hidedate = __addon__.getSetting('hidedate').upper() == 'TRUE'
+
+    hideexclusive = addon.getSetting('hideexclusivevideos').upper() == 'TRUE'
+    hideflag = addon.getSetting('hidefreeexclflag').upper() == 'TRUE'
     
     soup = BeautifulSoup(''.join(doc))
     section = soup.find('section')
@@ -87,14 +85,13 @@ def buildVideoDir(url, doc):
         
     ahrefs = section.findAll('a')
     
-    for ahref in ahrefs:     
+    for ahref in ahrefs:
         
         url = ahref['href']
-        payed = 'false'
         try:
-            payed = ahref['data-payed']
-        except:
-            pass
+            isPayedContent = ahref['data-payed'] == 'true'
+        except KeyError:
+            isPayedContent = False
         
         img = ahref.find('img')
         if(img):
@@ -112,15 +109,15 @@ def buildVideoDir(url, doc):
             title = HTMLParser().unescape(title)
         else:
             mode = 1
-            title = __language__(30003)
+            title = language(30003)
         
         #title = title.replace('<br>', ' - ')
         
         extraInfo = {}
-        if(payed == 'false'):
-            extraInfo['IsFreeContent'] = 'True'
+        if not isPayedContent:
+            extraInfo['isPayedContent'] = 'False'
         else:
-            extraInfo['IsFreeContent'] = 'False'
+            extraInfo['isPayedContent'] = 'True'
             if(hideexclusive):
                 #don't add exclusive videos to list
                 continue
@@ -137,8 +134,8 @@ def getVideoUrl(url, doc):
     xbmc.log('getVideoUrl: url=' +url)
     
     #check if we need to login
-    isFreeContent = xbmc.getInfoLabel( "ListItem.Property(IsFreeContent)" ) == 'True'
-    if(not isFreeContent):
+    isPayedContent = xbmc.getInfoLabel( "ListItem.Property(isPayedContent)" ) == 'True'
+    if(isPayedContent):
         success = login()
         if(not success):
             return
@@ -190,8 +187,8 @@ def getVideoUrl(url, doc):
     for key in jsonPlaylist:
         entry = jsonPlaylist[key]
         quality = entry['quality']
-        type = entry['type']
-        if(type == 'videomp4' and quality == videoquality):
+        videotype = entry['type']
+        if(videotype == 'videomp4' and quality == videoquality):
             videourl = entry['src']
             
     listitem = xbmcgui.ListItem(path=videourl)
@@ -208,7 +205,7 @@ def addDir(name, url, mode, iconimage):
     return ok
    
 
-def addLink(name, url, mode, iconimage, date, extraInfo = {}):
+def addLink(name, url, mode, iconimage, date, extraInfo):
     parameters = {'url' : url.encode('utf-8'), 'mode' : str(mode), 'name' : name.encode('utf-8')}
     u = sys.argv[0] +'?' +urllib.urlencode(parameters)
     ok = True
@@ -227,12 +224,12 @@ def addLink(name, url, mode, iconimage, date, extraInfo = {}):
 
 def login():
     
-    username = __addon__.getSetting('username')
+    username = addon.getSetting('username')
     xbmc.log('Logging in with username "%s"' %username)
-    password = __addon__.getSetting('password')
+    password = addon.getSetting('password')
     
-    if(username == '' or password == ''):
-        xbmcgui.Dialog().ok(PLUGINNAME, __language__(30102), __language__(30103))
+    if(not username or not password):
+        xbmcgui.Dialog().ok(PLUGINNAME, language(30102), language(30103))
         return False
     
     url = 'https://schalke04.de/account/login/'
@@ -244,25 +241,31 @@ def login():
 
     for form in br.forms():
         try:
-            form['email']
+            form['email'] = username
+            form['password'] = password
             br.form = form
             break
-        except:
-            pass        
-            
-    br.form['email'] = username
-    br.form['password'] = password
-    br.submit()
+        except mechanize.ControlNotFoundError:
+            pass
 
+    br.submit()
+    loginSuccessful = False
     response = br.response().read()
-    
-    #if there is a logout button, login was successful
-    if(response.find('Schalke TV KOMPLETT')):
+    soup = BeautifulSoup(''.join(response))
+    for textelement in soup(text='Schalke TV KOMPLETT'):
+        li = textelement.parent
+        try:
+            isChecked = li['class'] == 'checked'
+            loginSuccessful = isChecked
+        except KeyError:
+            loginSuccessful = False
+
+    if(loginSuccessful):
         xbmc.log('login successful')
         return True
     else:
         xbmc.log('login failed')
-        xbmcgui.Dialog().ok(PLUGINNAME, __language__(30100) %username.decode('utf-8'), __language__(30101))
+        xbmcgui.Dialog().ok(PLUGINNAME, language(30100) %username.decode('utf-8'), language(30101))
         return False
 
 
@@ -272,7 +275,7 @@ def getUrl(url):
         xbmc.log('Get url: '+url)
         browser.set_handle_robots(False)
         try:
-            browser.open(url)        
+            browser.open(url)
             response = browser.response().read()
         except Exception, (exc):
             xbmc.log('Error while opening url: ' +str(exc))
@@ -290,7 +293,7 @@ def runPlugin(url, doc):
         buildVideoDir(url, doc)
         
     elif mode == 2:
-        getVideoUrl(url, doc)    
+        getVideoUrl(url, doc)
 
 
 xbmc.log('S04TV: start addon')

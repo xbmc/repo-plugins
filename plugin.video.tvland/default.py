17dcc95bfb0b90542f3512880c7309b99f1cbb91
Lunatixz <Kgraer@gmail.com>
2018-01-13 00:22:04 -0500

    [plugin.video.tvland] 1.0.0
    missing screenshots

---
+++ plugin.video.tvland/default.py
@@ -0,0 +1,23 @@
+#   Copyright (C) 2018 Lunatixz
+#
+#
+# This file is part of TV Land.
+#
+# TV Land is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TV Land is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TV Land.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+
+#entrypoint
+if __name__ == '__main__':
+    from resources.lib import tvland

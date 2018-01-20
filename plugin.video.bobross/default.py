0a034cdf4dba29c0b2cf73c31d82cb25383fdc65
Lunatixz <Kgraer@gmail.com>
2018-01-19 22:16:14 -0500

    [plugin.video.bobross] 1.0.0

---
+++ plugin.video.bobross/default.py
@@ -0,0 +1,23 @@
+#   Copyright (C) 2018 Lunatixz
+#
+#
+# This file is part of Bob Ross - YouTube.
+#
+# Bob Ross - YouTube is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Bob Ross - YouTube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Bob Ross - YouTube.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# -*- coding: utf-8 -*-
+import xbmc
+if __name__ == '__main__':
+    xbmc.executebuiltin("ActivateWindow(10025,plugin://plugin.video.youtube/user/BobRossInc/)")

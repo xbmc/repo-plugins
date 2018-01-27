776e1158ce94fd80b2efe4ee26bbc3d53a74937f
skipmodea1 <skipmodea1@outlook.com>
2018-01-27 14:37:08 +0100

    [plugin.video.nlhardwareinfo] 1.0.8

--- plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_list_play.py
+++ plugin.video.nlhardwareinfo/resources/lib/nlhardwareinfo_list_play.py
@@ -4,24 +4,26 @@
 #
 # Imports
 #
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+from builtins import object
 import os
 import sys
-import urllib
-import urlparse
+import urllib.request, urllib.parse, urllib.error
 import requests
 from time import strptime
-import xbmc
 import xbmcgui
 import xbmcplugin
-from BeautifulSoup import BeautifulSoup
 
-from nlhardwareinfo_const import ADDON, SETTINGS, LANGUAGE, IMAGES_PATH, DATE, VERSION, HEADERS
+from .nlhardwareinfo_const import LANGUAGE, IMAGES_PATH, HEADERS, convertToUnicodeString, log, getSoup
 
 
 #
 # Main class
 #
-class Main:
+class Main(object):
     #
     # Init
     #
@@ -32,8 +34,7 @@ class Main:
         # Get the plugin handle as an integer number
         self.plugin_handle = int(sys.argv[1])
 
-        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s, %s = %s" % (
-                ADDON, VERSION, DATE, "ARGV", repr(sys.argv), "File", str(__file__)), xbmc.LOGDEBUG)
+        log("ARGV", repr(sys.argv))
 
         # Parse parameters...
         if len(sys.argv[2]) == 0:
@@ -41,9 +42,9 @@ class Main:
             self.video_list_page_url = "http://nl.hardware.info/tv/rss-private/streaming"
             self.next_page_possible = "False"
         else:
-            self.plugin_category = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['plugin_category'][0]
-            self.video_list_page_url = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['url'][0]
-            self.next_page_possible = urlparse.parse_qs(urlparse.urlparse(sys.argv[2]).query)['next_page_possible'][0]
+            self.plugin_category = urllib.parse.parse_qs(urllib.parse.urlparse(sys.argv[2]).query)['plugin_category'][0]
+            self.video_list_page_url = urllib.parse.parse_qs(urllib.parse.urlparse(sys.argv[2]).query)['url'][0]
+            self.next_page_possible = urllib.parse.parse_qs(urllib.parse.urlparse(sys.argv[2]).query)['next_page_possible'][0]
 
         if self.next_page_possible == 'True':
             # Determine current item number, next item number, next_url
@@ -61,9 +62,7 @@ class Main:
                     page_number_next_str = '00' + str(page_number_next)
                 self.next_url = str(self.video_list_page_url).replace(page_number_str, page_number_next_str)
 
-                xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                        ADDON, VERSION, DATE, "self.next_url", str(urllib.unquote_plus(self.next_url))),
-                             xbmc.LOGDEBUG)
+                log("self.next_url", self.next_url)
 
         #
         # Get the videos...
@@ -86,124 +85,79 @@ class Main:
         # Get HTML page
         #
         response = requests.get(self.video_list_page_url, headers=HEADERS)
+        # response.status
         html_source = response.text
-        html_source = html_source.encode('utf-8', 'ignore')
+        html_source = convertToUnicodeString(html_source)
 
-        # Parse response...
-        soup = BeautifulSoup(html_source)
+        # Parse response
+        soup = getSoup(html_source)
 
-        # Get the items from the RSS feed
         # <item>
-        # <title>Yarvik Xenta 97ic+ &#039;Retina&#039; Android tablet review</title>
-        # <description>Yarvik bracht onlangs de Xentra 97ic+ uit, een Android 4.1 tablet met hetzelfde high-res scherm dat ook in de Retina versies van Apple&#039;s iPad worden gebruikt. Voordeel van de tablet van Yarvik is dat slechts zo&#039;n 230 euro kost. Wij zochten uit of er ook nog ergens een addertje onder het gras zit.</description>
-        # <pubDate>Mon, 10 Jun 2013 12:34:00 +0200</pubDate>
-        # <enclosure url="http://content.hwigroup.net/videos/hwitv-ep498-yarvik/hwitv-ep498-yarvik-1.mp4" type="video/mpeg" />
-        # <enclosure url="http://content.hwigroup.net/images/video/video_thumb/000609.jpg" type="image/jpg" />
-        # <enclosure url="http://content.hwigroup.net/images/video/video_430/000609.jpg" type="image/jpg" />
-        # <guid isPermaLink="false">http://content.hwigroup.net/videos/hwitv-ep498-yarvik/hwitv-ep498-yarvik-1.mp4</guid>
-        # <enclosure url="http://youtube.com/watch?v=-TBPcTmZVWI" type="video/youtube" />
+        # <title>HP Elitebook x360 review</title>
+        # <description>De tijd dat HP's Elitebooks relatief saaie, zakelijke machines waren ligt achter ons. Want de nieuwe Elitebook x360 is weliswaar nog steeds een apparaat voor de zakelijke markt, maar het is vooral ook een dunne, lichte notebook met een fraai design.
+        # De x360 kan bovendien ook in tablet- en in tentmodus gebruikt worden, waardoor het apparaat niet alleen geschikt is om op te werken en presentaties te geven, maar ook om onderuit op de bank wat te surfen of een film te kijken.</description>
+        # <pubdate>Wed, 10 May 2017 15:45:00 +0200</pubdate>
+        # <enclosure type="video/mpeg" url="https://content.hwigroup.net/videos/hwitv-ep917-elitebook/hwitv-ep917-elitebook-1.mp4">
+        # <enclosure type="image/jpg" url="https://content.hwigroup.net/images/video/video_thumb/001059.jpg">
+        # <enclosure type="image/jpg" url="https://content.hwigroup.net/images/video/video_430/001059.jpg">
+        # <guid ispermalink="false">https://content.hwigroup.net/videos/hwitv-ep917-elitebook/hwitv-ep917-elitebook-1.mp4</guid>
+        # <enclosure type="video/youtube" url="http://youtube.com/watch?v=-0Fban2eHQo">
+        # </enclosure></enclosure></enclosure></enclosure>
         # </item>
         items = soup.findAll('item')
 
-        xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                ADDON, VERSION, DATE, "len(items)", str(len(items))), xbmc.LOGDEBUG)
+        log("len(items)", len(items))
 
         for item in items:
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                ADDON, VERSION, DATE, "item", str(item)), xbmc.LOGDEBUG)
+            # log("item)", item)
 
             # Get the 4 enclosure url's in the item
             # There must be a better way to do this with beautiful soup, but it'll have to do for now
             item_string = str(item)
-            start_pos_of_enclosure_1 = item_string.find('enclosure url="', 0)
-            start_pos_of_enclosure_url_1 = start_pos_of_enclosure_1 + len('enclosure url="')
+            start_pos_of_enclosure_1 = item_string.find('url="', 0)
+            start_pos_of_enclosure_url_1 = start_pos_of_enclosure_1 + len('url="')
             end_pos_of_enclosure_url_1 = item_string.find('"', start_pos_of_enclosure_url_1)
             enclosure_url_1 = item_string[start_pos_of_enclosure_url_1:end_pos_of_enclosure_url_1]
 
-            start_pos_of_enclosure_2 = item_string.find('enclosure url="', start_pos_of_enclosure_1 + 1)
-            start_pos_of_enclosure_url_2 = start_pos_of_enclosure_2 + len('enclosure url="')
+            start_pos_of_enclosure_2 = item_string.find('url="', start_pos_of_enclosure_1 + 1)
+            start_pos_of_enclosure_url_2 = start_pos_of_enclosure_2 + len('url="')
             end_pos_of_enclosure_url_2 = item_string.find('"', start_pos_of_enclosure_url_2)
             enclosure_url_2 = item_string[start_pos_of_enclosure_url_2:end_pos_of_enclosure_url_2]
 
             # this enclosure contains the hi res thumbnail
-            start_pos_of_enclosure_3 = item_string.find('enclosure url="', start_pos_of_enclosure_2 + 1)
-            start_pos_of_enclosure_url_3 = start_pos_of_enclosure_3 + len('enclosure url="')
+            start_pos_of_enclosure_3 = item_string.find('url="', start_pos_of_enclosure_2 + 1)
+            start_pos_of_enclosure_url_3 = start_pos_of_enclosure_3 + len('url="')
             end_pos_of_enclosure_url_3 = item_string.find('"', start_pos_of_enclosure_url_3)
             enclosure_url_3 = item_string[start_pos_of_enclosure_url_3:end_pos_of_enclosure_url_3]
 
             # this enclosure contains the youtube link
-            start_pos_of_enclosure_4 = item_string.find('enclosure url="', start_pos_of_enclosure_3 + 1)
-            start_pos_of_enclosure_url_4 = start_pos_of_enclosure_4 + len('enclosure url="')
+            start_pos_of_enclosure_4 = item_string.find('url="', start_pos_of_enclosure_3 + 1)
+            start_pos_of_enclosure_url_4 = start_pos_of_enclosure_4 + len('url="')
             end_pos_of_enclosure_url_4 = item_string.find('"', start_pos_of_enclosure_url_4)
             enclosure_url_4 = item_string[start_pos_of_enclosure_url_4:end_pos_of_enclosure_url_4]
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "enclosure_url_3", str(enclosure_url_3)), xbmc.LOGDEBUG)
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "enclosure_url_4", str(enclosure_url_4)), xbmc.LOGDEBUG)
+            log("enclosure_url_1", enclosure_url_1)
+            log("enclosure_url_2", enclosure_url_2)
+            log("enclosure_url_3", enclosure_url_3)
+            log("enclosure_url_4", enclosure_url_4)
 
             # Get youtube_id
             # http://youtube.com/watch?v=GCnXAEdPDSo
             youtube_id = enclosure_url_4[len("http://youtube.com/watch?v="):]
             youtube_url = 'plugin://plugin.video.youtube/play/?video_id=%s' % youtube_id
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "youtube_url", str(youtube_url)), xbmc.LOGDEBUG)
+            log("youtube_url", youtube_url)
 
             # Get thumbnail url
             thumbnail_url = enclosure_url_3
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "thumbnail_url", str(thumbnail_url)), xbmc.LOGDEBUG)
+            log("thumbnail_url", thumbnail_url)
 
             # Get title
             title = item.title.string
 
-            try:
-                title = title.encode('utf-8')
-            except:
-                pass
-
-            title = title.replace('-', ' ')
-            title = title.replace('/', ' ')
-            title = title.replace(' i ', ' I ')
-            title = title.replace(' ii ', ' II ')
-            title = title.replace(' iii ', ' III ')
-            title = title.replace(' iv ', ' IV ')
-            title = title.replace(' v ', ' V ')
-            title = title.replace(' vi ', ' VI ')
-            title = title.replace(' vii ', ' VII ')
-            title = title.replace(' viii ', ' VIII ')
-            title = title.replace(' ix ', ' IX ')
-            title = title.replace(' x ', ' X ')
-            title = title.replace(' xi ', ' XI ')
-            title = title.replace(' xii ', ' XII ')
-            title = title.replace(' xiii ', ' XIII ')
-            title = title.replace(' xiv ', ' XIV ')
-            title = title.replace(' xv ', ' XV ')
-            title = title.replace(' xvi ', ' XVI ')
-            title = title.replace(' xvii ', ' XVII ')
-            title = title.replace(' xviii ', ' XVIII ')
-            title = title.replace(' xix ', ' XIX ')
-            title = title.replace(' xx ', ' XXX ')
-            title = title.replace(' xxi ', ' XXI ')
-            title = title.replace(' xxii ', ' XXII ')
-            title = title.replace(' xxiii ', ' XXIII ')
-            title = title.replace(' xxiv ', ' XXIV ')
-            title = title.replace(' xxv ', ' XXV ')
-            title = title.replace(' xxvi ', ' XXVI ')
-            title = title.replace(' xxvii ', ' XXVII ')
-            title = title.replace(' xxviii ', ' XXVIII ')
-            title = title.replace(' xxix ', ' XXIX ')
-            title = title.replace(' xxx ', ' XXX ')
-            title = title.replace('  ', ' ')
-            # in the title of the item a single-quote "'" is represented as "&#039;" for some reason . Therefore this replace is needed to fix that.
-            title = title.replace("&#039;", "'")
-
-            xbmc.log(
-                    "[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "title", str(title)),
-                    xbmc.LOGDEBUG)
+            log("title", title)
 
             context_menu_items = []
             # Add refresh option to context menu
@@ -223,8 +177,6 @@ class Main:
 
             # Get description
             plot = item.description.string
-            # in the title of the item a single-quote "'" is represented as "&#039;" for some reason . Therefore this replace is needed to fix that.
-            plot = plot.replace("&#039;", "'")
 
             # Get pupdate
             pubdate = item.pubdate.string
@@ -236,12 +188,11 @@ class Main:
             minute = pubdate[len('Fri, 01 Mar 2016 17:'):len('Fri, 01 Mar 2016 17:') + 2]
             second = pubdate[len('Fri, 01 Mar 2016 17:40:'):len('Fri, 01 Mar 2016 17:40:') + 2]
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (
-                    ADDON, VERSION, DATE, "extracted pubdate", str(day + '/' + month_name + '/' + year + '/' + hour + '/' + minute + '/' + second)), xbmc.LOGDEBUG)
+            log("extracted pubdate", day + '/' + month_name + '/' + year + '/' + hour + '/' + minute + '/' + second)
 
             month_numeric = strptime(month_name, '%b').tm_mon
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "month_numeric", str(month_numeric)), xbmc.LOGDEBUG)
+            log("month_numeric", month_numeric)
 
             if len(str(month_numeric)) == 1:
                 month = '0' + str(month_numeric)
@@ -251,7 +202,7 @@ class Main:
             # Dateadded has this form: 2009-04-05 23:16:04
             dateadded = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second
 
-            xbmc.log("[ADDON] %s v%s (%s) debug mode, %s = %s" % (ADDON, VERSION, DATE, "dateadded", str(dateadded)), xbmc.LOGDEBUG)
+            log("dateadded", dateadded)
 
             meta = {'plot': plot,
                     'duration': '',
@@ -284,7 +235,7 @@ class Main:
             list_item.setProperty('IsPlayable', 'false')
             parameters = {"action": "list-play", "plugin_category": self.plugin_category, "url": str(self.next_url),
                           "next_page_possible": self.next_page_possible}
-            url = self.plugin_url + '?' + urllib.urlencode(parameters)
+            url = self.plugin_url + '?' + urllib.parse.urlencode(parameters)
             is_folder = True
             # Adding context menu items to context menu
             list_item.addContextMenuItems(context_menu_items, replaceItems=False)
@@ -299,6 +250,7 @@ class Main:
         xbmcplugin.addSortMethod(handle=self.plugin_handle, sortMethod=xbmcplugin.SORT_METHOD_NONE)
         # Finish creating a virtual folder.
         xbmcplugin.endOfDirectory(self.plugin_handle)
+
 
 def add_sort_methods():
     sort_methods = [xbmcplugin.SORT_METHOD_UNSORTED,xbmcplugin.SORT_METHOD_LABEL,xbmcplugin.SORT_METHOD_DATE,xbmcplugin.SORT_METHOD_DURATION,xbmcplugin.SORT_METHOD_EPISODE]

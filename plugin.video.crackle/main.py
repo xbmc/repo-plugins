c1d47845e3922d3dff065d1003e647442bacf473
eracknaphobia <eracknaphobia@hotmail.com>
2018-01-27 10:10:19 -0500

    [plugin.video.crackle] 2018.1.27

--- plugin.video.crackle/main.py
+++ plugin.video.crackle/main.py
@@ -1,10 +1,10 @@
 from resources.lib.globals import *
 
-params=get_params()
-media_id=None
-name=None
-mode=None
-stream_type=None
+params = get_params()
+media_id = None
+name = None
+mode = None
+stream_type = None
 
 try:
     media_id=urllib.unquote_plus(params["id"])
@@ -23,24 +23,21 @@ try:
 except:
     pass
 
-if mode==None:                    
-    #test()
-    #or url==None or len(url)<1
-    mainMenu()
-elif mode==100:
-    listShows()
+if mode is None:
+    main_menu()
 
-elif mode==101:    
-    listMovies()  
+elif mode == 100:
+    list_shows()
 
-elif mode==102:
-    getEpisodes(media_id)
+elif mode == 101:
+    list_movies()
 
-elif mode==103:    
-    if stream_type == "movies": media_id = getMovieID(media_id)
-    getStream(media_id)
-    
-elif mode==999:
-	deauthorize()
+elif mode == 102:
+    get_episodes(media_id)
+
+elif mode == 103:
+    if stream_type == "movies": media_id = get_movie_id(media_id)
+    get_stream(media_id)
+
 
 xbmcplugin.endOfDirectory(addon_handle)

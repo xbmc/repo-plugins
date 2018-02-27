cd906026d2d25c175d1b84bb6ecf6fb042ec2c66
sarbes <sarbes.kodi@gmail.com>
2018-02-27 02:09:25 +0100

    [plugin.video.funkmediathek] v1.3.1 (#1707)

--- plugin.video.funkmediathek/default.py
+++ plugin.video.funkmediathek/default.py
@@ -1,34 +1,30 @@
 # -*- coding: utf-8 -*-
 import libmediathek3 as libMediathek
-import xbmc
-import xbmcaddon
-import xbmcplugin
 import resources.lib.jsonparser as jsonParser
-import resources.lib.kalturaparser as kalturaParser
 
-base = 'https://www.funk.net'
-
-translation = xbmcaddon.Addon().getLocalizedString
-
-#squashShows = xbmcaddon.Addon().getSetting('squashShows') == 'true'
-#preferOV = xbmcaddon.Addon().getSetting('preferOV') == 'true'
-skipToSeries = xbmcaddon.Addon().getSetting('skipToSeries') == 'true'
+translation = libMediathek.getTranslation
+skipToSeries = libMediathek.getSetting('skipToSeries') == 'true'
 
 def main():
 	if skipToSeries:
-		params['url'] = 'https://api.funk.net/v1.0/content/series/?page=0&size=50'
+		params['url'] = 'https://api.funk.net/v1.1/content/series/?page=0&size=50'
 		return listDir()
 	else:
 		l = []
-		l.append({'_name':translation(30503), 'mode':'listDir', '_type': 'dir', 'url':'https://api.funk.net/v1.0/content/formats/?page=0&size=50'})
-		l.append({'_name':translation(30504),  'mode':'listDir',   '_type': 'dir', 'url':'https://api.funk.net/v1.0/content/series/?page=0&size=50'})
+		l.append({'_name':translation(30503,'plugin.video.funkmediathek'), 'mode':'listDir', '_type': 'dir', 'url':'https://api.funk.net/v1.1/content/formats/?page=0&size=50'})
+		l.append({'_name':translation(30504,'plugin.video.funkmediathek'), 'mode':'listDir', '_type': 'dir', 'url':'https://api.funk.net/v1.1/content/series/?page=0&size=50'})
 		return l
 	
 def listDir():
 	return jsonParser.parse(params['url'])
 	
 def play():
-	return kalturaParser.getVideoUrl(params['entryId'])
+	import nexx
+	nexx.operations = {'byid':'2835669fdcfe2d07351d633353bf87a8'}
+	nexx.cid = '114994613565243649'
+	nexx.channelId = '741'
+	nexx.origin = 'https://www.funk.net'
+	return nexx.getVideoUrl(params['sourceId'])
 
 
 modes = {
@@ -40,8 +36,6 @@ modes = {
 def list():	
 	global params
 	params = libMediathek.get_params()
-	global pluginhandle
-	pluginhandle = int(sys.argv[1])
 	
 	mode = params.get('mode','main')
 	if mode == 'play':
@@ -49,5 +43,5 @@ def list():
 	else:
 		l = modes.get(mode)()
 		libMediathek.addEntries(l)
-		xbmcplugin.endOfDirectory(int(sys.argv[1]),cacheToDisc=True)	
+		libMediathek.endOfDirectory()
 list()

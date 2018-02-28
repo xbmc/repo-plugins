ebf469c5699021ef389186e85ae03902d77f86fc
Kolja Lampe <razzeee@gmail.com>
2018-02-28 23:50:10 +0100

    [plugin.video.rocketbeans] 1.1.0

--- plugin.video.rocketbeans/resources/data/config.py
+++ plugin.video.rocketbeans/resources/data/config.py
@@ -1,3 +1,9 @@
 CHANNEL_ID = 'ROCKETBEANSTV'
 LETS_PLAY_CHANNEL_ID = 'UCtSP1OA6jO4quIGLae7Fb4g'
+GAME_TWO_CHANNEL_ID = 'UCFBapHA35loZ3KZwT_z3BsQ'
+
 GUIDE_URL = 'http://api.rbtv.rodney.io/api/1.0/schedule/schedule_linear.json'
+
+TWITCH_CHANNEL_ID = 47627824
+TWITCH_USER_LOGIN = 'rocketbeanstv'
+TWITCH_CLIENT_ID = 'NjdlYnBmaHlvaWNhYjVrcjB5N3B6b2NzZm9oczd0eQ=='
--- plugin.video.rocketbeans/resources/lib/youtube.py
+++ plugin.video.rocketbeans/resources/lib/youtube.py
@@ -1,27 +1,42 @@
 import re
+import time
+import HTMLParser
 import urllib2
 
-import xbmc
+
+class YoutubeStream:
+    def get_live_video_info_from_channel_id(self, channel_id):
+        request = urllib2.Request("https://www.youtube.com/c/%s/live" % channel_id)
+        video_id, title = self.get_video_info(request)
+        livestream_url = "plugin://plugin.video.youtube/play/?video_id=%s" % video_id
+        livestream_thumbnail = "https://i.ytimg.com/vi/%s/hqdefault_live.jpg#%s" % (
+            video_id, time.localtime())
+        return livestream_url, title, livestream_thumbnail
 
 
-def get_live_video_id_from_channel_id(channel_id):
-    request = urllib2.Request("https://www.youtube.com/c/%s/live" % channel_id)
-    response = urllib2.urlopen(request)
-    string_data = response.read()
-    lines = string_data.splitlines()
+    def get_video_info_from_video_id(self, video_id):
+        request = urllib2.Request("https://www.youtube.com/%s" % video_id)
+        return self.get_video_info(request)
 
-    re_video_url = re.compile(r'http://www.youtube.com/v/(?P<video_id>[^\?]+)')
-    re_video_title = re.compile(r'<title>(?P<title>[^\?]+) - YouTube</title>')
 
-    re_video_url_match = ""
-    re_video_title_match = ""
+    def get_video_info(self, request):
+        response = urllib2.urlopen(request)
+        string_data = response.read()
+        lines = string_data.splitlines()
 
-    for line in lines:
-        #xbmc.log(line)
-        if not re_video_url_match:
-            re_video_url_match = re.search(re_video_url, line)
-        if not re_video_title_match:
-            re_video_title_match = re.search(re_video_title, line)
+        re_video_url = re.compile(r'http://www.youtube.com/v/(?P<video_id>[^\?]+)')
+        re_video_title = re.compile(r'<title>(?P<title>[^\?]+) - YouTube</title>')
 
-        if re_video_url_match and re_video_title_match:
-            return (re_video_url_match.group('video_id'), re_video_title_match.group('title'))
+        re_video_url_match = ""
+        re_video_title_match = ""
+
+        for line in lines:
+            #xbmc.log(line)
+            if not re_video_url_match:
+                re_video_url_match = re.search(re_video_url, line)
+            if not re_video_title_match:
+                re_video_title_match = re.search(re_video_title, line)
+
+            if re_video_url_match and re_video_title_match:
+                return (re_video_url_match.group('video_id'), HTMLParser.HTMLParser().unescape(re_video_title_match.group('title')))
+

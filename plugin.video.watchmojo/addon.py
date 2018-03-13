55c95a2a11e2d4fed52cd7ac56c5fcddb0017d2f
William Forde <willforde@gmail.com>
2018-03-13 23:03:19 +0000

    [plugin.video.watchmojo] 3.0.1

--- plugin.video.watchmojo/addon.py
+++ plugin.video.watchmojo/addon.py
@@ -19,12 +19,13 @@
 
 from __future__ import unicode_literals
 from codequick import Route, Resolver, Listitem, run, utils
+import urlquick
 
 # Localized string Constants
 TAGS = 20459
 
 # Base url constructor
-url_constructor = utils.urljoin_partial("http://www.watchmojo.com")
+url_constructor = utils.urljoin_partial("https://www.watchmojo.com")
 
 
 # ###### Functions ###### #
@@ -34,13 +35,13 @@ def extract_videos(lbl_tags, elem, date_format):
     item.label = elem.findtext(".//div[@class='hptitle']").replace("\t", " ").strip()
     item.art["thumb"] = url_constructor(elem.find(".//img").get("src"))
 
-    duration = elem.find(".//img[@class='hpplay']").tail
-    if duration:
-        item.info["duration"] = duration.strip(";")
+    duration = elem.find(".//img[@class='hpplay']")
+    if duration is not None and duration.tail:
+        item.info["duration"] = duration.tail.strip(";")
 
     url = elem.find("a").get("href")
     item.info.date(elem.findtext(".//div[@class='hpdate']").strip(), date_format)
-    item.context.container(lbl_tags, tags, url=url)
+    item.context.container(tags, lbl_tags, url=url)
     item.context.related(related, url=url)
     item.set_callback(play_video, url=url)
     return item
@@ -49,38 +50,29 @@ def extract_videos(lbl_tags, elem, date_format):
 # ###### Callbacks ###### #
 
 @Route.register
-def root(plugin):
+def root(_):
     """
     Lists all categories and link's to 'Shows', 'MsMojo' and 'All videos'.
 
     site: http://www.watchmojo.com
 
-    :param Route plugin: Tools related to callback.
+    :param Route _: Tools related to callback.
     :return: A generator of listitems.
     """
-    # Item youtube link as a all videos option
-    yield Listitem.youtube("UCaWd5_7JhbQBe4dknZhsHJg")
+    # Add links to watchmojo youtube channels
+    yield Listitem.youtube("UCaWd5_7JhbQBe4dknZhsHJg")  # WatchMojo
+    yield Listitem.youtube("UCMm0YNfHOCA-bvHmOBSx-ZA", label="WatchMojo UK")
+    yield Listitem.youtube("UC3rLoj87ctEHCcS7BuvIzkQ", label="MsMojo")
 
     url = url_constructor("/")
-    source = plugin.request.get(url)
-
-    # Parse only the main category elements
+    source = urlquick.get(url)
     root_elem = source.parse()
-    for elem in root_elem.find(".//div[@id='owl-demo4']").iterfind("div"):
-        # Image element contains image url and label as the alt attribute
-        img_tag = elem.find("./a/img")
-
-        item = Listitem()
-        item.label = img_tag.get("alt")
-        item.art["thumb"] = url_constructor(img_tag.get("src"))
-        item.set_callback(video_list, url=elem.find("a").get("href").replace("/i/home/", "http://"))
-        yield item
 
     # Parse only the show category elements
     menu_elem = root_elem.find(".//ul[@class='top-ul left']")
     for elem in menu_elem.iterfind(".//a"):
         url = elem.get("href")
-        if url and elem.text and (url.startswith("/shows/") or url.startswith("/msmojo/")):
+        if url and elem.text and elem.text != "MsMojo":
             item = Listitem()
             item.label = elem.text
             item.set_callback(video_list, url=url)
@@ -99,13 +91,15 @@ def video_list(plugin, url):
     :return: A generator of listitems.
     """
     url = url_constructor(url)
-    source = plugin.request.get(url)
+    source = urlquick.get(url)
     lbl_tags = plugin.localize(TAGS)
 
     # Parse all the video elements
     root_elem = source.parse()
-    for elem in root_elem.iterfind(".//div[@class='item']"):
-        yield extract_videos(lbl_tags, elem, "%b %d, %Y")
+
+    for line in root_elem.iterfind(".//div[@class='owl-carousel margin-bottom']"):
+        for elem in line.iterfind(".//div[@class='item']"):
+            yield extract_videos(lbl_tags, elem, "%b %d, %Y")
 
     # Add link to next page if available
     next_page = root_elem.find(".//div[@class='cat-next']")
@@ -126,7 +120,7 @@ def related(plugin, url):
     :return: A generator of listitems.
     """
     url = url_constructor(url)
-    source = plugin.request.get(url)
+    source = urlquick.get(url)
     lbl_tags = plugin.localize(TAGS)
 
     # Parse all the video elements
@@ -140,21 +134,22 @@ def tags(plugin, url):
     """
     List tags for a video.
 
-    site: http://www.watchmojo.com/video/id/19268/
+    site: https://www.watchmojo.com/video/id/19268/
 
     :param Route plugin: Tools related to Route callbacks.
     :param unicode url: The url to a video.
     :return: A generator of listitems.
     """
+    plugin.category = plugin.localize(TAGS)
     url = url_constructor(url)
-    source = plugin.request.get(url)
+    source = urlquick.get(url)
 
     # Parse all video tags
     root_elem = source.parse("div", attrs={"id": "tags"})
     for elem in root_elem.iterfind("a"):
         item = Listitem()
         item.label = elem.text.title()
-        item.set_callback(video_list, url=elem.get("href"))
+        item.set_callback(video_list, url="%s1" % elem.get("href"))
         yield item
 
 
@@ -163,7 +158,7 @@ def play_video(plugin, url):
     """
     Resolve video url.
 
-    site: http://www.watchmojo.com/video/id/19268/
+    site: https://www.watchmojo.com/video/id/19268/
 
     :param Resolver plugin: Tools related to Resolver callbacks.
     :param unicode url: The url to a video.

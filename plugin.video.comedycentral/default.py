c63a494dd8e3ffb55d85f5b3964282ac4374428f
Lunatixz <128316+Lunatixz@users.noreply.github.com>
2018-03-17 16:51:36 -0400

    [plugin.video.comedycentral] 1.0.0 (#1737)
    update

---
+++ plugin.video.comedycentral/default.py
@@ -0,0 +1,23 @@
+#   Copyright (C) 2018 Lunatixz
+#
+#
+# This file is part of Comedy Central.
+#
+# Comedy Central is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Comedy Central is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Comedy Central.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+
+#entrypoint
+if __name__ == '__main__':
+    from resources.lib import comedycentral

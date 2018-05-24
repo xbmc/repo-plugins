3360db552c7f38337338bccdb16d214ffb320368
Lee Smith <LS80@users.noreply.github.com>
2018-05-24 21:32:39 +0100

    [plugin.video.btsportvideo] 0.3.0 (#1861)

--- plugin.video.btsportvideo/resources/lib/api.py
+++ plugin.video.btsportvideo/resources/lib/api.py
@@ -33,33 +33,43 @@ from datetime import datetime
 import time
 from collections import namedtuple
 import math
+import re
+import ast
 
 import requests
 from bs4 import BeautifulSoup
+
+
+class BTError(Exception):
+    '''Exception for errors related to BT video streams'''
+    pass
 
 
 CATEGORIES_URL = 'http://sport.bt.com/all-videos/videos-01364228997406'
 API_URL = 'http://api-search.sport.bt.com/search/sport/select'
 
 _Video = namedtuple('Video', 'title url thumbnail date duration')
+_Category = namedtuple('Category', 'title path')
 
 
-def _video_params(page, page_size):
-    '''Returns the common params for the BT Sport API'''
-    return dict(
-        fq='AssetType:BTVideo',
-        sort='publicationdate desc',
-        wt='json',
-        start=(page - 1) * page_size,
-        rows=page_size,
-    )
+class _Channel(namedtuple('Channel', 'name channel_id logo')):
+    '''Class holding information about a live channel'''
+    @property
+    def thumbnail(self):
+        '''Returns the channel thumbnail URL'''
+        return (
+            'http://images.atlas.metabroadcast.com/'
+            'shaman.metabroadcast.com/bt/channels/{filename}'.format(filename=self.logo)
+        )
 
 
-def _soup(path=''):
-    '''Returns a BeautifulSoup tree for the specified path'''
-    url = urljoin(CATEGORIES_URL, path)
-    response = requests.get(url)
-    return BeautifulSoup(response.text, 'html.parser')
+CHANNELS = [
+    _Channel(name='BT Sport 1', channel_id=2020, logo='BTSPORT_1_201805110826.jpg'),
+    _Channel(name='BT Sport 2', channel_id=2021, logo='BTSPORT_2_201805110826.jpg'),
+    _Channel(name='BT Sport 3', channel_id=2030, logo='BTSPORT_3_201805110828.jpg'),
+    _Channel(name='BT Sport//ESPN', channel_id=2022, logo='BTSPORT_ESPN_201805110829.jpg'),
+    _Channel(name='BoxNation', channel_id=2029, logo='BoxNation_Lozenge_201804201100.jpg')
+]
 
 
 def _date_from_str(date_str):
@@ -71,9 +81,29 @@ def _date_from_str(date_str):
 
 def categories():
     '''Generator for all sport categories on the website'''
-    for category in _soup().find('div', 'video-hub-nav')('a'):
-        if not category.text.startswith('All '):
-            yield category.text
+    html = requests.get(CATEGORIES_URL).text
+    match = re.search(r'BTSPORT\.cms\.videohub\.pagedetails = (.*?)\t', html, re.DOTALL)
+    pagedetails = ast.literal_eval(match.group(1))
+    yield  _Category(title=None, path=pagedetails['defaultpage']['pageurl'])
+    for category in pagedetails['pages']:
+        yield _Category(title=category['title'], path=category['pageurl'])
+
+
+def query_text(path):
+    '''Returns the API query for a category path'''
+    html = requests.get(urljoin('http://sport.bt.com/', path)).text
+    match = re.search(r'BTSPORT\.cms\.videohub\.properties = (.*?)\t', html, re.DOTALL)
+    properties = ast.literal_eval(match.group(1))
+    if not properties['tags']:
+        return None
+
+    properties['tags'] = ','.join('"{}"'.format(tag) for tag in properties['tags'].split(','))
+    query_fmt = 'tags:({tags})'
+    if 'competition' in properties:
+        query_fmt += ' OR (ccategory:("{ccategory}") AND competition:("{competition}"))'
+    elif 'ccategory' in properties:
+        query_fmt += ' OR ccategory:("{ccategory}")'
+    return query_fmt.format(**properties)
 
 
 def _videos(videos_response):
@@ -87,20 +117,67 @@ def _videos(videos_response):
         )
 
 
-def _video_results(page=1, page_size=10, **params):
-    params.update(_video_params(page, page_size))
+def video_results(query, page=1, page_size=10):
+    '''Returns a generator for videos matching a query, and the number of pages of results'''
+    params = dict(
+        q='AssetType:(BTVideo)',
+        fq=['Publist:btsport'],
+        sort='publicationdate desc',
+        wt='json',
+        start=(page - 1) * page_size,
+        rows=page_size
+    )
+    params['fq'].append(query)
     videos_response = requests.get(API_URL, params=params).json()['response']
     num_pages = int(math.ceil(float(videos_response['numFound']) / page_size))
     return _videos(videos_response), num_pages
 
 
-def videos(category=None, page=1, page_size=10):
-    '''Returns a generator for videos in a category and the number of pages'''
-    category = None if category.lower() == 'all' else category
-    query = 'tags:("{}")'.format(category) if category else 'Publist:btsport'
-    return _video_results(page, page_size, q=query)
-
-
 def search_results(term, page=1, page_size=10):
-    '''Returns a generator for video search results and the number of pages'''
-    return _video_results(page, page_size, q='text:{}'.format(term))
+    '''Returns a generator for video search results, and the number of pages of results'''
+    return video_results('text:("{}")'.format(term), page, page_size)
+
+
+def login(user, password):
+    '''Returns a session cookie. Returns None if the login failed'''
+    response = requests.post(
+        'https://signin1.bt.com/siteminderagent/forms/login.fcc',
+        data=dict(TARGET='https://home.bt.com/secure/', USER=user, PASSWORD=password)
+    )
+    return response.cookies.get('SMSESSION')
+
+
+def sport_login(session):
+    '''Returns an AVS cookie'''
+    html = requests.get(
+        'https://samlfed.bt.com/sportgetfedwebhls',
+        cookies=dict(SMSESSION=session)
+    ).text
+    soup = BeautifulSoup(html, 'html.parser')
+    saml_response = soup.find('input', attrs={'name': 'SAMLResponse'})['value']
+
+    response = requests.post(
+        'https://be.avs.bt.com/AVS/besc',
+        params=dict(action='LoginBT', channel='WEBHLS'),
+        data=dict(SAMLResponse=saml_response)
+    )
+    return response.cookies['avs_cookie']
+
+
+def hls_url(avs_cookie, channel_id):
+    '''Returns the HLS stream URL for a channel'''
+    if not avs_cookie:
+        return None
+
+    response = requests.get(
+        'https://be.avs.bt.com/AVS/besc',
+        params=dict(action='GetCDN', type='LIVE', id=channel_id, channel='WEBHLS', asJson='Y'),
+        cookies=dict(avs_cookie=avs_cookie)
+    )
+    result = response.json()
+    result_obj = result['resultObj']
+
+    if not result_obj:
+        raise BTError(result['message'])
+
+    return result_obj['src']

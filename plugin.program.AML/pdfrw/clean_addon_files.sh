83f9143a28085bae72184ed8518bd3eac8ee56d2
Wintermute0110 <wintermute0110@gmail.com>
2018-06-12 16:35:51 +0900

    Delete redundant file.

--- plugin.program.AML/pdfrw/clean_addon_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Clean uneeded files to pack addon ZIP for release.
-#
-echo "Cleaning pyo files"
-find -name '*.pyo' -print -exec rm '{}' ';'
-echo "Cleaning pyc files"
-find -name '*.pyc' -print -exec rm '{}' ';'

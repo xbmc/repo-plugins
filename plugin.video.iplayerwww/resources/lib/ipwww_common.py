846169f50d65ea38f1ec8f42064bd0171ab39e40
CaptainTK <14875435+CaptainTK@users.noreply.github.com>
2018-10-15 20:55:13 +0200

    [plugin.video.iplayerwww] 3.0.35

--- plugin.video.iplayerwww/resources/lib/ipwww_common.py
+++ plugin.video.iplayerwww/resources/lib/ipwww_common.py
@@ -55,6 +55,23 @@ def ParseImageUrl(url):
     return url.replace("{recipe}", "832x468")
 
 
+def getSubColor(line,  styles):
+    color = None
+    match = re.search(r'style="(.*?)"', line, re.DOTALL)
+    if match:
+        style = match.group(1)
+        color = [value for (style_id,value) in styles if style_id == style]
+    else:
+        # fallback: sometimes, there is direct formatting in the text
+        match = re.search(r'color="(.*?)"', line, re.DOTALL)
+        if match:
+            color = [match.group(1)]
+        else:
+            # fallback 2: sometimes, there is no formatting at all, use default
+            color = [value for (style_id,value) in styles if style_id == 's0']
+    return color
+
+
 def download_subtitles(url):
     # Download and Convert the TTAF format to srt
     # SRT:
@@ -80,7 +97,17 @@ def download_subtitles(url):
 
     txt = OpenURL(url)
 
+    styles = []
     # print txt
+
+    # get styles
+    styles = []
+    match = re.search(r'<styling>(.+?)</styling>', txt, re.DOTALL)
+    if match:
+        match = re.findall(r'<style.*?id="(.*?)".*?color="(.*?)"', match.group(1), re.DOTALL)
+        if match:
+            for id, color in match:
+                styles.append((id, color))
 
     i = 0
     prev = None
@@ -121,8 +148,9 @@ def download_subtitles(url):
             # ma['text'] = ma['text'].replace('<.*?>', '')
             # print ma
             if not prev:
-                # first match - do nothing wait till next line
+                # first match - only get the color, wait till next line
                 prev = ma
+                color = getSubColor(line, styles)
                 continue
 
             if prev['text'] == ma['text']:
@@ -131,13 +159,24 @@ def download_subtitles(url):
                 prev['end_mil'] = ma['end_mil']
             else:
                 i += 1
-                entry = "%d\n%s,%s --> %s,%s\n%s\n\n" % (
-                    i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], prev['text'])
+                if color:
+                    entry = "%d\n%s,%s --> %s,%s\n<font color=\"%s\">%s</font>\n\n" % (
+                        i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], color[0], prev['text'])
+                else:
+                    entry = "%d\n%s,%s --> %s,%s\n%s\n\n" % (
+                        i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], prev['text'])
                 prev = ma
         elif prev:
             i += 1
-            entry = "%d\n%s,%s --> %s,%s\n%s\n\n" % (
-                i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], prev['text'])
+            if color:
+                entry = "%d\n%s,%s --> %s,%s\n<font color=\"%s\">%s</font>\n\n" % (
+                    i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], color[0], prev['text'])
+            else:
+                entry = "%d\n%s,%s --> %s,%s\n%s\n\n" % (
+                    i, prev['start'], prev['start_mil'], prev['end'], prev['end_mil'], prev['text'])
+
+        # get color for this line
+        color = getSubColor(line, styles)
 
         if entry:
             fw.write(entry)

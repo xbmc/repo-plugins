36e0b30eb2d03cdf4d50cb8e0a80715c0f636928
Rechi <Rechi@users.noreply.github.com>
2018-11-23 15:08:07 +0100

    [plugin.video.ted.talks] remove

--- plugin.video.ted.talks/resources/lib/menu_util.py
+++
@@ -1,5 +0,0 @@
-def create_context_menu(getLS):
-    context_menu = []
-    context_menu += [(getLS(30097), 'Action(queue)')]
-    context_menu += [('Toggle watched', 'Action(ToggleWatched)')]
-    return context_menu

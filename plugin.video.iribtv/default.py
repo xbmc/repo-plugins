4be4af3a3dcd887e5d6b3ce7397523daa47a06e0
Amir Ammari <amirrammari@gmail.com>
2019-01-28 05:21:11 +0330

    [plugin.video.iribtv] 2.1.0

---
+++ plugin.video.iribtv/default.py
@@ -0,0 +1,30 @@
+import xbmc, xbmcgui, os, xbmcaddon, urllib2
+from BeautifulSoup import BeautifulSoup
+
+__settings__ = xbmcaddon.Addon()
+__language__ = __settings__.getLocalizedString
+home = __settings__.getAddonInfo('path')
+addonname = __settings__.getAddonInfo('name')
+icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
+li = xbmcgui.ListItem ('IRIB TV3')
+li.setArt({ 'thumb': icon })
+li.setInfo('video', {'plot': 'Live Stream'})
+
+livePageUrl = "https://www.tv3.ir/live"
+
+request = urllib2.Request(livePageUrl, headers={'User-Agent' : "Kodi"})
+try:
+    response = urllib2.urlopen(request)
+    html = response.read()
+
+    soup = BeautifulSoup(html)
+    stream = soup.find('video', preload='auto')
+
+    if stream is not None and stream['data-source'] is not None:
+            xbmc.Player().play(stream['data-source'], li, False)
+    else:
+        xbmcgui.Dialog().ok(addonname,  __language__(30002), __language__(30010))
+except urllib2.HTTPError, err:
+        xbmcgui.Dialog().ok(addonname,  __language__(30000) % err.code, __language__(30010))
+except:
+        xbmcgui.Dialog().ok(addonname,  __language__(30001), __language__(30010))

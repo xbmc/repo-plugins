a578c7ad69fda0ee5380679c6602017e092586bc
Jelle van der Waa <jelle@vdwaa.nl>
2019-02-04 21:01:29 +0100

    [plugin.video.fosdem] 0.0.2

--- plugin.video.fosdem/addon.py
+++ plugin.video.fosdem/addon.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import requests
 
 import routing
 from xbmcgui import ListItem
-from xbmcplugin import addDirectoryItem, endOfDirectory, setResolvedUrl, getSetting
+from xbmcplugin import addDirectoryItem, endOfDirectory, setResolvedUrl, getSetting, setContent
 
 # FIXME: BS4?
 import xml.etree.ElementTree as ET
 
-URL = 'https://fosdem.org/2018/schedule/xml'
+FORMAT_URL = 'https://fosdem.org/{}/schedule/xml'
 FORMATS = ['mp4', 'webm']
+YEARS_SHOWN = 5
 
 plugin = routing.Plugin()
-root = None
 
 
-def fetch_root():
-    r = requests.get(URL)
+def years():
+    now = datetime.now()
+    year = now.year
+
+    # Determine if FOSDEM happened this year already
+    if now.month < 2 and now.day < 3:
+        year -= 1
+
+    # Range does not include the end.
+    year += 1
+    return range(year - YEARS_SHOWN, year)
+
+
+def fetch_xml(year):
+    r = requests.get(FORMAT_URL.format(year))
     return ET.fromstring(r.text.encode('utf-8'))
+
 
 def contains_videos(links):
     videos = list(filter(lambda x: 'video.fosdem.org' in x,
@@ -42,35 +56,40 @@ def get_format():
 @plugin.route('/dir/<path:subdir>')
 def show_dir(subdir=''):
     if subdir == '':
-        url = plugin.url_for(show_dir, subdir='2018')
-        addDirectoryItem(plugin.handle, url, ListItem('2018'), True)
+        for year in years():
+            year = str(year)
+            url = plugin.url_for(show_dir, subdir=year)
+            addDirectoryItem(plugin.handle, url, ListItem(year), True)
     else:
+        root = fetch_xml(subdir)
         for day in root.findall('day'):
             number = day.attrib['index']
             text = 'Day {}'.format(number)
-            url = plugin.url_for(show_day, day=number)
+            url = plugin.url_for(show_day, year=subdir, day=number)
             addDirectoryItem(plugin.handle, url,
                              ListItem(text), True)
     endOfDirectory(plugin.handle)
 
 
-@plugin.route('/day/<day>')
-def show_day(day):
+@plugin.route('/day/<year>/<day>')
+def show_day(year, day):
     exp = './day[@index="{}"]/room'.format(day)
+    root = fetch_xml(year)
     for room in root.findall(exp):
         if not contains_videos(room.findall('./event/links/link')):
             continue
 
         name = room.attrib['name']
-        url = plugin.url_for(show_room, day=day, room=name)
+        url = plugin.url_for(show_room, year=year, day=day, room=name)
         addDirectoryItem(plugin.handle, url,
                          ListItem(name), True)
     endOfDirectory(plugin.handle)
 
 
-@plugin.route('/room/<day>/<room>')
-def show_room(day, room):
+@plugin.route('/room/<year>/<day>/<room>')
+def show_room(day, year, room):
     exp = './day[@index="{}"]/room[@name="{}"]/event'.format(day, room)
+    root = fetch_xml(year)
     for event in root.findall(exp):
         if not contains_videos(event.findall('./links/link')):
             continue
@@ -103,14 +122,17 @@ def show_room(day, room):
             'duration': seconds
         })
         url = plugin.url_for(show_event,
+                             year=year,
                              event_id=event_id)
         addDirectoryItem(plugin.handle, url, item, False)
+        setContent(plugin.handle, 'videos')
 
     endOfDirectory(plugin.handle)
 
 
-@plugin.route('/event/<event_id>')
-def show_event(event_id):
+@plugin.route('/event/<year>/<event_id>')
+def show_event(year, event_id):
+    root = fetch_xml(year)
     event = root.find('.//event[@id="{}"]'.format(event_id))
     videos = [link.attrib['href'] for link in event.findall('./links/link') if 'video.fosdem.org' in link.attrib['href']]
     if not videos:
@@ -128,5 +150,4 @@ def show_event(event_id):
 
 
 if __name__ == '__main__':
-    root = fetch_root()
     plugin.run()

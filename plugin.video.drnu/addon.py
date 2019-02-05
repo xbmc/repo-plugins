55dfe7e418e2039015bba262a7cc190b08343627
M <msj33@hotmail.com>
2019-02-05 20:48:23 +0100

    [plugin.video.drnu] 5.0.11

--- plugin.video.drnu/addon.py
+++ plugin.video.drnu/addon.py
@@ -1,7 +1,4 @@
 #
-#      Copyright (C) 2014 Tommy Winther
-#      http://tommy.winther.nu
-#
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2, or (at your option)

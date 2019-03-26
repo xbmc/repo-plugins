4c2dbf2a04ae63a612d3e51ae3c92d9dcc3d0495
Alexander Seiler <seileralex@gmail.com>
2019-03-26 16:28:14 +0100

    [plugin.audio.srfplayradio] v0.9.0

---
+++ plugin.audio.srfplayradio/README.md
@@ -0,0 +1,38 @@
+# SRF Play Radio
+
+<img src="https://github.com/goggle/plugin.audio.srfplayradio/raw/master/resources/icon.png" width="256">
+
+**SRF Play Radio** (plugin.audio.srfplayradio) is a Kodi plugin to access the audio library of the Swiss Radio and Television (SRF).
+
+## Goals of the plugin
+Bring the full power of the [SRF Play Radio media library](https://www.srf.ch/play/radio) on your TV (or another device) via [Kodi](https://kodi.tv/). The plugin aims to be close to the original media library, so that the experience of consuming media through the browser or through Kodi should be similar.
+
+## Features
+ - [x] Overview over all the shows.
+ - [x] List shows by topic.
+ - [x] Manage a list of your favourite SRF shows and get a list of your recently aired favourite shows.
+ - [x] Browse the content (shows, newest audios, most listened) of specific channels (SRF 1, SRF 2, SRF 3, SRF 4 News, SRF Musikwelle and SRF Virus). 
+ - [x] Live Radio.
+
+## Installation
+
+ - The plugin is available in the official "Kodi Add-on repository". This is the recommended way to get the plugin.
+ - You can download the newest release as a zip file from the [release section](https://github.com/goggle/plugin.audio.srfplayradio/releases) on github and manually install the plugin from the zip file. Keep in mind that you won't get any automatic updates if you choose this method.
+
+## Support this plugin
+How you can help improving this plugin:
+ - For bugs and feature requests use the [issue tracker](https://github.com/goggle/plugin.audio.srfplayradio/issues) on the github page.
+ - Feel free to submit a pull request. Please style your code according to [PEP 8](https://www.python.org/dev/peps/pep-0008/).
+
+If you like this plugin, consider to make a donation to the author:
+
+| Method | Link / Address | QR code |
+| --- | --- | --- |
+| Paypal | [Donation link](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZXAFRHTZGRARS) | ![Paypal link](https://raw.githubusercontent.com/goggle/plugin.video.srfplaytv/e62b52bb394eeee98c929895005bbc33e6028770/paypal.png) |
+| Bitcoin | 14d2as82kQ3mRoCSyrnHNoDi8PkFvxMMgP | ![Bitcoin address](https://raw.githubusercontent.com/goggle/plugin.video.srfplaytv/af1c696004d9b42c730dc55f7e66596ec3521b99/bitcoin.png) |
+
+
+## Screenshots
+![Main menu](https://raw.githubusercontent.com/goggle/plugin.audio.srfplayradio/master/resources/screenshot-01.png)
+![A list menu](https://raw.githubusercontent.com/goggle/plugin.audio.srfplayradio/master/resources/screenshot-02.png)
+![Episode information](https://raw.githubusercontent.com/goggle/plugin.audio.srfplayradio/master/resources/screenshot-03.png)
---
+++ plugin.audio.srfplayradio/resources/lib/srfplayradio.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019 Alexander Seiler
+#
+#
+# This file is part of plugin.video.srfplayradio.
+#
+# plugin.video.srfplayradio is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# plugin.video.srfplayradio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with plugin.video.srfplayradio.
+# If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import traceback
+
+try:  # Python 3
+    from urllib.parse import unquote_plus
+    from urllib.parse import parse_qsl
+except ImportError:  # Python 2
+    from urlparse import parse_qsl
+    from urllib import unquote_plus
+
+from kodi_six import xbmc, xbmcaddon, xbmcplugin
+import srgssr
+
+ADDON_ID = 'plugin.audio.srfplayradio'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME = REAL_SETTINGS.getAddonInfo('name')
+ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
+DEBUG = REAL_SETTINGS.getSettingBool('Enable_Debugging')
+CONTENT_TYPE = 'episodes'
+
+
+class SRFPlayRadio(srgssr.SRGSSR):
+    def __init__(self):
+        super(SRFPlayRadio, self).__init__(
+            int(sys.argv[1]), bu='srf', addon_id=ADDON_ID)
+
+
+def log(msg, level=xbmc.LOGDEBUG):
+    """
+    Logs a message using Kodi's logging interface.
+
+    Keyword arguments:
+    msg   -- the message to log
+    level -- the logging level
+    """
+    if DEBUG:
+        if level == xbmc.LOGERROR:
+            msg += ' ,' + traceback.format_exc()
+    xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + msg, level)
+
+
+def get_params():
+    return dict(parse_qsl(sys.argv[2][1:]))
+
+
+def run():
+    """
+    Run the plugin.
+    """
+    params = get_params()
+    try:
+        url = unquote_plus(params["url"])
+    except Exception:
+        url = None
+    try:
+        name = unquote_plus(params["name"])
+    except Exception:
+        name = None
+    try:
+        mode = int(params["mode"])
+    except Exception:
+        mode = None
+    try:
+        page_hash = unquote_plus(params['page_hash'])
+    except Exception:
+        page_hash = None
+    try:
+        page = unquote_plus(params['page'])
+    except Exception:
+        page = None
+
+    log('run, mode = %s, url = %s, name = %s, page_hash = %s, '
+        'page = %s' % (mode, url, name, page_hash, page))
+
+    if mode is None:
+        identifiers = [
+            'All_Shows',
+            'Shows_Topics',
+            'Favourite_Shows',
+            'Newest_Favourite_Shows',
+            'Radio_Channels',
+            # 'Newest_Audios',
+            'Most_Listened',
+            'Live_Radio',
+        ]
+        SRFPlayRadio().build_main_menu(identifiers)
+    elif mode == 10:
+        SRFPlayRadio().build_shows_menu('radio')
+    elif mode == 11:
+        SRFPlayRadio().build_favourite_radio_shows_menu()
+    elif mode == 12:
+        SRFPlayRadio().build_newest_favourite_menu(page=page, audio=True)
+    elif mode == 19:
+        SRFPlayRadio().manage_favourite_shows(audio=True)
+    elif mode == 20:
+        SRFPlayRadio().build_show_menu(name, page_hash=page_hash, audio=True)
+    elif mode == 21:
+        SRFPlayRadio().build_episode_menu(name, audio=True)
+    elif mode == 40:
+        SRFPlayRadio().build_radio_channels_menu()
+    elif mode == 41:
+        SRFPlayRadio().build_radio_channel_overview(name)
+    elif mode == 42:
+        SRFPlayRadio().build_shows_menu('radio', channel_id=name)
+    elif mode == 43:
+        SRFPlayRadio().build_audio_menu('Newest', 43, name, page=page)
+    elif mode == 44:
+        SRFPlayRadio().build_audio_menu('Most clicked', 44, name, page=page)
+    # elif mode == 45:
+    #     SRFPlayRadio().build_audio_menu('Newest', 45, page=page)
+    elif mode == 46:
+        SRFPlayRadio().build_audio_menu('Most clicked', 46, page=page)
+    elif mode == 47:
+        SRFPlayRadio().build_live_radio_menu()
+    elif mode == 48:
+        SRFPlayRadio().build_radio_topics_menu()
+    elif mode == 49:
+        SRFPlayRadio().build_radio_shows_by_topic(name)
+    elif mode == 50:
+        SRFPlayRadio().play_video(name, audio=True)
+    elif mode == 51:
+        SRFPlayRadio().play_livestream(name)
+
+    xbmcplugin.setContent(int(sys.argv[1]), CONTENT_TYPE)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_UNSORTED)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_NONE)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_LABEL)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_TITLE)
+    xbmcplugin.endOfDirectory(int(sys.argv[1]), cacheToDisc=True)

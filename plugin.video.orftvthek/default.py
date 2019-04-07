7b6f480bceab4738baa1aaab2b1f3a68204f1d39
s0faking <sofaking@gettingmoney.at>
2019-04-07 16:35:18 +0200

    [plugin.video.orftvthek] 0.9.0
    	geändert:       plugin.video.orftvthek/addon.xml
    	geändert:       plugin.video.orftvthek/resources/language/English/strings.po
    	geändert:       plugin.video.orftvthek/resources/language/German/strings.po

--- plugin.video.orftvthek/default.py
+++ plugin.video.orftvthek/default.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from future.standard_library import install_aliases
+install_aliases()
+
 import os.path
 import socket
 import sys
-import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
+
+import CommonFunctions as common
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
-
-import CommonFunctions as common
 
 from resources.lib.base import *
 from resources.lib.helpers import *
@@ -17,13 +22,7 @@ from resources.lib.serviceapi import *
 from resources.lib.htmlscraper import *
 from resources.lib.Scraper import *
 
-try:
-   import StorageServer
-except ImportError:
-   import storageserverdummy as StorageServer
-
 socket.setdefaulttimeout(30)
-cache = StorageServer.StorageServer("plugin.video.orftvthek", 999999)
 
 plugin = "ORF-TVthek-" + xbmcaddon.Addon().getAddonInfo('version')
 
@@ -47,20 +46,21 @@ videoProtocol = "http"
 #media resources
 resource_path = os.path.join( basepath, "resources" )
 media_path = os.path.join( resource_path, "media" )
-defaultbanner =  os.path.join(media_path,"default_banner.jpg")
-news_banner =  os.path.join(media_path,"news_banner.jpg")
-recently_added_banner =  os.path.join(media_path,"recently_added_banner.jpg")
-shows_banner =  os.path.join(media_path,"shows_banner.jpg")
-topics_banner =  os.path.join(media_path,"topics_banner.jpg")
-live_banner =  os.path.join(media_path,"live_banner.jpg")
-tips_banner =  os.path.join(media_path,"tips_banner.jpg")
-most_popular_banner =  os.path.join(media_path,"most_popular_banner.jpg")
-schedule_banner =  os.path.join(media_path,"schedule_banner.jpg")
-archive_banner =  os.path.join(media_path,"archive_banner.jpg")
-search_banner =  os.path.join(media_path,"search_banner.jpg")
-trailer_banner =  os.path.join(media_path,"trailer_banner.jpg")
-blacklist_banner =  os.path.join(media_path,"blacklist_banner.jpg")
-defaultbackdrop = os.path.join(media_path,"fanart.jpg")
+defaultbanner =  os.path.join(media_path,"default_banner_v2.jpg")
+news_banner =  os.path.join(media_path,"news_banner_v2.jpg")
+recently_added_banner =  os.path.join(media_path,"recently_added_banner_v2.jpg")
+shows_banner =  os.path.join(media_path,"shows_banner_v2.jpg")
+topics_banner =  os.path.join(media_path,"topics_banner_v2.jpg")
+live_banner =  os.path.join(media_path,"live_banner_v2.jpg")
+tips_banner =  os.path.join(media_path,"tips_banner_v2.jpg")
+most_popular_banner =  os.path.join(media_path,"most_popular_banner_v2.jpg")
+schedule_banner =  os.path.join(media_path,"schedule_banner_v2.jpg")
+archive_banner =  os.path.join(media_path,"archive_banner_v2.jpg")
+search_banner =  os.path.join(media_path,"search_banner_v2.jpg")
+trailer_banner =  os.path.join(media_path,"trailer_banner_v2.jpg")
+blacklist_banner =  os.path.join(media_path,"blacklist_banner_v2.jpg")
+focus_banner =  os.path.join(media_path,"focus_banner_v2.jpg")
+defaultbackdrop = os.path.join(media_path,"fanart_v2.jpg")
 
 #load settings
 useServiceAPI = Settings.serviceAPI()
@@ -84,7 +84,7 @@ link=params.get('link')
 if mode:
     debugLog("Mode: %s" % mode,'Info')
 if link:
-    debugLog("Link: %s" % urllib.unquote(link),'Info')
+    debugLog("Link: %s" % urllib.parse.unquote(link),'Info')
 
 
 def getMainMenu():
@@ -95,12 +95,13 @@ def getMainMenu():
     addDirectory((translation(30003)).encode("utf-8"),topics_banner,defaultbackdrop, "","","getThemen",pluginhandle)
     addDirectory((translation(30004)).encode("utf-8"),live_banner,defaultbackdrop, "","","getLive",pluginhandle)
     addDirectory((translation(30005)).encode("utf-8"),tips_banner,defaultbackdrop, "","","getTipps",pluginhandle)
+    if not useServiceAPI:
+        addDirectory((translation(30057)).encode("utf-8"),focus_banner,defaultbackdrop, "","","getFocus",pluginhandle)
     addDirectory((translation(30006)).encode("utf-8"),most_popular_banner,defaultbackdrop, "","","getMostViewed",pluginhandle)
     addDirectory((translation(30018)).encode("utf-8"),schedule_banner,defaultbackdrop, "","","getSchedule",pluginhandle)
     if not useServiceAPI:
-        addDirectory((translation(30049)).encode("utf-8"),schedule_banner,defaultbackdrop, "","","getArchiv",pluginhandle)
-    else:
-        addDirectory((translation(30027)).encode("utf-8"),trailer_banner,defaultbackdrop, "","","openTrailers",pluginhandle)
+        addDirectory((translation(30049)).encode("utf-8"),archive_banner,defaultbackdrop, "","","getArchiv",pluginhandle)
+    addDirectory((translation(30027)).encode("utf-8"),trailer_banner,defaultbackdrop, "","","openTrailers",pluginhandle)
     addDirectory((translation(30007)).encode("utf-8"),search_banner,defaultbackdrop, "","","getSearchHistory",pluginhandle)
     if Settings.blacklist():
         addDirectory((translation(30037)).encode("utf-8"),blacklist_banner,defaultbackdrop, "","","openBlacklist",pluginhandle)
@@ -128,13 +129,13 @@ if mode == 'openSeries':
         listCallback(False,pluginhandle)
         ok = xbmcgui.Dialog().yesno((translation(30047)).encode("utf-8"),(translation(30048)).encode("utf-8"))
         if ok:
-            debugLog("Starting Playlist for %s" % urllib.unquote(link),'Info')
+            debugLog("Starting Playlist for %s" % urllib.parse.unquote(link),'Info')
             tvthekplayer.play(playlist)               
     else:
         debugLog("Running Listcallback from no autoplay openseries","Info")
         listCallback(False,pluginhandle)
 elif mode == 'unblacklistShow':
-    heading = translation(30040).encode('UTF-8') % urllib.unquote(link).replace('+', ' ').strip()
+    heading = translation(30040).encode('UTF-8') % urllib.parse.unquote(link).replace('+', ' ').strip()
     if xbmcgui.Dialog().yesno(heading, heading):
         unblacklistItem(link)
         xbmc.executebuiltin('Container.Refresh')
@@ -155,6 +156,9 @@ elif mode == 'getLive':
     listCallback(False,pluginhandle)
 elif mode == 'getTipps':
     scraper.getTips()
+    listCallback(False,pluginhandle)
+elif mode == 'getFocus':
+    scraper.getFocus()
     listCallback(False,pluginhandle)
 elif mode == 'getNewShows':
     scraper.getNewest()
@@ -187,13 +191,13 @@ elif mode == 'openTrailers':
     scraper.getTrailers()
     listCallback(False,pluginhandle)
 elif mode == 'getSearchHistory':
-    scraper.getSearchHistory(cache);
+    scraper.getSearchHistory();
     listCallback(False,pluginhandle)
 elif mode == 'getSearchResults':
     if not link == None:
-        scraper.getSearchResults(urllib.unquote(link),cache)
+        scraper.getSearchResults(urllib.parse.unquote(link))
     else:
-        scraper.getSearchResults("",cache)
+        scraper.getSearchResults("")
     listCallback(False,pluginhandle)
 elif mode == 'openDate':
     scraper.getDate(link, params.get('from'))

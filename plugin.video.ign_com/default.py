d77a01f335138f6d534a369639629d70a8965bd7
skipmodea1 <skipmodea1@outlook.com>
2019-04-07 16:41:24 +0200

    [plugin.video.ign_com] 2.3.10

--- plugin.video.ign_com/default.py
+++ plugin.video.ign_com/default.py
@@ -16,6 +16,7 @@ import xbmcaddon
 import xbmc
 import os
 import requests
+import json
 
 ADDON = "plugin.video.ign_com"
 SETTINGS = xbmcaddon.Addon(id=ADDON)
@@ -24,10 +25,13 @@ IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'reso
 PLUGIN_HANDLE = int(sys.argv[1])
 BASE_URL = "https://www.ign.com"
 LATEST_VIDEOS_URL = "/videos?page=1&filter=all"
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+GAME_REVIEWS_URL = "https://mollusk.apis.ign.com/graphql"
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/50.0.2661.102 Safari/537.36'}
+HEADERS_JSON = {'content-type': 'application/json'}
 COOKIES = {}
-DATE = "2019-01-14"
-VERSION = "2.3.9"
+DATE = "2019-04-07"
+VERSION = "2.3.10"
 
 max_video_quality = SETTINGS.getSetting("maxVideoQualityRes")
 force_view_mode = bool(SETTINGS.getSetting("force_view_mode"))
@@ -49,13 +53,14 @@ def index():
             title = re.compile('data-video-title="(.+?)"', re.DOTALL).findall(html_source)
             add_link("***IGN-LIVESTREAM: " + title[0] + "***", video_url + ".m3u8", 'play_live_stream', "", "", "LIVE")
 
-    add_dir(LANGUAGE(30002), "/videos/all/filtergalleryajax?filter=all", 'list_videos', "")
-    add_dir("IGN Daily Fix", "/watch/daily-fix?category=videos&page=1", 'list_series_episodes', "")
-    add_dir(LANGUAGE(30003), "/videos/all/filtergalleryajax?filter=games-review", 'list_videos', "")
-    add_dir(LANGUAGE(30004), "/videos/all/filtergalleryajax?filter=games-trailer", 'list_videos', "")
-    add_dir(LANGUAGE(30005), "/videos/all/filtergalleryajax?filter=movies-trailer", 'list_videos', "")
-    add_dir(LANGUAGE(30006), "/videos/all/filtergalleryajax?filter=series", 'list_videos', "")
-    add_dir(LANGUAGE(30008), "", 'search', "")
+    add_dir(LANGUAGE(30002), "/videos/all/filtergalleryajax?filter=all", 'list_videos', "0", "")
+    add_dir(LANGUAGE(30010), "/watch/daily-fix?category=videos&page=1", 'list_series_episodes', "0", "")
+    add_dir(LANGUAGE(30009), GAME_REVIEWS_URL, 'list_videos_game_reviews', "0", "")
+    add_dir(LANGUAGE(30003), "/videos/all/filtergalleryajax?filter=games-review", 'list_videos', "0", "")
+    add_dir(LANGUAGE(30004), "/videos/all/filtergalleryajax?filter=games-trailer", 'list_videos', "0", "")
+    add_dir(LANGUAGE(30005), "/videos/all/filtergalleryajax?filter=movies-trailer", 'list_videos', "0", "")
+    add_dir(LANGUAGE(30006), "/videos/all/filtergalleryajax?filter=series", 'list_videos', "0", "")
+    add_dir(LANGUAGE(30008), "", 'search', "0", "")
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
     if force_view_mode:
@@ -64,6 +69,9 @@ def index():
 
 def list_videos(url):
     html_source = get_url(url)
+
+    # log("html_source", html_source)
+
     spl = html_source.split('<div class="grid_16 alpha bottom_2">')
     for i in range(1, len(spl), 1):
         entry = spl[i]
@@ -90,10 +98,65 @@ def list_videos(url):
             add_link(title, url, 'play_video', thumb, date + "\n" + desc, length)
 
     match_page = re.compile('<a id="moreVideos" href="(.+?)"', re.DOTALL).findall(html_source)
-    # page_count = re.compile('<a id="moreVideos" href=".+?page=(.+?)\&.+?"', re.DOTALL).findall(html_source)
     if len(match_page) > 0:
         url_next = match_page[0]
-        add_dir(LANGUAGE(30001), url_next, 'list_videos', os.path.join(IMAGES_PATH, 'next-page.png'))
+        add_dir(LANGUAGE(30001), url_next, 'list_videos', "0", os.path.join(IMAGES_PATH, 'next-page.png'))
+
+    xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
+    if force_view_mode:
+        xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
+
+
+
+def list_videos_game_reviews(url, start_index):
+
+    count = 20
+    html_source = post_url(url, start_index, count)
+
+    #log("html_source", html_source)
+
+    json_data = json.loads(html_source)
+
+    # log("json_data", json_data)
+
+    #[
+    #         # {
+    #         #     "data": {
+    #         #         "reviewFeed": {
+    #         #             "__typename": "ContentItemResponse",
+    #         #             "contentItems": [
+    #         #                 {
+    #         #                     "__typename": "Article",
+    #         #                     "canWatchRead": true,
+    #         #                     "commentCount": null,
+    #         #                     "contentType": "Article",
+    #         #                     "id": "5ca64df1e4b0b6ea190ce9db",
+    #         #                     "promoTitle": "Battlefield V: Firestorm Review",
+    #         #                     "publishDate": "2019-04-04T19:24:45+0000",
+    #         #                     "relatedMediaId": "71285f880dcbff2d88bfcb74d95860aa",
+    #         #                     "reviewScore": 7,
+    #         #                     "slug": "battlefield-v-firestorm-review",
+    #         #                     "thumbnailUrl": "https://assets1.ignimgs.com/2019/04/04/bfvfirestorm-blogroll-1554402013613_160w.jpg",
+    #         #                     "title": "Battlefield V: Firestorm Review",
+    #         #                     "url": "/articles/2019/04/04/battlefield-v-firestorm-review"
+    #         #                 },
+    #         #                 {
+    #         #                     "__typename": "Article",
+    for item in json_data['data']['reviewFeed']['contentItems']:
+        title = item['title']
+        url = item['url']
+        thumb = item['thumbnailUrl']
+        date = item['publishDate']
+        desc = title
+        length = ""
+        add_link(title, url, 'play_video', thumb, date + "\n" + desc, length)
+
+    start_index = int(start_index)
+    start_index = start_index + count
+
+    #log("start_index", start_index)
+
+    add_dir(LANGUAGE(30001), GAME_REVIEWS_URL, 'list_videos_game_reviews', start_index, os.path.join(IMAGES_PATH, 'next-page.png'))
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
     if force_view_mode:
@@ -106,6 +169,7 @@ def list_series_episodes(url):
         '<a.+?class="video-link".+?href="(.+?)".+?data-title="(.+?)".+?>.+?<img src="(.+?)" />.+?'
         '<div class="video-title">(.+?)</div>.+?<div class="video-duration">(.+?)</div>.+?<div class="ago">(.+?)</div>',
         re.DOTALL).findall(html_source)
+
     for i in range(0, len(match), 1):
         vidurl = BASE_URL + match[i][0]
         description = match[i][1]
@@ -118,9 +182,10 @@ def list_series_episodes(url):
 
     match_page = re.compile('<a class="next" href="://(.+?)">Next&nbsp;&raquo;</a>', re.DOTALL).findall(html_source)
     page_count = re.compile('<a class="next" href="://.+?page=(.+?)">Next&nbsp;&raquo;</a>', re.DOTALL).findall(html_source)
+
     if len(page_count) > 0:
         add_dir(LANGUAGE(30001),
-                match_page[0] + "&category=videos", 'list_series_episodes', os.path.join(IMAGES_PATH, 'next-page.png'))
+                match_page[0] + "&category=videos", 'list_series_episodes', "0", os.path.join(IMAGES_PATH, 'next-page.png'))
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
     if force_view_mode:
@@ -130,6 +195,7 @@ def list_series_episodes(url):
 def list_series(url):
     html_source = get_url(url)
     spl = html_source.split('<div class="grid_16 alpha bottom_2">')
+
     for i in range(1, len(spl), 1):
         entry = spl[i]
         match = re.compile('<li>(.+?)</li>', re.DOTALL).findall(entry)
@@ -143,7 +209,7 @@ def list_series(url):
         match = re.compile('src="(.+?)"', re.DOTALL).findall(entry)
         if len(match) > 0:
             thumb = match[0].replace("_small.jpg", ".jpg")
-        add_dir(title, url, 'list_videos', thumb, date)
+        add_dir(title, url, 'list_videos', "0", thumb, date)
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
     if force_view_mode:
@@ -162,6 +228,7 @@ def list_search_results(url):
     url_main = url
     html_source = get_url(url)
     spl = html_source.split('<div class="search-item"')
+
     for i in range(1, len(spl), 1):
         entry = spl[i]
         match = re.compile('src="(.+?)"', re.DOTALL).findall(entry)
@@ -178,13 +245,15 @@ def list_search_results(url):
         title = match[0][1]
         title = clean_title(title)
         add_link(title, url, 'play_video', thumb, "", length)
+
     match = re.compile('data-page="(.+?)"', re.DOTALL).findall(html_source)
     page = int(match[0])
     match = re.compile('data-total="(.+?)"', re.DOTALL).findall(html_source)
     max_page = int(int(match[0])/ 10)
     url_next = url_main.replace("page=" + str(page), "page=" + str(page + 1))
+
     if page < max_page:
-        add_dir(LANGUAGE(30001), url_next, 'list_search_results', os.path.join(IMAGES_PATH, 'next-page.png'))
+        add_dir(LANGUAGE(30001), url_next, 'list_search_results', "0", os.path.join(IMAGES_PATH, 'next-page.png'))
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
     if force_view_mode:
@@ -192,17 +261,25 @@ def list_search_results(url):
 
 
 def play_video(page_url):
-
     log("page_url", page_url)
 
-    match = re.compile(BASE_URL + "(.+)", re.DOTALL).findall(page_url)
-    vid = Video(match[0])
-    final_url = vid.get_vid_url(max_video_height)
+    if str(page_url).find("/articles/") >= 0:
+        page_url = convertToUnicodeString(page_url)
+        vid = Video(page_url)
+        final_url = vid.get_vid_url(max_video_height)
+    else:
+        match = re.compile(BASE_URL + "(.+)", re.DOTALL).findall(page_url)
+        vid = Video(match[0])
+        final_url = vid.get_vid_url(max_video_height)
 
     log("final_url", final_url)
 
-    list_item = xbmcgui.ListItem(path=final_url)
-    return xbmcplugin.setResolvedUrl(PLUGIN_HANDLE, True, list_item)
+    # if no video is found, there is nothing to play
+    if final_url == '':
+        pass
+    else:
+        list_item = xbmcgui.ListItem(path=final_url)
+        xbmcplugin.setResolvedUrl(PLUGIN_HANDLE, True, list_item)
 
 
 class Video(object):
@@ -221,45 +298,61 @@ class Video(object):
             .findall(html_source)
 
         temp_match_index = 0;
+        max_found_bitrate = '0'
+        previous_video_id_part_2 = ''
         for temp_match in temp_matches:
 
-            log("temp_match", temp_match)
+            # log("temp_match", temp_match)
 
-            start_url = temp_match[0]
-            year = temp_match[1]
-            month = temp_match[2]
-            day = temp_match[3]
-            resolution = temp_match[4]
-            video_id_part_1 = temp_match[5]
-            bitrate = temp_match[6]
-            video_id_part_2 = temp_match[7]
-            temp_url = start_url + "/zencoder/" + year + "/" + month + "/" + day + "/" + resolution + "/" + video_id_part_1 + "-" + bitrate + "-" + video_id_part_2
-            try:
+            # skip the item if it looks like a game reviews url
+            if str(temp_match).find("/articles/") >= 0:
 
-                log("temp_url trying", temp_url)
+                # log("temp_match skipped", temp_match)
 
-                ret = urllib.request.urlopen(temp_url)
-                if ret.code == 200:
+                pass
+            else:
+                start_url = temp_match[0]
+                year = temp_match[1]
+                month = temp_match[2]
+                day = temp_match[3]
+                resolution = temp_match[4]
+                video_id_part_1 = temp_match[5]
+                bitrate = temp_match[6]
+                video_id_part_2 = temp_match[7]
+                temp_url = start_url + "/zencoder/" + year + "/" + month + "/" + day + "/" + resolution + "/" + video_id_part_1 + "-" + bitrate + "-" + video_id_part_2
 
-                    log("temp_url added", temp_url)
+                # the first video should be the review video
+                if (previous_video_id_part_2 == '') or (video_id_part_2 == previous_video_id_part_2):
+                    # skip videos if the video we already found has the same or higher resolution
+                    if int(max_found_bitrate) >= int(bitrate):
+                        pass
+                    else:
+                        self.urls[video_height[temp_match_index]] = temp_url
 
-                    self.urls[video_height[temp_match_index]] = temp_url
-            except urllib.error.HTTPError as e:
-                xbmc.log('HTTPError: ' + temp_url)
-            except urllib.error.URLError as e:
-                xbmc.log('URLError: ' + temp_url)
+                        # log("temp_url added", temp_url)
 
-            temp_match_index = temp_match_index + 1
-            # We only need 5 temp_matches, after that it starts repeating itself
-            if temp_match_index > 4:
-                break
+                        max_found_bitrate = bitrate
+                        previous_video_id_part_2 = video_id_part_2
+
+                        temp_match_index = temp_match_index + 1
+                        # We only need 5 temp_matches, after that it starts repeating itself
+                        if temp_match_index > 4:
+                            break
 
     def get_vid_url(self, wanted_resolution):
         self._detect()
         final_url = ""
         for available_resolution in list(self.urls.keys()):
+
+            # log("avail_res", available_resolution)
+
+            # log("wanted_resolution", wanted_resolution)
+
             if int(available_resolution) <= wanted_resolution:
                 final_url = self.urls[available_resolution]
+
+                # log("setting final url", final_url)
+
         return final_url
 
 
@@ -285,17 +378,38 @@ def clean_url(title):
     return title
 
 
+def post_url(url, start_index, count):
+    complete_url = url
+    editors_choice = False
+    start_index = int(start_index)
+
+    body = {"operationName":"reviewsFeed","variables":{"filter":"Games","region":"us","startIndex":start_index,"count":count,"editorsChoice":editors_choice,"sortOption":"Latest","gamePlatformSlugs":[],"genreSlugs":[]},"query":"query reviewsFeed($filter: ReviewFilterEnum!, $sortOption: ReviewSortOptionEnum, $scoreRange: String, $gamePlatformSlugs: [String], $genreSlugs: [String], $editorsChoice: Boolean, $region: String, $count: Int, $startIndex: Int) {\n  reviewFeed(filter: $filter, sortOption: $sortOption, scoreRange: $scoreRange, gamePlatformSlugs: $gamePlatformSlugs, genreSlugs: $genreSlugs, editorsChoice: $editorsChoice, region: $region, count: $count, startIndex: $startIndex) {\n    pagination {\n      total\n      isMore\n      endIndex\n      __typename\n    }\n    contentItems {\n      ...contentItemRequiredFields\n      title\n      promoTitle\n      thumbnailUrl\n      commentCount\n      reviewScore\n      canWatchRead\n      relatedMediaId\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment contentItemRequiredFields on ContentItem {\n  id\n  publishDate\n  contentType\n  url\n  slug\n  __typename\n}\n"}
+
+    response = requests.post(complete_url, data=json.dumps(body), headers=HEADERS_JSON)
+    html_source = response.text
+    html_source = convertToUnicodeString(html_source)
+
+    # log("html_source", html_source)
+
+    return html_source
+
+
 def get_url(url):
     if url.find(BASE_URL) >= 0:
         complete_url = url
     else:
-        complete_url = BASE_URL + url
+        if url.find(GAME_REVIEWS_URL) >= 0:
+            complete_url = url
+        else:
+            complete_url = BASE_URL + url
 
     log("complete_url", complete_url)
 
     response = requests.get(complete_url, headers=HEADERS, cookies=COOKIES)
     html_source = response.text
     html_source = convertToUnicodeString(html_source)
+    html_source = clean_title(html_source)
+    html_source = html_source.replace("\\", "")
 
     # log("html_source", html_source)
 
@@ -328,8 +442,8 @@ def add_link(title, url, mode, iconimage, desc="", duration=""):
     return ok
 
 
-def add_dir(title, url, mode, iconimage, desc=""):
-    u = sys.argv[0] + '?url=' + urllib.parse.quote_plus(url) + '&mode=' + str(mode)
+def add_dir(title, url, mode, start_index, iconimage, desc=""):
+    u = sys.argv[0] + '?url=' + urllib.parse.quote_plus(url) + '&mode=' + str(mode) + "&start_index=" + str(start_index)
     liz = xbmcgui.ListItem(title, iconImage='DefaultFolder.png', thumbnailImage=iconimage)
     liz.setInfo(type='video', infoLabels={'title': title, 'plot': desc, 'plotoutline': desc})
     # let's remove any non-ascii characters from the title, to prevent errors with urllib.parse.parse_qs
@@ -377,6 +491,7 @@ def log(name_object, object):
 params = parameters_string_to_dict(sys.argv[2])
 mode = params.get('mode')
 url = params.get('url')
+start_index = params.get('start_index')
 
 if url is None:
     if SETTINGS.getSetting('onlyshownewvideocategory') == 'true':
@@ -386,6 +501,9 @@ if url is None:
 if mode == 'list_videos':
     url = urllib.parse.unquote_plus(url)
     list_videos(url)
+elif mode == 'list_videos_game_reviews':
+    url = urllib.parse.unquote_plus(url)
+    list_videos_game_reviews(url, start_index)
 elif mode == 'list_series':
     url = urllib.parse.unquote_plus(url)
     list_videos(url)

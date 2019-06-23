c20039277cf926b51653020b81962c4237861ab6
remichie <rudiparet@hotmail.com>
2019-06-23 16:02:43 +0100

    Update main.py

--- plugin.video.itvrs/main.py
+++ plugin.video.itvrs/main.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from resources.lib import kodilogging
 from resources.lib import itvrs
 
 import logging
 import xbmcaddon
 
-# Keep this file to a minimum, as Kodi
-# doesn't keep a compiled copy of this
+
 ADDON = xbmcaddon.Addon()
 kodilogging.config()
 
-
-from resources.lib import itvrs
 
 if __name__ == '__main__':
     itvrs.run()

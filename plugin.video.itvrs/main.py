2aa70e7a50a4be6f034bcd2372c44a4a29360973
remichie <rudiparet@hotmail.com>
2019-06-23 16:07:16 +0100

    Update main.py

--- plugin.video.itvrs/main.py
+++ plugin.video.itvrs/main.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-
 from resources.lib import itvrs
-
-import logging
-import xbmcaddon
-
-
-ADDON = xbmcaddon.Addon()
-kodilogging.config()
-
 
 if __name__ == '__main__':
     itvrs.run()

720bacbc112d1b5d371ae4c48821f17bc44783f6
remichie <rudiparet@hotmail.com>
2019-06-23 15:56:26 +0100

    Update main.py

--- plugin.video.itvrs/main.py
+++ plugin.video.itvrs/main.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from resources.lib import kodilogging
+from resources.lib import plugin
+from resources.lib import itvrs
+
+import logging
+import xbmcaddon
+
+# Keep this file to a minimum, as Kodi
+# doesn't keep a compiled copy of this
+ADDON = xbmcaddon.Addon()
+kodilogging.config()
+
 
 from resources.lib import itvrs
 

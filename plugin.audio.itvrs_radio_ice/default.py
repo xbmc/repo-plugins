8cb59561a01972e61297f95ce87843957c2952b3
remichie <rudiparet@hotmail.com>
2019-08-20 11:28:18 +0100

    Update default.py

--- plugin.audio.itvrs_radio_ice/default.py
+++ plugin.audio.itvrs_radio_ice/default.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
-import neverwise as nw, os
+
+import neverwise as nw
+import os
 import xbmcaddon
 import xbmcgui
 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
-line1 = "ITVRS - Radio Ice"
-line2 = "  "
-line3 = "Use Left Menu To Pause Or Stop"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3) 
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+line1 = 'ITVRS - Radio Ice'
+line2 = '  '
+line3 = 'Use Left Menu To Pause Or Stop'
 
-class itvrs_radio_ice(object):
+xbmcgui.Dialog().ok(addonname, line1, line2, line3)
 
-  def __init__(self):
 
-    li = nw.createListItem(nw.addonName, thumbnailImage = '{0}/resources/icon.png'.format(os.path.dirname(os.path.abspath(__file__))), streamtype = 'music', infolabels = { 'title' : 'ITVRS - Radio Ice' })
-    xbmc.Player().play('http://stream.zeno.fm/3611cwn8mbruv', li)
+class itvrs_radio_ice:
+
+    def __init__(self):
+
+        li = nw.createListItem(nw.addonName,
+                               thumbnailImage='{0}/resources/icon.png'.format(os.path.dirname(os.path.abspath(__file__))),
+                               streamtype='music',
+                               infolabels={'title': 'ITVRS - Radio Ice'})
+        xbmc.Player().play('http://stream.zeno.fm/3611cwn8mbruv', li)
 
 
 rr = itvrs_radio_ice()

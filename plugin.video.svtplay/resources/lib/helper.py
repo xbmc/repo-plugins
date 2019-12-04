9f4dd92034ab16c49bc41c5a8b197244bbbcb95b
Erik Lindqvist <eriksmejl@gmail.com>
2019-12-04 12:44:08 +0100

    [plugin.video.svtplay] 5.1.3

--- plugin.video.svtplay/resources/lib/helper.py
+++ plugin.video.svtplay/resources/lib/helper.py
@@ -145,6 +145,19 @@ def cleanUrl(video_url):
         newparas.append(para)
   return tmp[0]+"&".join(newparas).replace("?&", "?")
 
+def episodeUrlToShowUrl(url):
+  """
+  Returns the show URL from episode url.
+  Example: "/video/22132986/abel-och-fant/abel-och-fant-sasong-2-kupa-pa-rymmen" > "/abel-och-fant"
+
+  Returns None for single video items (movies etc)
+  """
+  new_url = None
+  stub_url = url.split("/")
+  if len(stub_url) >= 5:
+    new_url = "/" + stub_url[3]
+  return new_url
+
 def __errorMsg(msg):
   logging.error(msg)
 

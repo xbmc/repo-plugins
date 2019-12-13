9e7f398db82bafe0bd86683d948ee43e0a242c03
Peter Lin <linpeterp@gmail.com>
2019-12-12 21:55:54 -0800

    [plugin.video.wabc] v3.0.13
    website changes
    switch to json data
    highlight locked streams
    support local show overrides
    update language layout

--- plugin.video.wabc/resources/lib/scraper.py
+++ plugin.video.wabc/resources/lib/scraper.py
@@ -11,6 +11,8 @@ import HTMLParser
 import sys
 import os
 import json
+import time
+import zlib
 
 h = HTMLParser.HTMLParser()
 qp  = urllib.quote_plus
@@ -22,79 +24,115 @@ class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
       urls = {}
-      html = self.getRequest('https://abc.go.com/shows?category=a-z')
-      html = re.compile('<section  data-m-id=(.+?)<section  data-m-id=', re.DOTALL).search(html).group(1)
-      a = re.compile('data-sm-id="".+?href="(.+?)".+?class="tablet-source.+?srcset="(.+?) ',re.DOTALL).findall(html)
-      for url, thumb in a:
-        if not '/' in url:
-          continue
-        if urls.get(url,None) is None:
-          if url.endswith('/index'):
-              url = url.replace('/index','',1)
-          urls[url] = url
-          name = url.rsplit('/',1)[1]
-          name = name.replace('-',' ').title()
-          fanart = thumb
-          infoList ={}
-          infoList['Title'] = name
-          infoList['TVShowTitle'] = name
-          infoList['mediatype'] = 'tvshow'
-          contextMenu = [('Add To Library','XBMC.RunPlugin(%s?mode=DF&url=AL%s)' % (sys.argv[0], url))]
-          ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True, cm=contextMenu)
+      html = ''
+      try:
+        url = xbmc.translatePath('special://home/addons/plugin.video.wabc/resources/lib/shows.zip')
+        html = urllib.urlopen(url).read()
+        d = zlib.decompressobj(16+zlib.MAX_WBITS)
+        html = d.decompress(html)
+      except:
+        html = self.getRequest('https://abc.com/shows?category=A-Z')
+      html = re.compile("<script type=['\"]text/javascript['\"].?>window\['__abc_com__'\]=(.+?);</script>", re.DOTALL).search(html).group(1)
+      jo = json.loads(html);
+      modules = jo['page']['content']['shell']['module']
+      for m in modules:
+        if 'tiles' in m.keys():
+            tiles = m['tiles']
+            if isinstance(tiles,list):
+                for show in tiles:
+                    images = show.get('images')
+                    thumb = None
+                    if isinstance(images,list):
+                        thumb = images[len(images)-1].get('value')
+                    url = show.get('link').get('urlValue')
+                    if url is None or not '/' in url:
+                        continue
+                    if urls.get(url,None) is None and show.get('show') is not None:
+                        if url.endswith('/index'):
+                            url = url.replace('/index','',1)
+                        urls[url] = url
+                        name = show.get('title')
+                        fanart = thumb
+                        infoList ={}
+                        if 'about' in show.get('show').keys():
+                            infoList['Plot'] = h.unescape(show['show']['about'])
+                        infoList['Title'] = name
+                        infoList['TVShowTitle'] = name
+                        infoList['mediatype'] = 'tvshow'
+                        contextMenu = [('Add To Library','XBMC.RunPlugin(%s?mode=DF&url=AL%s)' % (sys.argv[0], url))]
+                        ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True, cm=contextMenu)
       return(ilist)
 
 
   def getAddonEpisodes(self,url,ilist, getFileData = False):
       if not url.startswith('http'):
-          url = 'https://abc.go.com'+url
+          u = 'https://abc.com'+url
       if not url.endswith('/episode-guide'):
-          url = url+'/episode-guide'
-      html = self.getRequest(url)
-      vids = re.compile('data-video-id="VDKA(.+?)".+?data-title="(.+?)".+?data-background="(.+?)".+?class="tablet-source".+?srcset="(.+?) .+?class="season-number(.+?)<.+?class="episode-number(.+?)<.+?class="m-episode-summary.+?<p>(.+?)</p>.+?<div class="m-episode-meta(.+?)</div',re.DOTALL).findall(html)
-      for url, name, fanart, thumb, season, episode, plot, meta in vids:
-          name = h.unescape(name.decode(UTF8))
-          name = h.unescape(name) # get rid of &apos; as well
-          plot = h.unescape(plot.decode(UTF8))
-          thumb = thumb.strip()
-          infoList = {}
-          season = season.split('>S',1)
-          if len(season) > 1 and season[1].strip().isdigit():
-              infoList['Season'] = int(season[1])
-          episode = episode.split('>E',1)
-          if len(season) > 1 and episode[1].strip().isdigit():
-              infoList['Episode'] = int(episode[1])
-          infoList['Title'] = name
-          infoList['Plot'] = plot
-          meta1 = re.compile('<span class="m-episode-meta-item m-episode-meta-duration">(.+?)</span>', re.DOTALL).findall(meta)
-          if meta1 is not None:
-              duration = 0
-              tmp = meta1[0].split(':')
-              for dur in tmp:
-                  duration = duration*60 + int(dur) 
-              infoList['Duration'] = duration
-
-          meta1 = re.compile('<span class="m-episode-meta-item">(.+?)</span>', re.DOTALL).findall(meta)
-          if meta1 is not None:
-              mo, day, year = meta1[0].split('/')
-              year = int(year)
-              if year < 55:
-                  year = year + 2000
-              else:
-                  year = year + 1900
-              infoList['Date'] = '%s-%s-%s' % ( str(year), mo, day)
-              infoList['Aired'] = infoList['Date']
-              infoList['Year'] = int(infoList['Aired'].split('-',1)[0])
-              if len(meta1)>1:
-                  infoList['MPAA'] = meta1[1]
-          infoList['TVShowTitle'] = xbmc.getInfoLabel('ListItem.TVShowTitle')
-          infoList['Studio'] = 'ABC'
-          infoList['mediatype'] = 'episode'
-          if getFileData == False:
-              ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart, infoList, isFolder=False)
-          else:
-              if infoList.get('Season') is not None and infoList.get('Episode') is not None:
-                  ilist.append((infoList['Season'], infoList['Episode'], url))
+          u = u+'/episode-guide'
+      html = self.getRequest(u)
+      if len(html) is 0 or re.search('module-404-wrapper', html, re.DOTALL):    #no root episode-guide
+          u = 'https://abc.com'+url
+          html = self.getRequest(u)
+          a = re.compile('<div class="tilegroup__group-action"><a href="(.+?)">See All</a>',re.DOTALL).findall(html)
+          if a is not None and len(a) > 0:                                      #only gets latest, should get all?
+              u = 'https://abc.com'+a[0]
+              html = self.getRequest(u)
+      html = re.compile("<script type='text/javascript' >window\['__abc_com__'\]=(.+?);</script>", re.DOTALL).search(html).group(1)
+      jo = json.loads(html)
+      tiles = None
+      video = None
+      modules = jo['page']['content']['shell']['module']
+      for m in modules:
+            if 'tiles' in m.keys():
+                tiles = m['tiles']
+                break
+            if 'video' in m.keys():
+                video = m['video']
+                break;
+      if tiles is None and isinstance(video,dict):      #page just has a single video player
+        self.addVideo(ilist,getFileData,video,None)
+      elif isinstance(tiles,list):                      #page has tiles of episodes
+            for t in tiles:
+                video = t.get('video')
+                images = t.get('images')
+                self.addVideo(ilist,getFileData,video,images)
+      if len(ilist) == 0:
+          xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (self.addonName, self.addon.getLocalizedString(30001), 5000, self.addonIcon))
       return(ilist)
+
+  def addVideo(self,ilist,getFileData,video,images):
+    if video is not None:  
+        season = video.get('seasonnumber')
+        episode = video.get('episodenumber')
+        plot = h.unescape(video.get('longdescription'))
+        airdate = video.get('displayAirtime')
+        aired = time.strptime(airdate, '%a, %d %b %Y %H:%M:%S')
+        url = video.get('id')[4:]
+        name = h.unescape(video.get('title'))
+        thumb = None
+        if isinstance(images,list):
+            thumb = images[len(images)-1].get('value')
+        fanart = thumb
+        infoList = {}
+        if season.isdigit():
+            infoList['Season'] = int(season)
+        if episode.isdigit():
+            infoList['Episode'] = int(episode)
+        infoList['Duration'] = int(video.get('duration')) / 60000
+        infoList['Date'] = time.strftime('%Y-%m-%d',aired)
+        infoList['Aired'] = infoList['Date']
+        infoList['Year'] = int(infoList['Aired'].split('-',1)[0])
+        infoList['MPAA'] = video.get('tvrating')
+        infoList['Title'] = '[COLOR=FFFF8040]' + name + '[/COLOR]' if int(video.get('accesslevel')) else name
+        infoList['Plot'] = plot
+        infoList['TVShowTitle'] = xbmc.getInfoLabel('ListItem.TVShowTitle')
+        infoList['Studio'] = 'ABC'
+        infoList['mediatype'] = 'episode'
+        if getFileData == False:
+              ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart, infoList, isFolder=False)
+        else:
+            if infoList.get('Season') is not None and infoList.get('Episode') is not None:
+                ilist.append((infoList['Season'], infoList['Episode'], url))
 
   def doFunction(self, url):
     func = url[0:2]
@@ -119,6 +157,9 @@ class myAddon(t1mAddon):
 
   def getAddonVideo(self,url):
       vd = uqp(url)
+      url = 'https://api.pluto.watchabc.go.com/api/ws/pluto/v1/module/videoplayer/2185737?brand=001&device=001&authlevel=0&layout=2185698&video=VDKA'+str(vd)
+      html = self.getRequest(url)
+      ua = re.compile('"ULNK","value":"(.+?)"', re.DOTALL).search(html).group(1)
       url = 'https://api.entitlement.watchabc.go.com/vp2/ws-secure/entitlement/2020/authorize.json'
       udata = 'video%5Fid=VDKA'+str(vd)+'&device=001&video%5Ftype=lf&brand=001'
       uheaders = self.defaultHeaders.copy()
@@ -133,12 +174,7 @@ class myAddon(t1mAddon):
           return
 
       sessionKey = a['uplynkData']['sessionKey']
-      if not '&cid=' in sessionKey:
-          oid, eid = re.compile('&oid=(.+?)&eid=(.+?)&', re.DOTALL).search(sessionKey).groups()
-          url = 'http://content.uplynk.com/ext/%s/%s.m3u8?%s' % (oid, eid, sessionKey)
-      else:
-          cid = re.compile('&cid=(.+?)&', re.DOTALL).search(sessionKey).group(1)
-          url = 'http://content.uplynk.com/%s.m3u8?%s' % (cid, sessionKey)
+      url = ua+'?'+sessionKey
       html = self.getRequest(url)
       url = re.compile('#UPLYNK-MEDIA0.+?http(.+?)\n',re.DOTALL).search(html).group(1)
       url = 'http'+url

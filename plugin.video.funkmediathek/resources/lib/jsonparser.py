7dc25b9630da6f77c452b154ccd004b727f967f8
dib <dib@scool.earth>
2019-12-16 21:08:51 +0100

    [plugin.video.funkmediathek] 1.5.0
        Migrate to API 4.0 / webapi
    
        changelog version 1.5.0-git
    
        use website data api and api 4.0 for images
        default header from libmediathek3
        prefer fomats and series
        ignore archiveformat for now
        use web image dimensions
    
        ship publication date again
    
        $ optipng icon.png (2249 bytes = 20.65% decrease)
    
        fix season missing for episodes
        introduce thumb image from imageLandscape
        provide fsk to episodes mpaa
    
        show archiveformat as format in the end
        harden against empty lists
    
        seasons fallback images from series
    
        addon document assets, translation adhere new structure
    
    Author: sarbes <sarbes.kodi@gmail.com>
    Date:   Mon Jul 16 18:53:30 2018 +0200
    
        fixed funk
    
        1.5.0
        - Migrated to API 3.1
    
        import f75af51c93b1eddf84066999d11a93a0b246af39
    
    Author: sarbes <sarbes.kodi@gmail.com>
    Date:   Sat Jun 30 11:24:41 2018 +0200
    
        fixed funk
        import 880c1c2025802e90e5fbad0c92f7fbcd85e56a92
        fixed linefeeds
        without compiled python files
        changes from version 1.4.2 from sarbes
    
    Author: sarbes <sarbes.kodi@gmail.com>
    Date:   Sun Jun 17 13:18:30 2018 +0200
    
        fixed funk
    
        79e8afc08eb25d3c6a0f48d038b40a3dbb4a2e88
        changes from version 1.4.1 form sarbes
    
    commit 7ecb78dfe33c13e8ccd932581dbd339fa6d3762a
    Author: sarbes <sarbes.kodi@gmail.com>
    Date:   Sat Mar 24 14:56:28 2018 +0100
    
        fixed funk
    
        129829f8577aa6f4401f4a2e1860340ab25d6120
        changes from 1.4.0
        fixed linefeeds

--- plugin.video.funkmediathek/resources/lib/jsonparser.py
+++ plugin.video.funkmediathek/resources/lib/jsonparser.py
@@ -1,98 +1,175 @@
 # -*- coding: utf-8 -*-
 import json
 import libmediathek3 as libMediathek
-base = 'https://api.funk.net/v1.1'
+base = 'https://www.funk.net/api/v4.0'
+webbase = 'https://www.funk.net/data/'
 
-auth = '29ad11c93ad19f816de90c1c8ae4ec0354c6b5e4ec5b3965f125248cfa60bfe1'
 fanart = libMediathek.fanart
-
-header = 	{
-			'Authorization':auth[::-1],
-			'User-Agent':'okhttp/3.2.0',
-			'Accept-Encoding':'gzip',
-			'Host':' api.funk.net',
-			}
 
 types = {
 'format':'formats',
 'series':'series',
 }
 
-def parse(url):
-	response = libMediathek.getUrl(url,header)
+#v3.0
+#auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnROYW1lIjoiY3VyYXRpb24tdG9vbCIsInNjb3BlIjoic3RhdGljLWNvbnRlbnQtYXBpLGN1cmF0aW9uLWFwaSxzZWFyY2gtYXBpIn0.q4Y2xZG8PFHai24-4Pjx2gym9RmJejtmK6lMXP5wAgc'
+#v3.1
+#auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnROYW1lIjoiY3VyYXRpb24tdG9vbC12Mi4wIiwic2NvcGUiOiJzdGF0aWMtY29udGVudC1hcGksY3VyYXRpb24tc2VydmljZSxzZWFyY2gtYXBpIn0.SGCC1IXHLtZYoo8PvRKlU2gXH1su8YSu47sB3S4iXBI'
+auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnROYW1lIjoid2ViYXBwLXYzMSIsInNjb3BlIjoic3RhdGljLWNvbnRlbnQtYXBpLGN1cmF0aW9uLWFwaSxuZXh4LWNvbnRlbnQtYXBpLXYzMSx3ZWJhcHAtYXBpIn0.mbuG9wS9Yf5q6PqgR4fiaRFIagiHk9JhwoKES7ksVX4'
+
+#auth = 'eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJjbGllbnROYW1lIjoiY3VyYXRpb24tdG9vbCIsInNjb3BlIjoic3RhdGljLWNvbnRlbnQtYXBpLGN1cmF0aW9uLWFwaSxzZWFyY2gtYXBpIn0.'
+#eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnROYW1lIjoiY3VyYXRpb24tdG9vbCIsInNjb3BlIjoic3RhdGljLWNvbnRlbnQtYXBpLGN1cmF0aW9uLWFwaSxzZWFyY2gtYXBpIn0.q4Y2xZG8PFHai24-4Pjx2gym9RmJejtmK6lMXP5wAgc
+#eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnROYW1lIjoiY3VyYXRpb24tdG9vbC12Mi4wIiwic2NvcGUiOiJzdGF0aWMtY29udGVudC1hcGksY3VyYXRpb24tc2VydmljZSxzZWFyY2gtYXBpIn0.SGCC1IXHLtZYoo8PvRKlU2gXH1su8YSu47sB3S4iXBI
+header = 	{
+			'Authorization':auth,
+			'Accept-Encoding':'gzip',
+			}
+
+def parseMain():
+	onlySeries = libMediathek.getSetting('skipToSeries') == 'true'
+	response = libMediathek.getUrl(webbase+'/static/channels',False)
 	j = json.loads(response)
 	l = []
-	if 'includes' in j:
-		part = 'includes'
-	else:
-		part = 'data'
-	for item in j[part]:
-		if item['type'] == 'series' or item['type'] == 'format':
-			d = _parseSeries(item,types[item['type']])
-			if d:
+	if not 'list' in j:
+		return l
+	for item in j['list']:
+		if not 'type' in item:
+			continue
+		elif item['type'] == 'format' or item['type'] == 'series':
+			try:
+				d = {}
+				d['_name'] = item['title']
+				if 'publicationDate' in item:
+					d['_airedISO8601'] = item['publicationDate']
+				if 'shortDescription' in item:
+					d['_plot'] = item['shortDescription']
+				if 'description' in item:
+					d['_plot'] = item['description']
+				if 'imageSquare' in item:
+					imgurl = base + '/thumbnails/' + item['imageSquare'] + '?quality=85&width=200'
+					d['_thumb'] = imgurl
+				if 'imageOrigin' in item and item['imageOrigin'] != None:
+					imgurl = base + '/thumbnails/' + item['imageOrigin'] + '?quality=85'
+					d['_fanart'] = imgurl
+				else:
+					d['_fanart'] = fanart
+				d['_type'] = 'dir'
+				d['id'] = item['alias']
+				if item['type'] == 'series':
+					d['mode'] = 'listSeasons'
+					if 'imageSquare' in item:
+						d['fallbackimage'] = item['imageSquare']
+				elif item['type'] == 'format':
+					d['mode'] = 'listVideos'
+				
 				l.append(d)
-		elif item['type'] == 'video':
-			l.append(_parseVideo(item))
+			except:
+				libMediathek.log(json.dumps(item))
+		elif item['type'] == 'archiveformat':
+			continue
 		else:
-			libMediathek.log('unkown type: '+item['type'])
+			continue
+	for item in j['list']:
+		if 'type' in item and item['type'] == 'archiveformat':
+			d = {}
+			d['_name'] = item['title']
+			if 'imageLandscape' in item:
+				imgurl = base + '/thumbnails/' + item['imageSquare'] + '?quality=85&width=244'
+				d['_thumb'] = imgurl
+			if 'imageOrigin' in item and item['imageOrigin'] != None:
+				imgurl = base + '/thumbnails/' + item['imageOrigin'] + '?quality=85'
+				d['_fanart'] = imgurl
+			else:
+				d['_fanart'] = fanart
+			d['id'] = item['alias']
+			d['_type'] = 'dir'
+			d['mode'] = 'listVideos'
+			l.append(d)
 	return l
 	
-def _parseSeries(j,type):
-	d = {}
-	if 'name' in j['attributes']:
-		if j['attributes']['name'] == 'funk Livestream': return False
-		d['_name'] = j['attributes']['name']
-	else:
-		if j['attributes']['title'] == 'funk Livestream': return False
-		d['_name'] = j['attributes']['title']
-	if 'thumbnail' in j['attributes']:
-		d['_thumb'] = j['attributes']['thumbnail']
-	else:
-		d['_thumb'] = fanart
-	if type == 'series':
-		d['_fanart'] = fanart
-	if 'description' in j['attributes']:
-		d['_plot'] = _cleanPlot(j['attributes']['description'])
-	d['_airedISO8601'] = j['attributes']['createdAt']
-	d['url'] = base + '/content/'+type+'/' + j['id'] + '?should_filter=false'#
-	d['_rating'] = str(j['attributes']['kickKeepRatio'] * 10)
-	d['mode'] = 'listDir'
-	d['_type'] = 'season'
-	return d
-
-def _parseVideo(j):
-	d = {}
-	d['_name'] = j['attributes']['title']
 	
-	d['_thumb'] = j['attributes']['image']['url']
-	if 'text' in j['attributes']:
-		d['_plot'] = _cleanPlot(j['attributes']['text'])
-		while '\n\n\n' in d['_plot']:
-			d['_plot'] = d['_plot'].replace('\n\n\n','\n\n')
-	d['_duration'] = str(j['attributes']['duration'])
-	if 'season' in j['attributes']:
-		d['_season'] = j['attributes']['season']
-	if 'episode' in j['attributes']:
-		d['_episode'] = j['attributes']['episode']
+def parseSeasons(id, params):
+	#response = libMediathek.getUrl(base+'/content/playlists/filter/?channelId=' + id + '&secondarySort=alias,ASC',header)
+	#https://www.funk.net/api/v3.1/webapp/playlists/byChannelAlias/alles-liebe-annette?sort=language,ASC
+	url = webbase + 'seasons/byChannelAlias/' + id
+	response = libMediathek.getUrl(url,False)
+	j = json.loads(response)
+	l = []
+	#libMediathek.log(response)
+	for item in j['list']:
+		d = {}
+		d['_name'] = item['title']
+		if 'shortDescription' in item:
+			d['_plot'] = item['shortDescription']
+		if 'description' in item:
+			d['_plot'] = item['description']
+		if 'fallbackimage' in params:
+			imgurl = base + '/thumbnails/' + params['fallbackimage'] + '?quality=85&width=244'
+			d['_thumb'] = imgurl
+		d['_type'] = 'season'
+		d['id'] = item['alias']
+		d['mode'] = 'listEpisodes'
+		l.append(d)
+	return l
+	
+def parseEpisodes(id):
+	#response = libMediathek.getUrl(base+'/content/playlists/'+id+'/videos/?size=100&secondarySort=episodeNr,ASC',header)
+	#https://www.funk.net/api/v3.1/webapp/videos/byPlaylistAlias/alles-liebe-annette-staffel-1?filterFsk=false&size=100&sort=episodeNr,ASC
+	url = base+'/webapp/videos/byPlaylistAlias/'+id+'?filterFsk=false&size=100&sort=episodeNr,ASC'
+	url = webbase + 'videos/bySeasonAlias/' + id
+	response = libMediathek.getUrl(url,False)
+	j = json.loads(response)
+	l = []
+	for item in j['list']:
+		d = {}
+		d['_name'] = item['title']
+		if 'shortDescription' in item:
+			d['_plot'] = item['shortDescription']
+		if 'description' in item:
+			d['_plot'] = item['description']
+		d['_duration'] = item['duration']
+		if 'imageLandscape' in item:
+			imgurl = base + '/thumbnails/' + item['imageLandscape'] + '?quality=85&width=244'
+			d['_thumb'] = imgurl
+		if 'seasonNr' in item:
+			# workaround bug in script.module.libmediathek3/lib/libmediathek3listing.py:addEntries():74
+			season = str(item['seasonNr'])
+			d['_Season'] = season
+			d['_season'] = season
+		if 'episodeNr' in item:
+			d['_episode'] = str(item['episodeNr'])
+		if 'fsk' in item:
+			d['_mpaa'] = str(item['fsk'])
 		d['_type'] = 'episode'
-	else:
+		d['sourceId'] = str(item['entityId'])
+		d['mode'] = 'play'
+		l.append(d)
+	return l
+	
+def parseVideos(id):
+	#https://www.funk.net/api/v3.0/content/videos/filter?channelId=auf-einen-kaffee-mit-moritz-neumeier&page=0&size=20
+	#response = libMediathek.getUrl(base+'/content/videos/filter?channelId='+id+'&page=0&size=100',header)
+	#https://www.funk.net/api/v3.1/webapp/playlists/byChannelAlias/auf-einen-kaffee-mit-moritz-neumeier?sort=language,ASC
+	#https://www.funk.net/api/v3.1/webapp/videos/byChannelAlias/auf-einen-kaffee-mit-moritz-neumeier?filterFsk=false&sort=creationDate,desc&page=0&size=20
+	url = webbase + 'videos/byChannelAlias/'+id
+	response = libMediathek.getUrl(url,False)
+	j = json.loads(response)
+	l = []
+	for item in j['list']:
+		d = {}
+		d['_name'] = item['title']
+		if 'shortDescription' in item:
+			d['_plot'] = item['shortDescription']
+		if 'publicationDate' in item:
+			d['_airedISO8601'] = item['publicationDate']
+		if 'description' in item:
+			d['_plot'] = item['description']
+		if 'imageLandscape' in item:
+			imgurl = base + '/thumbnails/' + item['imageLandscape'] + '?quality=85&width=244'
+			d['_thumb'] = imgurl
+		d['_duration'] = item['duration']
 		d['_type'] = 'video'
-	if 'fsk' in j['attributes']:
-		d['_mpaa'] = 'FSK ' + str(j['attributes']['fsk'])
-	
-	d['_airedISO8601'] = j['attributes']['createdAt']
-	
-	d['sourceId'] = j['attributes']['sourceId']
-	d['mode'] = 'play'
-	return d
-	
-def _cleanPlot(plot):
-	plot = plot.replace('<p>','')
-	plot = plot.replace('</p>','')
-	plot = plot.replace('<h4>','')
-	plot = plot.replace('</h4>','')
-	while '  ' in plot:
-		plot = plot.replace('  ',' ')
-	#plot = plot.replace('\n ','')
-	return plot
-	
+		d['sourceId'] = str(item['entityId'])
+		d['mode'] = 'play'
+		l.append(d)
+	return l

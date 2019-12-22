2b2c30a4e89aa32f5b0dee2d6e0909b7b449a7b4
Wintermute 0110 <wintermute0110@gmail.com>
2019-12-22 18:05:49 +0900

    [plugin.program.AML] 0.9.11 (#2604)

--- plugin.program.AML/resources/filters.py
+++ plugin.program.AML/resources/filters.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-#
+
 # Advanced MAME Launcher MAME filter engine.
-#
 
 # Copyright (c) 2016-2019 Wintermute0110 <wintermute0110@gmail.com>
 #
@@ -11,8 +10,8 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
 
 # --- Python standard library ---
 # Division operator: https://www.python.org/dev/peps/pep-0238/

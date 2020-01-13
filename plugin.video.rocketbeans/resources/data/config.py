a2c0fb09a3932c288b6b65184e110c754a386f27
Kolja Lampe <razzeee@gmail.com>
2020-01-13 21:28:38 +0100

    [plugin.video.rocketbeans] 1.2.1

--- plugin.video.rocketbeans/resources/data/config.py
+++ plugin.video.rocketbeans/resources/data/config.py
@@ -4,6 +4,8 @@ GAME_TWO_CHANNEL_ID = 'UCFBapHA35loZ3KZwT_z3BsQ'
 HAENGI_HQ_CHANNEL_ID = 'UCkfDws3roWo1GaA3pZUzfIQ'
 KINO_PLUS_CHANNEL_ID = 'UCSPhBkVTeztDGsz9-rdsNvw'
 
+GUIDE_URL = 'https://api.rocketbeans.tv/v1/schedule/legacy'
+
 TWITCH_CHANNEL_ID = 47627824
 TWITCH_USER_LOGIN = 'rocketbeanstv'
 TWITCH_CLIENT_ID = 'NjdlYnBmaHlvaWNhYjVrcjB5N3B6b2NzZm9oczd0eQ=='

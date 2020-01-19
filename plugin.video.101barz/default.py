3f049c32ef8be46ae014d740ef05427c6661d171
skipmodea1 <skipmodea1@outlook.com>
2020-01-19 12:43:20 +0100

    [plugin.video.101barz] 1.1.0

--- plugin.video.101barz/default.py
+++ plugin.video.101barz/default.py
@@ -12,105 +12,181 @@ import sys
 import xbmcaddon
 import xbmc
 import os
+import re
 import requests
 import json
+from bs4 import BeautifulSoup
 
 ADDON = "plugin.video.101barz"
 SETTINGS = xbmcaddon.Addon(id=ADDON)
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
 PLUGIN_HANDLE = int(sys.argv[1])
-BASE_URL = "https://101barz.bnnvara.nl/api/video?"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10115) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 COOKIES = {}
-DATE = "2019-02-12"
-VERSION = "1.0.0"
+DATE = "2020-01-15"
+VERSION = "1.1.0"
 
-SEARCHURL = "category=<categorystring>&search=<searchstring>&offset=0&limit=12"
-ALLESURL = "category=all&search=&offset=0&limit=12"
-STUDIOSESSIONSURL = "category=13&search=&offset=0&limit=12"
-VIDEOCLIPZURL = "category=14&search=&offset=0&limit=12"
-ROTJOCHLOUNGEURL = "category=25&search=&offset=0&limit=12"
-HITMAKERZURL = "category=27&search=&offset=0&limit=12"
-BARZ4BARZURL = "category=28&search=&offset=0&limit=12"
-TALENTVDMAANDURL = "category=29&search=&offset=0&limit=12"
+APIURL = "https://api.bnnvara.nl/bff/graphql"
+HEADERSJSON = {'content-type': 'application/json'}
+BASEURL  = "https://www.bnnvara.nl"
+VIDEOURL = "https://www.bnnvara.nl/101barz/tags/video?limit=" + str(SETTINGS.getSetting('numberofvideosshown'))
 
 
-def index():
-    add_dir(LANGUAGE(30002), ALLESURL, 'list_videos', "")
-    add_dir(LANGUAGE(30003), STUDIOSESSIONSURL, 'list_videos', "")
-    add_dir(LANGUAGE(30004), VIDEOCLIPZURL, 'list_videos', "")
-    add_dir(LANGUAGE(30005), ROTJOCHLOUNGEURL, 'list_videos', "")
-    add_dir(LANGUAGE(30006), HITMAKERZURL, 'list_videos', "")
-    add_dir(LANGUAGE(30007), BARZ4BARZURL, 'list_videos', "")
-    add_dir(LANGUAGE(30008), TALENTVDMAANDURL, 'list_videos', "")
+def list_videos(video_page_url):
 
-    xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
+    log("video_page_url", video_page_url)
 
+    # add search video_page_url
+    add_dir(LANGUAGE(30001), APIURL, 'search', "")
 
-def list_videos(url):
-    # add category number to search url
-    CATEGORY = "category="
-    start_pos = convertToUnicodeString(url).find(CATEGORY)
-    if start_pos >= 0:
-        end_pos = convertToUnicodeString(url).find("&", start_pos)
-        category_value = convertToUnicodeString(url)[start_pos + len(CATEGORY):end_pos]
-        search_url = convertToUnicodeString(SEARCHURL).replace("<categorystring>", category_value)
+    html_source = get_url(video_page_url)
 
-        # add search url
-        add_dir(LANGUAGE(30001), search_url, 'search', "")
+    # log(html_source, html_source)
 
-    html_source = get_url(url)
-    json_data = json.loads(html_source)
+    # Parse response
+    soup = getSoup(html_source)
 
-    for item in json_data['video']:
+    #<a href="/101barz/artikelen/wintersessie-josbros-2" class="tjcu1e-1 jDMFxC"><div class="sc-114vwi2-0 hkkNSo"><div class="j1n9w9-0 bczuOc"><img src="https://d2hpleul9br1of.cloudfront.net/w_500,h_500/s3-101barz/e3aad92c-8281-4b83-8d92-a5c3244d4ac6.jpg" alt="Afbeelding van Wintersessie: Josbros" class="j1n9w9-1 HiXpC"></div><div class="kyune6-1 bHNiuC"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" aria-label="artikel" class="sc-7guewg-0 jlynrr"><path fill="#FFF" fill-rule="evenodd" d="M3,4 L19,4 L19,7 L3,7 L3,4 Z M3,10 L21,10 L21,13 L3,13 L3,10 Z M3,16 L17,16 L17,19 L3,19 L3,16 Z"></path></svg></div><div class="j1n9w9-2 dzFFRu"><div class="sc-1ablp01-0 hHPbhp"><h2>Wintersessie: Josbros</h2></div><div class="sc-13rhqs6-0 hokqIi">Gisteren</div></div></div></a>
+    items = soup.findAll('a', attrs={'href': re.compile("^" + "/101barz/")})
+
+    log("len(items)", len(items))
+
+    for item in items:
 
         log("item", item)
 
+        # skips items that have the role "link"
+        if str(item).find('role="link"') >= 0:
+
+            log("skipping link item", item)
+
+            continue
+
+        title_and_date = item.select('div')[0].get_text(strip=True)
+
+        # determine the length of the date field. date field is f.e. "2 jan 2020" or "21 jan 2020"
+        testdate = convertToUnicodeString(title_and_date[(len(title_and_date)-11):])
+        if testdate[0:2].isnumeric():
+            if testdate[0:1] == "1":
+                date_length = 11
+            elif testdate[0:1] == "2":
+                date_length = 11
+            else:
+                date_length = 10
+        else:
+            date_length = 10
+
+        date = title_and_date[(len(title_and_date)-date_length):]
+        title = title_and_date[0:(len(title_and_date)-date_length)]
+
+        thumbnail_image_url = item.img['src']
+
+        video_page_url = item['href']
+        video_page_url = BASEURL + video_page_url
+
+        log("title_and_date", title_and_date)
+        log("date", date)
+        log("title", title)
+        log("thumbnail_image_url", thumbnail_image_url)
+        log("video_page_url", video_page_url)
+
+        desc = ""
         length = ""
-        title = item['title']
-        desc = item['subtitle']
-        date = item['publish_up']
-        thumbnail_image_url = item['card_image']
-        youtube_id = item['youtube_id']
-        youtube_url = 'plugin://plugin.video.youtube/play/?video_id=%s' % youtube_id
 
-        add_link(title, youtube_url, 'play_video', thumbnail_image_url, date + "\n" + desc, length)
-
-    # add next page url
-    url_next = url
-    OFFSET = "offset="
-    start_pos = convertToUnicodeString(url_next).find(OFFSET)
-    if start_pos >= 0:
-        end_pos = convertToUnicodeString(url_next).find("&", start_pos)
-        old_offset_value = convertToUnicodeString(url_next)[start_pos + len(OFFSET):end_pos]
-        old_offset_value = int(old_offset_value)
-        new_offset_value = old_offset_value + 12
-        old_offset_value = str(old_offset_value)
-        new_offset_value = str(new_offset_value)
-        url_next = convertToUnicodeString(url_next).replace(OFFSET + convertToUnicodeString(old_offset_value), OFFSET +
-                                                            convertToUnicodeString(new_offset_value))
-
-        add_dir(LANGUAGE(30009), url_next, 'list_videos', os.path.join(IMAGES_PATH, 'next-page.png'))
+        add_link(title, video_page_url, 'play_video', thumbnail_image_url, date + "\n" + desc, length)
 
     xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
 
 
-def search(url):
-    # decode the url
-    url = urllib.parse.unquote_plus(url)
-
+def search():
     keyboard = xbmc.Keyboard('', LANGUAGE(30008))
     keyboard.doModal()
     if keyboard.isConfirmed() and keyboard.getText():
         search_string = keyboard.getText().replace(" ", "+")
-        search_url = BASE_URL + convertToUnicodeString(url).replace("<searchstring>", search_string)
-        list_videos(search_url)
+        html_source = post_url(APIURL, 0, 500, search_string)
+        list_videos_json(html_source)
 
 
-def play_video(video_url):
-    list_item = xbmcgui.ListItem(path=video_url)
-    return xbmcplugin.setResolvedUrl(PLUGIN_HANDLE, True, list_item)
+def post_url(url, start_index, size, query):
+    start_index = int(start_index)
+    size = int(size)
+
+    body = {"operationName":"SearchQuery","variables":{"from":start_index,"size":size,"query":query},"query":"query SearchQuery($query: String!, $from: Int = 0, $size: Int = 10) {\n  search(keyword: $query, from: $from, size: $size) {\n    hits\n    results {\n      type\n      mediaType\n      title\n      uri\n      brand {\n        name\n        __typename\n      }\n      image {\n        title\n        url\n        __typename\n      }\n      publishDate\n      __typename\n    }\n    __typename\n  }\n}\n"}
+
+    response = requests.post(url, data=json.dumps(body), headers=HEADERSJSON)
+    json_source = response.text
+    json_source = convertToUnicodeString(json_source)
+
+    # log("json_source", json_source)
+
+    return json_source
+
+
+def list_videos_json(json_source):
+    json_data = json.loads(json_source)
+
+    # log("json_data", json_data)
+
+    #{"data":
+    #   {"search":
+    #       {"hits":103,
+    #       "results":
+    #           {"type":"Article","mediaType":null,"title":"#TBT: Heinek'n","uri":"/101barz/artikelen/tbt-heinekn",
+    #           "brand":{"name":"101Barz","__typename":"Brand"},
+    #           "image":{"title":"Heinie WS","url":"https://d2hpleul9br1of.cloudfront.net/
+    #           {format}/s3-101barz/09f92cab-6557-45c6-9323-545529a1fdd1.jpg","__typename":"PlayerImage"},
+    #           "publishDate":"2020-01-16T13:00:00.000Z","__typename":"SearchResultItem"},
+    #
+    #          {"type":"Video","mediaType":"Clip","title":"#TBT: Matthijs danst als Michael Jackson",
+    for item in json_data['data']['search']['results']:
+
+        log("item", item)
+
+        title = item['title']
+
+        video_page_url = item['uri']
+        video_page_url = BASEURL + video_page_url
+
+        thumbnail_image_url = item['image']['url']
+        # set the format of the thumbnail
+        thumbnail_image_url = convertToUnicodeString(thumbnail_image_url).replace("{format}","w_500,h_500")
+
+        date = item['publishDate']
+
+        log("date", date)
+        log("title", title)
+        log("thumbnail_image_url", thumbnail_image_url)
+        log("video_page_url", video_page_url)
+
+        desc = ""
+        length = ""
+
+        add_link(title, video_page_url, 'play_video', thumbnail_image_url, date + "\n" + desc, length)
+
+    xbmcplugin.endOfDirectory(PLUGIN_HANDLE)
+
+
+def play_video(video_page_url):
+    html_source = get_url(video_page_url)
+
+    # YoutubeEmbed","url":"https://www.youtube.com/embed/R1W-BLz7AXY"},"Article:b6ac6bca
+    search_term = "https://www.youtube.com/embed/"
+    html_source_unicode_string = convertToUnicodeString(html_source)
+    start_pos_search_term = html_source_unicode_string.find(search_term)
+    youtube_id = ""
+    if start_pos_search_term >= 0:
+        end_pos = html_source_unicode_string.find('"', start_pos_search_term)
+        youtube_id = html_source_unicode_string[start_pos_search_term + len(search_term):end_pos]
+        youtube_url = 'plugin://plugin.video.youtube/play/?video_id=%s' % youtube_id
+
+        log("youtube_url", youtube_url)
+
+    if youtube_id == "":
+        show_dialog(LANGUAGE(30611),LANGUAGE(30612))
+    else:
+        list_item = xbmcgui.ListItem(path=youtube_url)
+        return xbmcplugin.setResolvedUrl(PLUGIN_HANDLE, True, list_item)
 
 
 def show_dialog(title, text):
@@ -118,15 +194,10 @@ def show_dialog(title, text):
     dialog.ok(title, text)
 
 
-def get_url(url):
-    if convertToUnicodeString(url).find(BASE_URL) >= 0:
-        complete_url = url
-    else:
-        complete_url = BASE_URL + url
+def get_url(video_page_url):
+    log("video_page_url", video_page_url)
 
-    log("complete_url", complete_url)
-
-    response = requests.get(complete_url, headers=HEADERS, cookies=COOKIES)
+    response = requests.get(video_page_url, headers=HEADERS, cookies=COOKIES)
     html_source = response.text
     html_source = convertToUnicodeString(html_source)
 
@@ -148,7 +219,7 @@ def parameters_string_to_dict(parameters):
 
 
 def add_link(title, url, mode, thumbnail_image_url, desc="", duration=""):
-    u = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + convertToUnicodeString(mode)
+    u = sys.argv[0] + "?video_page_url=" + urllib.parse.quote_plus(url) + "&mode=" + convertToUnicodeString(mode)
     liz = xbmcgui.ListItem(label=title)
     liz.setArt({'thumb': thumbnail_image_url, 'icon': thumbnail_image_url})
     liz.setInfo(type="Video", infoLabels={"Title": title, "Plot": desc, "Duration": duration})
@@ -163,7 +234,7 @@ def add_link(title, url, mode, thumbnail_image_url, desc="", duration=""):
 
 
 def add_dir(title, url, mode, thumbnail_image_url, desc=""):
-    u = sys.argv[0] + '?url=' + urllib.parse.quote_plus(url) + '&mode=' + convertToUnicodeString(mode)
+    u = sys.argv[0] + '?video_page_url=' + urllib.parse.quote_plus(url) + '&mode=' + convertToUnicodeString(mode)
     liz = xbmcgui.ListItem(label=title)
     liz.setArt({'thumb': thumbnail_image_url, 'icon': thumbnail_image_url})
     liz.setInfo(type='video', infoLabels={'title': title, 'plot': desc, 'plotoutline': desc})
@@ -209,24 +280,27 @@ def log(name_object, object):
             ADDON, VERSION, DATE, name_object, "Unable to log the object due to an error while converting it to an unicode string"), xbmc.LOGDEBUG)
 
 
+def getSoup(html, default_parser="html5lib"):
+    soup = BeautifulSoup(html, default_parser)
+    return soup
+
+
 params = parameters_string_to_dict(sys.argv[2])
 mode = params.get('mode')
-url = params.get('url')
+url = params.get('video_page_url')
 
 if url is None:
-    if SETTINGS.getSettingBool('onlyshowallvideoscategory'):
-        mode = 'list_videos'
-        url = urllib.parse.quote_plus(BASE_URL + ALLESURL)
+    mode = 'list_videos'
+    url = urllib.parse.quote_plus(VIDEOURL)
 
-if mode == 'list_videos':
+if mode == 'search':
+    search()
+elif mode == 'list_videos':
     url = urllib.parse.unquote_plus(url)
     list_videos(url)
 elif mode == 'play_video':
     url = urllib.parse.unquote_plus(url)
     play_video(url)
-elif mode == 'search':
-    search(url)
 else:
     xbmc.log("[ADDON] %s debug mode, Python Version %s" % (ADDON, convertToUnicodeString(sys.version)), xbmc.LOGDEBUG)
     xbmc.log("[ADDON] %s v%s (%s) debug mode, is starting, ARGV = %s" % (ADDON, VERSION, DATE, repr(sys.argv)), xbmc.LOGDEBUG)
-    index()

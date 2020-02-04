f3546effdd469a0f5350daa6201421d153edd146
MohammadHosein Ammari <xmha97@gmail.com>
2020-02-04 03:54:52 -0800

    Fixed issue
    .me domain not working...

--- plugin.audio.navahang/main.py
+++ plugin.audio.navahang/main.py
@@ -43,7 +43,7 @@ ADDONVERSION = ADDON.getAddonInfo('version')
 CWD = ADDON.getAddonInfo('path')
 LANGUAGE = ADDON.getLocalizedString
 
-DataLink = 'http://navahang.me/webservice/GetMp3?type=Featured&from=0&to=50'
+DataLink = 'http://navahang.com/webservice/GetMp3?type=Featured&from=0&to=50'
 
 class LoadLister:
 

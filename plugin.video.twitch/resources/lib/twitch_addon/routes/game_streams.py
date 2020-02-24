d8e8d6ef8a2002f4a29cd9f54c8114c12923c0f3
anxdpanic <anxdpanic@gmail.com>
2020-02-24 13:14:56 -0500

    [plugin.video.twitch] 2.4.9

--- plugin.video.twitch/resources/lib/twitch_addon/routes/game_streams.py
+++ plugin.video.twitch/resources/lib/twitch_addon/routes/game_streams.py
@@ -22,14 +22,17 @@ def route(api, game, offset=0):
     utils.refresh_previews()
     kodi.set_view('videos', set_sort=True)
     per_page = utils.get_items_per_page()
-    streams = None
+
     all_items = list()
     requests = 0
-    while (per_page >= (len(all_items) + 1)) and (requests < MAX_REQUESTS):
+    total = 1000
+
+    while (per_page >= (len(all_items) + 1)) and (requests < MAX_REQUESTS) and (int(offset) <= 900):
         requests += 1
         languages = ','.join(utils.get_languages())
         streams = api.get_game_streams(game=game, offset=offset, limit=REQUEST_LIMIT, language=languages)
-        if (streams[Keys.TOTAL] > 0) and (Keys.STREAMS in streams):
+
+        if (total > 0) and (Keys.STREAMS in streams):
             filtered = \
                 blacklist_filter.by_type(streams, Keys.STREAMS, parent_keys=[Keys.CHANNEL], id_key=Keys._ID, list_type='user')
             last = None
@@ -42,16 +45,17 @@ def route(api, game, offset=0):
                 else:
                     break
             offset = utils.get_offset(offset, last, streams[Keys.STREAMS])
-            if (offset is None) or (streams[Keys.TOTAL] <= offset) or (streams[Keys.TOTAL] <= REQUEST_LIMIT):
+            if (offset is None) or (total <= offset) or (total <= REQUEST_LIMIT):
                 break
         else:
             break
+
     has_items = False
     if len(all_items) > 0:
         has_items = True
         for stream in all_items:
             kodi.create_item(converter.stream_to_listitem(stream))
-    if streams[Keys.TOTAL] > (offset + 1):
+    if (total - 100) > (int(offset) + 1) and (len(all_items) == per_page):
         has_items = True
         kodi.create_item(utils.link_to_next_page({'mode': MODES.GAMESTREAMS, 'game': game, 'offset': offset}))
     if has_items:

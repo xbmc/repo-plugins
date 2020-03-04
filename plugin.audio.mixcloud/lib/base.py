63ae87ad4febecd8064ac41fa6207b6a2262278b
jackyNIX <jackynix1@yahoo.com>
2020-03-04 21:43:20 +0100

    [plugin.audio.mixcloud] 3.0.0
          [new] python 3 migration
          [new] new logo, icons and other art

---
+++ plugin.audio.mixcloud/lib/base.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: jackyNIX
+
+Copyright (C) 2011-2020 jackyNIX
+
+This file is part of KODI Mixcloud Plugin.
+
+KODI Mixcloud Plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KODI Mixcloud Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with KODI Mixcloud Plugin.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+
+from .utils import Utils
+from .lang import Lang
+import sys
+import xbmc
+import xbmcplugin
+import xbmcgui
+
+
+
+# super class
+class BaseBuilder:
+
+    def __init__(self):
+        plugin_args = Utils.getArguments()
+        self.plugin_handle = int(sys.argv[1])
+        self.mode = plugin_args.get('mode', '')
+        self.key = plugin_args.get('key', '')
+        if plugin_args.get('offsetex', ''):
+            self.offset = [int(plugin_args.get('offset', '0')), int(plugin_args.get('offsetex', '0'))]
+        else:
+            self.offset = int(plugin_args.get('offset', '0'))
+        Utils.log('BaseBuilder.__init__(self = ' + self.__class__.__name__ + ', plugin_handle = ' + str(self.plugin_handle) + ', mode = ' + self.mode + ', key = ' + self.key + ', offset = ' + str(self.offset) + ')')
+
+    def execute(self):
+        Utils.log('BaseBuilder.execute()')
+        ret = self.build()
+        if ret != None:
+            xbmcplugin.endOfDirectory(handle = self.plugin_handle, succeeded = ret)
+
+    def build(self):
+        Utils.log('BaseBuilder.build()')
+        return True
+
+
+
+# super class for lists
+class BaseListBuilder(BaseBuilder):
+
+    def build(self):
+        Utils.log('BaseListBuilder.build()')
+        nextOffset = self.buildItems()
+        Utils.log('next offset: ' + str(nextOffset))
+        nextOffsetEx = None
+        if isinstance(nextOffset, list):
+            nextOffsetEx = nextOffset[1]
+            nextOffset = nextOffset[0]
+        if (nextOffset and (nextOffset > 0)) or ((nextOffsetEx != None) and (nextOffsetEx > 0)):
+            parameters = {'mode' : self.mode, 'key' : self.key, 'offset' : nextOffset}
+            if nextOffsetEx != None:
+                parameters['offsetex'] = nextOffsetEx
+            self.addFolderItem({'title' : Lang.MORE}, parameters)
+        return (nextOffset != -1)
+
+    def buildItems(self):
+        Utils.log('BaseListBuilder.buildItems()')
+        return 0
+
+    def addFolderItem(self, infolabels = {}, parameters = {}, img = '', contextmenuitems = []):
+        Utils.log('BaseListBuilder.addFolderItem(infolabels = ' + str(infolabels) + ', parameters = ' + str(parameters) + ', img = ' + img + ', contextmenuitems = ' + str(contextmenuitems) + ')')
+
+        listitem = xbmcgui.ListItem(infolabels['title'], infolabels['title'])
+        listitem.setArt({'icon' : img, 'thumb' : img})
+        listitem.setInfo('music', infolabels)
+
+        if contextmenuitems:
+            listitem.addContextMenuItems(contextmenuitems)
+
+        return xbmcplugin.addDirectoryItem(handle = self.plugin_handle, url = Utils.encodeArguments(parameters), listitem = listitem, isFolder = True)
+
+    def addAudioItem(self, infolabels = {}, parameters = {}, img = '', contextmenuitems = [], total = 0):
+        Utils.log('BaseListBuilder.addAudioItem(infolabels = ' + str(infolabels) + ', parameters = ' + str(parameters) + ', img = ' + img + ', contextmenuitems = ' + str(contextmenuitems) + ', total = ' + str(total) + ')')
+
+        listitem = xbmcgui.ListItem(infolabels['title'], infolabels['artist'])
+        listitem.setArt({'icon' : img, 'thumb' : img})
+        listitem.setInfo('music', infolabels)
+        listitem.setProperty('IsPlayable', 'true')
+
+        if contextmenuitems:
+            listitem.addContextMenuItems(contextmenuitems)
+
+        xbmcplugin.addDirectoryItem(handle = self.plugin_handle, url = Utils.encodeArguments(parameters), listitem = listitem, isFolder = False, totalItems = total)
+
+    def buildContextMenuItems(self, item):
+        contextMenuItems = []
+
+        if item.favorited == False:
+            contextMenuItems.append((Lang.ADD_TO_FAVORITES, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'post', 'key' : item.key + 'favorite/'}) + ')'))
+        elif item.favorited == True:
+            contextMenuItems.append((Lang.REMOVE_FROM_FAVORITES, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'delete', 'key' : item.key + 'favorite/'}) + ')'))
+
+        if item.listenlater == False:
+            contextMenuItems.append((Lang.ADD_TO_LISTEN_LATER, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'post', 'key' : item.key + 'listen-later/'}) + ')'))
+        elif item.listenlater == True:
+            contextMenuItems.append((Lang.REMOVE_FROM_LISTEN_LATER, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'delete', 'key' : item.key + 'listen-later/'}) + ')'))
+
+        userKey = item.user
+        if not userKey:
+            userKey = item.key
+
+        if item.following == False:
+            contextMenuItems.append((Lang.ADD_TO_FOLLOWINGS, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'post', 'key' : userKey + 'follow/'}) + ')'))
+        elif item.following == True:
+            contextMenuItems.append((Lang.REMOVE_FROM_FOLLOWINGS, 'XBMC.RunPlugin(' + Utils.encodeArguments({'mode' : 'delete', 'key' : userKey + 'follow/'}) + ')'))
+
+        # fake menu separator
+        # I do hope one day kodi will support menu separators
+        if len(contextMenuItems) > 0:
+            contextMenuItems.append(('----------------------------------------', ''))
+
+        return contextMenuItems
+
+
+
+# super class for user queries
+class QueryListBuilder(BaseListBuilder):
+
+    def buildItems(self):
+        query = self.key
+        if not query:
+            keyboard = xbmc.Keyboard(query)
+            keyboard.doModal()
+            if keyboard.isConfirmed():
+                query = keyboard.getText()
+        if query:
+            return self.buildQueryItems(query)
+        return -1
+    
+    def buildQueryItems(self, query):
+        Utils.log('QueryListBuilder.buildQueryItems(' + query + ')')
+        return 0
+
+
+
+
+
+class BaseList:
+
+    def __init__(self):
+        self.items = []
+        self.nextOffset = 0
+
+    def initTrackNumbers(self, offset):
+        index = offset
+        for item in self.items:
+            index += 1
+            item.infolabels['tracknumber'] = index
+            item.infolabels['count'] = index
+
+    def merge(self, baseLists = []):
+        listCount = len(baseLists)
+        Utils.log('merge lists: ' + str(listCount))
+        maxItems = int(Utils.getSetting('page_limit'))
+        index = []
+        count = []
+        curItems = []
+        for baseList in baseLists:
+            index.append(0)
+            count.append(len(baseList.items))
+            curItems.append(None)
+
+        for iMerged in range(maxItems):
+            # user aborted
+            if xbmc.Monitor().abortRequested():
+                break
+                
+            for iList in range(listCount):
+                if index[iList] < count[iList]:
+                    curItems[iList] = baseLists[iList].items[index[iList]]
+                else:
+                    curItems[iList] = None
+
+            iAdd = -1
+            for iList in range(listCount):
+                if curItems[iList]:
+                    if (iAdd == -1) or ((not curItems[iAdd].timestamp) and (curItems[iList])) or ((curItems[iAdd].timestamp) and (curItems[iList].timestamp) and (curItems[iList].timestamp > curItems[iAdd].timestamp)):
+                        iAdd = iList
+                    
+            if iAdd != -1:
+                self.items.append(curItems[iAdd])
+                index[iAdd] = index[iAdd] + 1
+            else:
+                break
+
+        Utils.log('merged result: ' + str(len(self.items)) + ' ' + str(self.items))
+        Utils.log('nextoffset: ' + str(index))
+        self.nextOffset = index
+
+    # limit list
+    def trim(self):
+        maxItems = int(Utils.getSetting('page_limit'))
+        while len(self.items) > maxItems:
+            self.items.pop()
+
+
+
+class BaseListItem:
+
+    def __init__(self):
+        self.key = None
+        self.user = None
+        self.image = None
+        self.timestamp = None
+        self.favorited = None
+        self.listenlater = None
+        self.following = None
+        self.infolabels = {}
+
+    def setKey(self, sourceData, sourceKey):
+        if sourceKey in sourceData and sourceData[sourceKey]:
+            self.key = sourceData[sourceKey]
+        else:
+            self.key = None
+        return self.key
+
+    def setUser(self, sourceData, sourceKey):
+        if sourceKey in sourceData and sourceData[sourceKey]:
+            self.user = sourceData[sourceKey]
+        else:
+            self.user = None
+        return self.user
+
+    def setImage(self, sourceData, sourceKey):
+        if sourceKey in sourceData and sourceData[sourceKey]:
+            self.image = sourceData[sourceKey]
+        else:
+            self.image = None
+        return self.image
+
+    def setTimestamp(self, sourceData, sourceKey):
+        if sourceKey in sourceData and sourceData[sourceKey]:
+            self.timestamp = sourceData[sourceKey]
+        else:
+            self.timestamp = None
+        return self.timestamp
+
+    def setFavorited(self, sourceData, sourceKey):
+        if sourceKey in sourceData:
+            self.favorited = sourceData[sourceKey]
+        else:
+            self.favorited = None
+        return self.favorited
+
+    def setListenLater(self, sourceData, sourceKey):
+        if sourceKey in sourceData:
+            self.listenlater = sourceData[sourceKey]
+        else:
+            self.listenlater = None
+        return self.listenlater
+
+    def setFollowing(self, sourceData, sourceKey):
+        if sourceKey in sourceData:
+            self.following = sourceData[sourceKey]
+        else:
+            self.following = None
+        return self.following
+
+    def __repr__(self):
+        return  'BaseListItem(key: ' + str(self.key) + ', user: ' + str(self.user) + ', image: ' + str(self.image) + ', timestamp: ' + str(self.timestamp) + ', favorited: ' + str(self.favorited) + ', listen-later: ' + str(self.listenlater) + ', following: ' + str(self.following) + ', infolabels: ' + str(self.infolabels) + ')'
---
+++ plugin.audio.mixcloud/lib/history.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: jackyNIX
+
+Copyright (C) 2011-2020 jackyNIX
+
+This file is part of KODI Mixcloud Plugin.
+
+KODI Mixcloud Plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KODI Mixcloud Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with KODI Mixcloud Plugin.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+
+import os
+import sys
+import json
+from datetime import datetime
+import xbmc
+import xbmcaddon
+from .utils import Utils 
+
+
+
+# variables
+__addon__ = xbmcaddon.Addon('plugin.audio.mixcloud')
+CACHED_HISTORY = {}
+
+
+
+class History:
+
+    def __init__(self, name):
+        self.name = name
+        self.data = []
+        self.readFile()
+
+    def readFile(self):
+        starttime = datetime.now()
+        self.data = []
+        filepath = xbmc.translatePath(__addon__.getAddonInfo('profile')) + self.name + '.json'
+        Utils.log('reading json file: ' + filepath)
+        try:
+            # read file
+            if os.path.exists(filepath):
+                text_file = open(filepath, 'r')
+                self.data = json.loads(text_file.read())
+                text_file.close()
+                self.trim()
+            elif __addon__.getSetting(self.name+'_list'):
+                # convert old 2.4.x settings
+                list_data = __addon__.getSetting(self.name + '_list').split(', ')
+                for list_entry in list_data:
+                    json_entry = {}
+                    list_fields = list_entry.split('=')
+                    for list_field in list_fields:
+                        if len(json_entry) == 0:
+                            json_entry['key'] = list_field
+                        elif len(json_entry) == 1:
+                            json_entry['value'] = list_field
+                    self.data.append(json_entry)
+                    self.trim()
+                Utils.log('convert old 2.4.x settings: ' + self.name + ' -> ' + json.dumps(self.data))
+                self.writeFile()
+                __addon__.setSetting(self.name + '_list', None)
+
+        except Exception as e:
+            Utils.log('unable to read json file: ' + filepath, e)
+        elapsedtime = datetime.now() - starttime
+        Utils.log('read ' + str(len(self.data)) + ' items in ' + str(elapsedtime.seconds) + '.' + str(elapsedtime.microseconds) + ' seconds')
+        return self.data
+        
+    def writeFile(self):
+        filepath = xbmc.translatePath(__addon__.getAddonInfo('profile')) + self.name + '.json'
+        try:
+            text_file = open(filepath, 'w+')
+            text_file.write(json.dumps(self.data, indent = 4 * ' '))
+            text_file.close()
+        except Exception as e:
+            Utils.log('unable to write json file: ' + filepath, e)
+
+    # add data and write file
+    def add(self, json_entry = {}):
+        try:
+            json_entry['timestamp'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+            self.data.insert(0, json_entry)
+            self.trim()
+            self.writeFile()
+        except Exception as e:
+            Utils.log('unable to add to json', e)
+
+    # limit list
+    def trim(self):
+        json_max = 1
+        if __addon__.getSetting(self.name + '_max'):
+            json_max = int(__addon__.getSetting(self.name + '_max'))
+        while len(self.data) > json_max:
+            # user aborted
+            if xbmc.Monitor().abortRequested():
+                break
+                
+            self.data.pop()
+
+    # clear list
+    def clear(self):
+        Utils.log('clear json sfile')
+        self.data = []
+
+    @staticmethod
+    def getHistory(name):
+        history = CACHED_HISTORY.get(name)
+        if not history:
+            history = History(name)
+            CACHED_HISTORY[name] = history
+        return history

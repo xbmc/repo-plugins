964330d174b49cf51ad503cd656709a6ea055473
learningit <scottyroscoe13@gmail.com>
2020-03-24 20:32:09 -0400

    plugin.video.diy 3.0.11 (#2769)

--- plugin.video.diy/resources/lib/scraper.py
+++ plugin.video.diy/resources/lib/scraper.py
@@ -17,14 +17,12 @@ h = HTMLParser.HTMLParser()
 qp  = urllib.quote_plus
 uqp = urllib.unquote_plus
 UTF8     = 'utf-8'
-DIYBASE  = 'http://www.diynetwork.com%s'
 
 class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
-     addonLanguage  = self.addon.getLocalizedString
-     html = self.getRequest('http://www.diynetwork.com/shows/full-episodes')
-     html = re.compile('<div class="capsule editorialPromo parbase section">(.+?)<div class="container-aside">', re.DOTALL).search(html).group(1)
+     html = self.getRequest('https://www.diynetwork.com/shows/full-episodes')
+     html = re.compile('<section class="o-Capsule o-EditorialPromo(.+?)<div class="container-aside">', re.DOTALL).search(html).group(1)
      a = re.compile('m-MediaBlock--PLAYLIST">.+?href="(.+?)".+?data-src="(.+?)".+?HeadlineText.+?>(.+?)<.+?AssetInfo">(.+?)<', re.IGNORECASE | re.DOTALL).findall(html)
      fanart = self.addonFanart
      for i, (url, thumb, name, vidcnt) in list(enumerate(a[1:], start=1)):
@@ -41,15 +39,19 @@ class myAddon(t1mAddon):
 
        html = self.getRequest(url.rsplit('/',1)[0])
        plot = re.compile('"og:description" content="(.+?)"',re.DOTALL).search(html)
-       if plot is not None: plot = plot.group(1)
-       else: plot = name
+       if plot is not None:
+          plot = plot.group(1)
+       else:
+          plot = name
        infoList['TVShowTitle'] = name
-       infoList['Title']       = name
-       infoList['Studio']      = 'DIY'
-       infoList['Genre']       = ''
+       infoList['Title'] = name
+       infoList['Studio'] = 'DIY'
+       infoList['Genre'] = ''
        epinum = vidcnt.split(' ',1)[0]
-       if epinum.isdigit():    infoList['Episode'] = int(epinum)
-       else: infoList['Episode'] = 0
+       if epinum.isdigit():
+          infoList['Episode'] = int(epinum)
+       else: 
+          infoList['Episode'] = 0
        infoList['Plot'] = h.unescape(plot)
        infoList['mediatype'] = 'tvshow'
        ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True)
@@ -57,41 +59,40 @@ class myAddon(t1mAddon):
 
 
   def getAddonEpisodes(self,url,ilist):
-        addonLanguage  = self.addon.getLocalizedString
-        self.defaultVidStream['width']  = 1280
-        self.defaultVidStream['height'] = 720
         url = uqp(url)
-        if not url.startswith('http:'):
-            url = 'http:'+url
+        if not url.startswith('http'):
+            url = 'https:'+url
         showName = xbmc.getInfoLabel('ListItem.Title')
-        html  = self.getRequest(url)
-        html    = re.compile('<div id="video-player".+?type="text/x-config">(.+?)</script>',re.DOTALL).search(html)
+        html = self.getRequest(url)
+        html = re.compile('<div id="video-player".+?type="text/x-config">(.+?)</script>',re.DOTALL).search(html)
         if html != None:
               a = json.loads(html.group(1))
         else:
-              xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( self.addonName, addonLanguage(30011) , 5000) )
+#              xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( self.addonName, addonLanguage(30011) , 5000) )
               return(ilist)
         vids = a['channels'][0]['videos']
 
         for i,b in list(enumerate(vids, start=1)):
-           url     = b['releaseUrl']
-           dirty = True
-           name    = h.unescape(b['title'])
-           fanart  = 'http://www.hgtv.com%s' % b['thumbnailUrl']
-           thumb   = 'http://www.hgtv.com%s' % b['thumbnailUrl']
+           url = b['releaseUrl']
+           name = h.unescape(b['title'])
+           fanart = 'http://www.hgtv.com%s' % b['thumbnailUrl']
+           thumb = 'http://www.hgtv.com%s' % b['thumbnailUrl']
 
            infoList = {}
-           infoList['Duration']    = b['length']
-           infoList['Title']       = name
-           infoList['Studio']      = b['publisherId']
+           infoList['Duration'] = b['length']
+           infoList['Title'] = name
+           infoList['Studio'] = b['publisherId']
            html = self.getRequest(url)
            mpaa = re.compile('ratings="(.+?)"',re.DOTALL).search(html)
-           if mpaa is not None: infoList['MPAA'] = mpaa.group(1).split(':',1)[1]
+           if mpaa is not None:
+              infoList['MPAA'] = mpaa.group(1).split(':',1)[1]
            epinum = re.compile('"episodeNumber" value=".(.+?)H"',re.DOTALL).search(html)
-           if epinum is not None: infoList['Episode'] = int(epinum.group(1).replace('M',''), 16)
+           if epinum is not None:
+              infoList['Episode'] = int(epinum.group(1).replace('M',''), 16)
            seanum  = re.compile('"episodeNumber" value="(.+?)H"',re.DOTALL).search(html)
-           if seanum is not None: infoList['Season']  = int(seanum.group(1).replace('M','').replace('S',''),16)/256
-           infoList['Plot']        = h.unescape(b["description"])
+           if seanum is not None:
+              infoList['Season'] = int(seanum.group(1).replace('M','').replace('S',''),16)/256
+           infoList['Plot'] = h.unescape(b["description"])
            infoList['TVShowTitle'] = showName
            infoList['mediatype'] = 'episode'
            ilist = self.addMenuItem(name,'GV', ilist, url, thumb, fanart, infoList, isFolder=False)
@@ -100,8 +101,8 @@ class myAddon(t1mAddon):
 
 
   def getAddonVideo(self,url):
-   html   = self.getRequest(uqp(url))
-   suburl =''
+   html = self.getRequest(uqp(url))
+   suburl = ''
    subs = re.compile('<textstream src="(.+?)"',re.DOTALL).findall(html)
    for st in subs:
       if '.srt' in st:
@@ -116,7 +117,8 @@ class myAddon(t1mAddon):
      xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s)' % ( self.addonName, msg , 5000) )
      return
    liz = xbmcgui.ListItem(path = url)
-   if suburl != "" : liz.setSubtitles([suburl])
+   if suburl != "" :
+      liz.setSubtitles([suburl])
    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
 
 

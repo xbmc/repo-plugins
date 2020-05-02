06d10366048d43de1fc95f755b904d5f574c4b05
Michaël Arnauts <michael.arnauts@destiny.be>
2020-05-02 09:58:13 +0200

    [plugin.video.vtm.go] 1.0.2

--- plugin.video.vtm.go/resources/lib/vtmgo/vtmgoauth.py
+++ plugin.video.vtm.go/resources/lib/vtmgo/vtmgoauth.py
@@ -4,8 +4,8 @@
 from __future__ import absolute_import, division, unicode_literals
 
 import hashlib
-import json
-import random
+import re
+
 import requests
 
 from resources.lib.kodiwrapper import LOG_DEBUG, from_unicode, LOG_INFO
@@ -34,8 +34,6 @@ class VtmGoAuth:
         self._proxies = kodi.get_proxies()
 
         self._token = None
-        # self._name = None
-        # self._accountId = None
 
     def has_credentials_changed(self):
         """ Check if credentials have changed """
@@ -106,78 +104,52 @@ class VtmGoAuth:
         # Create new session object. This keeps the cookies across requests.
         session = requests.sessions.session()
 
-        # Get login form. This sets some cookies we need.
-        session.get('https://login.persgroep.net/authorize', params={
-            'redirect_uri': 'vtmgo://callback/oidc',
-            'client_id': 'vtm-go-android',
-            'response_type': 'code',
-            'state': self._generate_random_id(22),
-            'nonce': self._generate_random_id(22),
-            'scope': 'openid profile'
-        }, headers={
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36'
+        # Yes, we have accepted the cookies
+        session.cookies.set('pws', 'functional|analytics|content_recommendation|targeted_advertising|social_media')
+        session.cookies.set('pwv', '1')
+
+        # Start login flow
+        response = session.get('https://vtm.be/vtmgo/aanmelden?redirectUrl=https://vtm.be/vtmgo', proxies=self._proxies)
+        response.raise_for_status()
+
+        # Send login credentials
+        response = session.post('https://login2.vtm.be/login/emailfirst/password?client_id=vtm-go-web', data={
+            'userName': self._kodi.get_setting('username'),
+            'password': self._kodi.get_setting('password'),
+            'jsEnabled': 'true',
         }, proxies=self._proxies)
+        response.raise_for_status()
 
-        # Now, send the login details. We will be redirected to vtmgo:// when we succeed. We then can extract an authorizationCode that we need to continue.
-        try:
-            response = session.post('https://login2.vtm.be/login/emailfirst/password?client_id=vtm-go-android', data={
-                'userName': self._kodi.get_setting('username'),
-                'password': self._kodi.get_setting('password'),
-                'jsEnabled': 'true',
-            }, proxies=self._proxies)
+        if 'errorBlock-OIDC-004' in response.text:  # E-mailadres is niet gekend.
+            raise InvalidLoginException()
 
-            if 'errorBlock-OIDC-004' in response.text:  # E-mailadres is niet gekend.
-                raise InvalidLoginException()
+        if 'errorBlock-OIDC-003' in response.text:  # Wachtwoord is niet correct.
+            raise InvalidLoginException()
 
-            if 'errorBlock-OIDC-003' in response.text:  # Wachtwoord is niet correct.
-                raise InvalidLoginException()
+        if 'OIDC-999' in response.text:  # Ongeldige login.
+            raise InvalidLoginException()
 
-            if 'OIDC-999' in response.text:  # Ongeldige login.
-                raise InvalidLoginException()
+        # Extract state and code
+        matches_state = re.search(r'name="state" value="([^"]+)', response.text)
+        if matches_state:
+            state = matches_state.group(1)
+        else:
+            raise LoginErrorException(code=101)  # Could not extract authentication code
 
-            raise LoginErrorException(code=100)  # Unknown error while logging in
+        matches_code = re.search(r'name="code" value="([^"]+)', response.text)
+        if matches_code:
+            code = matches_code.group(1)
+        else:
+            raise LoginErrorException(code=101)  # Could not extract authentication code
 
-        except requests.exceptions.InvalidSchema as exc:
-            # We get back an url like this: vtmgo://callback/oidc?state=yyyyyyyyyyyyyyyyyyyyyy&code=xxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxx
-            # I found no other way to get this url then by parsing the Exception message. :(
-            import re
-            matches = re.search(r"code=([^']+)", str(exc))
-            if matches:
-                code = matches.group(1)
-            else:
-                raise LoginErrorException(code=101)  # Could not extract authentication code
-
-        # Okay, final stage. We now need to use our authorizationCode to get a valid JWT.
-        response = session.post('https://lfvp-api.dpgmedia.net/authorize', json={
-            'authorizationCode': code,
-            'authorizationCodeCallbackUrl': 'vtmgo://callback/oidc',
-            'clientId': 'vtm-go-android',
-        }, headers={
-            'x-app-version': '8',
-            'x-persgroep-mobile-app': 'true',
-            'x-persgroep-os': 'android',
-            'x-persgroep-os-version': '23',
+        # Okay, final stage. We now need to POST our state and code to get a valid JWT.
+        response = session.post('https://vtm.be/vtmgo/login-callback', data={
+            'state': state,
+            'code': code,
         }, proxies=self._proxies)
+        response.raise_for_status()
 
-        if response.status_code == 426:
-            raise LoginErrorException(code=102)  # Update required
-
-        if response.status_code not in [200, 204]:
-            raise Exception('Error %s.' % response.status_code)
-
-        tokens = json.loads(response.text)
-
-        self._token = tokens.get('jsonWebToken')
-        # self._name = tokens.get('name')
-        # self._accountId = tokens.get('accountId')
+        # Get JWT from cookies
+        self._token = session.cookies.get('lfvp_auth')
 
         return self._token
-
-    @staticmethod
-    def _generate_random_id(length=32):
-        """ Generate a random id of the specified length.
-        :type length: int
-        :rtype str
-        """
-        letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-        return ''.join(random.choice(letters) for i in range(length))

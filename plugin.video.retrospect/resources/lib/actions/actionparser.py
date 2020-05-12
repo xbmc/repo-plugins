e17661030b221052306b65238cbc2a844b47055e
Bas Rieter <basrieter@gmail.com>
2020-05-12 21:13:57 +0200

    [plugin.video.retrospect] v5.2.4+matrix.1

--- plugin.video.retrospect/resources/lib/actions/actionparser.py
+++ plugin.video.retrospect/resources/lib/actions/actionparser.py
@@ -11,6 +11,54 @@ from resources.lib.mediaitem import MediaItem
 
 
 class ActionParser(object):
+    # Mapping from old to new
+    __lookup = {
+        "chn_nos2010": "channel.nos.nos2010",
+        "chn_een": "channel.be.een",
+        "chn_ketnet": "channel.be.ketnet",
+        "chn_rtbf": "channel.be.rtbf",
+        "chn_sporza": "channel.be.sporza",
+        "chn_vier": "channel.be.vier",
+        "chn_vrtnu": "channel.be.vrtnu",
+        "chn_vtm": "channel.be.vtm",
+        "chn_vtmbe": "channel.be.vtmbe",
+        "chn_srf": "channel.de.srf",
+        "chn_tvse": "channel.mtg.tvse",
+        "chn_viafree": "channel.mtg.viafree",
+        "chn_mtvnl": "channel.mtv.mtvnl",
+        "chn_southpark": "channel.mtv.southpark",
+        "chn_nickelodeon": "channel.nick.nickelodeon",
+        "chn_nickjr": "channel.nick.nickjr",
+        "chn_nrkno": "channel.no.nrkno",
+        "chn_nosnl": "channel.nos.nosnl",
+        "chn_schooltv": "channel.nos.schooltv",
+        "chn_at5": "channel.regionalnl.at5",
+        "chn_flevo": "channel.regionalnl.flevo",
+        "chn_gelderland": "channel.regionalnl.gelderland",
+        "chn_l1": "channel.regionalnl.l1",
+        "chn_lokaal": "channel.regionalnl.lokaal",
+        "chn_rpoapp": "channel.regionalnl.rpoapp",
+        "chn_rtl": "channel.rtlnl.rtl",
+        "chn_kijknl": "channel.sbsnl.kijknl",
+        "chn_oppetarkiv": "channel.se.oppetarkiv",
+        "chn_sbs": "channel.se.sbs",
+        "chn_svt": "channel.se.svt",
+        "chn_tv4se": "channel.se.tv4se",
+        "chn_urplay": "channel.se.urplay",
+        "chn_24classic": "channel.streams.24classic",
+        "chn_radio538": "channel.streams.radio538",
+        "chn_bbc": "channel.uk.bbc",
+        "chn_amt": "channel.videos.amt",
+        "chn_channel9": "channel.videos.channel9",
+        "chn_dumpert": "channel.videos.dumpert",
+        "chn_eredivisie": "channel.videos.eredivisie",
+        "chn_extreme": "channel.videos.extreme",
+        "chn_hardwareinfo": "channel.videos.hardwareinfo",
+        "chn_ons": "channel.videos.ons",
+        "chn_pathenl": "channel.videos.pathenl",
+        "chn_twit": "channel.videos.twit"
+    }
+
     def __init__(self, addon_name, handle, params):
         """
 
@@ -89,9 +137,7 @@ class ActionParser(object):
 
         params = dict()
         if channel:
-            params[keyword.CHANNEL] = channel.moduleName
-            if channel.channelCode:
-                params[keyword.CHANNEL_CODE] = channel.channelCode
+            params[keyword.CHANNEL] = channel.url_id
 
         params[keyword.ACTION] = action
 
@@ -151,6 +197,13 @@ class ActionParser(object):
                 result[k] = v
 
             # if the channelcode was empty, it was stripped, add it again.
+            if keyword.CHANNEL in result and "-" in result[keyword.CHANNEL]:
+                id_parts = result[keyword.CHANNEL].split("-")
+                result[keyword.CHANNEL] = id_parts[0]
+                result[keyword.CHANNEL_CODE] = id_parts[1]
+            elif keyword.CHANNEL in result and "." not in result[keyword.CHANNEL]:
+                result[keyword.CHANNEL] = ActionParser.__lookup[result[keyword.CHANNEL]]
+
             if keyword.CHANNEL_CODE not in result:
                 Logger.debug("Adding ChannelCode=None as it was missing from the dict: %s", result)
                 result[keyword.CHANNEL_CODE] = None

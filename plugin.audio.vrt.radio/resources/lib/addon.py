8b7afb5151d990efa205653032d1ccf6ee2fe888
Dag Wieers <dag@wieers.com>
2020-06-17 03:12:59 +0200

    [plugin.audio.vrt.radio] 0.1.3
    v0.1.3 (2020-06-17)
    - Small fix to IPTV Manager support
    
    v0.1.2 (2020-06-02)
    - Switch from MP3 to AAC by default
    - Added channel Radio 2 Bene Bene
    - Added channel StuBru Hooray
    - Added channel StuBru Bruut
    - Added support for IPTV Manager

--- plugin.audio.vrt.radio/resources/lib/addon.py
+++ plugin.audio.vrt.radio/resources/lib/addon.py
@@ -5,18 +5,19 @@
 ''' This is the actual VRT Radio audio plugin entry point '''
 
 from __future__ import absolute_import, division, unicode_literals
-
-from channels import CHANNELS
 import routing
+from data import CHANNELS
+
 from xbmc import getCondVisibility
 from xbmcgui import ListItem
 from xbmcplugin import addDirectoryItems, addSortMethod, endOfDirectory, setResolvedUrl, SORT_METHOD_LABEL, SORT_METHOD_UNSORTED
 
-plugin = routing.Plugin()
+plugin = routing.Plugin()  # pylint: disable=invalid-name
 
 
 @plugin.route('/')
 def main_menu(path=''):
+    """The VRT Radio plugin main menu"""
 
     has_white_icons = getCondVisibility('System.HasAddon("resource.images.studios.white")') == 1
     if has_white_icons:
@@ -26,16 +27,17 @@ def main_menu(path=''):
         icon_path = 'DefaultAddonMusic.png'
         thumb_path = 'DefaultAddonMusic.png'
 
-    has_coloured_icons = getCondVisibility('System.HasAddon("resource.images.studios.coloured")') == 1
-    if has_coloured_icons:
-        thumb_path = 'resource://resource.images.studios.coloured/%(studio)s.png'
+    # NOTE: Wait for resource.images.studios.coloured v0.16 to be released
+    # has_coloured_icons = getCondVisibility('System.HasAddon("resource.images.studios.coloured")') == 1
+    # if has_coloured_icons:
+    #     thumb_path = 'resource://resource.images.studios.coloured/%(studio)s.png'
 
     radio_items = []
     for channel in CHANNELS:
         icon = icon_path % channel
         thumb = thumb_path % channel
         # url = plugin.url_for(play, name=channel.get('name'))
-        url = channel.get('mp3_128')
+        url = channel.get('aac_128')
 
         item = ListItem(
             label=channel.get('label'),
@@ -44,7 +46,7 @@ def main_menu(path=''):
             thumbnailImage=thumb,
             path=url,
         )
-        item.setArt(dict(thumb=thumb, icon=icon, fanart=icon))
+        item.setArt(dict(thumb=channel.get('logo'), icon=icon, fanart=icon))
         item.setInfo(type='video', infoLabels=dict(
             mediatype='music',
             plot='[B]%(label)s[/B]\n[I]%(tagline)s[/I]\n\n[COLOR yellow]%(website)s[/COLOR]' % channel,
@@ -66,5 +68,22 @@ def play(name):
     setResolvedUrl(plugin.handle, True, listitem=ListItem(path=stream))
 
 
-if __name__ == '__main__':
-    plugin.run()
+@plugin.route('/iptv/channels')
+def iptv_channels():
+    """Return JSON-M3U formatted data for all live channels"""
+    from iptvmanager import IPTVManager
+    port = int(plugin.args.get('port')[0])
+    IPTVManager(port).send_channels()
+
+
+@plugin.route('/iptv/epg')
+def iptv_epg():
+    """Return JSON-M3U formatted data for all live channels"""
+    from iptvmanager import IPTVManager
+    port = int(plugin.args.get('port')[0])
+    IPTVManager(port).send_epg()
+
+
+def run(argv):
+    ''' Addon entry point from wrapper '''
+    plugin.run(argv)

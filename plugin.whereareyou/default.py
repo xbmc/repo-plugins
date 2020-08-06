61957e1d3d61f4db27a20deadfc72512dd464ba8
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-08-06 12:25:37 -1000

    [plugin.whereareyou] 0.4.0
    - added ability to swithc to external apps using Harmony Hub
    - fix for looping dialog box if called from home window

--- plugin.whereareyou/default.py
+++ plugin.whereareyou/default.py
@@ -1,12 +1,4 @@
-from kodi_six import xbmc, xbmcaddon
 from resources.lib.whereareyou import Main
 
-addon        = xbmcaddon.Addon()
-addonversion = addon.getAddonInfo('version')
-preamble     = '[Where Are You]'
-
-
 if ( __name__ == "__main__" ):
-    xbmc.log( '%s script version %s started' % (preamble, addonversion), xbmc.LOGINFO )
-    action = Main()
-xbmc.log( '%s script stopped' % preamble, xbmc.LOGINFO )
+    Main()

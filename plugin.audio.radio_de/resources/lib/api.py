bc95604afec0bd5492bd5073a7d573c0ebce3980
enenbot <estagio.dei@gmail.com>
2020-12-08 14:56:34 +0000

    [plugin.audio.radio_de] 3.0.5+matrix.0 (#3247)
    Co-authored-by: enenbot <92enen@gmail.com>

--- plugin.audio.radio_de/resources/lib/api.py
+++ plugin.audio.radio_de/resources/lib/api.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#     Copyright (C) 2012 Tristan Fischer (sphere@dersphere.de)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+'''
+ *  Copyright (C) 2019- enen92 (enen92@kodi.tv)
+ *  Copyright (C) 2012-2019 Tristan Fischer (sphere@dersphere.de)
+ *  This file is part of plugin.audio.radio_de
+ *
+ *  SPDX-License-Identifier: GPL-2.0-only
+ *  See LICENSE.txt for more information.
+'''
+
 import json
 import sys
 import random

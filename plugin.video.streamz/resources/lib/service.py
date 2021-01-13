78bb0c8676b34e31047952cf2cd1658bfbbd55ae
michaelarnauts <michael.arnauts@gmail.com>
2021-01-13 15:49:18 +0000

    [plugin.video.streamz] 1.0.4

--- plugin.video.streamz/resources/lib/service.py
+++ plugin.video.streamz/resources/lib/service.py
@@ -12,7 +12,6 @@ from resources.lib import kodilogging, kodiutils
 from resources.lib.modules.proxy import Proxy
 from resources.lib.streamz.exceptions import NoLoginException
 
-kodilogging.config()
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -80,4 +79,5 @@ class BackgroundService(Monitor):
 
 def run():
     """ Run the BackgroundService """
+    kodilogging.config()
     BackgroundService().run()

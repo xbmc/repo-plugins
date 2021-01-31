db0337ff7d74fe73553c29a927153041961beac0
fossdd <fossdd@tutanota.com>
2021-01-31 18:20:38 +0000

    [plugin.audio.weareone] v1.0.1 - Download Stream-List for URLs and Names - Change quality in Settings

--- plugin.audio.weareone/resources/lib/plugin.py
+++ plugin.audio.weareone/resources/lib/plugin.py
@@ -8,6 +8,8 @@ from resources.lib import kodilogging
 from xbmcgui import ListItem
 from xbmcplugin import addDirectoryItem, endOfDirectory
 
+import get_streams
+
 
 ADDON = xbmcaddon.Addon()
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
@@ -17,14 +19,18 @@ plugin = routing.Plugin()
 
 @plugin.route('/')
 def index():
-    addDirectoryItem(plugin.handle, "http://listen.technobase.fm/mp3", ListItem("TechnoBase.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.housetime.fm/mp3", ListItem("HouseTime.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.hardbase.fm/mp3", ListItem("HardBase.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.trancebase.fm/mp3", ListItem("TranceBase.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.coretime.fm/mp3", ListItem("CoreTime.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.clubtime.fm/mp3", ListItem("ClubTime.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.teatime.fm/mp3", ListItem("TeaTime.FM"))
-    addDirectoryItem(plugin.handle, "http://listen.replay.fm/mp3", ListItem("Replay.FM"))
+    quality_setting = kodiutils.get_setting("quality")
+    quality = None
+    if quality_setting == u'0':
+        quality = "hd"
+    if quality_setting == u'1':
+        quality = "high"
+    if quality_setting == u'2':
+        quality = "low"
+    if quality == None:
+        quality = "high"
+    for stream in get_streams.get_all():
+        addDirectoryItem(plugin.handle, stream[quality], ListItem(stream["name"]))
     endOfDirectory(plugin.handle)
 
 def run():

6142791c3893e706f94131e236dee26caea4ecdd
Max <maxbambi@libero.it>
2021-02-09 20:53:24 +0100

    [plugin.video.raitv] 3.5.1 (#3362)

--- plugin.video.raitv/default.py
+++ plugin.video.raitv/default.py
@@ -145,6 +145,8 @@ def show_tgr_list(mode, url):
 def play(url, pathId="", srt=[]):
     xbmc.log("Playing...")
     
+    ct = ""
+    key = ""
     if pathId != "":
         xbmc.log("PathID: " + pathId)
 
@@ -156,7 +158,6 @@ def play(url, pathId="", srt=[]):
             srtUrl = ""
         else:
             raiplay = RaiPlay(Addon)
-            xbmc.log("Url: " + raiplay.getUrl(pathId))
             metadata = raiplay.getVideoMetadata(pathId)
             url = metadata["content_url"]
             srtUrl = metadata["subtitles"]
@@ -165,22 +166,36 @@ def play(url, pathId="", srt=[]):
             xbmc.log("SRT URL: " + srtUrl)
             srt.append(srtUrl)
 
-    # Handle RAI relinker
-    if url[:53] == "http://mediapolis.rai.it/relinker/relinkerServlet.htm" or \
-        url[:56] == "http://mediapolisvod.rai.it/relinker/relinkerServlet.htm" or \
-        url[:58] == "http://mediapolisevent.rai.it/relinker/relinkerServlet.htm":
+    if "relinkerServlet" in url:
+        url = url.replace ("https:", "http:")
         xbmc.log("Relinker URL: " + url)
         relinker = Relinker()
-        url = relinker.getURL(url)
-    
+        params = relinker.getURL(url)
+        url = params.get('url','')
+        ct = params.get('ct','')
+        key = params.get('key','')
+        
     # Add the server to the URL if missing
     if url[0] == "/":
         url = raiplay.baseUrl[:-1] + url
+    
     xbmc.log("Media URL: " + url)
+    xbmc.log("Media format: %s - License Url: %s" % (ct,key))
     
     # Play the item
-    try: item=xbmcgui.ListItem(path=url + '|User-Agent=' + urllib.quote_plus(Relinker.UserAgent))
-    except: item=xbmcgui.ListItem(path=url + '|User-Agent=' + urllib.parse.quote_plus(Relinker.UserAgent))
+    try: 
+        item=xbmcgui.ListItem(path=url + '|User-Agent=' + urllib.quote_plus(Relinker.UserAgent))
+    except: 
+        item=xbmcgui.ListItem(path=url + '|User-Agent=' + urllib.parse.quote_plus(Relinker.UserAgent))
+    
+    if "dash" in ct :
+        item.setProperty('inputstreamaddon', 'inputstream.adaptive')
+        item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+        item.setMimeType('application/dash+xml')
+        if key:
+            item.setProperty("inputstream.adaptive.license_type", 'com.widevine.alpha')
+            item.setProperty("inputstream.adaptive.license_key",  key + '||R{SSM}|')
+    
     if len(srt) > 0:
         item.setSubtitles(srt)
     xbmcplugin.setResolvedUrl(handle=handle, succeeded=True, listitem=item)
@@ -483,6 +498,13 @@ def show_ondemand_root():
  
             if not (item["name"] in ("Teatro", "Musica")):
                 liStyle = xbmcgui.ListItem(item["name"])
+                
+                # new urls 
+                # i.e. change "/raiplay/programmi/?json" to "/raiplay/tipologia/programmi/index.json"
+                m = re.findall("raiplay/(.*?)/[?]json", item["PathID"])
+                if m:
+                    item["PathID"] = "/raiplay/tipologia/%s/index.json" % m[0]   
+
                 addDirectoryItem({"mode": "ondemand", "path_id": item["PathID"], "sub_type": item["sub-type"]}, liStyle)
     
     # add new item not in old json
@@ -518,7 +540,7 @@ def show_ondemand_list(pathId):
     # indice ottenuto dal json
     raiplay = RaiPlay(Addon)
     xbmc.log("Url: %s" % raiplay.getUrl(pathId) )
-    
+
     index = raiplay.getIndexFromJSON(pathId)
     xbmc.log(str(index))
 
--- plugin.video.raitv/resources/lib/relinker.py
+++ plugin.video.raitv/resources/lib/relinker.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
+import xbmc
 import urllib
 import resources.lib.utils as utils
+import re
+import json
 
 PY3 = sys.version_info.major >= 3
 
@@ -24,9 +27,9 @@ class Relinker:
     # Firefox 52 ESR on Linux
     # UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
     # Chrome 64 on Windows 10
-    # UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282 Safari/537.36"
+    UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
     # Raiplay android app
-    UserAgent = "Android 4.2.2 (smart) / RaiPlay 2.1.3 / WiFi"
+    #UserAgent = "Android 4.2.2 (smart) / RaiPlay 2.1.3 / WiFi"
     
     def __init__(self):
         opener = urllib2.build_opener()
@@ -48,15 +51,60 @@ class Relinker:
         
         if "output" in qs:
             del(qs['output'])
-        qs['output'] = "20"
+        
+        #qs['output'] = "20" # only url
+        qs['output'] = "56" # xml stream data  
         
         query = urlencode(qs, True)
         url = urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
                 
         try:
             response = utils.checkStr(urllib2.urlopen(url).read())
-            mediaUrl = response.strip()
-
+            #mediaUrl = response.strip()
+            #xbmc.log(response)
+            
+            #find real url
+            content = re.findall("<url type=\"content\">(.*?)</url>", response)
+            if content:
+                #<![CDATA[https://dashaz-dc-euwe.akamaized.net/subtl_proxy/6ed6fac0-ae71-4dd7-b4be-d8921d4948b9/20200713102433_12778339.ism/manifest(format=mpd-time-csf,filter=medium_1200-2400).mpd?hdnea=st=1599391792~exp=1599391942~acl=/*~hmac=27e952b0f784662684fe65fb4717152d8644e2a9f3ad575ece21738dfbe88263]]>
+                url = re.findall("<!\[CDATA\[(.*?)\]\]>",content[0]) 
+                if url:
+                    #find type of stream
+                    ct = re.findall("<ct>(.*?)</ct>", response)
+                    if ct:
+                        #find license key
+                        #<license_url><![CDATA[{"drmLicenseUrlValues":[{"drm":"WIDEVINE","licenceUrl":"https://mediaservicerainet02.keydelivery.northeurope.media.azure.net/Widevine/?kid=5ca7736f-7e27-49fc-80b2-bde49c8c0259&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cm46bWljcm9zb2Z0OmF6dXJlOm1lZGlhc2VydmljZXM6Y29udGVudGtleWlkZW50aWZpZXIiOiI1Y2E3NzM2Zi03ZTI3LTQ5ZmMtODBiMi1iZGU0OWM4YzAyNTkiLCJpc3MiOiJodHRwOi8vcmFpcGxheSIsImF1ZCI6InVybjpyYWlwbGF5X2hkY3BfdjFfc2wxX3NkIiwiZXhwIjoxNTk5MzkyOTc0fQ.ZwmxivCnx8Gz-GRVu5twKIrjsAdK6jczAT2JZLnO9mw","name":"Widevine Token Restricted JWT HDCP_V1_SL1_SD","audience":"urn:raiplay_hdcp_v1_sl1_sd"},{"drm":"PLAYREADY","licenceUrl":"https://mediaservicerainet02.keydelivery.northeurope.media.azure.net/PlayReady/?kid=5ca7736f-7e27-49fc-80b2-bde49c8c0259&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cm46bWljcm9zb2Z0OmF6dXJlOm1lZGlhc2VydmljZXM6Y29udGVudGtleWlkZW50aWZpZXIiOiI1Y2E3NzM2Zi03ZTI3LTQ5ZmMtODBiMi1iZGU0OWM4YzAyNTkiLCJpc3MiOiJodHRwOi8vcmFpcGxheSIsImF1ZCI6InVybjpyYWlwbGF5IiwiZXhwIjoxNTk5MzkyOTc0fQ.pih3hCVRTJyNkgYGj42oCb8-Tya-4-PPC-PlZn38n2M","name":"Playready Token Restricted JWT","audience":"urn:raiplay"}]}]]></license_url>
+                        licenseUrl = re.findall("<license_url>(.*?)</license_url>", response) 
+                        if licenseUrl:
+                            #xbmc.log(licenseUrl[0])
+                            licenseJson = re.findall("<!\[CDATA\[(.*?)\]\]>",licenseUrl[0])
+                            if licenseJson:
+                                #xbmc.log(licenseJson[0])
+                                try:
+                                    licenseJson = json.loads(licenseJson[0])
+                                
+                                    xbmc.log(str(licenseJson))
+                                    licenseData = licenseJson.get('drmLicenseUrlValues',[])
+                                    key = ''  
+                                    for l in licenseData:
+                                        if "WIDEVINE" in l.get("drm",""):
+                                            key = l.get("licenceUrl",'')
+                                            
+                                    return {'url': url[0],'ct': ct[0],'key': key}
+                                    
+                                except:
+                                    return {'url': url[0],'ct': ct[0],'key':''}
+                        else:
+                            return {'url': url[0],'ct': ct[0],'key':''}
+                    
+                    else:
+                        return {'url': url[0],'ct':'','key':''}
+                    
+                else:
+                    return {'url':'','ct':'','key':''}
+            else:    
+                return {'url':'','ct':'','key':''}
+                
             # Workaround to normalize URL if the relinker doesn't
             try: 
                 mediaUrl = urllib.quote(mediaUrl, safe="%/:=&?~#+!$,;'@()*[]")
@@ -65,4 +113,4 @@ class Relinker:
             return mediaUrl
 
         except HTTPError:
-            return ''
+            return {'url':'','type':'','key':''}

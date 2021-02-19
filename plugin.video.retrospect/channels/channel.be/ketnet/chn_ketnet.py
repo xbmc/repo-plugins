080c931182fce7da99b985672f3f6759f604789a
Bas Rieter <basrieter@gmail.com>
2021-02-19 10:29:49 +0100

    [plugin.video.retrospect] v5.2.23 (#3371)

--- plugin.video.retrospect/channels/channel.be/ketnet/chn_ketnet.py
+++ plugin.video.retrospect/channels/channel.be/ketnet/chn_ketnet.py
@@ -3,13 +3,10 @@
 from resources.lib import chn_class
 
 from resources.lib.mediaitem import MediaItem
-from resources.lib.helpers.languagehelper import LanguageHelper
-from resources.lib.regexer import Regexer
 from resources.lib.logger import Logger
 from resources.lib.urihandler import UriHandler
-from resources.lib.parserdata import ParserData
 from resources.lib.helpers.jsonhelper import JsonHelper
-from resources.lib.streams.m3u8 import M3u8
+from resources.lib.helpers.htmlentityhelper import HtmlEntityHelper
 
 
 class Channel(chn_class.Channel):
@@ -33,33 +30,24 @@ class Channel(chn_class.Channel):
 
         if self.channelCode == "ketnet":
             self.noImage = "ketnetimage.jpg"
-            self.mainListUri = "https://www.ketnet.be/kijken"
+            self.mainListUri = self.__get_graph_url("content/ketnet/nl/kijken.model.json")
             self.baseUrl = "https://www.ketnet.be"
             self.mediaUrlRegex = r'playerConfig\W*=\W*(\{[\w\W]{0,2000}?);(?:.vamp|playerConfig)'
 
-        elif self.channelCode == "cobra":
-            self.noImage = "cobraimage.png"
-            self.mainListUri = "http://www.cobra.be/cm/cobra/cobra-mediaplayer"
-            self.baseUrl = "http://www.cobra.be"
+        else:
+            raise IndexError("Unknow channel code {}".format(self.channelCode))
 
-        self.swfUrl = "%s/html/flash/common/player.swf" % (self.baseUrl,)
+        self._add_data_parser(
+            self.mainListUri, json=True, name="MainList Parser for GraphQL",
+            parser=["data", "page", "pagecontent",
+                    ("id", "/content/ketnet/nl/kijken/jcr:content/root/swimlane_64598007", 0),
+                    "items"], creator=self.create_typed_item)
 
-        episode_regex = r'<a[^>]+href="(?<url>/kijken[^"]+)"[^>]*>\W*<img[^>]+src="' \
-                        r'(?<thumburl>[^"]+)"[^>]+alt="(?<title>[^"]+)"'
-        episode_regex = Regexer.from_expresso(episode_regex)
-        self._add_data_parser(self.mainListUri, match_type=ParserData.MatchExact,
-                              parser=episode_regex, creator=self.create_episode_item)
-
-        self._add_data_parser("*", preprocessor=self.select_video_section)
-
-        video_regex = Regexer.from_expresso(r'<a title="(?<title>[^"]+)" href="(?<url>[^"]+)"[^>]*>'
-                                            r'\W+<img src="(?<thumburl>[^"]+)"[^<]+<span[^<]+[^<]+'
-                                            r'[^>]+></span>\W+(?<description>[^<]+)')
-        self._add_data_parser("*", parser=video_regex, creator=self.create_video_item,
-                              updater=self.update_video_item)
-
-        folder_regex = Regexer.from_expresso(r'<span class="more-of-program" rel="/(?<url>[^"]+)">')
-        self._add_data_parser("*", parser=folder_regex, creator=self.create_folder_item)
+        self._add_data_parser("https://senior-bff.ketnet.be/graphql?query=query%20GetPage%28",
+                              name="Generic GraphQL Parser", json=True,
+                              parser=["data", "page", "tabs", ("type", "playlists", 0), "playlists"],
+                              creator=self.create_typed_item, updater=self.update_video_item,
+                              postprocessor=self.check_single_folder)
 
         #===============================================================================================================
         # non standard items
@@ -70,45 +58,37 @@ class Channel(chn_class.Channel):
         # ====================================== Actual channel setup STOPS here =======================================
         return
 
-    def select_video_section(self, data):
-        """ Performs pre-process actions for data processing
+    # noinspection PyUnusedLocal
+    def check_single_folder(self, data, items):
+        """ Performs post-process actions for data processing.
 
-        :param str|unicode data: The retrieve data that was loaded for the current item and URL.
+        Accepts an data from the process_folder_list method, BEFORE the items are
+        processed. Allows setting of parameters (like title etc) for the channel.
+        Inside this method the <data> could be changed and additional items can
+        be created.
+
+        The return values should always be instantiated in at least ("", []).
+
+        :param str|JsonHelper data:     The retrieve data that was loaded for the
+                                         current item and URL.
+        :param list[MediaItem] items:   The currently available items
 
         :return: A tuple of the data and a list of MediaItems that were generated.
-        :rtype: tuple[str|JsonHelper,list[MediaItem]]
+        :rtype: list[MediaItem]
 
         """
 
-        Logger.info("Performing Pre-Processing")
-        items = []
+        Logger.info("Performing Post-Processing")
+        if len(items) == 1:
+            item = items[0]
+            if item.type == "folder" and item.items:
+                return item.items
 
-        end_of_section = data.rfind('<div class="ketnet-abc-index">')
-        if end_of_section > 0:
-            data = data[:end_of_section]
+        Logger.debug("Post-Processing finished")
+        return items
 
-        # find the first main video
-        json_data = Regexer.do_regex(self.mediaUrlRegex, data)
-        if not json_data:
-            Logger.debug("No show data found as JSON")
-            return data, items
-
-        Logger.trace(json_data[0])
-        json = JsonHelper(json_data[0])
-        title = json.get_value("title")
-        url = json.get_value("mzid") or self.parentItem.url
-        item = MediaItem(title, url)
-        item.type = 'video'
-        item.description = json.get_value("description", fallback=None)
-        item.thumb = json.get_value("image", fallback=self.noImage)
-        item.complete = False
-        items.append(item)
-
-        Logger.debug("Pre-Processing finished")
-        return data, items
-
-    def create_folder_item(self, result_set):
-        """ Creates a MediaItem of type 'folder' using the result_set from the regex.
+    def create_typed_item(self, result_set):
+        """ Creates a new MediaItem for a typed item.
 
         This method creates a new MediaItem from the Regular Expression or Json
         results <result_set>. The method should be implemented by derived classes
@@ -122,9 +102,95 @@ class Channel(chn_class.Channel):
         """
 
         Logger.trace(result_set)
+        type_id = result_set["__typename"]
+        if type_id == "Program":
+            return self.create_program_item(result_set)
+        elif type_id == "Playlist":
+            return self.create_playlist_item(result_set)
+        elif type_id == "Video":
+            return self.create_video_item(result_set)
+        else:
+            Logger.warning("Unknown Graph Type: %s", type_id)
 
-        result_set["title"] = LanguageHelper.get_localized_string(LanguageHelper.MorePages)
-        return chn_class.Channel.create_folder_item(self, result_set)
+        return None
+
+    def create_program_item(self, result_set):
+        """ Creates a new MediaItem for an episode.
+
+        This method creates a new MediaItem from the Regular Expression or Json
+        results <result_set>. The method should be implemented by derived classes
+        and are specific to the channel.
+
+        :param list[str]|dict[str,str] result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'folder'.
+        :rtype: MediaItem|None
+
+        """
+
+        url = self.__get_graph_url(result_set["id"])
+        item = MediaItem(result_set["title"], url)
+        item.poster = result_set["posterUrl"]
+        return item
+
+    def create_playlist_item(self, result_set):
+        """ Creates a new MediaItem for a playlist of seasons.
+
+        This method creates a new MediaItem from the Regular Expression or Json
+        results <result_set>. The method should be implemented by derived classes
+        and are specific to the channel.
+
+        :param dict result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'folder'.
+        :rtype: MediaItem|None
+
+        """
+
+        title = result_set["title"]
+        url = "{}/{}".format(self.parentItem.url, title)
+        item = MediaItem(result_set["title"], url)
+        item.poster = result_set.get("imageUrl")
+        items = result_set["items"]
+        for sub_item in items:
+            video_item = self.create_typed_item(sub_item)
+            if video_item:
+                item.items.append(video_item)
+        return item
+
+    def create_video_item(self, result_set):
+        """ Creates a MediaItem of type 'video' using the result_set from the regex.
+
+        This method creates a new MediaItem from the Regular Expression or Json
+        results <result_set>. The method should be implemented by derived classes
+        and are specific to the channel.
+
+        If the item is completely processed an no further data needs to be fetched
+        the self.complete property should be set to True. If not set to True, the
+        self.update_video_item method is called if the item is focussed or selected
+        for playback.
+
+        :param dict result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'video' or 'audio' (despite the method's name).
+        :rtype: MediaItem|None
+
+        """
+
+        Logger.trace(result_set)
+        video_id = result_set["id"]
+        item = MediaItem(result_set["titlePlaylist"], self.__get_graph_url(video_id))
+        item.type = "video"
+        item.description = result_set.get("description")
+        item.thumb = result_set.get("imageUrl")
+
+        duration = int(result_set.get("duration", 0) / 1000)
+        if duration:
+            item.set_info_label("duration", duration)
+
+        allowed_region = result_set.get("allowedRegion", "").lower()
+        item.isGeoLocked = allowed_region != "world"
+        return item
 
     def update_video_item(self, item):
         """ Updates an existing MediaItem with more data.
@@ -149,69 +215,24 @@ class Channel(chn_class.Channel):
         """
 
         Logger.debug('Starting update_video_item for %s (%s)', item.name, self.channelName)
-        if item.url.startswith("http"):
-            data = UriHandler.open(item.url)
-            json_data = Regexer.do_regex(self.mediaUrlRegex, data)
-
-            json = JsonHelper(json_data[0])
-            mzid = json.get_value("mzid")
-            if not mzid:
-                item.url = json.get_value("source", "hls")
-                return self.__update_from_source(item)
-        else:
-            mzid = item.url
+        data = UriHandler.open(item.url)
+        json_data = JsonHelper(data)
+        player_info = json_data.get_value("data", "page", "vrtPlayer")
+        client_code = player_info["clientCode"]
+        media_refernce_pbs = player_info["mediaReference"]
 
         hls_over_dash = self._get_setting("hls_over_dash", 'false') == 'true'
 
         from resources.lib.streams.vualto import Vualto
-        v = Vualto(self, "ketnet@prod")
-        item = v.get_stream_info(item, mzid, hls_over_dash=hls_over_dash)
+        v = Vualto(self, client_code)
+        item = v.get_stream_info(item, media_refernce_pbs, hls_over_dash=hls_over_dash)
         return item
 
-    def __update_from_source(self, item):
-        """ Updates an existing MediaItem with more data.
-
-        Used to update none complete MediaItems (self.complete = False). This
-        could include opening the item's URL to fetch more data and then process that
-        data or retrieve it's real media-URL.
-
-        The method should at least:
-        * cache the thumbnail to disk (use self.noImage if no thumb is available).
-        * set at least one MediaItemPart with a single MediaStream.
-        * set self.complete = True.
-
-        if the returned item does not have a MediaItemPart then the self.complete flag
-        will automatically be set back to False.
-
-        :param MediaItem item: the original MediaItem that needs updating.
-
-        :return: The original item with more data added to it's properties.
-        :rtype: MediaItem
-
-        """
-
-        Logger.debug('Starting update_video_item for %s (%s)', item.name, self.channelName)
-
-        if not item.url.endswith("m3u8"):
-            data = UriHandler.open(item.url)
-            json_data = Regexer.do_regex(self.mediaUrlRegex, data)
-            if not json_data:
-                Logger.error("Cannot find JSON stream info.")
-                return item
-
-            json = JsonHelper(json_data[0])
-            Logger.trace(json.json)
-            stream = json.get_value("source", "hls")
-            if stream is None:
-                stream = json.get_value("mzsource", "hls")
-            Logger.debug("Found HLS: %s", stream)
-        else:
-            stream = item.url
-
-        part = item.create_new_empty_media_part()
-        for s, b in M3u8.get_streams_from_m3u8(stream):
-            item.complete = True
-            part.append_media_stream(s, b)
-
-        # var playerConfig = {"id":"mediaplayer","width":"100%","height":"100%","autostart":"false","image":"http:\/\/www.ketnet.be\/sites\/default\/files\/thumb_5667ea22632bc.jpg","brand":"ketnet","source":{"hls":"http:\/\/vod.stream.vrt.be\/ketnet\/_definst_\/mp4:ketnet\/2015\/12\/Ben_ik_familie_van_R001_A0023_20151208_143112_864.mp4\/playlist.m3u8"},"analytics":{"type_stream":"vod","playlist":"Ben ik familie van?","program":"Ben ik familie van?","episode":"Ben ik familie van?: Warre - Aflevering 3","parts":"1","whatson":"270157835527"},"title":"Ben ik familie van?: Warre - Aflevering 3","description":"Ben ik familie van?: Warre - Aflevering 3"}
-        return item
+    def __get_graph_url(self, id):
+        graph_query = "query GetPage($id: String!) { page(id: $id) { ... on Program { pageType ...program __typename } ... on Video { pageType ...video __typename } ... on Pagecontent { pageType ...pagecontent __typename } __typename } }  fragment program on Program { id title header { ...header __typename } activeTab tabs { name title type playlists { ...seasonOverviewTabItem __typename } pagecontent { ...pagecontentItem __typename } __typename } __typename }  fragment seasonOverviewTabItem on Playlist { name title type imageUrl items { id titlePlaylist subtitlePlaylist scaledPoster { ...scaledImage __typename } allowedRegion duration episodeNr seasonTitle imageUrl availableUntilDate publicationDate description __typename } __typename }  fragment video on Video { id videoType titleVideodetail subtitleVideodetail scaledPoster { ...scaledImage __typename } description availableUntilDate publicationDate duration episodeNr vrtPlayer { ...vrtPlayerFragment __typename } suggestions { id titleSuggestion subtitleSuggestion scaledPoster { ...scaledImage __typename } __typename } playlists { name title items { id titlePlaylist subtitlePlaylist scaledPoster { ...scaledImage __typename } description __typename } __typename } activePlaylist trackingData { programName seasonName episodeName episodeNr episodeBroadcastDate __typename } __typename }  fragment pagecontent on Pagecontent { pagecontent { ...pagecontentItem __typename } __typename }  fragment pagecontentItem on PagecontentItem { ... on Header { ...header __typename } ... on Highlight { ...highlight __typename } ... on Swimlane { ...swimlane __typename } __typename }  fragment highlight on Highlight { type title description link linkItem { ... on Game { id __typename } ... on Program { id __typename } ... on Story { id __typename } ... on Theme { id __typename } ... on Video { id __typename } __typename } buttonText imageUrl size __typename }  fragment swimlane on Swimlane { id type title style items { ... on Video { ...swimlaneVideo __typename } ... on Program { ...swimlaneProgram __typename } __typename } __typename }  fragment swimlaneVideo on Video { id type title imageUrl titleSwimlane subtitleSwimlane duration __typename }  fragment swimlaneProgram on Program { id type title accentColor imageUrl logoUrl posterUrl __typename }  fragment scaledImage on ScaledImage { small medium large __typename }  fragment vrtPlayerFragment on VrtPlayerConfig { mediaReference aggregatorUrl clientCode __typename }  fragment header on Header { type title description imageUrl logoUrl __typename }"
+        graph_id = "{{\"id\": \"{}\"}}".format(id)
+        graph_url = "https://senior-bff.ketnet.be/graphql?query={}&variables={}".format(
+            HtmlEntityHelper.url_encode(graph_query),
+            HtmlEntityHelper.url_encode(graph_id)
+        )
+        return graph_url

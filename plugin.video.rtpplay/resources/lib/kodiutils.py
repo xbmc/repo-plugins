93568f07d39a0a490f08e7b0fbd6358a4a4ad05f
enen92 <92enen@gmail.com>
2021-02-24 19:37:11 +0000

    [plugin.video.rtpplay] 5.0.13

--- plugin.video.rtpplay/resources/lib/kodiutils.py
+++ plugin.video.rtpplay/resources/lib/kodiutils.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
+import os
 
 import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcvfs
 import sys
-import os
 import logging
 import json as json
+import re
+import base64
 
 PY3 =  sys.version_info > (3, 0)
 
 if PY3:
+    from urllib.request import urlopen
+    from urllib.parse import unquote
     from html.parser import HTMLParser
 else:
+    from urllib2 import urlopen
+    from urlparse import unquote
     from HTMLParser import HTMLParser
-
 
 # read settings
 ADDON = xbmcaddon.Addon()
+PROFILE = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
+TEMP = os.path.join(PROFILE, 'temp', '')
 
 if PY3:
     ICON = xbmcvfs.translatePath(ADDON.getAddonInfo("icon"))
@@ -121,3 +128,65 @@ def kodi_json_request(params):
         logger.warn("[%s] %s" %
                     (params['method'], response['error']['message']))
         return None
+
+
+def find_stream_url(html):
+    try:
+        for m in re.finditer(r'decodeURIComponent(?:(\s+)?)\((?:(\s+)?)\["(.*?)"]', html):
+            url = unquote(m.group(3).replace(" ", "").replace('","', ""))
+            if url.startswith("http"):
+                return url
+            if url.endswith(".mp4"):
+                continue
+            url = base64.b64decode(url).decode('utf-8')
+            if url.startswith("http"):
+                return url
+    except:
+        pass
+    urls = ["ondemand.rtp.pt", "streaming.rtp.pt", "live.rtp.pt"]
+    for base_url in urls:
+        try:
+            for m in re.finditer(r'"https://(.+?)' + base_url + '(.*?)"', html):
+                if m.group(2) and "preview" not in m.group(2):
+                    return "https://" + m.group(1) + base_url + m.group(2)
+        except:
+            pass
+    raise ValueError
+
+
+def convertVttSrt(fileContents):
+    # taken from https://github.com/jansenicus/vtt-to-srt.py/blob/master/vtt_to_srt.py#L29
+    replacement = re.sub(r'(\d\d:\d\d:\d\d).(\d\d\d) --> (\d\d:\d\d:\d\d).(\d\d\d)(?:[ \-\w]+:[\w\%\d:]+)*\n',
+                         r'\1,\2 --> \3,\4\n', fileContents)
+    replacement = re.sub(r'(\d\d:\d\d).(\d\d\d) --> (\d\d:\d\d).(\d\d\d)(?:[ \-\w]+:[\w\%\d:]+)*\n',
+                         r'\1,\2 --> \3,\4\n', replacement)
+    replacement = re.sub(r'(\d\d).(\d\d\d) --> (\d\d).(\d\d\d)(?:[ \-\w]+:[\w\%\d:]+)*\n', r'\1,\2 --> \3,\4\n',
+                         replacement)
+    replacement = re.sub(r'WEBVTT\n', '', replacement)
+    replacement = re.sub(r'Kind:[ \-\w]+\n', '', replacement)
+    replacement = re.sub(r'Language:[ \-\w]+\n', '', replacement)
+    replacement = re.sub(r'<c[.\w\d]*>', '', replacement)
+    replacement = re.sub(r'</c>', '', replacement)
+    replacement = re.sub(r'<\d\d:\d\d:\d\d.\d\d\d>', '', replacement)
+    replacement = re.sub(r'::[\-\w]+\([\-.\w\d]+\)[ ]*{[.,:;\(\) \-\w\d]+\n }\n', '', replacement)
+    replacement = re.sub(r'Style:\n##\n', '', replacement)
+    return replacement
+
+
+def find_subtitles(html):
+    try:
+        match = re.search(r'["\']\s*?http(.*?)\.vtt\s*?["\']', html)
+        if match and len(match.groups()) > 0:
+            url = "http" + match.group(1) + ".vtt"
+            last_slash = url.rfind("/")
+            if last_slash != -1:
+                id = url[last_slash + 1:len(url) - 4]
+                if not os.path.exists(TEMP):
+                    os.makedirs(TEMP)
+                file = os.path.join(TEMP, "{id}.srt".format(id=id))
+                response = urlopen(url)
+                with open(file, "w") as local_file:
+                    local_file.write(convertVttSrt(response.read().decode('utf-8')))
+                return file
+    except Exception:
+        pass

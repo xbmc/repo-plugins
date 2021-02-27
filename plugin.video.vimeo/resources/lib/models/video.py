900d8a788baf09dcd144f0da0b7c648c773969d7
Jay Linski <jaylinski@users.noreply.github.com>
2021-02-27 17:06:09 +0100

    [plugin.video.vimeo] 6.0.0 (#3406)

---
+++ plugin.video.vimeo/resources/lib/models/video.py
@@ -0,0 +1,57 @@
+import urllib.parse
+import xbmcaddon
+import xbmcgui
+
+from resources.lib.models.list_item import ListItem
+
+live = xbmcaddon.Addon().getLocalizedString(30902)
+trailer = xbmcaddon.Addon().getLocalizedString(30903)
+
+
+class Video(ListItem):
+    thumb = ""
+    uri = ""
+    info = {}
+    hasSubtitles = None
+
+    def to_list_item(self, addon, addon_base):
+        text_tracks = "{}/texttracks".format(self.uri) if self.hasSubtitles else ""
+
+        list_item = xbmcgui.ListItem(label=(self._get_label_prefix() + self.label))
+        list_item.setArt({
+            "thumb": self.thumb,
+            "poster": self.info.get("picture", ""),
+        })
+        list_item.setCast([
+            {
+                "name": self.info.get("user", ""),
+                "role": addon.getLocalizedString(30401),
+                "thumbnail": self.info.get("userThumb", ""),
+            },
+        ])
+        list_item.setInfo("video", {
+            "artist": [self.info.get("user", "")],
+            "duration": self.info.get("duration"),
+            "plot": self.info.get("description", ""),
+            "title": self.label,
+            "year": self.info.get("date")[:4],
+        })
+        list_item.setProperty("isPlayable", "true")
+        list_item.setProperty("mediaUrl", self.uri)
+        list_item.setProperty("textTracks", text_tracks)
+
+        url = addon_base + "/play/?" + urllib.parse.urlencode({
+            "uri": self.uri,
+            "texttracks": text_tracks,
+        })
+
+        return url, list_item, False
+
+    def _get_label_prefix(self):
+        if self.info.get("onDemand"):
+            return "[{}] ".format(trailer)
+
+        if self.info.get("live"):
+            return "[{}] ".format(live)
+
+        return ""
---
+++ plugin.video.vimeo/resources/lib/vimeo/client.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from functools import wraps
+import json
+import requests
+from .auth.client_credentials import ClientCredentialsMixin
+from .auth.authorization_code import AuthorizationCodeMixin
+from .auth.device_code import DeviceCodeMixin
+from .exceptions import APIRateLimitExceededFailure
+
+
+class VimeoClient(ClientCredentialsMixin, AuthorizationCodeMixin, DeviceCodeMixin):
+    """Client handle for the Vimeo API."""
+
+    API_ROOT = "https://api.vimeo.com"
+    HTTP_METHODS = {'head', 'get', 'post', 'put', 'patch', 'options', 'delete'}
+    ACCEPT_HEADER = "application/vnd.vimeo.*;version=3.4"
+    USER_AGENT = "pyvimeo 1.2.0; (http://developer.vimeo.com/api/docs)"
+
+    def __init__(self, token=None, key=None, secret=None, *args, **kwargs):
+        """Prep the handle with the authentication information."""
+        self.token = token
+        self.app_info = (key, secret)
+        self._requests_methods = dict()
+
+        # Make sure we have enough info to be useful.
+        assert token is not None or (key is not None and secret is not None)
+
+    # Internally we back this with an auth mechanism for Requests.
+    @property
+    def token(self):
+        """Get the token string from the class _BearerToken."""
+        return self._token.token
+
+    @token.setter
+    def token(self, value):
+        self._token = _BearerToken(value) if value else None
+
+    def __getattr__(self, name):
+        """
+        Method used to get the function for the verb that was just requested.
+
+        From here we can apply the authentication information we have.
+        """
+        if name not in self.HTTP_METHODS:
+            raise AttributeError("%r is not an HTTP method" % name)
+
+        # Get the Requests based function to use to preserve their defaults.
+        request_func = getattr(requests, name, None)
+        if request_func is None:
+            raise AttributeError(
+                "%r could not be found in the backing lib" % name
+            )
+
+        @wraps(request_func)
+        def caller(url, jsonify=True, **kwargs):
+            """Hand off the call to Requests."""
+            headers = kwargs.get('headers', dict())
+            headers['Accept'] = self.ACCEPT_HEADER
+            headers['User-Agent'] = self.USER_AGENT
+
+            if jsonify \
+                    and 'data' in kwargs \
+                    and isinstance(kwargs['data'], (dict, list)):
+                kwargs['data'] = json.dumps(kwargs['data'])
+                headers['Content-Type'] = 'application/json'
+
+            kwargs['timeout'] = kwargs.get('timeout', (1, 30))
+            kwargs['auth'] = kwargs.get('auth', self._token)
+            kwargs['headers'] = headers
+            if not url[:4] == "http":
+                url = self.API_ROOT + url
+
+            response = request_func(url, **kwargs)
+            if response.status_code == 429:
+                raise APIRateLimitExceededFailure(
+                    response, 'Too many API requests'
+                )
+            return response
+        return caller
+
+
+class _BearerToken(requests.auth.AuthBase):
+    """Model the bearer token and apply it to the request."""
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, request):
+        request.headers['Authorization'] = 'Bearer ' + self.token
+        return request

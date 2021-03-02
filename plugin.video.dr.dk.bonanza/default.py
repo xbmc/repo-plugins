e1216cac4992c5c0bb6558d9a19348141c11fc47
Rasmus Lundsgaard <termopeten@gmail.com>
2021-03-02 21:50:37 +0100

    [plugin.video.dr.dk.bonanza] 3.2.0 (#3424)
    * [plugin.video.dr.dk.bonanza] 3.2.0
    
    * code review

---
+++ plugin.video.dr.dk.bonanza/default.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from resources.lib import addon
+import sys
+
+# Start of Module
+if __name__ == "__main__":
+    handle = addon.Bonanza(plugin_url=sys.argv[0], plugin_handle=int(sys.argv[1]))
+    handle.route(sys.argv[2])

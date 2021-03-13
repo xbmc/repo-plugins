3e3e21314c74ada915de629acd69c942eba3de65
skipmodea1 <skipmodea1@outlook.com>
2021-03-13 16:42:16 +0100

    [plugin.video.worldstarhiphop] 1.0.13+matrix.1

---
+++ plugin.video.worldstarhiphop/addon.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#
+# Imports
+#
+# the YDStreamExtractor needs to be before the future imports. Otherwise u get an 'check hostname' error.
+import YDStreamExtractor
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+import sys
+import urllib.parse
+import xbmc
+
+from resources.lib.worldstarhiphop_const import ADDON, DATE, VERSION, BASEURL
+
+# Parse parameters...
+if len(sys.argv[2]) == 0:
+    #
+    # Main menu
+    #
+    xbmc.log(f"[ADDON] {ADDON}, Python Version {sys.version}", xbmc.LOGDEBUG)
+    xbmc.log(f"[ADDON] {ADDON} v{VERSION} ({DATE}) is starting, ARGV = {repr(sys.argv)}", xbmc.LOGDEBUG )
+
+    from resources.lib import worldstarhiphop_list as plugin
+
+else:
+    action = urllib.parse.parse_qs(urllib.parse.urlparse(sys.argv[2]).query)['action'][0]
+    #
+    # List
+    #
+    if action == 'list':
+        from resources.lib import worldstarhiphop_list as plugin
+    #
+    # Play
+    #
+    elif action == 'play':
+        from resources.lib import worldstarhiphop_play as plugin
+    #
+    # Search
+    #
+    elif action == 'search':
+        from resources.lib import worldstarhiphop_search as plugin
+
+plugin.Main()

43d6a3ca7fbfc030db3ab1019f40b410ac39a5bf
lusum <lusumdev@zoho.eu>
2021-03-14 22:11:40 +0100

    [plugin.audio.ampache@matrix] 2.0.0+matrix.1

--- plugin.audio.ampache/resources/lib/utils.py
+++ plugin.audio.ampache/resources/lib/utils.py
@@ -1,21 +1,70 @@
 import time
 import datetime
 import xbmcaddon
+import sys
 
 #main plugin/service library
 ampache = xbmcaddon.Addon("plugin.audio.ampache")
 
-def otype_to_type(object_type):
+def otype_to_mode(object_type, object_subtype=None):
+    mode = None
+    if object_type == 'artists':
+        mode = 1
+    elif object_type == 'albums':
+        mode = 2
+    elif object_type == 'songs':
+        mode = 3
+    elif object_type == 'playlists':
+        mode = 4
+    elif object_type == 'podcasts':
+        mode = 5
+    elif object_type == 'videos':
+        mode = 8
+    elif object_type == 'tags' or object_type == 'genres':
+        if object_subtype == 'tag_artists' or object_subtype == 'genre_artists':
+            mode = 19
+        elif object_subtype == 'tag_albums' or object_subtype == 'genre_albums':
+            mode = 20
+        elif object_subtype == 'tag_songs' or object_subtype == 'genre_songs':
+            mode = 21
+
+    return mode
+
+def mode_to_tags(mode):
+    if(int(ampache.getSetting("api-version"))) < 500000:
+        if mode == 19:
+            return "tags","tag_artists"
+        if mode == 20:
+            return "tags","tag_albums"
+        if mode == 21:
+            return "tags","tag_songs"
+    else:
+        if mode == 19:
+            return "genres","genre_artists"
+        if mode == 20:
+            return "genres","genre_albums"
+        if mode == 21:
+            return "genres","genre_songs"
+
+def otype_to_type(object_type,object_subtype=None):
     if object_type == 'albums':
         return 'album'
     elif object_type == 'artists':
         return 'artist'
-    elif object_type == 'songs':
-        return 'song'
     elif object_type == 'playlists':
         return 'playlist'
     elif object_type == 'tags':
         return 'tag'
+    elif object_type == 'genres':
+        return 'genre'
+    elif object_type == 'podcasts':
+        return 'podcast'
+    elif object_type == 'videos':
+        return 'video'
+    elif object_type == 'songs':
+        if object_subtype == 'podcast_episodes':
+            return 'podcast_episode'
+        return 'song'
     return None
 
 def int_to_strBool(s):
@@ -37,13 +86,29 @@ def strBool_to_bool(s):
         raise ValueError
 
 def check_tokenexp():
-    try:
-        tokenexp = int(ampache.getSetting("token-exp"))
-        if int(time.time()) > tokenexp:
-            return True
-        return False
-    except:
+    session_time = ampache.getSetting("session_expire")
+    if session_time is None or session_time == "":
         return True
+
+    #from python 3.7 we can easly compare the dates, otherwise we use the old
+    #method
+
+    if sys.version_info >= (3, 7):
+        try:
+            s_time = datetime.datetime.fromisoformat(session_time)
+            if datetime.datetime.now(datetime.timezone.utc) > s_time:
+                return True
+            return False
+        except:
+            return False
+    else:
+        try:
+            tokenexp = int(ampache.getSetting("token-exp"))
+            if int(time.time()) > tokenexp:
+                return True
+            return False
+        except:
+            return True
 
 def get_time(time_offset):
     d = datetime.date.today()
@@ -79,13 +144,22 @@ def get_objectId_from_fileURL( file_url ):
     #i use two kind of object_id, i don't know, but sometime i have different
     #url, btw, no problem, i handle both and i solve the problem in this way
     try:
-            object_id=int(params["object_id"])
-            xbmc.log("AmpachePlugin::object_id " + str(object_id), xbmc.LOGDEBUG)
+            object_id=params["object_id"]
+            xbmc.log("AmpachePlugin::object_id " + object_id, xbmc.LOGDEBUG)
     except:
             pass
     try:
-            object_id=int(params["oid"])
-            xbmc.log("AmpachePlugin::object_id " + str(object_id), xbmc.LOGDEBUG)
+            object_id=params["oid"]
+            xbmc.log("AmpachePlugin::object_id " + object_id, xbmc.LOGDEBUG)
     except:
             pass
     return object_id
+
+def getRating(rating):
+    if rating:
+        #converts from five stats ampache rating to ten stars kodi rating
+        rating = int(float(rating)*2)
+    else:
+        #zero equals no rating
+        rating = 0
+    return rating

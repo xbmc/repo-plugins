0fa267219067252076c6f00b89301281e988a755
Ikosse <andrelofgren@hotmail.co.uk>
2021-04-08 15:15:29 +0200

    plugin.video.eventvods 1.0.1

---
+++ plugin.video.eventvods/resources/lib/utils.py
@@ -0,0 +1,33 @@
+import re
+import requests
+
+def get_page_json(url):
+    page_response = requests.get(url)
+    return page_response.json()
+
+
+def time_to_seconds(time_string):
+    pattern_time = re.compile("[dhmsDHMS]")
+    pattern_units = re.compile("[0-9]+")
+    units = re.split(pattern_units, time_string)
+    time = re.split(pattern_time, time_string)
+    time_dict = dict(zip(units[1:], time[:-1]))
+
+    seconds = 0
+    for (unit, sec) in time_dict.items():
+        if unit.lower() == "d":
+            seconds += int(sec)*86400
+        elif unit.lower() == "h":
+            seconds += int(sec)*3600
+        elif unit.lower() == "m":
+            seconds += int(sec)*60
+        elif unit.lower() == "s":
+            seconds += int(sec)
+
+    return seconds
+
+
+def convert_date_to_kodi_format(date):
+    kodi_date = date.split("T")[0]
+    kodi_date = ":".join(reversed(kodi_date.split("-")))
+    return kodi_date

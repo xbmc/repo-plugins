a8082c7d31b82f5c07d5621154f2e3ad0607179d
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-04-11 13:13:09 -1000

    [plugin.whereareyou] 0.4.3+matrix.1
    v.0.4.3
    - fix for crash if using Harmony Hub Control with an activity

--- plugin.whereareyou/resources/lib/waysettings.py
+++ plugin.whereareyou/resources/lib/waysettings.py
@@ -1,17 +1,18 @@
 
 from resources.lib.kodisettings import *
 
-SETTINGSLIST = [ {'name': 'mappings', 'default': ''},
-                 {'name': 'harmonycontrol', 'default': False},
-                 {'name': 'hub_ip', 'default': ''},
-                 {'name': 'timeout', 'default': 30},
-                 {'name': 'delay', 'default': 250},
-                 {'name': 'harmonyadvanced', 'default': False},
-                 {'name': 'use_custom_skin_menu', 'default': True},
-                 {'name': 'include_skin_mods', 'default': True},
-                 {'name': 'version_upgrade', 'default': ''},
-                 {'name': 'debug', 'default': False}
-               ]
+SETTINGSLIST = [{'name': 'mappings', 'default': ''},
+                {'name': 'harmonycontrol', 'default': False},
+                {'name': 'hub_ip', 'default': ''},
+                {'name': 'timeout', 'default': 30},
+                {'name': 'delay', 'default': 250},
+                {'name': 'harmonyadvanced', 'default': False},
+                {'name': 'use_custom_skin_menu', 'default': True},
+                {'name': 'include_skin_mods', 'default': True},
+                {'name': 'version_upgrade', 'default': ''},
+                {'name': 'debug', 'default': False}
+                ]
+
 
 def loadSettings():
     settings = {}
@@ -24,13 +25,13 @@ def loadSettings():
     settings['ADDONICON'] = ADDONICON
     settings['ADDONLANGUAGE'] = ADDONLANGUAGE
     for item in SETTINGSLIST:
-        if isinstance( item['default'], bool ):
+        if isinstance(item['default'], bool):
             getset = getSettingBool
-        elif isinstance( item['default'], int ):
+        elif isinstance(item['default'], int):
             getset = getSettingInt
-        elif isinstance( item['default'], float ):
+        elif isinstance(item['default'], float):
             getset = getSettingNumber
         else:
             getset = getSettingString
-        settings[item['name']] = getset( item['name'], item['default'] )
+        settings[item['name']] = getset(item['name'], item['default'])
     return settings

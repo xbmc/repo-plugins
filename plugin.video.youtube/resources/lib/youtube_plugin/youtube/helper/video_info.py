1a15776f493298df400c0119cc162ce347d1b637
anxdpanic <anxdpanic@users.noreply.github.com>
2021-06-16 14:26:15 -0400

    [plugin.video.youtube] 6.8.14+matrix.1 (#3641)

--- plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/video_info.py
+++ plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/video_info.py
@@ -576,7 +576,7 @@ class VideoInfo(object):
 
     @staticmethod
     def get_player_response(html):
-        response = None
+        response = {}
 
         found = re.search(
                 r'ytInitialPlayerResponse\s*=\s*(?P<response>{.+?})\s*;\s*(?:var\s+meta|</script|\n)', html
@@ -769,9 +769,12 @@ class VideoInfo(object):
         meta_info['channel']['author'] = meta_info['channel']['author'].encode('raw_unicode_escape')
 
         if PY2:
-            meta_info['video']['title'] = meta_info['video']['title'].decode('utf-8')
-            meta_info['channel']['author'] = meta_info['channel']['author'].decode('utf-8')
-
+            try:
+                meta_info['video']['title'] = meta_info['video']['title'].decode('utf-8')
+                meta_info['channel']['author'] = meta_info['channel']['author'].decode('utf-8')
+            except UnicodeDecodeError:
+                meta_info['video']['title'] = meta_info['video']['title'].decode('raw_unicode_escape')
+                meta_info['channel']['author'] = meta_info['channel']['author'].decode('raw_unicode_escape')
         else:
             meta_info['video']['title'] = meta_info['video']['title'].decode('raw_unicode_escape')
             meta_info['channel']['author'] = meta_info['channel']['author'].decode('raw_unicode_escape')

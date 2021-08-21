db4dc1e01e61ff3e0000a5c3f4b508e15f770e26
Micah Galizia <micahgalizia@gmail.com>
2021-08-21 13:32:32 -0400

    [plugin.video.cbc] 4.0.7+matrix.1
    * Bump client API version number to compatible value
    * Code cleanup based on pylint and flake8

--- plugin.video.cbc/default.py
+++ plugin.video.cbc/default.py
@@ -1,11 +1,18 @@
-from resources.lib.utils import log, getAuthorizationFile
-from resources.lib.livechannels import *
-from resources.lib.liveprograms import *
-from resources.lib.shows import *
-from resources.lib.cbc import *
-from urllib.parse import urlencode
-import xbmc, xbmcplugin, xbmcgui, xbmcaddon, os, urllib.parse
+"""Default plugin module."""
+import os
+from urllib.parse import urlencode, parse_qs, parse_qsl
+
+import xbmc
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
 import inputstreamhelper
+
+from resources.lib.cbc import CBC
+from resources.lib.utils import log, getAuthorizationFile
+from resources.lib.livechannels import LiveChannels
+from resources.lib.liveprograms import LivePrograms
+from resources.lib.shows import Shows, CBCAuthError
 
 getString = xbmcaddon.Addon().getLocalizedString
 
@@ -21,7 +28,9 @@ if sys.argv[1] == 'logout':
 
 addon_handle = int(sys.argv[1])
 
+
 def authorize():
+    """Authorize the client."""
     prog = xbmcgui.DialogProgress()
     prog.create(getString(30001))
     cbc = CBC()
@@ -46,45 +55,49 @@ def authorize():
 
 
 def play(labels, image, url):
+    """Play the stream using the configured player."""
     item = xbmcgui.ListItem(labels['title'], path=url)
-    item.setArt({ 'thumb': image, 'poster': image })
+    item.setArt({'thumb': image, 'poster': image})
     item.setInfo(type="Video", infoLabels=labels)
     helper = inputstreamhelper.Helper('hls')
     if not xbmcaddon.Addon().getSettingBool("ffmpeg") and helper.check_inputstream():
-        item.setProperty('inputstreamaddon','inputstream.adaptive')
+        item.setProperty('inputstreamaddon', 'inputstream.adaptive')
         item.setProperty('inputstream.adaptive.manifest_type', 'hls')
     xbmcplugin.setResolvedUrl(addon_handle, True, item)
 
-def playSmil(smil, labels, image):
+
+def play_smil(smil, labels, image):
+    """Play an SMIL file."""
     cbc = CBC()
     url = cbc.parseSmil(smil)
     return play(labels, image, url)
 
 
-def playShow(values):
+def play_show(values):
+    """Play a show."""
     smil = values['smil'][0]
     image = values['image'][0]
     labels = values['labels'][0]
-    labels = urllib.parse.parse_qs(labels)
+    labels = parse_qs(labels)
     for key in list(labels.keys()):
         labels[key] = labels[key][0]
     shows = Shows()
     try:
         res = shows.getStream(smil)
-    except CBCAuthError as e:
-        log('(playShows) auth failed. retrying', True)
+    except CBCAuthError as ex:
+        log('(play_show) auth failed. retrying...', True)
         if not authorize():
-            log('(playShows) auth retry failed', True)
+            log('(play_show) auth retry failed: {}'.format(ex), True)
             return
-        log('(playShows) auth retry successful', True)
+        log('(play_show) auth retry successful', True)
         try:
             res = shows.getStream(smil)
-        except CBCAuthError as e:
-            if e.payment:
-                log('(playShows) getStream failed because login required', True)
+        except CBCAuthError as ex:
+            if ex.payment:
+                log('(play_show) getStream failed because login required', True)
                 xbmcgui.Dialog().ok(getString(30010), getString(30011))
             else:
-                log('(playShows) getStream failed despite successful auth retry', True)
+                log('(play_show) getStream failed despite successful auth retry', True)
                 xbmcgui.Dialog().ok(getString(30010), getString(30012))
             return
 
@@ -92,6 +105,7 @@ def playShow(values):
 
 
 def liveProgramsMenu():
+    """Populate the menu with live programs."""
     xbmcplugin.setContent(addon_handle, 'videos')
     progs = LivePrograms()
     prog_list = progs.getLivePrograms()
@@ -100,13 +114,14 @@ def liveProgramsMenu():
         # skip unavailable streams
         if not prog['availabilityState'] == 'available':
             continue
-        elif prog['availableDate'] == 0:
+
+        if prog['availableDate'] == 0:
             continue
 
         labels = cbc.getLabels(prog)
         image = cbc.getImage(prog)
         item = xbmcgui.ListItem(labels['title'])
-        item.setArt({ 'thumb': image, 'poster': image })
+        item.setArt({'thumb': image, 'poster': image})
         item.setInfo(type="Video", infoLabels=labels)
         item.setProperty('IsPlayable', 'true')
         values = {
@@ -123,6 +138,7 @@ def liveProgramsMenu():
 
 
 def liveChannelsMenu():
+    """Populate the menu with live channels."""
     xbmcplugin.setContent(addon_handle, 'videos')
     chans = LiveChannels()
     chan_list = chans.getLiveChannels()
@@ -131,21 +147,22 @@ def liveChannelsMenu():
         labels = cbc.getLabels(channel)
         image = cbc.getImage(channel)
         item = xbmcgui.ListItem(labels['title'])
-        item.setArt({ 'thumb': image, 'poster': image })
+        item.setArt({'thumb': image, 'poster': image})
         item.setInfo(type="Video", infoLabels=labels)
         item.setProperty('IsPlayable', 'true')
-        values = {
+        query_values = {
             'smil': channel['content'][0]['url'],
             'labels': urlencode(labels),
             'image': image
         }
-        url = sys.argv[0] + "?" + urlencode(values)
+        url = sys.argv[0] + "?" + urlencode(query_values)
         xbmcplugin.addDirectoryItem(addon_handle, url, item, False)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 
-def showsMenu(values):
+def play_menu(values):
+    """Populate the menu with shows."""
     cbc = CBC()
     shows = Shows()
     if 'smil' in values:
@@ -162,40 +179,39 @@ def showsMenu(values):
     prog.create(getString(30003))
     try:
         show_list = shows.getShows(url, progress_callback = prog.update)
-    except CBCAuthError as e:
-        log('(showsMenu) auth failed. retrying', True)
+    except CBCAuthError:
+        log('(play_menu) auth failed. retrying', True)
         if not authorize():
-            log('(showsMenu) auth retry failed', True)
+            log('(play_menu) auth retry failed', True)
             return
-        log('(showsMenu) auth retry successful', True)
+        log('(play_menu) auth retry successful', True)
         try:
             show_list = shows.getShows(url, progress_callback = prog.update)
-        except CBCAuthError as e:
-            log('(showsMenu) getShows failed despite successful auth retry', True)
+        except CBCAuthError:
+            log('(play_menu) getShows failed despite successful auth retry', True)
             return
 
     # if the first episode is video, assume all are video
-    isVideo = True if 'video' in show_list[0] else False
-    xbmcplugin.setContent(addon_handle, 'episodes' if isVideo else 'tvshows')
+    is_video = 'video' in show_list[0]
+    xbmcplugin.setContent(addon_handle, 'episodes' if is_video else 'tvshows')
 
     prog.close()
     for show in show_list:
-        if show['url'] == None:
+        if show['url'] is None:
             continue
-        isVideo = show['video'] if 'video' in show else False
+        is_video = show['video'] if 'video' in show else False
         labels = cbc.getLabels(show)
         image = show['image'] if 'image' in show else None
         item = xbmcgui.ListItem(labels['title'])
 
-
         item.setInfo(type="Video", infoLabels=labels)
-        item.setProperty('IsPlayable', 'true' if isVideo else 'false')
+        item.setProperty('IsPlayable', 'true' if is_video else 'false')
         if 'duration' in show:
             item.addStreamInfo('video', {'duration':show['duration']})
-        item.setArt({ 'thumb': image, 'poster': image })
+        item.setArt({'thumb': image, 'poster': image})
         values = {
             'smil': show['url'],
-            'video': show['video'] if isVideo else None,
+            'video': show['video'] if is_video else None,
             'image': image
         }
 
@@ -205,21 +221,21 @@ def showsMenu(values):
             values['labels'] = urlencode(labels)
 
         plugin_url = sys.argv[0] + "?" + urlencode(values)
-        xbmcplugin.addDirectoryItem(addon_handle, plugin_url, item, not isVideo)
+        xbmcplugin.addDirectoryItem(addon_handle, plugin_url, item, not is_video)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 
-def mainMenu():
-
+def main_menu():
+    """Populate the menu with the main menu items."""
     xbmcplugin.setContent(addon_handle, 'videos')
     for menu_item in [LIVE_CHANNELS, LIVE_PROGRAMS, SHOWS]:
-        labels = { 'title': menu_item }
+        labels = {'title': menu_item}
         item = xbmcgui.ListItem(menu_item)
         item.setInfo(type="Video", infoLabels=labels)
-        values = { 'menu': menu_item }
+        query_values = {'menu': menu_item}
         xbmcplugin.addDirectoryItem(handle=addon_handle,
-                                    url=sys.argv[0] + "?" + urlencode(values),
+                                    url=sys.argv[0] + "?" + urlencode(query_values),
                                     listitem=item,
                                     isFolder=True)
 
@@ -234,11 +250,11 @@ if len(sys.argv[2]) == 0:
     if not os.path.exists(getAuthorizationFile()):
         authorize()
 
-    mainMenu()
+    main_menu()
 else:
-    values = urllib.parse.parse_qs(sys.argv[2][1:])
+    values = parse_qs(sys.argv[2][1:])
     if 'video' in values and values['video'][0] == 'True':
-        playShow(values)
+        play_show(values)
     elif 'menu' in values:
         menu = values['menu'][0]
         if menu == LIVE_CHANNELS:
@@ -246,9 +262,6 @@ else:
         elif menu == LIVE_PROGRAMS:
             liveProgramsMenu()
         elif menu == SHOWS:
-            showsMenu(values)
+            play_menu(values)
     elif 'smil' in values:
-        smil = values['smil'][0]
-        labels = dict(urllib.parse.parse_qsl(values['labels'][0]))
-        image = values['image'][0]
-        playSmil(smil, labels, image)
+        play_smil(values['smil'][0], dict(parse_qsl(values['labels'][0])), values['image'][0])

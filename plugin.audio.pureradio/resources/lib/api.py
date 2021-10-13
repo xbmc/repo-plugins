4a5e94a319425e21f68144abcb48293ce7730688
Cubitus <b.dommel@gmail.com>
2021-10-13 15:49:30 +0200

    update version to 3.0.4

--- plugin.audio.pureradio/resources/lib/api.py
+++ plugin.audio.pureradio/resources/lib/api.py
@@ -3,18 +3,6 @@ import json
 import urllib.request as request
 from xml.etree import ElementTree as ET
 
-stream_url = 'https://pureradio.one/stream'
-podcast_url = 'https://pureradio.one/spreaker'
-
-# functions with web interface
-def retrieve_streamurl():
-    """ This function return the stream url """
-    return json.loads(request.urlopen(stream_url).read().decode('utf-8'))
-
-def retrieve_podcasturl():
-    """ This function return the podcast url """
-    return json.loads(request.urlopen(podcast_url).read().decode('utf-8'))
-
 def retrieve_podcasts(url):
     """ This function return a dictonary of podcast items """
     return ET.fromstring(request.urlopen(url=url, timeout=20).read().decode('utf-8'))

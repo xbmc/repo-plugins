3e8f458fe99d5cc68272e9e98fd9464aa7a89227
André Löfgren <andrelofgren@hotmail.co.uk>
2021-11-07 12:40:57 +0100

    [plugin.video.teliaplay-se] v1.2.1+matrix.1

--- plugin.video.teliaplay-se/resources/lib/timeutils.py
+++ plugin.video.teliaplay-se/resources/lib/timeutils.py
@@ -7,29 +7,16 @@ class TimestampsException(Exception):
     pass
 
 
-def unit_conversion(method):
-    def get_unit(*args, **kwargs):
-        try:
-            return args[1]
-        except IndexError:
-            try:
-                return kwargs["units"]
-            except KeyError:
-                return "s"
-
-    @functools.wraps(method)
-    def unit_conversion_wrapper(*args, **kwargs):
-        if get_unit(*args, **kwargs) == "s":
-            factor = 1
-        elif get_unit(*args, **kwargs) == "ms":
-            factor = 1000
-        elif get_unit(*args, **kwargs) == "us":
-            factor = 1000000
-        else:
-            unit = get_unit(*args, **kwargs)
-            raise TimestampsException("Invalid time unit '{0}'".format(unit))
-        return int(method(*args, **kwargs) * factor)
-    return unit_conversion_wrapper
+def unit_conversion_factor(units):
+    if units == "s":
+        factor = 1
+    elif units == "ms":
+        factor = 1000
+    elif units == "us":
+        factor = 1000000
+    else:
+        raise TimestampsException("Invalid time unit '{0}'".format(units))
+    return factor
 
 
 class TimezoneStamps():
@@ -37,58 +24,23 @@ class TimezoneStamps():
     def __init__(self, area):
         self.timezone = pytz.timezone(area)
 
-    @unit_conversion
-    def yesterday(self, units="s"):
-        yesterday_date = datetime.now(self.timezone) - timedelta(1)
-        yesterday_date = datetime(
-            yesterday_date.year, yesterday_date.month, yesterday_date.day, 0,
-            0, 0, 0
-        )
-        return datetime.timestamp(yesterday_date)
-
-    @unit_conversion
-    def today(self, units="s"):
-        today_date = datetime.now(self.timezone)
+    def today(self, day_offset=0, units="s"):
+        factor = unit_conversion_factor(units)
+        today_date = datetime.now(self.timezone) + timedelta(days=day_offset)
         today_date = datetime(
             today_date.year, today_date.month, today_date.day, 0, 0,
             0, 0
         )
-        return datetime.timestamp(today_date)
+        return int(datetime.timestamp(today_date) * factor)
 
-    @unit_conversion
     def now(self, units="s"):
+        factor = unit_conversion_factor(units)
         now_date = datetime.now(self.timezone)
-        return datetime.timestamp(now_date)
-
-    @unit_conversion
-    def tonight(self, units="s"):
-        tonight_date = datetime.now(self.timezone)
-        tonight_date = datetime(
-            tonight_date.year, tonight_date.month, tonight_date.day, 20, 0,
-            0, 0
-        )
-        return datetime.timestamp(tonight_date)
-
-    @unit_conversion
-    def tomorrow(self, units="s"):
-        tomorrow_date = datetime.now(self.timezone) + timedelta(1)
-        tomorrow_date = datetime(
-            tomorrow_date.year, tomorrow_date.month, tomorrow_date.day, 0, 0,
-            0, 0
-        )
-        return datetime.timestamp(tomorrow_date)
+        return int(datetime.timestamp(now_date) * factor)
 
     @staticmethod
-    def local_datetime_str(timestamp, units, time_format):
-        if units == "s":
-            factor = 1
-        elif units == "ms":
-            factor = 1000
-        elif units == "us":
-            factor = 1000000
-        else:
-            raise TimestampsException("Invalid time unit '{0}'".format(units))
-
+    def local_datetime_str(timestamp, time_format, units):
+        factor = unit_conversion_factor(units)
         datetime_object = datetime.fromtimestamp(timestamp // factor)
         return datetime_object.strftime(time_format)
 

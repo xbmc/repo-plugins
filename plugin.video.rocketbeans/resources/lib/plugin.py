8f3d775c2acf760d5d5f8a634e54b7b4d222072a
razzeee <razzeee@gmail.com>
2021-12-12 18:11:36 +0000

    [plugin.video.rocketbeans] 1.2.4

--- plugin.video.rocketbeans/resources/lib/plugin.py
+++ plugin.video.rocketbeans/resources/lib/plugin.py
@@ -11,7 +11,7 @@ from xbmcaddon import Addon
 
 
 plugin = routing.Plugin()
-setContent(plugin.handle, 'videos')
+setContent(plugin.handle, "videos")
 
 livestream_thumbnail = None
 livestream_url = None
@@ -20,98 +20,72 @@ __addon__ = Addon()
 
 
 def getString(string_id):
-    return __addon__.getLocalizedString(string_id).encode('utf-8', 'ignore')
+    return __addon__.getLocalizedString(string_id)
 
 
 def get_livestream():
     if __addon__.getSetting("stream") == 0:
         t = TwitchStream(config.TWITCH_USER_LOGIN)
-        livestream_url, title, livestream_thumbnail = t.url, t.title, t.thumbnail
+        return t.url, t.title, t.thumbnail
     else:
-        livestream_url, title, livestream_thumbnail = YoutubeStream(
-        ).get_live_video_info_from_channel_id(config.CHANNEL_ID)
-    return livestream_url, livestream_thumbnail, title
+        return YoutubeStream().get_live_video_info_from_channel_id(config.CHANNEL_ID)
 
 
-@plugin.route('/')
+@plugin.route("/")
 def index():
     livestream_url, livestream_thumbnail, title = get_livestream()
-    li = createListItem(
-        ('Live | %s' % title),
-        livestream_thumbnail,
-        True,
-        getString(32001),
-        0
-    )
-    addDirectoryItem(
-        plugin.handle,
-        livestream_url,
-        li
-    )
+    if livestream_url:
+        li = createListItem(
+            ("Live | %s" % title), livestream_thumbnail, True, getString(32001), 0
+        )
+        addDirectoryItem(plugin.handle, livestream_url, li)
 
     url = "plugin://plugin.video.youtube/user/%s/" % config.CHANNEL_ID
-    addDirectoryItem(
-        plugin.handle,
-        url,
-        ListItem(getString(32007)),
-        True
-    )
-
-    url = "plugin://plugin.video.youtube/channel/%s/" % config.ROCKET_BEANS_GAMING_CHANNEL_ID
-    addDirectoryItem(
-        plugin.handle,
-        url,
-        ListItem(getString(32006)),
-        True
-    )
+    addDirectoryItem(plugin.handle, url, ListItem(getString(32007)), True)
 
     addDirectoryItem(
         plugin.handle,
-        "plugin://plugin.video.youtube/channel/%s/" % config.KINO_PLUS_CHANNEL_ID,
-        ListItem(getString(32009)),
-        True
+        "plugin://plugin.video.twitch/?mode=channel_video_list&broadcast_type=upload&channel_id=%s"
+        % (config.TWITCH_CHANNEL_ID),
+        ListItem(getString(32004)),
+        True,
     )
+
+    url = (
+        "plugin://plugin.video.youtube/channel/%s/"
+        % config.ROCKET_BEANS_GAMING_CHANNEL_ID
+    )
+    addDirectoryItem(plugin.handle, url, ListItem(getString(32006)), True)
 
     addDirectoryItem(
         plugin.handle,
         "plugin://plugin.video.youtube/channel/%s/" % config.GAME_TWO_CHANNEL_ID,
         ListItem(getString(32005)),
-        True
-    )
-
-    addDirectoryItem(
-        plugin.handle,
-        "plugin://plugin.video.youtube/channel/%s/" % config.KINO_PLUS_CHANNEL_ID,
-        ListItem(getString(32009)),
-        True
+        True,
     )
 
     addDirectoryItem(
         plugin.handle,
         "plugin://plugin.video.youtube/channel/%s/" % config.LETS_PLAY_CHANNEL_ID,
         ListItem(getString(32008)),
-        True
+        True,
     )
 
     addDirectoryItem(
         plugin.handle,
-        "plugin://plugin.video.twitch/?mode=channel_video_list&broadcast_type=upload&channel_id=%s" % (
-            config.TWITCH_CHANNEL_ID),
-        ListItem(getString(32004)),
-        True
+        "plugin://plugin.video.youtube/channel/%s/" % config.KINO_PLUS_CHANNEL_ID,
+        ListItem(getString(32009)),
+        True,
     )
 
     addDirectoryItem(
-        plugin.handle,
-        plugin.url_for(guide),
-        ListItem(getString(32003)),
-        True
+        plugin.handle, plugin.url_for(guide), ListItem(getString(32003)), True
     )
 
     endOfDirectory(plugin.handle)
 
 
-@plugin.route('/guide')
+@plugin.route("/guide")
 def guide():
     guide_items = show_guide()
 
@@ -119,50 +93,26 @@ def guide():
         title, video_id, duration, game, is_live_now = guide_item
         if video_id:
             url = "plugin://plugin.video.youtube/play/?video_id=%s" % video_id
-            thumbnail = "https://i.ytimg.com/vi/%s/hqdefault.jpg" % (
-                video_id)
+            thumbnail = "https://i.ytimg.com/vi/%s/hqdefault.jpg" % (video_id)
 
             li = createListItem(
                 title,
                 thumbnail,
                 True,
-                '[B]' + str(getString(32002)) + '[/B]: ' +
-                game if game else '',
-                duration
+                "[B]" + str(getString(32002)) + "[/B]: " + game if game else "",
+                duration,
             )
-            addDirectoryItem(
-                plugin.handle,
-                url,
-                li
-            )
+            addDirectoryItem(plugin.handle, url, li)
         else:
             if is_live_now:
                 livestream_url, livestream_thumbnail, _ = get_livestream()
                 li = createListItem(
-                    title,
-                    livestream_thumbnail,
-                    True,
-                    getString(32001),
-                    0
+                    title, livestream_thumbnail, True, getString(32001), 0
                 )
-                addDirectoryItem(
-                    plugin.handle,
-                    livestream_url,
-                    li
-                )
+                addDirectoryItem(plugin.handle, livestream_url, li)
             else:
-                li = createListItem(
-                    title,
-                    '',
-                    False,
-                    '',
-                    0
-                )
-                addDirectoryItem(
-                    plugin.handle,
-                    '',
-                    li
-                )
+                li = createListItem(title, "", False, "", 0)
+                addDirectoryItem(plugin.handle, "", li)
     endOfDirectory(plugin.handle)
 
 
@@ -171,17 +121,17 @@ def createListItem(label, thumbnailImage, isPlayable, plot, duration):
         label=label,
     )
 
-    li.setArt({'thumb': thumbnailImage})
+    li.setArt({"thumb": thumbnailImage})
 
     if isPlayable:
         infoLabels = {}
-        infoLabels['plot'] = plot
+        infoLabels["plot"] = plot
 
         if duration > 0:
-            infoLabels['duration'] = duration
+            infoLabels["duration"] = duration
 
-        li.setInfo(type='video', infoLabels=infoLabels)
-        li.setProperty('isPlayable', 'true')
+        li.setInfo(type="video", infoLabels=infoLabels)
+        li.setProperty("isPlayable", "true")
 
     return li
 

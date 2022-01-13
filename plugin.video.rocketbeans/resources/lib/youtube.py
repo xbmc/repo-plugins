351ea0b37a146316d094408f586e0c262ecc333d
Kolja Lampe <razzeee@gmail.com>
2022-01-13 11:51:52 +0100

    [plugin.video.rocketbeans] 1.2.4 (#3801)

--- plugin.video.rocketbeans/resources/lib/youtube.py
+++ plugin.video.rocketbeans/resources/lib/youtube.py
@@ -9,22 +9,30 @@ import urllib.parse
 class YoutubeStream:
     def get_live_video_info_from_channel_id(self, channel_id):
         request = urllib.request.Request(
-            "https://www.youtube.com/c/%s/live" % channel_id)
+            "https://www.youtube.com/c/%s/live" % channel_id
+        )
         video_id, title = self.get_video_info(request)
+        if not video_id:
+            return (None, None, None)
+
         livestream_url = "plugin://plugin.video.youtube/play/?video_id=%s" % video_id
         livestream_thumbnail = "https://i.ytimg.com/vi/%s/hqdefault_live.jpg#%s" % (
-            video_id, time.localtime())
+            video_id,
+            time.localtime(),
+        )
         return livestream_url, title, livestream_thumbnail
 
     def get_video_info(self, request):
         response = urllib.request.urlopen(request)
         string_data = response.read()
-        lines = string_data.decode('utf-8').splitlines()
+        lines = string_data.decode("utf-8").splitlines()
 
         re_video_url = re.compile(
-            r'https://i.ytimg.com/vi/(?P<video_id>[^\/]+)/maxresdefault_live.jpg')
+            r"https://i.ytimg.com/vi/(?P<video_id>[^\/]+)/maxresdefault_live.jpg"
+        )
         re_video_title = re.compile(
-            r'"title":"(?P<title>[^\?]+)","lengthSeconds":".","isLive":true')
+            r'"title":"(?P<title>[^\?]+)","lengthSeconds":".","isLive":true'
+        )
 
         re_video_url_match = ""
         re_video_title_match = ""
@@ -37,5 +45,7 @@ class YoutubeStream:
                 re_video_title_match = re.search(re_video_title, line)
 
             if re_video_url_match and re_video_title_match:
-                title = re_video_title_match.group('title')
-                return (re_video_url_match.group('video_id'), title)
+                title = re_video_title_match.group("title")
+                return (re_video_url_match.group("video_id"), title)
+
+        return (None, None)

2ac87b34cbffd5f829eaa43ec36e9543dd191d76
Adriano Macchietto <adriano@macchietto.com>
2022-01-20 20:16:37 -0800

    [plugin.video.lbry] 0.1.8+leia.1

--- plugin.video.lbry/resources/lib/local.py
+++ plugin.video.lbry/resources/lib/local.py
@@ -12,25 +12,34 @@ tr = ADDON.getLocalizedString
 def get_profile_path(rpath):
     return xbmc.translatePath(ADDON.getAddonInfo('profile') + rpath)
 
+def create_profile_directory_if_missing():
+    profile_path = ADDON.getAddonInfo('profile')
+    if not xbmcvfs.exists(profile_path):
+        xbmcvfs.mkdir(profile_path)
+
+def open_file(name, mode):
+    if mode == 'w':
+        create_profile_directory_if_missing()
+    return xbmcvfs.File(get_profile_path(name), mode)
+
 def load_channel_subs():
     channels = []
     try:
-        f = xbmcvfs.File(get_profile_path('channel_subs'), 'r')
-        lines = f.readBytes()
+        f = open_file('channel_subs', 'r')
+        lines = f.readBytes().decode('utf-8')
+        for line in lines.split('\n'):
+            items = line.split('#')
+            if len(items) < 2:
+                continue
+            channels.append((items[0],items[1]))
         f.close()
     except Exception as e:
-        pass
-    lines = lines.decode('utf-8')
-    for line in lines.split('\n'):
-        items = line.split('#')
-        if len(items) < 2:
-            continue
-        channels.append((items[0],items[1]))
+        xbmcgui.Dialog().notification(tr(30104), str(e), xbmcgui.NOTIFICATION_ERROR)
     return channels
 
 def save_channel_subs(channels):
     try:
-        f = xbmcvfs.File(get_profile_path('channel_subs'), 'w')
+        f = open_file('channel_subs', 'w')
         for (name, claim_id) in channels:
             f.write(bytearray(name.encode('utf-8')))
             f.write('#')
@@ -43,20 +52,19 @@ def save_channel_subs(channels):
 def load_playlist(name):
     items = []
     try:
-        f = xbmcvfs.File(get_profile_path(name + '.list'), 'r')
-        lines = f.readBytes()
+        f = open_file(name + '.list', 'r')
+        lines = f.readBytes().decode('utf-8')
+        for line in lines.split('\n'):
+            if line != '':
+                items.append(line)
         f.close()
     except Exception as e:
-        pass
-    lines = lines.decode('utf-8')
-    for line in lines.split('\n'):
-        if line != '':
-            items.append(line)
+        xbmcgui.Dialog().notification(tr(30104), str(e), xbmcgui.NOTIFICATION_ERROR)
     return items
 
 def save_playlist(name, items):
     try:
-        f = xbmcvfs.File(get_profile_path(name + '.list'), 'w')
+        f = open_file(name + '.list', 'w')
         for item in items:
             f.write(bytearray(item.encode('utf-8')))
             f.write('\n')

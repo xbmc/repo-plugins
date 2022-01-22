63876442837210e46fd90b5ae14d6e40043344ea
michaelarnauts <michael.arnauts@gmail.com>
2022-01-22 14:50:33 +0100

    [plugin.video.hdaustria] 1.0.5+matrix.1

---
+++ plugin.video.hdaustria/resources/lib/modules/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" Modules """
+from __future__ import absolute_import, division, unicode_literals
+
+SETTINGS_ADULT_ASK_PIN = 0
+SETTINGS_ADULT_HIDE = 1
+SETTINGS_ADULT_ALLOW = 2

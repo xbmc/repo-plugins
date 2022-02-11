fb0a07c6ec2899f27326902e094885b6c0d19508
s0faking <sofaking@gettingmoney.at>
2022-02-11 17:54:58 +0100

    [plugin.video.orftvthek] 0.12.4+matrix.1

--- plugin.video.orftvthek/resources/lib/Settings.py
+++ plugin.video.orftvthek/resources/lib/Settings.py
@@ -27,24 +27,6 @@ def userAgent():
     return __addon__.getSetting('userAgent')
 
 
-def videoQuality(quality_list):
-    default_return_index = 2
-    videoQuality = __addon__.getSetting('videoQuality')
-    try:
-        return quality_list[int(videoQuality)]
-    except (IndexError, ValueError):
-        return quality_list[default_return_index]
-
-
-def videoDelivery(delivery_list):
-    default_return_index = 0
-    if serviceAPI():
-        videoDeliveryProgressive = __addon__.getSetting('videoDeliveryProgressive')
-        if videoDeliveryProgressive == "true":
-            return delivery_list[1]
-    return delivery_list[default_return_index]
-
-
 def autoPlayPrompt():
     return __addon__.getSetting("autoPlayPrompt") == "true"
 

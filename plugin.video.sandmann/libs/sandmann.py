4ac77b3c69d894a7f1b68ad4bef14915614708e0
sorax <sorax@lilith.local>
2022-03-06 21:30:44 +0100

    [plugin.video.sandmann] v2.0.0

---
+++ plugin.video.sandmann/libs/sandmann.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 sorax
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+
+import sys
+
+from libs.episodes import getEpisodes
+
+
+# -- Addon --
+addon = xbmcaddon.Addon()
+addon_handle = int(sys.argv[1])
+addon_name = addon.getAddonInfo("name")
+addon_icon = addon.getAddonInfo("icon")
+
+base_path = sys.argv[0]
+
+# -- Constants --
+episodes_url = "https://api.ardmediathek.de/page-gateway/widgets/ard/asset/Y3JpZDovL3JiYi1vbmxpbmUuZGUvc2FuZG1hbm4"
+
+# -- Settings --
+dgs = addon.getSettingInt("dgs2")
+interval = addon.getSettingInt("interval2")
+quality = addon.getSettingInt("quality2")
+update = addon.getSettingInt("update2")
+
+
+def sandmann():
+    episodes_list = getEpisodes(episodes_url, quality)
+
+    item_list = []
+    for episode in episodes_list:
+        if dgs == 0 and episode["dgs"] == False:
+            item_list.append((episode["stream"], getListItem(episode), False))
+        if dgs == 1:
+            item_list.append((episode["stream"], getListItem(episode), False))
+        if dgs == 2 and episode["dgs"] == True:
+            item_list.append((episode["stream"], getListItem(episode), False))
+
+    xbmcplugin.addDirectoryItems(addon_handle, item_list, len(item_list))
+
+    if update == 1:
+        li_refresh = xbmcgui.ListItem(label=addon.getLocalizedString(30020))
+        xbmcplugin.addDirectoryItem(addon_handle, base_path, li_refresh, True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+def getListItem(item):
+    li = xbmcgui.ListItem(label=item["title"])
+    li.setArt({
+        "thumb": item["thumb"],
+        "fanart": item["fanart"]
+    })
+    li.setInfo(
+        type="video",
+        infoLabels={
+            "title": item["title"],
+            "plot": item["desc"],
+            "duration": item["duration"]
+        }
+    )
+    li.setProperty("IsPlayable", "true")
+    return li

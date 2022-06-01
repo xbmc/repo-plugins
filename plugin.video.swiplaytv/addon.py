7a8a87dd2f772286a997052e138c1ceb3d23b4d7
Alexander Seiler <seileralex@gmail.com>
2022-06-01 04:29:59 +0200

    [plugin.video.swiplaytv] 2.1.0

---
+++ plugin.video.swiplaytv/addon.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2018 Alexander Seiler
+#
+#
+# This file is part of plugin.video.swiplaytv.
+#
+# plugin.video.swiplaytv is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# plugin.video.swiplaytv is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with plugin.video.swiplaytv.
+# If not, see <http://www.gnu.org/licenses/>.
+
+from resources.lib import swiplaytv
+
+if __name__ == '__main__':
+    swiplaytv.run()

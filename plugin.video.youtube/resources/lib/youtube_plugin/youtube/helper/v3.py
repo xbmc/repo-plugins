1ebfb22d8af9da86a3e5a29b63eac2e0faa44d67
anxdpanic <anxdpanic@users.noreply.github.com>
2022-12-01 18:16:10 +0000

    [plugin.video.youtube] 6.8.21

--- plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/v3.py
+++ plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/v3.py
@@ -12,6 +12,7 @@ from ...youtube.helper import yt_context_menu
 from ... import kodion
 from ...kodion import items
 from . import utils
+from ...youtube.helper import tags
 
 
 def _process_list_response(provider, context, json_data):
@@ -78,6 +79,17 @@ def _process_list_response(provider, context, json_data):
                 context_menu = []
                 yt_context_menu.append_subscribe_to_channel(context_menu, provider, context, channel_id)
                 channel_item.set_context_menu(context_menu)
+            
+            #default_language = yt_item['brandingSettings']['channel'].get('defaultLanguage')
+            #country = yt_item['brandingSettings']['channel'].get('country')
+            default_language = ''
+            country = ''
+            if not default_language:
+                default_language = ''
+            if not country:
+                country = ''
+            tags.create_channel(channel_id, title, item_uri, image, kind)                
+            
             result.append(channel_item)
             channel_id_dict[channel_id] = channel_item
         elif kind == 'guidecategory':
@@ -106,10 +118,18 @@ def _process_list_response(provider, context, json_data):
             item_uri = context.create_uri(['channel', channel_id], item_params)
             channel_item = items.DirectoryItem(title, item_uri, image=image)
             channel_item.set_fanart(provider.get_fanart(context))
-
+            channel_item.set_channel_id(channel_id)
             # map channel id with subscription id - we need it for the unsubscription
             subscription_id_dict[channel_id] = yt_item['id']
 
+            default_language = ''
+            country = ''
+            if not default_language:
+                default_language = ''
+            if not country:
+                country = ''
+            tags.create_channel(channel_id, title, item_uri, image, kind)
+            
             result.append(channel_item)
             channel_id_dict[channel_id] = channel_item
         elif kind == 'playlist':
@@ -259,6 +279,14 @@ def _process_list_response(provider, context, json_data):
                 item_uri = context.create_uri(['channel', channel_id], item_params)
                 channel_item = items.DirectoryItem(title, item_uri, image=image)
                 channel_item.set_fanart(provider.get_fanart(context))
+                default_language = ''
+                country = ''
+                if not default_language:
+                    default_language = ''
+                if not country:
+                    country = ''
+                tags.create_channel(channel_id, title, item_uri, image, kind)                
+                
                 result.append(channel_item)
                 channel_id_dict[channel_id] = channel_item
             else:
@@ -297,6 +325,8 @@ def response_to_items(provider, context, json_data, sort=None, reverse_sort=Fals
 
     if sort is not None:
         result = sorted(result, key=sort, reverse=reverse_sort)
+
+    result = utils.filter_short_videos(context, result)
 
     # no processing of next page item
     if not process_next_page:

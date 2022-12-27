8b5f46c2409fcd01faa77d28aed8e38c12696405
Tobias Gruetzmacher <tobias-git@23.gs>
2022-12-27 17:36:07 +0100

    [plugin.video.media-ccc-de] 0.3.0+matrix.1

--- plugin.video.media-ccc-de/resources/lib/http.py
+++ plugin.video.media-ccc-de/resources/lib/http.py
@@ -5,10 +5,12 @@ import requests
 
 from .stream import Streams
 from .recording import Recordings
+from .relive import Relives, ReliveRecordings
 from . import gui
 
 BASE_URL = 'https://media.ccc.de/public/'
 LIVE_URL = 'https://streaming.media.ccc.de/streams/v2.json'
+RELIVE_URL = 'https://cdn.c3voc.de/relive/index.json'
 
 # BASE_URL = 'http://127.0.0.1:3000/public/'
 # LIVE_URL = 'http://127.0.0.1:3000/stream_v2.json'
@@ -46,4 +48,22 @@ def fetch_live():
         return Streams(req.json())
     except requests.exceptions.RequestException as e:
         gui.err('Can\'t fetch streams: %s' % e)
+        raise FetchError(e)
+
+
+def fetch_relive_index():
+    try:
+        req = requests.get(RELIVE_URL)
+        return Relives(req.json())
+    except requests.exceptions.RequestException as e:
+        gui.err('Can\'t fetch relive index: %s' % e)
+        raise FetchError(e)
+
+
+def fetch_relive_recordings(url):
+    try:
+        req = requests.get(url)
+        return ReliveRecordings(req.json())
+    except requests.exceptions.RequestException as e:
+        gui.err('Can\'t fetch relive recordings: %s' % e)
         raise FetchError(e)

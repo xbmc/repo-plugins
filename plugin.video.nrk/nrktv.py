c664f5af5289c20db3bf5c1f4e825875bb8d0a6c
andrea99boban <55617060+andrea99boban@users.noreply.github.com>
2023-03-25 11:25:16 +0100

    Update nrktv.py

--- plugin.video.nrk/nrktv.py
+++ plugin.video.nrk/nrktv.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# adattato a kodi 20.0 da ^zZz^
+# adattato a kodi 20.0 da a99b
 
 
 import datetime

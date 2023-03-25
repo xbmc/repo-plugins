87cca8d69757e26083f79c248de1ceb06c9c49e4
andrea99boban <55617060+andrea99boban@users.noreply.github.com>
2023-03-25 11:24:55 +0100

    Update nrktv.py

--- plugin.video.nrk/nrktv.py
+++ plugin.video.nrk/nrktv.py
@@ -140,16 +140,16 @@ class Program(Series):
         media_urls = []
 
         manifest = _get('/playback/manifest/{}'.format(r.get('id')))
-        # adattato a kodi 20.0 da ^zZz^
+        # adapted to Kodi 20.0 by a99b
         try:
             if manifest.json()['playability'] == 'playable':
                 media_urls = list( map(lambda x: x['url'], manifest.json()['playable']['assets']) )
                 media_urls
-        except:  # il manifest della nuova api apparentemente e' gia' un dict
+        except:  # the manifest seems to already be a dict
             if manifest['playability'] == 'playable':
                 media_urls = list( map(lambda x: x['url'], manifest['playable']['assets']) )
                 media_urls            
-        ########## fine modifica ##########
+        ########## end modification ##########
         images = _image_url_key_standardize(r.get('image', {}).get('webImages', None))
         duration = _duration_to_seconds(r.get('duration', 0))
         legal_age = r.get('legalAge', None)

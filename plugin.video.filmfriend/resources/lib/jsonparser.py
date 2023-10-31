dbba7f617f94d19d159d0f4952abff9b9d565a38
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2023-10-31 11:21:01 +0100

    [plugin.video.filmfriend] 1.0.1

--- plugin.video.filmfriend/resources/lib/jsonparser.py
+++ plugin.video.filmfriend/resources/lib/jsonparser.py
@@ -2,6 +2,8 @@
 import requests
 import json
 import libmediathek4utils as lm4utils
+import pyjwt as jwt
+import time
 
 base = 'https://api.vod.filmwerte.de/api/v1/'
 
@@ -58,14 +60,37 @@ if s in ['system','']:
 else:
 	lang = languages[s]
 
+def fetchJson(url,headers=None):
+	response = requests.get(url,headers=headers)
+	if response.status_code > 299:
+		raise RuntimeError(f"Fetching '{url}' failed with code '{response.status_code}' and optional message '{response.text}'")
+
+	return response.json()
+
 def parseMain():
 	j = requests.get(f'{base}tenant-groups/21960588-0518-4dd3-89e5-f25ba5bf5631/navigation').json()
 
+def parseWatchList(params,content='videos'):
+	_checkTokenExpired()
+	headers = {
+		'Authorization':f'Bearer {lm4utils.getSetting("access_token")}'
+	}
+
+	j = fetchJson(f'https://api.tenant.frontend.vod.filmwerte.de/v11/{lm4utils.getSetting("tenant")}/watchlist{params}',headers)
+	return parseResponse(j,content)
+
 def parseSearch(params,content='videos'):
-	j = requests.get(f'{base}/tenant-groups/fba2f8b5-6a3a-4da3-b555-21613a88d3ef/search{params}').json()
+	j = fetchJson(f'{base}/tenant-groups/fba2f8b5-6a3a-4da3-b555-21613a88d3ef/search{params}')
+	return parseResponse(j,content)
+
+def parseResponse(responseJson,content='videos'):
 	res = {'items':[],'content':content,'pagination':{'currentPage':0}}
-	for item in j['results']:
-		result = item['result']
+	for item in responseJson['results']:
+		result = item
+		if 'result' in item:
+			result = item['result']
+		else:
+			result = item[item['kind'].lower()]
 		if item['kind'] == 'Series':
 			d = {'type':'tvshow', 'params':{'mode':'listSearch', 'content':'tvshows'}, 'metadata':{'art':{}}}
 			d['metadata']['name'] = _getString(result,'title')
@@ -96,7 +121,7 @@ def parseSearch(params,content='videos'):
 			res['items'].append(d)
 
 
-		elif item['kind'] == 'Video':
+		elif item['kind'] == 'Video' or item['kind'] == 'Movie':
 			d = {'type':'movie', 'params':{'mode':'playVideo'}, 'metadata':{'art':{},'actors':[],'directors':[],'artists':[],'writers':[],'genres':[],'credits':[]}}
 			d['metadata']['name'] = _getString(result,'title')
 			if 'originalTitle' in result:
@@ -116,18 +141,18 @@ def parseSearch(params,content='videos'):
 				d['metadata']['aired'] = result['releaseDate'][:10]
 			d['metadata']['art'] = _getArt(result)
 
-
-			for participant in result['participations']:
-				if participant['kind'] in ['Actor', 'Voice']:
-					d['metadata']['actors'].append({'role':participant.get('englishDescription',''),'name':_getName(participant)})
-				elif participant['kind'] in ['Director', 'Producer']:
-					d['metadata']['directors'].append(_getName(participant))
-				elif participant['kind'] == 'Composer':
-					d['metadata']['artists'].append(_getName(participant))
-				elif participant['kind'] in ['Writer', 'Editor']:
-					d['metadata']['writers'].append(_getName(participant))
-				elif participant['kind'] in ['Misc', 'Camera']:
-					d['metadata']['credits'].append(_getName(participant))
+			if 'participations' in result:
+				for participant in result['participations']:
+					if participant['kind'] in ['Actor', 'Voice']:
+						d['metadata']['actors'].append({'role':participant.get('englishDescription',''),'name':_getName(participant)})
+					elif participant['kind'] in ['Director', 'Producer']:
+						d['metadata']['directors'].append(_getName(participant))
+					elif participant['kind'] == 'Composer':
+						d['metadata']['artists'].append(_getName(participant))
+					elif participant['kind'] in ['Writer', 'Editor']:
+						d['metadata']['writers'].append(_getName(participant))
+					elif participant['kind'] in ['Misc', 'Camera']:
+						d['metadata']['credits'].append(_getName(participant))
 			if 'genres' in result:
 				for genre in result['genres']:
 					d['metadata']['genres'].append(_getString(genre,'name'))
@@ -136,34 +161,37 @@ def parseSearch(params,content='videos'):
 			res['items'].append(d)
 	return res
 
-def getVideoUrl(video):
+def getVideoUrl(videoId):
+	_checkTokenExpired()
 	headers = {
 		'Authorization':f'Bearer {lm4utils.getSetting("access_token")}'
 	}
 
-	r = requests.get(f'{base}customers(end-user)/me',headers=headers).text
-	if r == '':
-		token = _getNewToken()
-		if not token:
-			lm4utils.displayMsg(lm4utils.getTranslation(30507),lm4utils.getTranslation(30508))
-			return {}
-		headers = {
-			'Authorization':f'Bearer {token}'
-		}
-		r = requests.get(f'{base}customers(end-user)/me',headers=headers).text
-
-	j = json.loads(r)
-	id = j['id']
-
-	j = requests.get(f'{base}customers(tenant)/{lm4utils.getSetting("tenant")}/customers(end-user)/{id}/videos/{video}/uri?pin=undefined',headers=headers).json()
-	url = f'{j["mpegDashUri"]}'
+	videoInfo = requests.get(f'https://api.tenant.frontend.vod.filmwerte.de/v11/{lm4utils.getSetting("tenant")}/movies/{videoId}/uri',headers=headers).json()
+	url = f'{videoInfo["mpegDash"]}'
 	wvheaders = '&content-type='
-	licenseserverurl = f'{j["widevineLicenseServerUri"]}|{wvheaders}|R{{SSM}}|'
+	licenseserverurl = f'{videoInfo["widevineLicenseServerUri"]}|{wvheaders}|R{{SSM}}|'
 	return {'media':[{'url':url, 'licenseserverurl':licenseserverurl, 'type': 'video', 'stream':'DASH'}]}
+
+def _checkTokenExpired():
+	tokenString = lm4utils.getSetting("access_token")
+	isExpired = False
+	try:
+		token = jwt.decode(tokenString, key="", algorithms=["RSA256"], options={"verify_signature":False, "verify_aud":False})
+		expiry = token['exp']
+		if expiry <= time.time():
+			isExpired = True
+	except jwt.exceptions.ExpiredSignatureError:
+		isExpired = True
+	
+	if isExpired:
+		lm4utils.log("Access token for filmfried.de expired. Will fetch new token.")
+		_getNewToken()
 
 def _getNewToken():
 	refresh_token = lm4utils.getSetting('refresh_token')
 	if refresh_token == '':
+		lm4utils.log("Cannot fetch new access token for filmfried.de. Refresh token is missing.")
 		return False
 	files = {'client_id':(None, f'tenant-{lm4utils.getSetting("tenant")}-filmwerte-vod-frontend'),'grant_type':(None, 'refresh_token'),'refresh_token':(None, refresh_token),'scope':(None, 'filmwerte-vod-api offline_access')}
 	j = requests.post('https://api.vod.filmwerte.de/connect/token', files=files).json()
@@ -179,7 +207,10 @@ def _getString(d,k):
 		else:
 			return d[english[k]]
 	except:
-		return ''
+		try:
+			return d.get(k)
+		except:
+			return ''
 
 def _getName(participant):
 	if 'firstName' in participant['person']:
@@ -192,15 +223,28 @@ def _getArt(item):
 	fanart = ''
 	poster = ''
 	banner = ''
-	for art in item['artworks']:
-		if art['kind'] == 'Thumbnail' and thumb == '':
-			thumb = art['uri']['thumbnail2x']
-		elif art['kind'] == 'Thumbnail' and fanart == '':
-			fanart = art['uri']['resolution4x']
-		elif art['kind'] == 'Background':
-			fanart = art['uri']['resolution1080']
-		elif art['kind'] == 'CoverPortrait' and poster == '':
-			poster = art['uri']['thumbnail4x']
-		elif art['kind'] == 'Teaser' and banner == '':
-			banner = art['uri']['resolution720']
+	if 'artworkUris' in item:
+		for art in item['artworkUris']:
+			if art['kind'] == 'Thumbnail' and thumb == '':
+				thumb = art['resolution2x']
+			elif art['kind'] == 'Thumbnail' and fanart == '':
+				fanart = art['resolution4x']
+			elif art['kind'] == 'Background':
+				fanart = art['resolution1080']
+			elif art['kind'] == 'CoverPortrait' and poster == '':
+				poster = art['resolution4x']
+			elif art['kind'] == 'Teaser' and banner == '':
+				banner = art['resolution720']
+	else:
+		for art in item['artworks']:
+			if art['kind'] == 'Thumbnail' and thumb == '':
+				thumb = art['uri']['thumbnail2x']
+			elif art['kind'] == 'Thumbnail' and fanart == '':
+				fanart = art['uri']['resolution4x']
+			elif art['kind'] == 'Background':
+				fanart = art['uri']['resolution1080']
+			elif art['kind'] == 'CoverPortrait' and poster == '':
+				poster = art['uri']['thumbnail4x']
+			elif art['kind'] == 'Teaser' and banner == '':
+				banner = art['uri']['resolution720']
 	return {'thumb':thumb, 'fanart':fanart, 'poster':poster, 'banner':banner}

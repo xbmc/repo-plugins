6641a3613233d376c9d6f543c1c9a8ca704f521e
eracknaphobia <eracknaphobia@hotmail.com>
2024-07-23 14:49:43 +0000

    [plugin.video.crackle] 2024.7.22

--- plugin.video.crackle/main.py
+++ plugin.video.crackle/main.py
@@ -6,6 +6,8 @@ name = None
 mode = None
 stream_type = None
 genre_id = None
+page_num = 1
+icon_url = ''
 
 if 'id' in params:
     media_id = urllib.unquote_plus(params["id"])
@@ -17,8 +19,12 @@ if 'type' in params:
     stream_type = urllib.unquote_plus(params["type"])
 if 'genre_id' in params:
     genre_id = urllib.unquote_plus(params["genre_id"])
+if 'page_num' in params:
+    page_num = int(params["page_num"])
+if 'icon_url' in params:
+    icon_url = urllib.unquote_plus(params["icon_url"])
 
-if mode is None:
+if mode is None or mode == 1:
     main_menu()
 
 elif mode == 99:
@@ -26,19 +32,14 @@ elif mode == 99:
 
 elif mode == 100:
     if genre_id is not None:
-        if media_id == 'shows':
-            list_shows(genre_id)
-        elif media_id == 'movies':
-            list_movies(genre_id)
-
-# elif mode == 101:
-#     list_movies()
+        list_movies_shows(media_id, genre_id, page_num)
 
 elif mode == 102:
-    get_episodes(media_id)
+    get_children(media_id, icon_url)
 
-elif mode == 103:
-    if stream_type == "movies": media_id = get_movie_id(media_id)
+elif mode == 103:    
+    if stream_type == 'movies':
+        media_id = get_movie_id(media_id)
     get_stream(media_id)
 
 elif mode == 104:
